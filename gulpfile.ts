bc69aa54fa0e24afacf65bfd61dd6b37e7016f90
Damyan Petev <damyan.petev@gmail.com>
2016-04-25 15:22:55 +0300

    moving transpile to gulp build

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,8 +3,28 @@ import * as sass from 'gulp-sass';
 import * as sourcemaps from 'gulp-sourcemaps';
 import * as autoprefixer from 'gulp-autoprefixer';
 import * as tsc from 'typescript';
+import * as ts from 'gulp-typescript';
+
 var concat = require('gulp-concat'),
     cleanCSS = require('gulp-clean-css');
+
+var tsProject = ts.createProject('tsconfig.json', {
+        typescript: tsc
+    }),
+    tsSources: Array<string> = ["./src/**/*.ts",
+        "./samples/**/*.ts",
+        "./tests/**/*.ts",
+        "./tools/**/*.ts"];
+  
+gulp.task("build", ["build.css", "build.js"]);  
+
+gulp.task("build.js", function () {
+     return gulp.src(tsSources.concat("./typings/main.d.ts"), { base: "." })
+    .pipe(sourcemaps.init())
+    .pipe(ts(tsProject))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("."));
+});
 
 gulp.task("build.css", (done: any) => {
     return gulp.src(["src/**/*.scss"])
@@ -18,7 +38,7 @@ gulp.task("build.css", (done: any) => {
         // Reinitialise sourcemaps, loading inline sourcemap.
         .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(autoprefixer({
-			browsers: ['last 2 versions'],
+			browsers: ["last 2 versions"],
 			cascade: false
 		}))
         
@@ -27,21 +47,23 @@ gulp.task("build.css", (done: any) => {
         
         // save source map separately 
         .pipe(sourcemaps.write("./"))
-        .pipe(gulp.dest('./dist'))
+        .pipe(gulp.dest("./dist"))
         
         .pipe(concat("zero-blocks.css"))
         .pipe(cleanCSS())
-        .pipe(gulp.dest('./dist')); 
+        .pipe(gulp.dest("./dist")); 
 });
 
 /**
  * Watchers
  */
 
-gulp.task("watch", function () {
+gulp.task("watch", ["build.css:watch", "build.js:watch"]);
 
+gulp.task("build.css:watch", function () {
+    gulp.watch("src/**/*.scss", ["build.css"]);
 });
 
-gulp.task('build.css:watch', function () {
-    gulp.watch("src/**/*.scss", ['build.css']);
+gulp.task("build.js:watch", function () {
+    gulp.watch(tsSources, ["build.js"]);
 });
