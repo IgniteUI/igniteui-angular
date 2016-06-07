b351c138b2ac502720eadd80bf01a0bd56c8fc3f
Damyan Petev <damyan.petev@gmail.com>
2016-06-07 16:31:23 +0300

    updating bundle, misc cleanup

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config){
             'node_modules/systemjs/dist/system.src.js',
 
             // Polyfills
-            'node_modules/es6-shim/es6-shim.js',
+            'node_modules/core-js/client/shim.min.js',
 
             // Reflect and Zone.js
             'node_modules/reflect-metadata/Reflect.js',
