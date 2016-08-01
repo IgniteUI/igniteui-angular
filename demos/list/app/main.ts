7166ef606bef9c4d094c68b7634b148b53246790
petko5kov <petko_5kov@yahoo.com>
2016-08-01 20:40:52 +0300

    #39 - Add list events: filtering and filtered

--- demos/list/app/main.ts
+++ demos/list/app/main.ts
@@ -8,15 +8,27 @@ import * as Infragistics from '../../../src/main';
     directives: [
         Infragistics.Header,
         Infragistics.Item,
-        Infragistics.List
+        Infragistics.List,
+        //Infragistics.ListItemOption
     ]
 })
 
 export class AppComponent {
-    navItems: Array<Object> = [
+    private navItems: Array<Object> = [
             { text: "Nav1", link: "#" }, 
             { text: "Nav2", link: "#" }, 
             { text: "Nav3", link: "#" }, 
             { text: "Nav4", link: "#" }
         ];
+
+    filteredHandler(args) {
+        console.log(args);
+    }
+
+    /*var options: Array<HTMLElement> = [
+        new ListItemOption("<div></div>");
+        new ListItemOption();
+        new ListItemOption();
+        new ListItemOption();
+    ];*/
 }
