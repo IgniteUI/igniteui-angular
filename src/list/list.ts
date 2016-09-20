7d2e5ff42ba9b9adfd4dbbde7e4653f141a797ad
petko5kov <petko_5kov@yahoo.com>
2016-09-20 18:42:09 +0300

    Restore list.ts file

--- src/list/list.ts
+++ src/list/list.ts
@@ -1,19 +1,20 @@
-import { Component, Input, Output, AfterContentInit, ContentChildren, QueryList, EventEmitter, Renderer } from '@angular/core';
+import { Component, Input, Output, AfterContentInit, ContentChildren, QueryList, EventEmitter, Renderer, NgModule } from '@angular/core';
+import { CommonModule } from "@angular/common";
 import { FilterPipe, FilterOptions } from './filter-pipe';
 import { ListItem, ListHeader } from './items';
 
-declare var module: any;
+//declare var module: any;
 
 @Component({
     selector: 'ig-list',
     host: { 'role': 'list' },
-    pipes: [ FilterPipe ],
+    //pipes: [ FilterPipe ],
     moduleId: module.id, // commonJS standard
-    directives: [ ListItem, ListHeader ],
+    //directives: [ ListItem, ListHeader ],
     templateUrl: 'list-content.html'
 })
 
-export class List implements AfterContentInit { 
+export class List implements AfterContentInit {
     private _innerStyle: string = "ig-list-inner";
     private _items: ListItem[];
 
@@ -26,20 +27,20 @@ export class List implements AfterContentInit {
     @Output() filtering = new EventEmitter(false); // synchronous event emitter
     @Output() filtered = new EventEmitter();
 
-    constructor(private _renderer: Renderer) {        
+    constructor(private _renderer: Renderer) {
     }
 
     ngAfterContentInit() {
         var self = this;
-        if(this.searchInputId) {
-            if(!this.searchInputElement) {
+        if (this.searchInputId) {
+            if (!this.searchInputElement) {
                 this.searchInputElement = this.getFilterInputElement(this.searchInputId);
             }
-            
-            if(this.searchInputElement) {
+
+            if (this.searchInputElement) {
                 this._renderer.listen(this.searchInputElement, 'input', this.filter.bind(this));
-            }            
-        }        
+            }
+        }
     }
 
     addItem(item: ListItem) {
@@ -53,14 +54,14 @@ export class List implements AfterContentInit {
     filter() {
         var inputValue, result, filteringArgs, filteredArgs, items;
 
-        if(this.searchInputElement) {
+        if (this.searchInputElement) {
             filteringArgs = { cancel: false };
 
             this.filtering.emit(filteringArgs);
 
-            if(filteringArgs.cancel) {
-                return; 
-            }            
+            if (filteringArgs.cancel) {
+                return;
+            }
 
             this.filterOptions = this.filterOptions || new FilterOptions();
             this.filterOptions.items = this.filterOptions.items || this.items;
@@ -69,10 +70,18 @@ export class List implements AfterContentInit {
 
             filteredArgs = { result: result }
             this.filtered.emit(filteredArgs);
-        }        
+        }
     }
 
     private getFilterInputElement(id: string) {
         return <HTMLInputElement>document.getElementById(id);
     }
+}
+
+@NgModule({
+    declarations: [List, ListItem, ListHeader],
+    imports: [CommonModule],
+    exports: [List, ListItem, ListHeader]
+})
+export class ListModule {
 }
