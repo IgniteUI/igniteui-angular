668cb603a13bc464273648dd333b90b5d7bd7c58
petko5kov <petko_5kov@yahoo.com>
2016-09-27 19:08:53 +0300

    Add FilterModule to app.module.ts

--- demos/app/app.module.ts
+++ demos/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
+import { FilterModule } from '../../src/list/filter';
 
 import {
     IgInput, CarouselModule , CheckboxModule, SwitchModule,
@@ -28,6 +29,7 @@ import { routing } from "./app.routing";
         TabBarModule,
         ListModule,
         ButtonModule,
+        FilterModule,
         routing
     ],
     declarations: [
