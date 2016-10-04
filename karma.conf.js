6f48abf348ddc12b384aebf4594f5d456310b627
Zdravko Kolev <ZKolev@infragistics.com>
2016-10-04 17:54:49 +0300

    Add tests and improve functionalities by the spec

--- karma.conf.js
+++ karma.conf.js
@@ -45,6 +45,8 @@ module.exports = function(config) {
       'node_modules/zone.js/dist/async-test.js',
       'node_modules/zone.js/dist/fake-async-test.js',
 
+      'dist/zero-blocks.css',
+
       // RxJs
       { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
       { pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false },
@@ -83,7 +85,7 @@ module.exports = function(config) {
 
     exclude: [],
     preprocessors: {
-      'src/**/*.js' : ['coverage']
+      'src/**/!(*.spec).js' : ['coverage']
     },
     reporters: ['mocha', 'coverage'],
 
