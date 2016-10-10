51655d47243455afc9dbe72bb41d62a954f45e4b
Simeon Simeonoff <ssimeonoff@infragistics.com>
2016-10-10 18:33:43 +0300

    (improvement) button background and color can now be set on igButton (improvement) fab button box shadow (improvement) ripple effect opacity adjustments

--- demos/app/buttonssample.component.ts
+++ demos/app/buttonssample.component.ts
@@ -14,6 +14,14 @@ import { ButtonModule } from "../../src/button/button";
         </div>
         <br>
         <div style="width: 120px; margin: 10px">
+            <span igButton="raised" igButtonColor="#FBB13C" igButtonBackground="#340068" igRipple="white">Raised</span>  
+        </div>
+        <br>
+        <div style="width: 120px; margin: 10px">
+            <span igButton="raised" igButtonColor="yellow" igButtonBackground="black" igRipple="yellow">Test</span>
+        </div>
+        <br>
+        <div style="width: 120px; margin: 10px">
             <span igButton="gradient" igRipple>Gradient</span>
         </div>
         <br>
@@ -23,14 +31,20 @@ import { ButtonModule } from "../../src/button/button";
         <br>
         <div style="margin: 10px; margin: 10px">
             <span igButton="fab" igRipple="black">
-                <i class="material-icons">alarm_add</i>
+                <i class="material-icons">add</i>
+            </span>
+        </div>
+        <br>
+        <div style="margin: 10px; margin: 10px">
+            <span igButton="fab" igButtonColor="#484848" igButtonBackground="white" igRipple="#484848">
+                <i class="material-icons">edit</i>
             </span>
         </div>
         <div style="display: flex; flex-wrap: row; padding: 10px 0">
             <span igButton="icon" igRipple igRippleCentered="true">
                 <i class="material-icons">search</i>
             </span>
-            <span igButton="icon" igRipple="#E41C77" igRippleCentered="true">
+            <span igButton="icon" igRipple igButtonColor="#E41C77" igRippleCentered="true">
                 <i class="material-icons">favorite</i>
             </span>
             <span igButton="icon" igRipple igRippleCentered="true">
