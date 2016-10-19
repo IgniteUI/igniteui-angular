b35bc114c2c7fbe2cec1c669f9f006ba1c907211
d.georgiev.91 <d.georgiev.91@gmail.com>
2016-10-19 15:12:02 +0300

    Adding navbar component and sample for it

---
+++ src/navbar/navbar.ts
@@ -0,0 +1,40 @@
+import { Component, Input, NgModule } from '@angular/core';
+
+import { HammerGesturesManager } from "../core/touch";
+import { Location } from '@angular/common';
+
+import { ButtonModule } from "../../src/button/button";
+
+@Component({
+    selector: "ig-navbar",
+    moduleId: module.id, 
+    templateUrl: "navbar.html",
+    styleUrls: [ "navbar.scss" ],
+    providers: [HammerGesturesManager]
+})
+export class Navbar {
+    
+    @Input() title: string;
+
+    constructor(private _location: Location) {
+    }
+
+    canGoBack() {
+        var isStackHisoryEmpty = window.history.length == 0;
+
+        return isStackHisoryEmpty;
+    }
+
+    navigateBack($event) {
+        this._location.back();
+    }
+}
+
+
+@NgModule({
+    imports: [ButtonModule],
+    declarations: [Navbar],
+    exports: [Navbar]
+})
+export class NavbarModule {
+}
