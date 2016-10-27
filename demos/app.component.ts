7bc46f8c072c33c7b145c59daab5932439afec8d
petko5kov <petko_5kov@yahoo.com>
2016-10-27 18:45:46 +0300

    Move inline styling into component styles; Handle small screen styles

--- demos/app.component.ts
+++ demos/app.component.ts
@@ -2,7 +2,50 @@ import { Component, ElementRef } from "@angular/core";
 
 @Component({
     selector: 'sample-app',
-    templateUrl: 'demos/app.component.html'
+    templateUrl: 'demos/app.component.html',
+    styles: [`
+        .samples-container{
+            margin-top: 15em;
+        }
+        .code-container {
+            margin-top: 5em; 
+            margin-bottom: 5em; 
+            overflow: auto
+        }
+        .samples-container .text-content, 
+        .component-links,
+        .directive-links {
+            width: 50%;
+        }
+        .component-links {
+            float: left;
+        }
+        .directive-links {
+            float: right;
+        }
+        .codebox .header .title {
+            font-weight: bold;
+        }
+        .codebox .header .html,
+        .codebox .header .ts {
+            float: right;
+        }
+        .codebox .header .ts {
+            margin-right: 20px;
+        }
+        .codebox .content {
+            margin-top:20px;
+        }
+        .codebox .content pre {
+            border: 0; 
+            background-color: #fff;
+        }
+        @media (max-width: 767px) {
+            .samples-container {
+                margin-top: 5em;
+            }            
+        }
+        `]
 })
 export class AppComponent {
     private _el: ElementRef;
@@ -12,32 +55,32 @@ export class AppComponent {
     constructor(private el: ElementRef) {
         this._el = el;
         this.markup = `
-<div>
-    <span class="componentTitle">Switch</span><br>
-    <span class="componentDesc">A component that lets the user toggle between checked and unchecked states.</span><br><br>
-    <ig-switch [(ngModel)]="user.subscribed"></ig-switch>
-    <ig-switch [(ngModel)]="!user.subscribed"></ig-switch>
-</div>
+            <div>
+                <span class="componentTitle">Switch</span><br>
+                <span class="componentDesc">A component that lets the user toggle between checked and unchecked states.</span><br><br>
+                <ig-switch [(ngModel)]="user.subscribed"></ig-switch>
+                <ig-switch [(ngModel)]="!user.subscribed"></ig-switch>
+            </div>
         `;
         this.typescriptCode = `
-import { Component } from "@angular/core";
+            import { Component } from "@angular/core";
 
-@Component({
-    selector: "input-sample",
-    templateUrl: "demos/inputs/inputsample.component.html"
-})
-export class InputSampleComponent {
-    placeholder = "Please enter a value";
+            @Component({
+                selector: "input-sample",
+                templateUrl: "demos/inputs/inputsample.component.html"
+            })
+            export class InputSampleComponent {
+                placeholder = "Please enter a value";
 
-    user = {
-        name: 'John Doe',
-        password: '1337s3cr3t',
-        comment: "N/A",
-        registered: true,
-        subscribed: false,
-        favouriteVarName: 'Foo'
-    };
-}`;
+                user = {
+                    name: 'John Doe',
+                    password: '1337s3cr3t',
+                    comment: "N/A",
+                    registered: true,
+                    subscribed: false,
+                    favouriteVarName: 'Foo'
+                };
+            }`;
     }
 
     ngOnInit() {
