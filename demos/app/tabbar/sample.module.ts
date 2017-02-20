e02f24d391fc9d584fc22a6e3348ee46e314c93e
petko5kov <petko_5kov@yahoo.com>
2017-02-20 11:48:31 +0200

    #68 - extend the sample of tabbar to demonstrate loading content from  file

--- demos/app/tabbar/sample.module.ts
+++ demos/app/tabbar/sample.module.ts
@@ -1,9 +1,27 @@
 import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 import { IgxComponentsModule, IgxDirectivesModule } from "../../../src/main";
-import { TabBarSampleComponent } from "./sample.component";
+import { TabBarSampleComponent, CustomContentComponent } from "./sample.component";
+
+const tabbarRoutes: Routes = [
+    {
+        path: 'tabbar',
+        component: TabBarSampleComponent,
+        children: [
+            { path: 'tabbarInnerPath', component: CustomContentComponent, outlet: 'tabPanelOutlet' }
+        ]
+    }
+];
 
 @NgModule({
-    imports: [IgxComponentsModule, IgxDirectivesModule],
-    declarations: [TabBarSampleComponent]
+    imports: [
+        IgxComponentsModule,
+        IgxDirectivesModule,
+        RouterModule.forChild(tabbarRoutes)
+    ],
+    declarations: [
+        TabBarSampleComponent,
+        CustomContentComponent
+    ]
 })
-export class TabBarSampleModule {}
+export class TabBarSampleModule { }
