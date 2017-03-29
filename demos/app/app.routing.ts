0dc3390ea948cac9d5ae32975f76495497a339a2
Simeon Simeonoff <ssimeonov@infragistics.com>
2017-03-29 18:00:03 +0300

    (ADD) Sample heading styling

--- demos/app/app.routing.ts
+++ demos/app/app.routing.ts
@@ -22,7 +22,7 @@ import {DataOperationsSampleComponent} from "./data-operations/sample.component"
 const appRoutes: Routes = [
     {
         path: '',
-        redirectTo: '/grid',
+        redirectTo: '/card',
         pathMatch: 'full'
     },
     {
