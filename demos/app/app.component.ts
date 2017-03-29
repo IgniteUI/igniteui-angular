77822c052d6e8276009a9d9a2bd82d9037621461
Simeon Simeonoff <ssimeonov@infragistics.com>
2017-03-29 16:35:56 +0300

    (ADD) Replace button navigation with side navigation

--- demos/app/app.component.ts
+++ demos/app/app.component.ts
@@ -2,29 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'sample-app',
-    template:`
-        <h1 style="text-align: center">Ignite UI JS Blocks Samples</h1>
-        <nav style="display: flex; justify-content: center; flex-wrap: wrap">
-            <span igxButton routerLink="/inputs">Inputs</span>
-            <span igxButton routerLink="/carousel">Carousel</span>
-            <span igxButton routerLink="/tabbar">TabBar</span>
-            <span igxButton routerLink="/list">List</span>
-            <span igxButton routerLink="/buttons">Buttons</span>
-            <span igxButton routerLink="/buttonGroup">ButtonGroup</span>
-            <span igxButton routerLink="/avatar">Avatar</span>
-            <span igxButton routerLink="/navigation-drawer">Navigation Drawer</span>
-            <span igxButton routerLink="/navbar">Navbar</span>
-            <span igxButton routerLink="/dialog">Dialog</span>
-            <span igxButton routerLink="/progressbar">Progressbar</span>
-            <span igxButton routerLink="/icon">Icon</span>
-            <span igxButton routerLink="/snackbar">Snackbar</span>
-            <span igxButton routerLink="/grid">Grid</span>
-            <span igxButton routerLink="/toast">Toast</span>
-            <span igxButton routerLink="/card">Card</span>
-            <span igxButton routerLink="/range">Range</span>
-            <span igxButton routerLink="/data-operations">Data Operations</span>
-        </nav>
-        <router-outlet></router-outlet>
-    `
+    templateUrl: 'demos/app/app.component.html',
+    styleUrls: ['demos/app/app.component.css']
 })
 export class AppComponent {}
