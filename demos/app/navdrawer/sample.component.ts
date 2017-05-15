e9adbea0daebac19ad12c5705be2e57a8cacf14d
Simeon Simeonoff <ssimeonov@infragistics.com>
2017-05-15 18:01:12 +0300

    (IMPROVE) Utilize the Navdrawer Component in the Demos Page (DEL) Previous navigation-drawer sample

---
+++ demos/app/navdrawer/sample.component.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core";
+import { AppComponent } from "./../app.component";
+
+@Component({
+    moduleId: module.id,
+    selector: "navbar-sample",
+    styleUrls: ["../app.samples.css", "./sample.component.css"],
+    templateUrl: "sample.component.html"
+})
+export class NavdrawerSampleComponent {
+    constructor(private app: AppComponent) {}
+
+    private toggle() {
+        const { navdrawer, drawerState: { open } } = this.app;
+        open ? navdrawer.open() : navdrawer.close();
+    }
+
+    private pinned() {
+        this.app.drawerState.pin = !this.app.drawerState.pin;
+        this.toggle();
+    }
+}
