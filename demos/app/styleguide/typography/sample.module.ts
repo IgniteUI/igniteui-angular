ee8261a3a7cfc5625da5ff5adda0290d789a9463
Simeon Simeonoff <ssimeonov@infragistics.com>
2017-06-30 10:36:22 +0300

    Add typography demo.

---
+++ demos/app/styleguide/typography/sample.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import { IgxTypographySampleComponent } from "./sample.component";
+
+@NgModule({
+    declarations: [IgxTypographySampleComponent],
+    imports: []
+})
+export class IgxTypographySampleModule {}
