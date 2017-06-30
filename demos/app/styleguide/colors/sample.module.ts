7c145c5e189fe76d8b7c48ba952e7268dd2f5972
Simeon Simeonoff <ssimeonov@infragistics.com>
2017-06-30 10:35:08 +0300

    Add colors demo.

---
+++ demos/app/styleguide/colors/sample.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import { IgxColorsSampleComponent } from "./sample.component";
+
+@NgModule({
+    declarations: [IgxColorsSampleComponent],
+    imports: []
+})
+export class IgxColorsSampleModule {}
