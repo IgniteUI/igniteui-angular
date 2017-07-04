245305748d4242c64814f540ca8424a1f2d49835
Simeon Simeonoff <ssimeonov@infragistics.com>
2017-07-04 15:14:32 +0300

    Change calendar sample to be inside a card.

--- demos/app/calendar/sample.module.ts
+++ demos/app/calendar/sample.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 
-import { IgxCalendarModule } from "../../../src/main";
+import { IgxCalendarModule, IgxCardModule } from "../../../src/main";
 import { IgxCalendarSampleComponent } from "./sample.component";
 
 @NgModule({
     declarations: [IgxCalendarSampleComponent],
-    imports: [IgxCalendarModule, CommonModule]
+    imports: [IgxCalendarModule, IgxCardModule, CommonModule]
 })
 export class IgxCalendarSampleModule {}
