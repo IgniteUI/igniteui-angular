39403bbb82c213777a109b5d53067f2a5597e316
Aleksandar Kamenov <AKamenov@infragistics.com>
2017-07-13 16:46:37 +0300

    remove igxDatePicker from icon demos

--- demos/app/icon/sample.component.ts
+++ demos/app/icon/sample.component.ts
@@ -7,11 +7,4 @@ import { IgxIconModule } from "../../../src/main";
     styleUrls: ["../app.samples.css", "./sample.component.css"],
     templateUrl: "icon.sample.html"
 })
-export class IconSampleComponent {
-    private val = new Date(2017, 7, 7);
-
-    public customFormatter = (_: Date) => {
-        return `${_.getFullYear()}/${_.getMonth()}/${_.getDate()}`;
-    }
-
-}
+export class IconSampleComponent {}
--- demos/app/icon/sample.module.ts
+++ demos/app/icon/sample.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from "@angular/core";
 import { IgxIconModule } from "../../../src/main";
 import { IconSampleComponent } from "./sample.component";
-import { IgxDatePickerModule } from "../../../src/date-picker/date-picker.component"
 
 @NgModule({
     declarations: [IconSampleComponent],
-    imports: [IgxIconModule, IgxDatePickerModule]
-})
+    imports: [IgxIconModule]
+ })
 export class IconSampleModule {}
