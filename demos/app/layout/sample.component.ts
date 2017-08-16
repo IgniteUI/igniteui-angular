4682115b796d92698b3b3cda44874aaab63a3873
Simeon Simeonoff <ssimeonov@infragistics.com>
2017-08-16 13:44:38 +0300

    Add layout directive demo sample.

---
+++ demos/app/layout/sample.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    moduleId: module.id,
+    selector: "layout-sample",
+    styleUrls: ["./sample.component.css", "../app.samples.css"],
+    templateUrl: "./sample.component.html"
+})
+export class LayoutSampleComponent { }
