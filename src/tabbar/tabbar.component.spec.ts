9200452adbb85754f8691e2d5a90acab93f104c4
INFRAGISTICS\DDincheva <desislava9dincheva@gmail.com>
2018-02-07 15:19:00 +0200

    removed unnecessary code

--- src/tabbar/tabbar.component.spec.ts
+++ src/tabbar/tabbar.component.spec.ts
@@ -3,7 +3,7 @@ import { async, TestBed } from "@angular/core/testing";
 import { By } from "@angular/platform-browser";
 import { IgxTabBarComponent, IgxTabBarModule, IgxTabComponent, IgxTabPanelComponent } from "./tabbar.component";
 
-fdescribe("TabBar", () => {
+describe("TabBar", () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [TabBarTestComponent, BottomTabBarTestComponent],
