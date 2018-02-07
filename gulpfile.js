14c49ad359a0963b911eb01ba505b79da14678cd
Simeon Simeonoff <ssimeonov@infragistics.com>
2018-02-07 20:43:38 +0200

    build(styles): switch to gulp-sass with sourcemaps
    Use gulp-sass to build themes and move the compiled themes to
    dest/styles.

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,12 @@
 
 const autoprefixer = require("autoprefixer");
 const gulp = require("gulp");
-const sass = require("node-sass");
-const postcss = require("postcss");
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const postcss = require("gulp-postcss");
 const inlineTemplates = require("gulp-inline-ng2-template");
 const exec = require("child_process").exec;
 const fs = require("fs");
-
-const prefixer = postcss([autoprefixer({
-    browsers: ["last 5 versions", "> 3%"],
-    cascade: false,
-    grid: true
-})]);
-
-function compileSass(path, ext, file, callback) {
-    let compiledCss = sass.renderSync({
-        file: path,
-        outputStyle: "compressed"
-    });
-    callback(null, prefixer.process(compiledCss.css).css);
-}
-
 
 const INLINE_TEMPLATES = {
     SRC: "./src/**/*.ts",
@@ -30,14 +16,17 @@ const INLINE_TEMPLATES = {
         base: "/src",
         target: "es6",
         removeLineBreaks: true,
-        useRelativePaths: true,
-        styleProcessor: compileSass
+        useRelativePaths: true
     }
 };
 
-const THEMING = {
-    SRC: "./src/core/styles/**/*",
-    DIST: "dist/core/styles"
+const STYLES = {
+    SRC: "./src/core/styles/themes/presets/*",
+    DIST: "./dist/styles",
+    MAPS: "./maps",
+    CONFIG: {
+        outputStyle: "compressed"
+    }
 }
 
 gulp.task("make-packagejson", () => {
@@ -56,30 +45,26 @@ gulp.task("make-packagejson", () => {
     });
 });
 
-
 gulp.task("build-style", () => {
-    let result = sass.renderSync({
-        file: "src/core/styles/themes/presets/igniteui-angular.scss",
-        outputStyle: "compressed",
-        includePaths: ["./src/core/styles/"],
-        outFile: "dist/styles/igniteui-angular.css"
-    });
+    const prefixer = postcss([autoprefixer({
+        browsers: ["last 5 versions", "> 3%"],
+        cascade: false,
+        grid: true
+    })]);
 
-    fs.writeFile("dist/igniteui-angular.css", prefixer.process(result.css).css, (err) => {
-        if (err) throw err;
-    });
-
-    return gulp.src(THEMING.SRC)
-        .pipe(gulp.dest(THEMING.DIST));
+    return gulp.src(STYLES.SRC)
+        .pipe(sourcemaps.init())
+        .pipe(sass.sync(STYLES.CONFIG).on('error', sass.logError))
+        .pipe(prefixer)
+        .pipe(sourcemaps.write(STYLES.MAPS))
+        .pipe(gulp.dest(STYLES.DIST));
 });
-
 
 gulp.task("inline-templates", () => {
     return gulp.src(INLINE_TEMPLATES.SRC)
         .pipe(inlineTemplates(INLINE_TEMPLATES.CONFIG))
         .pipe(gulp.dest(INLINE_TEMPLATES.DIST));
 });
-
 
 gulp.task("build:esm", ["inline-templates"], (callback) => {
     exec("npm run ngcompile && npm run build:styles", function (err, stdout, stderr) {
@@ -88,7 +73,6 @@ gulp.task("build:esm", ["inline-templates"], (callback) => {
     });
 });
 
-
 gulp.task("build:esm:watch", ["build:esm"], () => {
-    gulp.watch("src/**/*", ["build:esm", "build-style"]);
+    gulp.watch("src/**/*", ["build:esm"]);
 });
--- karma.conf.ts
+++ karma.conf.ts
@@ -8,7 +8,7 @@ export default (config) => {
         frameworks: ["jasmine"],
 
         files: [
-            "./dist/igniteui-angular.css",
+            "./dist/styles/igniteui-angular.css",
             "./node_modules/hammerjs/hammer.js",
             "./node_modules/hammer-simulator/index.js",
             "karma-test-entry.ts"
