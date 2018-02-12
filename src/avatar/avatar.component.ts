43eef8be5c2d28134fe1134a7def6865dd2119f7
Simeon Simeonoff <ssimeonov@infragistics.com>
2018-02-12 15:56:29 +0200

    refactor(avatar): further refactoring
    remove unnecessary getters and setters adhere to angular styleguide

--- src/avatar/avatar.component.ts
+++ src/avatar/avatar.component.ts
@@ -12,9 +12,9 @@ import {
 import { IgxIconModule } from "../icon/icon.component";
 
 export enum Size {
-    SMALL,
-    MEDIUM,
-    LARGE
+    SMALL = "small",
+    MEDIUM = "medium",
+    LARGE = "large"
 }
 
 @Component({
@@ -22,91 +22,80 @@ export enum Size {
     templateUrl: "avatar.component.html"
 })
 export class IgxAvatarComponent implements OnInit {
-    @ViewChild("image") public image: ElementRef;
-    @ViewChild("initialsImage") public initialsImage: ElementRef;
-    @ViewChild("imageTemplate", { read: TemplateRef }) protected imageTemplate: TemplateRef<any>;
-    @ViewChild("initialsTemplate", { read: TemplateRef }) protected initialsTemplate: TemplateRef<any>;
-    @ViewChild("iconTemplate", { read: TemplateRef }) protected iconTemplate: TemplateRef<any>;
+    @ViewChild("image")
+    public image: ElementRef;
 
-    @HostBinding("attr.aria-label") public ariaLabel = "avatar";
-    @HostBinding("attr.role") public role = "img";
+    @ViewChild("imageTemplate", { read: TemplateRef })
+    protected imageTemplate: TemplateRef<any>;
+
+    @ViewChild("initialsTemplate", { read: TemplateRef })
+    protected initialsTemplate: TemplateRef<any>;
+
+    @ViewChild("iconTemplate", { read: TemplateRef })
+    protected iconTemplate: TemplateRef<any>;
+
+    @HostBinding("attr.aria-label")
+    public ariaLabel = "avatar";
+
+    @HostBinding("attr.role")
+    public role = "img";
+
     @HostBinding("attr.class")
     public get classes() {
-        if (this.isRounded) {
-            return `igx-avatar--rounded igx-avatar--${this.size}`;
+        if (this.roundShape) {
+            return `igx-avatar--rounded igx-avatar--${this._size}`;
         }
-        return `igx-avatar igx-avatar--${this.size}`;
+        return `igx-avatar igx-avatar--${this._size}`;
     }
 
-    public sizeEnum = Size;
     public roleDescription: string;
-    private _size: string;
-    private _bgColor: string;
-    private _icon: string;
-    private _initials: string;
-    private _src: string;
-
-    @Input("roundShape") public roundShape = "false";
-    @Input() public color = "";
+    private _size: string | Size = "small";
 
     @Input()
-    get size(): string {
-        return this._size === undefined ? "small" : this._size;
-    }
-
-    set size(value: string) {
-        const sizeType = this.sizeEnum[value.toUpperCase()];
-        this._size = sizeType === undefined ? "small" : value.toLowerCase();
-    }
+    public roundShape = false;
 
     @Input()
-    get bgColor(): string {
-        return this._bgColor;
+    public color: string;
+
+    @Input()
+    public bgColor: string;
+
+    @Input()
+    public initials: string;
+
+    @Input()
+    public icon: string;
+
+    @Input()
+    public src: string;
+
+    @Input()
+    public get size(): string | Size {
+        return this._size;
     }
 
-    set bgColor(value: string) {
-        const color = value === "" ? "lightgrey" : value;
-        this._bgColor = color;
-    }
-
-    get isRounded(): boolean {
-        return this.roundShape.toUpperCase() === "TRUE" ? true : false;
+    public set size(value: string | Size) {
+        switch (value) {
+            case "small":
+            case "medium":
+            case "large":
+                this._size = value;
+                break;
+            default:
+                this._size = "small";
+        }
     }
 
     get template() {
-        if (this._src) {
+        if (this.src) {
             return this.imageTemplate;
         }
 
-        if (this._initials) {
+        if (this.initials) {
             return this.initialsTemplate;
         }
 
         return this.iconTemplate;
-    }
-
-    @Input() public get initials(): string {
-        return this._initials;
-    }
-
-    public set initials(value: string) {
-        this._initials = value;
-    }
-
-    @Input() public get icon(): string {
-        return this._icon;
-    }
-
-    public set icon(value: string) {
-        this._icon = value;
-    }
-
-    @Input() public get src(): string {
-        return this._src;
-    }
-
-    public set src(value: string) {
-        this._src = value;
     }
 
     constructor(public elementRef: ElementRef) { }
@@ -116,9 +105,9 @@ export class IgxAvatarComponent implements OnInit {
     }
 
     private getRole() {
-        if (this._initials) {
+        if (this.initials) {
             return "initials type avatar";
-        } else if (this._src) {
+        } else if (this.src) {
             return "image type avatar";
         } else {
             return "icon type avatar";
