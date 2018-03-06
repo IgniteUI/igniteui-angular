9377f2796d0f2d9837195a850ca8a601e68855a8
Simeon Simeonoff <ssimeonov@infragistics.com>
2018-03-06 10:32:58 +0200

    refactor(demos): add samples using font-awesome icons

--- demos/app/icon/sample.module.ts
+++ demos/app/icon/sample.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from "@angular/core";
-import { IgxIconService } from "../../lib/icon/icon.service";
 import { IgxIconModule } from "../../lib/main";
 import { PageHeaderModule } from "../pageHeading/pageHeading.module";
 import { IconSampleComponent } from "./sample.component";
 
 @NgModule({
     declarations: [IconSampleComponent],
-    imports: [IgxIconModule, PageHeaderModule],
-    providers: [IgxIconService]
+    imports: [IgxIconModule, PageHeaderModule]
 })
 export class IconSampleModule { }
