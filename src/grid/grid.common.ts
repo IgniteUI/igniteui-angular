6c0854f88350145b079762c61d39f127d5af339e
Radoslav Karaivanov <RKaraivanov@infragistics.com>
2018-03-07 13:49:18 +0200

    IgxGrid and dynamic components with OnPush detection.
    Added a decorator for the grid components created dynamically,
    to trigger change detection.
    For additional information check
    https://github.com/angular/angular/issues/14087
    Improved filtering dialog positioning.
    Fixed a bug with `equals` filtering condition and improved
    initial filtering condition when opening the filter UI.
    Removed workarounds in the interaction between the grid component
    and the virtualization directive.

--- src/grid/grid.common.ts
+++ src/grid/grid.common.ts
@@ -1,4 +1,5 @@
-import { Directive, TemplateRef } from "@angular/core";
+import { ChangeDetectorRef, Directive, TemplateRef } from "@angular/core";
+import { IgxGridAPIService } from "./api.service";
 
 @Directive({
     selector: "[igxCell]"
@@ -31,4 +32,41 @@ export class IgxCellFooterTemplateDirective {
 export class IgxCellEditorTemplateDirective {
 
     constructor(public template: TemplateRef<any>) { }
+}
+
+export interface IGridBus {
+    gridID: string;
+    cdr: ChangeDetectorRef;
+    gridAPI: IgxGridAPIService;
+}
+
+/**
+ * Decorates a setter or a method of a component implementing the IGridBus
+ * interface triggering change detection in the parent grid when it is called.
+ * If `markForCheck` is set to true it will also mark for check the instance
+ * containing the setter/method.
+ */
+export function autoWire(markForCheck = false) {
+    return function decorator(target: IGridBus, name: string, descriptor: any) {
+        const old = descriptor.value || descriptor.set;
+
+        const wrapped = function(...args) {
+            const result = old.apply(this, args);
+            if (markForCheck) {
+                this.cdr.markForCheck();
+            }
+            this.gridAPI.notify(this.gridID);
+            return result;
+        };
+
+        if (descriptor.set) {
+            descriptor.set = wrapped;
+        } else if (descriptor.value) {
+            descriptor.value = wrapped;
+        } else {
+            throw Error("Can bind only to setter properties and methods");
+        }
+
+        return descriptor;
+    };
 }
