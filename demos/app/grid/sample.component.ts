708be351e4acfc49429c1ad5cecf6b660fe4d1c1
Konstantin Dinev <kdinev@mail.bw.edu>
2018-03-19 15:28:21 +0200

    Renaming the selection event

--- demos/app/grid/sample.component.ts
+++ demos/app/grid/sample.component.ts
@@ -76,7 +76,7 @@ export class RemoteService {
     }
 
     private buildUrl(dataState: IDataState): string {
-        let qS: string = "";
+        let qS = "";
         if (dataState && dataState.paging) {
             const skip = dataState.paging.index * dataState.paging.recordsPerPage;
             const top = dataState.paging.recordsPerPage;
@@ -117,7 +117,7 @@ export class GridSampleComponent {
     public newRecord = "";
     public editCell;
     constructor(private localService: LocalService,
-        private remoteService: RemoteService) { }
+    private remoteService: RemoteService) { }
     public ngOnInit(): void {
         this.data = this.localService.records;
         this.remote = this.remoteService.remoteData;
@@ -207,7 +207,7 @@ export class GridSampleComponent {
     }
 
     public selectCell(event) {
-        this.selectedCell = event.cell;
+        this.selectedCell = event;
     }
 
     public addRow() {
