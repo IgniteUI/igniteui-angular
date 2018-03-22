b979a6bc7d6e3002d7e385b5885b34a7e2427e29
INFRAGISTICS\tiliev <tiliev@infragistics.com>
2018-03-22 09:56:32 +0200

    Improved input group validation.

--- src/directives/input/input.directive.ts
+++ src/directives/input/input.directive.ts
@@ -8,18 +8,22 @@ import {
     HostListener,
     Inject,
     NgModule,
+    OnDestroy,
     OnInit,
     Optional,
     Renderer2,
     Self
 } from "@angular/core";
 import { FormGroup, NgModel } from "@angular/forms";
+import { Subscription } from "rxjs/Subscription";
 import { IgxInputGroupComponent } from "../../main";
 
 @Directive({
     selector: "[igxInput]"
 })
-export class IgxInputDirective implements OnInit {
+export class IgxInputDirective implements OnInit, OnDestroy {
+    private _statusChanges$: Subscription;
+
     constructor(
         @Inject(forwardRef(() => IgxInputGroupComponent))
         public inputGroup: IgxInputGroupComponent,
@@ -43,7 +47,7 @@ export class IgxInputDirective implements OnInit {
         this.inputGroup.isFocused = false;
         if (this.ngModel) {
             this.inputGroup.isValid = this.inputGroup.isInvalid = false;
-            if (!this.ngModel.control.valid && this.ngModel.control.touched) {
+            if (!this.ngModel.valid && this.ngModel.touched) {
                 this.inputGroup.isInvalid = true;
             }
         }
@@ -53,16 +57,6 @@ export class IgxInputDirective implements OnInit {
     public onInput(event) {
         const value: string = this.element.nativeElement.value;
         this.inputGroup.isFilled = value && value.length > 0;
-
-        if (this.ngModel && (this.ngModel.validator || this.ngModel.asyncValidator)) {
-            if (this.ngModel.control.valid) {
-                this.inputGroup.isValid = true;
-                this.inputGroup.isInvalid = false;
-            } else {
-                this.inputGroup.isInvalid = true;
-                this.inputGroup.isValid = false;
-            }
-        }
     }
 
     ngOnInit() {
@@ -88,6 +82,23 @@ export class IgxInputDirective implements OnInit {
             this.isTextArea = true;
         } else {
             this.isInput = true;
+        }
+
+        if (this.ngModel) {
+            this._statusChanges$ = this.ngModel.statusChanges.subscribe(this.onStatusChanged.bind(this));
+        }
+    }
+
+    ngOnDestroy() {
+        if (this._statusChanges$) {
+            this._statusChanges$.unsubscribe();
+        }
+    }
+
+    protected onStatusChanged(status: string) {
+        if (!this.ngModel.control.pristine && (this.ngModel.validator || this.ngModel.asyncValidator)) {
+            this.inputGroup.isValid = this.ngModel.valid;
+            this.inputGroup.isInvalid = this.ngModel.invalid;
         }
     }
 
