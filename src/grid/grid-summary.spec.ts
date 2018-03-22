0d02cf39c293e58de3d28e026e9eea2891e80995
INFRAGISTICS\DDincheva <desislava9dincheva@gmail.com>
2018-03-22 11:29:21 +0200

    added tests for summaries

--- src/grid/grid-summary.spec.ts
+++ src/grid/grid-summary.spec.ts
@@ -4,20 +4,18 @@ import { By } from "@angular/platform-browser";
 import { IgxGridComponent } from "./grid.component";
 import { IgxGridModule } from "./index";
 
-fdescribe("IgxGrid - Summaries", () => {
+describe("IgxGrid - Summaries", () => {
     const summaryClass = ".igx-grid-summary";
     const summaryLabel = ".igx-grid-summary-item__label";
     const summaryValue = "igx-grid-summary-item__result";
     const itemClass = "igx-grid-summary-item";
     const hiddenItemClass = "igx-grid-summary-item--inactive";
-    const summaryResultClass = "igx-grid-summary-item__result--left-align";
-    const numberSummaryResultClass = "igx-grid-summary-item__result";
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
                 NoActiveSummariesComponent,
-                SummariedColumnComponent
+                SummaryColumnComponent
             ],
             imports: [IgxGridModule.forRoot()]
         })
@@ -45,23 +43,23 @@ fdescribe("IgxGrid - Summaries", () => {
         expect(grid.getColumnByName("OrderDate").hasSummary).toBe(false);
     });
     it("should have summary per each column that 'hasSummary'= true", () => {
-        const fixture = TestBed.createComponent(SummariedColumnComponent);
+        const fixture = TestBed.createComponent(SummaryColumnComponent);
         fixture.detectChanges();
 
         expect(fixture.debugElement.query(By.css(summaryClass))).toBeDefined();
 
         let summaries = 0;
-        const summariedColumns = fixture.componentInstance.instance.columnList.filter((col) => col.hasSummary === true).length;
+        const summariedColumns = fixture.componentInstance.grid1.columnList.filter((col) => col.hasSummary === true).length;
         fixture.debugElement.queryAll(By.css(summaryClass)).forEach((x) => {
-            if (x.childNodes.length > 0) {summaries ++; }
+            summaries ++;
         });
         expect(summaries).toBe(summariedColumns);
     });
     it("should have count summary for string and boolean data types", () => {
-        const fixture = TestBed.createComponent(SummariedColumnComponent);
+        const fixture = TestBed.createComponent(SummaryColumnComponent);
         fixture.detectChanges();
 
-        const sum = fixture.componentInstance.instance;
+        const sum = fixture.componentInstance.grid1;
         const summaries = fixture.debugElement.queryAll(By.css("igx-grid-summary"));
 
         let index = 0;
@@ -75,10 +73,10 @@ fdescribe("IgxGrid - Summaries", () => {
         });
     });
     it("should have count, min, max, avg and sum summary for numeric data types", () => {
-        const fixture = TestBed.createComponent(SummariedColumnComponent);
+        const fixture = TestBed.createComponent(SummaryColumnComponent);
         fixture.detectChanges();
 
-        const sum = fixture.componentInstance.instance;
+        const sum = fixture.componentInstance.grid1;
         const summaries = fixture.debugElement.queryAll(By.css("igx-grid-summary"));
 
         let index = 0;
@@ -96,10 +94,10 @@ fdescribe("IgxGrid - Summaries", () => {
         });
     });
     it("should have count, earliest and latest summary for numeric data types", () => {
-        const fixture = TestBed.createComponent(SummariedColumnComponent);
+        const fixture = TestBed.createComponent(SummaryColumnComponent);
         fixture.detectChanges();
 
-        const sum = fixture.componentInstance.instance;
+        const sum = fixture.componentInstance.grid1;
         const summaries = fixture.debugElement.queryAll(By.css("igx-grid-summary"));
 
         let index = 0;
@@ -115,11 +113,10 @@ fdescribe("IgxGrid - Summaries", () => {
         });
     });
     it("should make inactive/active summary function by click on it's label", () => {
-        const fixture = TestBed.createComponent(SummariedColumnComponent);
+        const fixture = TestBed.createComponent(SummaryColumnComponent);
         fixture.detectChanges();
 
         const summary = fixture.debugElement.queryAll(By.css("igx-grid-summary"))[3];
-        const item = summary.queryAll(By.css(itemClass));
         const min: DebugElement = summary.query(By.css("[title='Min']"));
 
         expect(min.parent.nativeElement.classList.contains(itemClass)).toBeTruthy();
@@ -131,6 +128,78 @@ fdescribe("IgxGrid - Summaries", () => {
         fixture.detectChanges();
 
         expect(min.parent.nativeElement.classList.contains(itemClass)).toBeTruthy();
+    });
+    it("should recalculate summary functions onRowAdded", () => {
+        const fixture = TestBed.createComponent(SummaryColumnComponent);
+        fixture.detectChanges();
+
+        const grid = fixture.componentInstance.grid1;
+        const summaries = fixture.debugElement.queryAll(By.css(summaryClass));
+        const addButton: DebugElement =  fixture.debugElement.query(By.css("#btnAddRow"));
+        let countValue;
+        summaries.forEach((summary) => {
+            const countLabel = summary.query(By.css("[title='Count']"));
+            countValue = countLabel.nativeElement.nextSibling.innerText;
+        });
+        expect(+countValue).toBe(grid.rowList.length);
+
+        addButton.triggerEventHandler("click", null);
+        fixture.detectChanges();
+
+        summaries.forEach((summary) => {
+            const countLabel = summary.query(By.css("[title='Count']"));
+            countValue = countLabel.nativeElement.nextSibling.innerText;
+        });
+
+        expect(+countValue).toBe(grid.rowList.length);
+    });
+    it("should recalculate summary functions onRowDeleted", () => {
+        const fixture = TestBed.createComponent(SummaryColumnComponent);
+        fixture.detectChanges();
+
+        const grid = fixture.componentInstance.grid1;
+        const summaries = fixture.debugElement.queryAll(By.css(summaryClass));
+        const addButton: DebugElement =  fixture.debugElement.query(By.css("#btnDeleteRow"));
+        let countValue;
+        summaries.forEach((summary) => {
+            const countLabel = summary.query(By.css("[title='Count']"));
+            countValue = countLabel.nativeElement.nextSibling.innerText;
+        });
+        expect(+countValue).toBe(grid.rowList.length);
+
+        addButton.triggerEventHandler("click", null);
+        fixture.detectChanges();
+
+        summaries.forEach((summary) => {
+            const countLabel = summary.query(By.css("[title='Count']"));
+            countValue = countLabel.nativeElement.nextSibling.innerText;
+        });
+        expect(+countValue).toBe(grid.rowList.length);
+    });
+    it("should recalculate summary functions updateRow", () => {
+        const fixture = TestBed.createComponent(SummaryColumnComponent);
+        fixture.detectChanges();
+
+        const grid = fixture.componentInstance.grid1;
+        const summaries = fixture.debugElement.queryAll(By.css(summaryClass));
+        const updateButton: DebugElement =  fixture.debugElement.query(By.css("#btnUpdateRow"));
+        const productNameCell = grid.getCellByColumn(0, "ProductName");
+        const unitsInStockCell = grid.getCellByColumn(0, "UnitsInStock");
+        let countValue;
+        summaries.forEach((summary) => {
+            const countLabel = summary.query(By.css("[title='Count']"));
+            countValue = countLabel.nativeElement.nextSibling.innerText;
+        });
+        expect(+countValue).toBe(grid.rowList.length);
+        expect(productNameCell.value).toBe("Chai");
+        expect(unitsInStockCell.value).toBe(2760);
+
+        updateButton.triggerEventHandler("click", null);
+        fixture.detectChanges();
+
+        expect(+countValue).toBe(grid.rowList.length);
+        expect(productNameCell.value).toBe("Spearmint");
+        expect(unitsInStockCell.value).toBe(1);
     });
 });
 
@@ -177,7 +246,7 @@ export class  NoActiveSummariesComponent{
 
 @Component({
     template: `
-        <igx-grid [data]="data">
+        <igx-grid #grid1 [data]="data">
             <igx-column field="ProductID" header="Product ID">
             </igx-column>
             <igx-column field="ProductName" [hasSummary]="true">
@@ -189,9 +258,12 @@ export class  NoActiveSummariesComponent{
             <igx-column field="OrderDate" width="200px" [dataType]="'date'" [sortable]="true" [hasSummary]="true">
             </igx-column>
         </igx-grid>
+        <button id="btnAddRow" (click)="addRow()">Add new row</button>
+        <button id="btnDeleteRow" (click)="deleteRow()">Delete first row</button>
+        <button id="btnUpdateRow" (click)="updateRow()">Update first row</button>
     `
 })
-export class  SummariedColumnComponent{
+export class  SummaryColumnComponent{
 
     public data = [
         { ProductID: 1, ProductName: "Chai", InStock: true, UnitsInStock: 2760, OrderDate: new Date("2005-03-21") },
@@ -205,7 +277,19 @@ export class  SummariedColumnComponent{
         { ProductID: 9, ProductName: "Teatime Chocolate Biscuits", InStock: true, UnitsInStock: 6998, OrderDate: new Date("2025-12-25") },
         { ProductID: 10, ProductName: "Chocolate", InStock: true, UnitsInStock: 20000, OrderDate: new Date("2018-03-01") }
     ];
-    @ViewChild(IgxGridComponent, { read: IgxGridComponent })
-    public instance: IgxGridComponent;
+    @ViewChild("grid1", { read: IgxGridComponent })
+    public grid1: IgxGridComponent;
 
+    public addRow() {
+        this.grid1.addRow({
+            ProductID: 11, ProductName: "Belgian Chocolate", InStock: true, UnitsInStock: 99000, OrderDate: new Date("2018-03-01")
+        });
+    }
+    public deleteRow() {
+        this.grid1.deleteRow(0);
+    }
+    public updateRow() {
+        this.grid1.updateRow({
+            ProductID: 1, ProductName: "Spearmint", InStock: true, UnitsInStock: 1, OrderDate: new Date("2005-03-21") }, 0);
+    }
 }
