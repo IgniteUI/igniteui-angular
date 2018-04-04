9c04f336ad1df73c50495e6d172529614bdf44ec
Radoslav Karaivanov <RKaraivanov@infragistics.com>
2018-04-04 12:16:17 +0300

    fix(igxGrid): Header height is not taken into account
    Closes #900

--- src/grid/grid.component.ts
+++ src/grid/grid.component.ts
@@ -315,6 +315,7 @@ export class IgxGridComponent implements OnInit, OnDestroy, AfterContentInit, Af
 
         this.initColumns(this.columnList, (col: IgxColumnComponent) => this.onColumnInit.emit(col));
         this.columnListDiffer.diff(this.columnList);
+        this.markForCheck();
 
         this.columnList.changes
             .pipe(takeUntil(this.destroy$))
