50055d1fcbd062d0705088bda11c07652487a34f
INFRAGISTICS\DDincheva <desislava9dincheva@gmail.com>
2018-04-04 16:40:50 +0300

    fix(IgxGridHeaderComponent): alignment  according column data type #796

--- src/grid/grid-header.component.ts
+++ src/grid/grid-header.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DoCheck, HostBinding, HostListener, Input, OnInit } from "@angular/core";
+import { DataType } from "../data-operations/data-util";
 import { SortingDirection } from "../data-operations/sorting-expression.interface";
 import { IgxGridAPIService } from "./api.service";
 import { IgxColumnComponent } from "./column.component";
@@ -38,6 +39,11 @@ export class IgxGridHeaderComponent implements IGridBus, OnInit, DoCheck {
     @HostBinding("class.desc")
     get descending() {
         return this.sortDirection === SortingDirection.Desc;
+    }
+
+    @HostBinding("class.igx-grid__th--number")
+    get columnType() {
+        return this.column.dataType === DataType.Number;
     }
 
     @HostBinding("class.igx-grid__th--sorted")
