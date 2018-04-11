73c5c61d1123e468736ba0d1bca9f62b150beca3
AMarinov <amarinov@infragistics.com>
2018-04-11 12:38:06 +0300

    test(row-selection): Clicking other cell is not [de]selecting the row #879

--- src/grid/grid-selection.spec.ts
+++ src/grid/grid-selection.spec.ts
@@ -532,6 +532,52 @@ describe("IgxGrid - Row Selection", () => {
         expect(grid.getRowByIndex(0).isSelected).toBeFalsy();
         expect(grid.getRowByIndex(1).isSelected).toBeTruthy();
     }));
+
+    it("Clicking any other cell is not selecting the row", async(() => {
+        const fix = TestBed.createComponent(GridWithPagingAndSelectionComponent);
+        fix.detectChanges();
+        const grid = fix.componentInstance.gridSelection2;
+        expect(grid.rowList.length).toEqual(50, "All 50 rows should initialized");
+        const firstRow = grid.getRowByIndex(0);
+        const rv =  fix.debugElement.query(By.css(".igx-grid__td"));
+        expect(firstRow).toBeDefined();
+        expect(firstRow.isSelected).toBeFalsy();
+
+        rv.nativeElement.dispatchEvent(new Event("focus"));
+
+        fix.whenStable().then(() => {
+            fix.detectChanges();
+            rv.triggerEventHandler("click", {});
+            return fix.whenStable();
+        }).then(() => {
+            fix.detectChanges();
+            expect(firstRow.isSelected).toBeFalsy();
+        });
+    }));
+
+    it("Clicking any other cell is not deselecting the row", async(() => {
+        const fix = TestBed.createComponent(GridWithPagingAndSelectionComponent);
+        fix.detectChanges();
+        const grid = fix.componentInstance.gridSelection2;
+        expect(grid.rowList.length).toEqual(50, "All 50 rows should initialized");
+        const firstRow = grid.getRowByIndex(0);
+        const rv =  fix.debugElement.query(By.css(".igx-grid__td"));
+        expect(rv).toBeDefined();
+        expect(firstRow).toBeDefined();
+        const targetCheckbox: HTMLElement = firstRow.nativeElement.querySelector(".igx-checkbox__input");
+        expect(firstRow.isSelected).toBeFalsy();
+        targetCheckbox.click();
+        fix.whenStable().then(() => {
+            fix.detectChanges();
+            expect(firstRow.isSelected).toBeTruthy();
+            rv.nativeElement.dispatchEvent(new Event("focus"));
+            rv.triggerEventHandler("click", {});
+            return fix.whenStable();
+        }).then(() => {
+            fix.detectChanges();
+            expect(firstRow.isSelected).toBeTruthy();
+        });
+    }));
 });
 
 @Component({
