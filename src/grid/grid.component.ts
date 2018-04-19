5e0d6c800398d76d123c542abe6923b89bc82394
NikolayAlipiev <nikolay.alipiev@gmail.com>
2018-04-19 13:19:14 +0300

    fix(igx-grid): apply checked state to headerCheckbox prop #1061

--- src/grid/grid.component.ts
+++ src/grid/grid.component.ts
@@ -989,6 +989,7 @@ export class IgxGridComponent implements OnInit, OnDestroy, AfterContentInit, Af
     }
 
     public onHeaderCheckboxClick(event) {
+        this.allRowsSelected = event.checked;
         const newSelection =
             event.checked ?
                 this.filteredData ?
