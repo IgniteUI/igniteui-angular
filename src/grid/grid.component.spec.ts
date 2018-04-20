bc827a563260d39a8baf80bc91940e3b4cdf1432
Vasya Kacheshmarova <vasq1989@gmail.com>
2018-04-20 18:22:59 +0300

    Test sfor issue 1054

--- src/grid/grid.component.spec.ts
+++ src/grid/grid.component.spec.ts
@@ -1,4 +1,4 @@
-import { Component, DebugElement, ViewChild } from "@angular/core";
+import { Component, DebugElement, ViewChild, ChangeDetectorRef } from "@angular/core";
 import { async, fakeAsync, TestBed, tick } from "@angular/core/testing";
 import { By } from "@angular/platform-browser";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
@@ -10,7 +10,8 @@ describe("IgxGrid - input properties", () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
-                IgxGridTestComponent
+                IgxGridTestComponent, IgGridTest5x5Component, IgGridTest10x30Component,
+                IgGridTest30x1000Component, IgGridTest150x20000Component
             ],
             imports: [
                 NoopAnimationsModule, IgxGridModule.forRoot()]
@@ -76,6 +77,48 @@ describe("IgxGrid - input properties", () => {
             window.getComputedStyle(gridHeader.children[0].nativeElement).width
         );
     });
+    it("col width should be >=136px - grid 5x5", () => {
+        const fix = TestBed.createComponent(IgGridTest5x5Component);
+        fix.detectChanges();
+
+        const grid = fix.componentInstance.gridMinDefaultColWidth;
+
+        expect(grid.columns[0].width).not.toBeLessThan(136);
+        expect(grid.columns[2].width).not.toBeLessThan(136);
+        expect(grid.width).toMatch("100%");
+    });
+
+    it("col width should be >=136px - grid 10x30", () => {
+        const fix = TestBed.createComponent(IgGridTest10x30Component);
+        fix.detectChanges();
+
+        const grid = fix.componentInstance.gridMinDefaultColWidth;
+
+        expect(grid.columns[0].width).not.toBeLessThan(136);
+        expect(grid.columns[4].width).not.toBeLessThan(136);
+        expect(grid.width).toMatch("100%");
+    });
+
+    fit("col width should be >=136px - grid 30x1000", () => {
+        const fix = TestBed.createComponent(IgGridTest30x1000Component);
+        fix.detectChanges();
+
+        const grid = fix.componentInstance.gridMinDefaultColWidth;
+        expect(grid.columns[0].width).not.toBeLessThan(136);
+        expect(grid.columns[4].width).not.toBeLessThan(136);
+        expect(fix.componentInstance.isHorizonatScrollbarVisible()).toBe(true);
+    });
+
+    it("col width should be >=136px - grid 150x20000", () => {
+        const fix = TestBed.createComponent(IgGridTest150x20000Component);
+        fix.detectChanges();
+
+        const grid = fix.componentInstance.gridMinDefaultColWidth;
+
+        expect(grid.columns[0].width).not.toBeLessThan(136);
+        expect(grid.columns[4].width).not.toBeLessThan(136);
+        expect(grid.calcHeight - grid.calcWidth).toBeLessThan(0);
+    });
 });
 
 @Component({
@@ -86,8 +129,191 @@ describe("IgxGrid - input properties", () => {
     </igx-grid></div>`
 })
 export class IgxGridTestComponent {
-    public data = [{ index: 1, value: 1}];
+    public data = [{ col1: 1}];
 
     @ViewChild("grid", { read: IgxGridComponent })
     public grid: IgxGridComponent;
+}
+@Component({
+    template: `
+    <igx-grid #gridMinDefaultColWidth [data]="data" (onColumnInit)="init($event)" >
+        <igx-column *ngFor="let col of cols" [field]="col.key" [header]="col.key" [dataType]="col.dataType"></igx-column>
+    </igx-grid>
+    `
+})
+export class IgGridTest5x5Component {
+    public cols;
+    public data;
+
+    @ViewChild("gridMinDefaultColWidth", { read: IgxGridComponent })
+    public gridMinDefaultColWidth: IgxGridComponent;
+
+    constructor(private _cdr: ChangeDetectorRef) {
+        this.generateColumns(5);
+        this.generateData(this.cols.length, 5);
+    }
+
+    init(column) {
+        column.hasSummary = true;
+    }
+    public generateData(columns, rows) {
+        this.data = [];
+
+        for (let r = 0; r < rows; r++) {
+            const record = {};
+            for (let c = 0; c < columns; c++) {
+                record[this.cols[c].key] = c * r;
+            }
+            this.data.push(record);
+        }
+    }
+    public generateColumns(count) {
+        this.cols = [];
+        for (let i = 0; i < count; i++) {
+            this.cols.push({
+                key: "col" +  i,
+                dataType: "number"
+            });
+        }
+        return this.cols;
+    }
+}
+@Component({
+    template: `
+    <igx-grid #gridMinDefaultColWidth [data]="data" (onColumnInit)="init($event)">
+        <igx-column *ngFor="let col of cols" [field]="col.key" [header]="col.key" [dataType]="col.dataType"></igx-column>
+    </igx-grid>
+    `
+})
+export class IgGridTest10x30Component {
+    public cols;
+    public data;
+
+    @ViewChild("gridMinDefaultColWidth", { read: IgxGridComponent })
+    public gridMinDefaultColWidth: IgxGridComponent;
+
+    constructor(private _cdr: ChangeDetectorRef) {
+        this.generateColumns(10);
+        this.generateData(this.cols.length, 30);
+    }
+
+    init(column) {
+        column.hasSummary = true;
+    }
+
+    public generateData(columns, rows) {
+        this.data = [];
+
+        for (let r = 0; r < rows; r++) {
+            const record = {};
+            for (let c = 0; c < columns; c++) {
+                record[this.cols[c].key] = c * r;
+            }
+            this.data.push(record);
+        }
+    }
+    public generateColumns(count) {
+        this.cols = [];
+        for (let i = 0; i < count; i++) {
+            this.cols.push({
+                key: "col" +  i,
+                dataType: "number"
+            });
+        }
+        return this.cols;
+    }
+}
+@Component({
+    template: `
+    <igx-grid #gridMinDefaultColWidth [data]="data" (onColumnInit)="init($event)" [width]="'1500px'" >
+        <igx-column *ngFor="let col of cols" [field]="col.key" [header]="col.key" [dataType]="col.dataType"></igx-column>
+    </igx-grid>
+    `
+})
+export class IgGridTest30x1000Component {
+    public cols;
+    public data;
+
+    @ViewChild("gridMinDefaultColWidth", { read: IgxGridComponent })
+    public gridMinDefaultColWidth: IgxGridComponent;
+
+    constructor(private _cdr: ChangeDetectorRef) {
+        this.generateColumns(30);
+        this.generateData(this.cols.length, 1000);
+    }
+
+    init(column) {
+        column.hasSummary = true;
+    }
+
+    public generateData(columns, rows) {
+        this.data = [];
+
+        for (let r = 0; r < rows; r++) {
+            const record = {};
+            for (let c = 0; c < columns; c++) {
+                record[this.cols[c].key] = c * r;
+            }
+            this.data.push(record);
+        }
+    }
+    public generateColumns(count) {
+        this.cols = [];
+        for (let i = 0; i < count; i++) {
+            this.cols.push({
+                key: "col" +  i,
+                dataType: "number"
+            });
+        }
+        return this.cols;
+    }
+    public isHorizonatScrollbarVisible() {
+        const scrollbar = this.gridMinDefaultColWidth.parentVirtDir.getHorizontalScroll();
+        return scrollbar.offsetWidth < scrollbar.children[0].offsetWidth;
+    }
+}
+@Component({
+    template: `
+    <igx-grid #gridMinDefaultColWidth [data]="data" (onColumnInit)="init($event)" [width]="'1500px'" >
+        <igx-column *ngFor="let col of cols" [field]="col.key" [header]="col.key" [dataType]="col.dataType"></igx-column>
+    </igx-grid>
+    `
+})
+export class IgGridTest150x20000Component {
+    public cols;
+    public data;
+
+    @ViewChild("gridMinDefaultColWidth", { read: IgxGridComponent })
+    public gridMinDefaultColWidth: IgxGridComponent;
+
+    constructor(private _cdr: ChangeDetectorRef) {
+        this.generateColumns(150);
+        this.generateData(this.cols.length, 20000);
+    }
+
+    init(column) {
+        column.hasSummary = true;
+    }
+
+    public generateData(columns, rows) {
+        this.data = [];
+
+        for (let r = 0; r < rows; r++) {
+            const record = {};
+            for (let c = 0; c < columns; c++) {
+                record[this.cols[c].key] = c * r;
+            }
+            this.data.push(record);
+        }
+    }
+    public generateColumns(count) {
+        this.cols = [];
+        for (let i = 0; i < count; i++) {
+            this.cols.push({
+                key: "col" + i,
+                dataType: "number"
+            });
+        }
+        return this.cols;
+    }
 }
