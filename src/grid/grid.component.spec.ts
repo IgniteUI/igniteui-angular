e53b7bc123606b9ac6db64b639e89e1ee3c88358
Tsanna Belcheva <cannabelcheva90@gmail.com>
2018-04-20 14:39:50 +0300

    test(igxGrid): Test grid default width and initially set column width, #1054

--- src/grid/grid.component.spec.ts
+++ src/grid/grid.component.spec.ts
@@ -6,11 +6,14 @@ import { IgxRippleModule } from "../directives/ripple/ripple.directive";
 import { IgxGridComponent } from "./grid.component";
 import { IgxGridModule } from "./index";
 
-describe("IgxGrid - input properties", () => {
+fdescribe("IgxGrid - input properties", () => {
+    const MIN_COL_WIDTH = "136px";
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
-                IgxGridTestComponent
+                IgxGridTestComponent,
+                IgxGridTestDefaultWidthHeight
             ],
             imports: [
                 NoopAnimationsModule, IgxGridModule.forRoot()]
@@ -76,6 +79,195 @@ describe("IgxGrid - input properties", () => {
             window.getComputedStyle(gridHeader.children[0].nativeElement).width
         );
     });
+
+    it("Test rendering of data with 5 columns and 5 rows where 2 of the columns have width set", () => {
+        const fix = TestBed.createComponent(IgxGridTestDefaultWidthHeight);
+        fix.detectChanges();
+        const grid = fix.componentInstance.grid2;
+        fix.componentInstance.generateColumns(5);
+        fix.componentInstance.generateData(5, 5);
+        fix.detectChanges();
+
+        expect(grid.width).toEqual("100%");
+        expect(grid.columns[0].width).toEqual("200px");
+        expect(grid.columns[4].width).toEqual("200px");
+
+        expect(parseInt(grid.columns[1].width, 10)).toEqual(parseInt(grid.columns[3].width, 10));
+        expect(parseInt(grid.columns[2].width, 10)).toEqual(parseInt(grid.columns[1].width, 10));
+
+        grid.columns.forEach((column) => {
+            if (column.index != 0 && column.index != 4) {
+                expect(column.width).toEqual(MIN_COL_WIDTH) || expect(column.width > MIN_COL_WIDTH).toEqual(true);
+            }
+        });
+
+        expect(grid.scr.nativeElement.firstElementChild.hidden).toEqual(true);
+        expect(grid.scr.nativeElement.style.display).toEqual("");
+    });
+
+    it("Test rendering of data with 5 columns and 5 rows where 2 of the columns have width set and grid has width", () => {
+        const fix = TestBed.createComponent(IgxGridTestDefaultWidthHeight);
+        fix.detectChanges();
+        const grid = fix.componentInstance.grid2;
+        grid.width = "800px";
+        fix.componentInstance.generateColumns(5);
+        fix.componentInstance.generateData(5, 5);
+        fix.detectChanges();
+
+        expect(grid.width).toEqual("800px");
+        expect(grid.columns[0].width).toEqual("200px");
+        expect(grid.columns[4].width).toEqual("200px");
+
+        expect(parseInt(grid.columns[1].width, 10)).toEqual(parseInt(grid.columns[3].width, 10));
+        expect(parseInt(grid.columns[2].width, 10)).toEqual(parseInt(grid.columns[1].width, 10));
+
+        grid.columns.forEach((column) => {
+            if (column.index != 0 && column.index != 4) {
+                expect(column.width).toEqual(MIN_COL_WIDTH) || expect(column.width > MIN_COL_WIDTH).toEqual(true);
+            }
+        });
+
+        expect(grid.scr.nativeElement.firstElementChild.hidden).toEqual(true);
+        //expect(grid.scr.nativeElement.style.display).toEqual("");
+        expect(grid.calcWidth - grid.totalWidth < 0).toEqual(true);
+    });
+
+    it("Test rendering of data with 5 columns and 30 rows where 3 of the columns have width set", () => {
+        const fix = TestBed.createComponent(IgxGridTestDefaultWidthHeight);
+        fix.detectChanges();
+        const grid = fix.componentInstance.grid2;
+        fix.componentInstance.generateColumns(5);
+        fix.componentInstance.generateData(5, 30);
+        fix.detectChanges();
+
+        expect(grid.width).toEqual("100%");
+        expect(grid.columns[0].width).toEqual("200px");
+        expect(grid.columns[4].width).toEqual("200px");
+
+        expect(parseInt(grid.columns[1].width, 10)).toEqual(parseInt(grid.columns[3].width, 10));
+        expect(parseInt(grid.columns[2].width, 10)).toEqual(parseInt(grid.columns[1].width, 10));
+
+        grid.columns.forEach((column) => {
+            if (column.index != 0 && column.index != 4) {
+                expect(column.width).toEqual(MIN_COL_WIDTH) || expect(column.width > MIN_COL_WIDTH).toEqual(true);
+            }
+        });
+
+        expect(grid.scr.nativeElement.firstElementChild.hidden).toEqual(true);
+        //expect(grid.scr.nativeElement.style.display).toEqual("");
+    });
+
+    it("Test rendering of data with 30 columns and 1000 rows where 5 of the columns have width set", () => {
+        const fix = TestBed.createComponent(IgxGridTestDefaultWidthHeight);
+        fix.detectChanges();
+        const grid = fix.componentInstance.grid2;
+        fix.componentInstance.generateColumns(30);
+        fix.componentInstance.generateData(30, 1000);
+        fix.detectChanges();
+
+        expect(grid.width).toEqual("100%");
+        expect(grid.columns[0].width).toEqual("200px");
+        expect(grid.columns[3].width).toEqual("200px");
+        expect(grid.columns[5].width).toEqual("200px");
+        expect(grid.columns[10].width).toEqual("200px");
+        expect(grid.columns[25].width).toEqual("200px");
+
+        expect(parseInt(grid.columns[1].width, 10)).toEqual(parseInt(grid.columns[3].width, 10));
+        expect(parseInt(grid.columns[2].width, 10)).toEqual(parseInt(grid.columns[1].width, 10));
+
+        grid.columns.forEach((column) => {
+            if (column.index != 0 && column.index != 3 && column.index != 5 &&
+                    column.index != 10 && column.index != 25) {
+                expect(column.width).toEqual(MIN_COL_WIDTH) || expect(column.width > MIN_COL_WIDTH).toEqual(true);
+            }
+        });
+
+        expect(grid.scr.nativeElement.firstElementChild.hidden).toEqual(true);
+        //expect(grid.scr.nativeElement.style.display).toEqual("");
+    });
+
+    it("Test rendering of data with 30 columns and 1000 rows where 5 of the columns have width set and grid has width", () => {
+        const fix = TestBed.createComponent(IgxGridTestDefaultWidthHeight);
+        fix.detectChanges();
+        const grid = fix.componentInstance.grid2;
+        grid.width = "800px";
+        fix.componentInstance.generateColumns(30);
+        fix.componentInstance.generateData(30, 1000);
+        fix.detectChanges();
+
+        expect(grid.width).toEqual("800px");
+        expect(grid.columns[0].width).toEqual("200px");
+        expect(grid.columns[3].width).toEqual("200px");
+        expect(grid.columns[5].width).toEqual("200px");
+        expect(grid.columns[10].width).toEqual("200px");
+        expect(grid.columns[25].width).toEqual("200px");
+
+        grid.columns.forEach((column) => {
+            if (column.index != 0 && column.index != 3 && column.index != 5 &&
+                    column.index != 10 && column.index != 25) {
+                expect(column.width).toEqual(MIN_COL_WIDTH) || expect(column.width > MIN_COL_WIDTH).toEqual(true);
+            }
+        });
+
+        expect(grid.scr.nativeElement.firstElementChild.hidden).toEqual(false);
+        expect(grid.calcWidth - grid.totalWidth < 0).toEqual(true);
+        //expect(grid.scr.nativeElement.style.display).toEqual("");
+    });
+
+    it("Test rendering of data with 150 columns and 20000 rows where 5 of the columns have width set", () => {
+        const fix = TestBed.createComponent(IgxGridTestDefaultWidthHeight);
+        fix.detectChanges();
+        const grid = fix.componentInstance.grid2;
+        fix.componentInstance.generateColumns(150);
+        fix.componentInstance.generateData(150, 20000);
+        fix.detectChanges();
+
+        expect(grid.width).toEqual("100%");
+        expect(grid.columns[0].width).toEqual("500px");
+        expect(grid.columns[3].width).toEqual("500px");
+        expect(grid.columns[5].width).toEqual("500px");
+        expect(grid.columns[10].width).toEqual("500px");
+        expect(grid.columns[50].width).toEqual("500px");
+
+        grid.columns.forEach((column) => {
+            if (column.index != 0 && column.index != 3 && column.index != 5 &&
+                    column.index != 10 && column.index != 50) {
+                expect(column.width).toEqual(MIN_COL_WIDTH) || expect(column.width > MIN_COL_WIDTH).toEqual(true);
+            }
+        });
+
+        expect(grid.scr.nativeElement.firstElementChild.hidden).toEqual(false);
+        expect(grid.calcWidth - grid.totalWidth < 0).toEqual(true);
+        //expect(grid.scr.nativeElement.style.display).toEqual("");
+    });
+
+    fit("Test rendering of data with 150 columns and 20000 rows where 5 of the columns have width set and grid has width", () => {
+        const fix = TestBed.createComponent(IgxGridTestDefaultWidthHeight);
+        fix.detectChanges();
+        const grid = fix.componentInstance.grid2;
+        grid.width = "800px";
+        fix.componentInstance.generateColumns(150);
+        fix.componentInstance.generateData(150, 20000);
+        fix.detectChanges();
+
+        expect(grid.width).toEqual("800px");
+        expect(grid.columns[0].width).toEqual("500px");
+        expect(grid.columns[3].width).toEqual("500px");
+        expect(grid.columns[5].width).toEqual("500px");
+        expect(grid.columns[10].width).toEqual("500px");
+        expect(grid.columns[50].width).toEqual("500px");
+
+        grid.columns.forEach((column) => {
+            if (column.index != 0 && column.index != 3 && column.index != 5 &&
+                    column.index != 10 && column.index != 50) {
+                expect(column.width).toEqual(MIN_COL_WIDTH) || expect(column.width > MIN_COL_WIDTH).toEqual(true);
+            }
+        });
+
+        expect(grid.scr.nativeElement.firstElementChild.hidden).toEqual(false);
+        expect(grid.calcWidth - grid.totalWidth < 0).toEqual(true);
+        //expect(grid.scr.nativeElement.style.display).toEqual("");
+    });
 });
 
 @Component({
@@ -87,7 +279,65 @@ describe("IgxGrid - input properties", () => {
 })
 export class IgxGridTestComponent {
     public data = [{ index: 1, value: 1}];
+    @ViewChild("grid") public grid: IgxGridComponent;
+}
 
-    @ViewChild("grid", { read: IgxGridComponent })
-    public grid: IgxGridComponent;
+@Component({
+    template: `<igx-grid #grid2 style="margin-bottom: 20px;" [data]="data" (onColumnInit)="initColumns($event)">
+                <igx-column *ngFor="let c of cols" [field]="c.field" [header]="c.header" [hasSummary]="true">
+                </igx-column>
+                </igx-grid>`
+})
+export class IgxGridTestDefaultWidthHeight {
+    public data =[];
+    public cols = []
+    @ViewChild("grid2") public grid2: IgxGridComponent;
+    
+    ngOnInit () {
+        //this.cols = this.generateColumns(5);
+        //this.data = this.generateData(5, 5);
+    }
+    initColumns(column) {
+        switch (this.grid2.columnList.length) {
+            case 5: 
+                if (column.index == 0 || column.index == 4) {
+                    column.width = "200px";
+                }
+            break;
+            case 30: 
+                if (column.index == 0 || column.index == 5 || column.index == 3 || column.index == 10 || column.index == 25) {
+                    column.width = "200px";
+                }
+            break;
+            case 150: 
+                if (column.index == 0 || column.index == 5 || column.index == 3 || column.index == 10 || column.index == 50) {
+                    column.width = "500px";
+                }
+            break;
+        }
+        
+      }
+    public generateColumns(count) {
+        this.cols = [];
+        for (let i = 0; i < count; i++) {
+            this.cols.push({
+                field: "col" + i,
+                header: "col" + i
+            })
+        }
+        return this.cols;
+    }
+    public generateData(columns, rows) {
+        const data = [], cols = [];
+     
+        for (let r = 0; r < rows; r++) {
+          const record = {};
+          for (let c = 0; c < columns; c++) {
+            record[this.cols[c].field] = c * r;
+          }
+          this.data.push(record);
+        }
+     
+        return this.data;
+    }
 }
