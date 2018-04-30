71ef5547469193b11c118f2f92005cc205a48c83
Marin Popov <mpopov@infragistics.com>
2018-04-30 14:36:32 +0300

    style(drop-down): Adding some basic styling
    (cherry picked from commit 5b549da)

--- src/drop-down/drop-down-item.component.ts
+++ src/drop-down/drop-down-item.component.ts
@@ -1,23 +1,21 @@
-import { Component, ElementRef, forwardRef, HostBinding, HostListener, Inject, Input, OnInit } from "@angular/core";
-import { IgxDropDownComponent, ISelectionEventArgs } from "./drop-down.component";
+import {Component, ElementRef, forwardRef, HostBinding, HostListener, Inject, Input, OnInit} from "@angular/core";
+import {IgxDropDownComponent, ISelectionEventArgs} from "./drop-down.component";
 
 @Component({
     selector: "igx-drop-down-item",
-    templateUrl: "drop-down-item.component.html",
-    styles: [
-        ":host { display: block; background-color: white; border: 1px solid white; }",
-        ":host.selected { background-color: #1A73E8; border: 1px solid #1A73E8; }",
-        ":host.focused { border: 1px solid #8bb8f4; color: red; }",
-        ":host.disabled { background-color: grey; border: 1px solid grey; }",
-        ":host.header { font-weight: bold; }"
-    ]
+    templateUrl: "drop-down-item.component.html"
 })
+
 export class IgxDropDownItemComponent implements OnInit {
+    @HostBinding("class.igx-drop-down__item")
+    public cssClass = "igx-drop-down__item";
+
     private _isFocused = false;
 
     get isSelected() {
         return this.dropDown.selectedItem === this;
     }
+
     @Input() set isSelected(value: boolean) {
         if (this.isSelected === value) {
             return;
@@ -27,15 +25,16 @@ export class IgxDropDownItemComponent implements OnInit {
     }
 
     @HostBinding("attr.aria-selected")
-    @HostBinding("class.selected")
+    @HostBinding("class.igx-drop-down__item--selected")
     get selectedStyle(): boolean {
         return this.isSelected;
     }
 
-    @HostBinding("class.focused")
+    @HostBinding("class.igx-drop-down__item--focused")
     get isFocused() {
         return this._isFocused;
     }
+
     set isFocused(value: boolean) {
         if (this.isDisabled || this.isHeader) {
             this._isFocused = false;
@@ -51,17 +50,18 @@ export class IgxDropDownItemComponent implements OnInit {
     }
 
     @Input()
-    @HostBinding("class.header")
+    @HostBinding("class.igx-drop-down__header")
     public isHeader = false;
 
     @Input()
-    @HostBinding("class.disabled")
+    @HostBinding("class.igx-drop-down__item--disabled")
     public isDisabled = false;
 
     constructor(
         @Inject(forwardRef(() => IgxDropDownComponent)) public dropDown: IgxDropDownComponent,
         private elementRef: ElementRef
-    ) { }
+    ) {
+    }
 
     @HostListener("click", ["$event"]) clicked(event) {
         if (this.isDisabled || this.isHeader) {
