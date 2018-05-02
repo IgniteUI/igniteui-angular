6992bba4404ec81c0ac0f9335e66b10e0907b336
Stefan Stoyanov <sstoyanov@infragistics.com>
2018-05-02 15:08:51 +0300

    fix(grid): Preserve the set width in %, #1212

--- src/grid/grid.component.ts
+++ src/grid/grid.component.ts
@@ -817,7 +817,7 @@ export class IgxGridComponent implements OnInit, OnDestroy, AfterContentInit, Af
             this._height = this.rowBasedHeight <= viewPortHeight ? null : viewPortHeight.toString();
         } else {
             const parentHeight = this.nativeElement.parentNode.getBoundingClientRect().height;
-            this._height = this.rowBasedHeight <= parentHeight ? null : "100%";
+            this._height = this.rowBasedHeight <= parentHeight ? null : this._height;
         }
         this.calculateGridHeight();
         this.cdr.detectChanges();
