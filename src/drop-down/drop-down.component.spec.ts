324db30332e379a2c1d094287eac5d5d9904f39a
AMarinov <amarinov@infragistics.com>
2018-05-03 13:42:27 +0300

    chore(dropdown): Editing the spec file #984

--- src/drop-down/drop-down.component.spec.ts
+++ src/drop-down/drop-down.component.spec.ts
@@ -1,8 +1,9 @@
 import { Component, ContentChildren, ViewChild } from "@angular/core";
 import { async, TestBed } from "@angular/core/testing";
 import { By } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { IgxToggleDirective, IgxToggleModule } from "../directives/toggle/toggle.directive";
+import { BrowserAnimationsModule, NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { IgxToggleActionDirective, IgxToggleDirective, IgxToggleModule } from "../directives/toggle/toggle.directive";
+import { IgxDropDownItemComponent } from "./drop-down-item.component";
 import { IgxDropDownComponent, IgxDropDownModule } from "./drop-down.component";
 
 describe("IgxDropDown ", () => {
@@ -11,7 +12,7 @@ describe("IgxDropDown ", () => {
             declarations: [
                 IgxDropDownTestComponent
             ],
-            imports: [IgxDropDownModule, BrowserAnimationsModule]
+            imports: [IgxDropDownModule, BrowserAnimationsModule, NoopAnimationsModule, IgxToggleModule]
         })
         .compileComponents();
     }));
@@ -23,8 +24,18 @@ describe("IgxDropDown ", () => {
         expect(list.length).toEqual(4);
     });
 
-    xit("should select item by click", () => {
-        //To DO
+    fit("should select item by click", () => {
+        const fixture = TestBed.createComponent(IgxDropDownTestComponent);
+        fixture.detectChanges();
+        const button = fixture.debugElement.query(By.css("button")).nativeElement;
+        const list = fixture.componentInstance.dropdown.items;
+        const item = list[0] as IgxDropDownItemComponent;
+        expect(list).toBeDefined();
+        expect(list.length).toEqual(4);
+        // tslint:disable-next-line:no-debugger
+        debugger;
+        button.click();
+        expect(item.isFocused).toBe(true);
     });
 
     xit("should select item by SPACE/ENTER", () => {
@@ -60,19 +71,28 @@ describe("IgxDropDown ", () => {
     template: `
     <button (click)="toggleDropDown()">Toggle</button>
     <igx-drop-down (onSelection)="onSelection($event)">
-        <igx-drop-down-item *ngFor="let item of items" isDisabled={{item.disabled}} isHeader={{item.header}}>
+        <igx-drop-down-item *ngFor="let item of items">
             {{ item.field }}
         </igx-drop-down-item>
     </igx-drop-down>
-    <div style="position: fixed; width: 100px; height: 100px; background-color: red; top: 50px; left: 300px; z-index: 9000;"></div>
-    `
+    <div style="position: fixed; width: 100px; height: 100px; background-color: red; top: 50px; left: 300px; z-index: 9000;"></div>`
 })
-export class IgxDropDownTestComponent {
-    public  items: any[] = [
-         { field: "Cables" },
-         { field: "Switches", disabled: true, header: true },
-         { field: "Switches", disabled: false },
-         { field: "Batteries", disabled: true }
+ class IgxDropDownTestComponent {
+
+    @ViewChild(IgxDropDownComponent) public dropdown: IgxDropDownComponent;
+
+    public items: any[] = [
+        { field: "Nav1" },
+        { field: "Nav2" },
+        { field: "Nav3" },
+        { field: "Nav4" }
     ];
-    @ViewChild(IgxDropDownComponent) public igxDropDown: IgxDropDownComponent;
+
+    public toggleDropDown() {
+        this.dropdown.toggleDropDown();
+    }
+
+    public onSelection(ev) {
+        console.log(ev);
+    }
 }
