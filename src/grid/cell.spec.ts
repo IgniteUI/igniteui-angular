64446237be3c19176b8bc8b8a65b78ba379070ac
Stamen Stoychev <chronos.stz@gmail.com>
2018-05-08 18:12:58 +0300

    chore(igxGridCellComponent): fixing typo in spec file

--- src/grid/cell.spec.ts
+++ src/grid/cell.spec.ts
@@ -98,7 +98,7 @@ describe("IgxGrid - Cell component", () => {
             declarations: [
                 DefaultGridComponent,
                 CtrlKeyKeyboardNagivationComponent,
-                VirtualtGridComponent,
+                VirtualGridComponent,
                 GridWithEditableColumnComponent,
                 NoColumnWidthGridComponent
             ],
@@ -369,7 +369,7 @@ describe("IgxGrid - Cell component", () => {
     }));
 
     it("should fit last cell in the available display container when there is vertical scroll.", () => {
-        const fix = TestBed.createComponent(VirtualtGridComponent);
+        const fix = TestBed.createComponent(VirtualGridComponent);
         fix.detectChanges();
         const rows = fix.componentInstance.instance.rowList;
         rows.forEach((item) => {
@@ -378,7 +378,7 @@ describe("IgxGrid - Cell component", () => {
     });
 
     it("should use default column width for cells with width in %.", () => {
-        const fix = TestBed.createComponent(VirtualtGridComponent);
+        const fix = TestBed.createComponent(VirtualGridComponent);
         fix.componentInstance.defaultWidth = "25%";
         fix.detectChanges();
         const rows = fix.componentInstance.instance.rowList;
@@ -388,7 +388,7 @@ describe("IgxGrid - Cell component", () => {
     });
 
     it("should scroll first row into view when pressing arrow up", async(() => {
-        const fix = TestBed.createComponent(VirtualtGridComponent);
+        const fix = TestBed.createComponent(VirtualGridComponent);
         fix.detectChanges();
 
         // the 2nd sell on the row with index 1
@@ -425,7 +425,7 @@ describe("IgxGrid - Cell component", () => {
     }));
 
     it("should not reduce the width of last pinned cell when there is vertical scroll.", () => {
-        const fix = TestBed.createComponent(VirtualtGridComponent);
+        const fix = TestBed.createComponent(VirtualGridComponent);
         fix.detectChanges();
         const columns = fix.componentInstance.instance.columnList;
         const lastCol: IgxColumnComponent = columns.last;
@@ -451,7 +451,7 @@ describe("IgxGrid - Cell component", () => {
     });
 
     it("keyboard navigation - should allow vertical navigation in virtualized grid.", (done) => {
-        const fix = TestBed.createComponent(VirtualtGridComponent);
+        const fix = TestBed.createComponent(VirtualGridComponent);
         fix.detectChanges();
         const grid = fix.componentInstance.instance;
         const cell = grid.getCellByColumn(4, "index");
@@ -472,7 +472,7 @@ describe("IgxGrid - Cell component", () => {
         navigateVerticallyToIndex(grid, cell, 100, cbFunc);
     });
     it("keyboard navigation - should allow horizontal navigation in virtualized grid.", (done) => {
-        const fix = TestBed.createComponent(VirtualtGridComponent);
+        const fix = TestBed.createComponent(VirtualGridComponent);
         const cols = [];
         for (let i = 0; i < 10; i++) {
             cols.push({field: "col" + i});
@@ -495,7 +495,7 @@ describe("IgxGrid - Cell component", () => {
         navigateHorizontallyToIndex(grid, cell, 9, cbFunc);
     });
     it("keyboard navigation - should allow horizontal navigation in virtualized grid with pinned cols.", (done) => {
-        const fix = TestBed.createComponent(VirtualtGridComponent);
+        const fix = TestBed.createComponent(VirtualGridComponent);
         const cols = [];
         for (let i = 0; i < 10; i++) {
             cols.push({field: "col" + i});
@@ -521,7 +521,7 @@ describe("IgxGrid - Cell component", () => {
     });
 
     it("keyboard navigation - should allow vertical navigation in virtualized grid with pinned cols.", (done) => {
-        const fix = TestBed.createComponent(VirtualtGridComponent);
+        const fix = TestBed.createComponent(VirtualGridComponent);
         fix.detectChanges();
         const grid = fix.componentInstance.instance;
         grid.pinColumn("index");
@@ -711,7 +711,7 @@ export class CtrlKeyKeyboardNagivationComponent {
         </igx-grid>
     `
 })
-export class VirtualtGridComponent {
+export class VirtualGridComponent {
 
     @ViewChild(IgxGridComponent, { read: IgxGridComponent })
     public instance: IgxGridComponent;
