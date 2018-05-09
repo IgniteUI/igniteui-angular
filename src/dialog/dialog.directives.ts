1675a5ccd969c728af8316966c1826ffa69232b4
SAndreeva <sandreeva@infragistics.com>
2018-05-09 12:20:34 +0300

    feat(dialog): add default classes to directives #1238

--- src/dialog/dialog.directives.ts
+++ src/dialog/dialog.directives.ts
@@ -1,12 +1,20 @@
 import { DOCUMENT } from "@angular/common";
-import { Directive } from "@angular/core";
+import { Directive, HostBinding } from "@angular/core";
 
 @Directive({
     selector: "igx-dialog-title,[igxDialogTitle]"
 })
-export class IgxDialogTitleDirective { }
+export class IgxDialogTitleDirective {
+
+    @HostBinding("class.igx-dialog__window-title")
+    public defaultStyle = true;
+ }
 
 @Directive({
     selector: "igx-dialog-actions,[igxDialogActions]"
 })
-export class IgxDialogActionsDirective { }
+export class IgxDialogActionsDirective {
+
+    @HostBinding("class.igx-dialog__window-actions")
+    public defaultClass = true;
+ }
