d79fe4cc5cd6413b3c3fad682f260a48afe721aa
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2018-05-18 17:12:30 +0300

    build(*): Removed old src directory
    Removed html reporter from tests

--- oldsrc/directives/suffix/suffix.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive, HostBinding } from "@angular/core";
-
-@Directive({
-    selector: "igx-suffix,[igxSuffix]"
-})
-export class IgxSuffixDirective {
-    @HostBinding("class.igx-input-group__bundle-suffix")
-    public defaultClass = true;
-}
--- projects/igniteui-angular/karma.conf.js
+++ projects/igniteui-angular/karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
