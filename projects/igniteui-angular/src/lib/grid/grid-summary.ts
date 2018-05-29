4c22de05f5fb955805fecfa461a73bffdd90c1cc
Konstantin Dinev <kdinev@mail.bw.edu>
2018-05-29 16:12:37 +0300

    refactor(grid): Making the summaries aggregators static #1291

--- projects/igniteui-angular/src/lib/grid/grid-summary.ts
+++ projects/igniteui-angular/src/lib/grid/grid-summary.ts
@@ -10,6 +10,9 @@ export interface IgxSummaryResult {
     summaryResult: any;
 }
 export class IgxSummaryOperand {
+    public static count(data?: any[]): any {
+        return data.length;
+    }
 
     public operate(data?: any[]): IgxSummaryResult[] {
         if (!data) {
@@ -19,14 +22,40 @@ export class IgxSummaryOperand {
         return [{
             key: 'count',
             label: 'Count',
-            summaryResult: this.count(data)
+            summaryResult: IgxSummaryOperand.count(data)
         }];
-    }
-    public count(data?: any[]): any {
-        return data.length;
     }
 }
 export class IgxNumberSummaryOperand extends IgxSummaryOperand {
+
+    public static min(data?: any[]): any {
+        if (data && data.length > 0) {
+            return data.reduce((a, b) => Math.min(a, b));
+        } else {
+            return;
+        }
+    }
+    public static max(data?: any[]): any {
+        if (data && data.length > 0) {
+            return data.reduce((a, b) => Math.max(a, b));
+        } else {
+            return;
+        }
+    }
+    public static sum(data?: any[]): any {
+        if (data && data.length > 0) {
+            return data.reduce((a, b) => +a + +b);
+        } else {
+            return;
+        }
+    }
+    public static average(data?: any[]): any {
+        if (data && data.length > 0) {
+            return this.sum(data) / this.count(data);
+        } else {
+            return;
+        }
+    }
 
     public operate(data?: any[]): IgxSummaryResult[] {
         if (!data) {
@@ -37,52 +66,37 @@ export class IgxNumberSummaryOperand extends IgxSummaryOperand {
         result.push({
             key: 'min',
             label: 'Min',
-            summaryResult: this.min(data)});
+            summaryResult: IgxNumberSummaryOperand.min(data)});
         result.push({
             key: 'max',
             label: 'Max',
-            summaryResult: this.max(data)});
+            summaryResult: IgxNumberSummaryOperand.max(data)});
         result.push({
             key: 'sum',
             label: 'Sum',
-            summaryResult: this.sum(data)});
+            summaryResult: IgxNumberSummaryOperand.sum(data)});
         result.push({
             key: 'average',
             label: 'Avg',
-            summaryResult: this.average(data)});
+            summaryResult: IgxNumberSummaryOperand.average(data)});
         return result;
-    }
-
-    public min(data?: any[]): any {
-        if (data && data.length > 0) {
-            return data.reduce((a, b) => Math.min(a, b));
-        } else {
-            return;
-        }
-    }
-    public max(data?: any[]): any {
-        if (data && data.length > 0) {
-            return data.reduce((a, b) => Math.max(a, b));
-        } else {
-            return;
-        }
-    }
-    public sum(data?: any[]): any {
-        if (data && data.length > 0) {
-            return data.reduce((a, b) => +a + +b);
-        } else {
-            return;
-        }
-    }
-    public average(data?: any[]): any {
-        if (data && data.length > 0) {
-            return this.sum(data) / this.count(data);
-        } else {
-            return;
-        }
     }
 }
 export class IgxDateSummaryOperand extends IgxSummaryOperand {
+    public static latest(data?: any[]) {
+        if (data && data.length > 0) {
+            return data.sort((a, b) => new Date(b).valueOf() - new Date(a).valueOf())[0];
+        } else {
+            return;
+        }
+    }
+    public static earliest(data?: any[]) {
+        if (data && data.length > 0) {
+            return data.sort((a, b) => new Date(b).valueOf() - new Date(a).valueOf())[data.length - 1];
+        } else {
+            return;
+        }
+    }
 
     public operate(data?: any[]): IgxSummaryResult[] {
         if (!data) {
@@ -93,25 +107,11 @@ export class IgxDateSummaryOperand extends IgxSummaryOperand {
         result.push({
             key: 'earliest',
             label: 'Earliest',
-            summaryResult: this.earliest(data)});
+            summaryResult: IgxDateSummaryOperand.earliest(data)});
         result.push({
             key: 'latest',
             label: 'Latest',
-            summaryResult: this.latest(data)});
+            summaryResult: IgxDateSummaryOperand.latest(data)});
         return result;
-    }
-    public latest(data?: any[]) {
-        if (data && data.length > 0) {
-            return data.sort((a, b) => new Date(b).valueOf() - new Date(a).valueOf())[0];
-        } else {
-            return;
-        }
-    }
-    public earliest(data?: any[]) {
-        if (data && data.length > 0) {
-            return data.sort((a, b) => new Date(b).valueOf() - new Date(a).valueOf())[data.length - 1];
-        } else {
-            return;
-        }
     }
 }
