73dbd22e8d76faee1647c5d1acbfc7bcb290297e
wnvko <milko.venkov@gmail.com>
2018-05-31 15:48:05 +0300

    test(igxDropDown): add unit test for show, hide and hideAll. Closes #1387

--- projects/igniteui-angular/src/lib/services/overlay/overlay.spec.ts
+++ projects/igniteui-angular/src/lib/services/overlay/overlay.spec.ts
@@ -9,7 +9,7 @@ import { TestBed } from '@angular/core/testing';
 import { BrowserModule, By } from '@angular/platform-browser';
 import { IgxOverlayService } from './overlay';
 
-describe('igxOverlay', () => {
+fdescribe('igxOverlay', () => {
     beforeEach(async () => {
         TestBed.configureTestingModule({
             imports: [
@@ -21,7 +21,7 @@ describe('igxOverlay', () => {
             providers: [IgxOverlayService]
         }).compileComponents();
     });
-    fit('Unit - OverlayElement should return a div attached to Document\'s body', () => {
+    it('Unit - OverlayElement should return a div attached to Document\'s body', () => {
         const fixture = TestBed.createComponent(EmptyPageComponent);
         fixture.detectChanges();
 
@@ -29,8 +29,83 @@ describe('igxOverlay', () => {
         fixture.whenStable().then(() => {
             const overlayDiv = fixture.debugElement.nativeElement.parentElement.lastChild;
             expect(overlayDiv).toBeDefined();
+            expect(overlayDiv.style.display).toEqual('block');
             expect(overlayDiv.classList.contains('overlay')).toBeTruthy();
             fixture.componentInstance.overlay.hideAll();
+        });
+    });
+    it('Unit - Should show component passed to overlay',  () => {
+        const fixture = TestBed.createComponent(EmptyPageComponent);
+        fixture.detectChanges();
+
+        fixture.componentInstance.buttonElement.nativeElement.click();
+        fixture.whenStable().then(() => {
+            const overlayDiv = fixture.debugElement.nativeElement.parentElement.lastChild;
+            expect(overlayDiv).toBeDefined();
+            expect(overlayDiv.style.display).toEqual('block');
+            expect(overlayDiv.children.length).toEqual(1);
+            expect(overlayDiv.children[0].localName).toEqual('ng-component');
+
+            fixture.componentInstance.overlay.hideAll();
+        });
+    });
+    fit('Unit - Hide() should hide component and overlay',  () => {
+        const fixture = TestBed.createComponent(EmptyPageComponent);
+        fixture.detectChanges();
+
+        fixture.componentInstance.overlay.show(SimpleDynamicComponent);
+        fixture.componentInstance.overlay.show(SimpleDynamicComponent);
+        fixture.whenStable().then(() => {
+            fixture.detectChanges();
+            const overlayDiv = fixture.debugElement.nativeElement.parentElement.lastChild;
+            expect(overlayDiv).toBeDefined();
+            expect(overlayDiv.style.display).toEqual('block');
+            expect(overlayDiv.children.length).toEqual(2);
+            expect(overlayDiv.children[0].localName).toEqual('ng-component');
+            expect(overlayDiv.children[1].localName).toEqual('ng-component');
+
+            fixture.componentInstance.overlay.hide(1);
+            return fixture.whenStable();
+        }).then(() => {
+            fixture.detectChanges();
+            const overlayDiv = fixture.debugElement.nativeElement.parentElement.lastChild;
+            expect(overlayDiv).toBeDefined();
+            expect(overlayDiv.style.display).toEqual('block');
+            expect(overlayDiv.children.length).toEqual(1);
+            expect(overlayDiv.children[0].localName).toEqual('ng-component');
+
+            fixture.componentInstance.overlay.hide(0);
+            return fixture.whenStable();
+        }).then(() => {
+            fixture.detectChanges();
+            const overlayDiv = fixture.debugElement.nativeElement.parentElement.lastChild;
+            expect(overlayDiv).toBeDefined();
+            expect(overlayDiv.style.display).toEqual('none');
+            expect(overlayDiv.children.length).toEqual(0);
+        });
+    });
+    fit('Unit - HideAll() should hide all components and overlay',  () => {
+        const fixture = TestBed.createComponent(EmptyPageComponent);
+        fixture.detectChanges();
+        fixture.componentInstance.overlay.show(SimpleDynamicComponent);
+        fixture.componentInstance.overlay.show(SimpleDynamicComponent);
+        fixture.whenStable().then(() => {
+            fixture.detectChanges();
+            const overlayDiv = fixture.debugElement.nativeElement.parentElement.lastChild;
+            expect(overlayDiv).toBeDefined();
+            expect(overlayDiv.style.display).toEqual('block');
+            expect(overlayDiv.children.length).toEqual(2);
+            expect(overlayDiv.children[0].localName).toEqual('ng-component');
+            expect(overlayDiv.children[1].localName).toEqual('ng-component');
+
+            fixture.componentInstance.overlay.hideAll();
+            return fixture.whenStable();
+        }).then(() => {
+            fixture.detectChanges();
+            const overlayDiv = fixture.debugElement.nativeElement.parentElement.lastChild;
+            expect(overlayDiv).toBeDefined();
+            expect(overlayDiv.style.display).toEqual('none');
+            expect(overlayDiv.children.length).toEqual(0);
         });
     });
 });
