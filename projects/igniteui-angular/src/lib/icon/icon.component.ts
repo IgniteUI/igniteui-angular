e5a264af083c9c68c8361a4ac876ff0444d1cac9
AMishev <AMishev@DEV-AMISHEV>
2018-06-01 16:36:29 +0300

    docs(igxIcon): Add api docs for igxIcon

--- projects/igniteui-angular/src/lib/icon/icon.component.ts
+++ projects/igniteui-angular/src/lib/icon/icon.component.ts
@@ -32,63 +32,188 @@ export class IgxIconComponent implements OnInit {
     @ViewChild('explicitLigature', { read: TemplateRef })
     private explicitLigature: TemplateRef<HTMLElement>;
 
+    /**
+     *  This allows you to disable `igx-icon` class. By default it's applied.
+     *```html
+     *@ViewChild("MyIcon")
+     *public icon: IgxIconComponent;
+     *ngAfterViewInit() {
+     *    icon.cssClass = false;
+     * }
+     * ```
+     */
     @HostBinding('class.igx-icon')
     public cssClass = 'igx-icon';
 
+    /**
+     *  This allows you to disable the `aria-hidden` attribute. By default it's applied.
+     *```html
+     *@ViewChild("MyIcon")
+     *public icon: IgxIconComponent;
+     *ngAfterViewInit() {
+     *    icon.ariaHidden = false;
+     * }
+     * ```
+     */
     @HostBinding('attr.aria-hidden')
     public ariaHidden = true;
 
+    /**
+    *    An @Input property that sets the value of the `id` attribute.
+    *```html
+    *<igx-icon id="igx-icon-1" fontSet="material" name="settings" color="blue" [isActive]="false"></igx-icon>
+    *```
+    */
     @HostBinding('attr.id')
     @Input()
     public id = `igx-icon-${NEXT_ID++}`;
 
+    /**
+    *    An @Input property that sets the value of the `fontSet`. By default it's "material".
+    *```html
+    *<igx-icon fontSet="material" name="settings" color="blue" [isActive]="false"></igx-icon>
+    *```
+    */
     @Input('fontSet')
     public font: string;
 
+    /**
+    *    An @Input property that allows you to disable the `active` property. By default it's applied.
+    *```html
+    *<igx-icon [isActive]="false" fontSet="material" name="settings" color="blue"></igx-icon>
+    *```
+    */
     @Input('isActive')
     public active = true;
 
+    /**
+    *    An @Input property that allows you to change the `iconColor` of the icon.
+    *```html
+    *<igx-icon color="blue" [isActive]="true" fontSet="material" name="settings" ></igx-icon>
+    *```
+    */
     @Input('color')
     public iconColor: string;
 
+    /**
+    *    An @Input property that allows you to change the iconName of the icon. You can provide either ligature `name` or glyph `iconName`, but not both at the same time.
+    *```html
+    *<igx-icon name="question_answer" color="blue" [isActive]="true" fontSet="material"></igx-icon>
+    *```
+    */
     @Input('name')
     public iconName: string;
 
+    /**
+    *    An @Input property that allows you to change the `glyphName` of the icon. You can provide either ligature `name` or glyph `iconName`, but not both at the same time.
+    *```html
+    *<igx-icon iconName="question_answer" color="blue" [isActive]="true" fontSet="material"></igx-icon>
+    *```
+    */
     @Input('iconName')
     public glyphName: string;
 
-    constructor(public el: ElementRef, private iconService: IgxIconService) {
+    /**
+     * An ElementRef property of the `igx-icon` component.
+     */
+    public el:ElementRef;
+
+    constructor(private _el: ElementRef, private iconService: IgxIconService) {
+        this.el = _el;
         this.font = this.iconService.defaultFontSet;
         this.iconService.registerFontSetAlias('material', 'material-icons');
     }
 
+    /**
+     * @hidden
+     */
     ngOnInit() {
         this.checkInputProps();
         this.updateIconClass();
     }
 
+        /**
+     *   An accessor that returns the value of the font property.
+     *```html
+     *@ViewChild("MyIcon")
+     *public icon: IgxIconComponent;
+     *ngAfterViewInit() {
+     *    let iconFont = this.icon.getFontSet;
+     * }
+     * ```
+     */
     get getFontSet(): string {
         return this.font;
     }
 
+    /**
+     *   An accessor that returns the value of the active property.
+     *```html
+     *@ViewChild("MyIcon")
+     *public icon: IgxIconComponent;
+     *ngAfterViewInit() {
+     *    let iconActive = this.icon.getActive;
+     * }
+     * ```
+     */
     get getActive(): boolean {
         return this.active;
     }
 
+    /**
+     *   An accessor that returns the opposite value of the active property.
+     *```html
+     *@ViewChild("MyIcon")
+     *public icon: IgxIconComponent;
+     *ngAfterViewInit() {
+     *    let iconActive = this.icon.getInactive;
+     * }
+     * ```
+     */
     @HostBinding('class.igx-icon--inactive')
     get getInactive(): boolean {
         return !this.active;
     }
 
+    /**
+     *   An accessor that returns the opposite value of the `iconColor` property.
+     *```html
+     *@ViewChild("MyIcon")
+     *public icon: IgxIconComponent;
+     *ngAfterViewInit() {
+     *    let iconActive = this.icon.getIconColor;
+     * }
+     * ```
+     */
     @HostBinding('style.color')
     get getIconColor(): string {
         return this.iconColor;
     }
 
+    /**
+     *   An accessor that returns the value of the iconName property.
+     *```html
+     *@ViewChild("MyIcon")
+     *public icon: IgxIconComponent;
+     *ngAfterViewInit() {
+     *    let iconName = this.icon.getIconName;
+     * }
+     * ```
+     */
     get getIconName(): string {
         return this.iconName;
     }
 
+    /**
+     *   An accessor that returns a TemplateRef to explicit, implicit or no ligature.
+     *```html
+     *@ViewChild("MyIcon")
+     *public icon: IgxIconComponent;
+     *ngAfterViewInit() {
+     *    let iconTemplate = this.icon.template;
+     * }
+     * ```
+     */
     get template(): TemplateRef<HTMLElement> {
         if (this.glyphName) {
             return this.noLigature;
