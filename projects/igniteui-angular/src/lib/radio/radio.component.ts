585c5fcfe7599806ac4259caf1055edf3b0395ab
Hristo Popov <HPopov@infragistics.com>
2018-06-13 09:41:50 +0300

    docs(radio): Adding some code snippets. #1535

--- projects/igniteui-angular/src/lib/radio/radio.component.ts
+++ projects/igniteui-angular/src/lib/radio/radio.component.ts
@@ -57,37 +57,46 @@ export class IgxRadioComponent implements ControlValueAccessor {
     @ViewChild('placeholderLabel') public placeholderLabel;
 
     /**
-     * Sets the `id` of the radio component.
-     * If not set, the `id` of the first radio component will `"igx-radio-0"`.
+     * Sets/gets the `id` of the radio component.
+     * If not set, the `id` of the first radio component will be `"igx-radio-0"`.
      * ```html
      * <igx-radio id = "my-first-radio"></igx-radio>
+     * ```
+     * ```typescript
+     * let radioId =  this.radio.id;
      * ```
      * @memberof IgxRadioComponent
      */
     @HostBinding('attr.id')
     @Input() public id = `igx-radio-${nextId++}`;
     /**
-     * Sets the id of the `label` element in the radio component.
+     * Sets/gets the id of the `label` element in the radio component.
      * If not set, the id of the `label` in the first radio component will be `"igx-radio-0-label"`
      * ```html
      * <igx-radio labelId = "Label1"></igx-radio>
+     * ```
+     * ```typescript
+     * let labelId =  this.radio.labelId;
      * ```
      * @memberof IgxRadioComponent
      */
     @Input() public labelId = `${this.id}-label`;
     /**
-     * Sets the position of the `label` in the radio component.
+     * Sets/gets the position of the `label` in the radio component.
      * If not set, `labelPosition` will have value `"after"`;
      * ```html
      * <igx-radio labelPosition = "before"></igx-radio>
+     * ```
+     * ```typescript
+     * let labelPosition =  this.radio.labelPosition;
      * ```
      * @memberof IgxRadioComponent
      */
     @Input() public labelPosition: RadioLabelPosition | string = 'after';
     /**
-     * Sets/gets the `value`attribute of the radio component.
+     * Sets/gets the `value`attribute.
      * ```html
-     * <igx-radio [value] = "1"></igx-radio>
+     * <igx-radio [value] = "'radioButtonValue'"></igx-radio>
      * ```
      * ```typescript
      * let value =  this.radio.value;
@@ -107,59 +116,74 @@ export class IgxRadioComponent implements ControlValueAccessor {
  */
 @Input() public name: string;
 /**
- * Sets the value of the `tabindex` attribute of the radio component.
+ * Sets the value of the `tabindex` attribute.
  * ```html
  * <igx-radio [tabindex] = "1"></igx-radio>
+ * ```
+ * ```typescript
+ * let tabIndex =  this.radio.tabindex;
  * ```
  * @memberof IgxRadioComponent
  */
 @Input() public tabindex: number = null;
 /**
- * Sets the `disableRipple` attribute of the radio component.
+ * Enables/disables the ripple effect on the radio button..
  * If not set, the `disableRipple` will have value `false`.
  * ```html
  * <igx-radio [disableRipple] = "true"></igx-radio>
+ * ```
+ * ```typescript
+ * let isDisabledRipple =  this.radio.disableRipple;
  * ```
  * @memberof IgxRadioComponent
  */
 @Input() public disableRipple = false;
 /**
- * Sets whether the state of the radio component should be required.
+ * Sets/gets whether the radio button is required.
  * If not set, `required` will have value `false`.
  * ```html
  * <igx-radio [required] = "true"></igx-radio>
+ * ```
+ * ```typescript
+ * let isRequired =  this.radio.required;
  * ```
  * @memberof IgxRadioComponent
  */
 @Input() public required = false;
 /**
- * Sets the `aria-labelledby` attribute of the radio component.
+ * Sets/gets the `aria-labelledby` attribute of the radio component.
  * If not set, the `aria-labelledby` will be equal to the value of `labelId` attribute.
  * ```html
  * <igx-radio aria-labelledby = "Radio1"></igx-radio>
+ * ```
+ * ```typescript
+ * let ariaLabelledBy = this.radio.ariaLabelledBy;
  * ```
  * @memberof IgxRadioComponent
  */
 @Input('aria-labelledby')
     public ariaLabelledBy = this.labelId;
 /**
- *Sets the `aria-label` attribute of the radio component.
+ * Sets/gets the `aria-label` attribute of the radio component.
  * ```html
  * <igx-radio aria-label = "Radio1"></igx-radio>
+ * ```
+ * ```typescript
+ * let ariaLabel =  this.radio.ariaLabel;
  * ```
  * @memberof IgxRadioComponent
  */
 @Input('aria-label')
     public ariaLabel: string | null = null;
 /**
- * An even that is emitted when the radio state is changed.
+ * An event that is emitted after the radio value is changed.
  * Provides references to the `IgxRadioComponent` and the `value` property as event arguments.
  * @memberof IgxRadioComponent
  */
 @Output()
     readonly change: EventEmitter<IChangeRadioEventArgs> = new EventEmitter<IChangeRadioEventArgs>();
 /**
- * Return the class of the radio component.
+ * Returns the class of the radio component.
  * ```typescript
  * let radioClass = this.radio.cssClass;
  * ```
@@ -168,42 +192,46 @@ export class IgxRadioComponent implements ControlValueAccessor {
 @HostBinding('class.igx-radio')
     public cssClass = 'igx-radio';
 /**
- * Sets whether the radio is checked.
+ * Sets/gets  the `checked` attribute.
  * Default value is `false`.
  * ```html
  * <igx-radio [checked] = "true"></igx-radio>
+ * ```
+ * ```typescript
+ * let isChecked =  this.radio.checked;
  * ```
  * @memberof IgxRadioComponent
  */
 @HostBinding('class.igx-radio--checked')
     @Input() public checked = false;
 /**
- * Sets whether the radio is desabled.
+ * Sets/gets  the `disabled` attribute.
  * Default value is `false`.
  * ```html
- * <igx-radio [desabled] = "true"></igx-radio>
+ * <igx-radio [disabled] = "true"></igx-radio>
+ * ```
+ * ```typescript
+ * let isDisabled =  this.radio.disabled;
  * ```
  * @memberof IgxRadioComponent
  */
 @HostBinding('class.igx-radio--disabled')
     @Input() public disabled = false;
 /**
- * Sets whether the radio component is on focus.
+ * Sets/gets whether the radio component is on focus.
  * Default value is `false`.
  * ```typescript
  * this.radio.focus = true;
+ * ```
+ * ```typescipt
+ * let isFocused =  this.radio.focused;
  * ```
  * @memberof IgxRadioComponent
  */
 @HostBinding('class.igx-radio--focused')
     public focused = false;
 /**
- * Sets the id of the `input` element in the radio component.
- * If not set, the `input` of the first radio component will have an id = `"igx-radio-0-input"`.
- * ```typescript
- * this.radio.inputId = "Input1";
- * ```
- * @memberof IgxRadioComponent
+ *@hidden
  */
 public inputId = `${this.id}-input`;
     /**
@@ -240,9 +268,7 @@ public _onLabelClick() {
         this.select();
     }
 /**
- * If `disabled` is `false`, sets the `checked` value of the radio component to `true`.
- * Removes the focus on the radio.
- * Emits an event after the radio is selected.
+ * Selects the current radio button.
  * ```typescript
  * this.radio.select();
  * ```
@@ -259,7 +285,11 @@ public select() {
         this._onChangeCallback(this.value);
     }
 /**
- *@hidden
+ * Checks whether the provided value is consistent to the current radio button.
+ * If it is, the checked attribute will have value `true`;
+ * ```typescript
+ * this.radio.writeValue('radioButtonValue');
+ * ```
  */
 public writeValue(value: any) {
         this._value = value;
