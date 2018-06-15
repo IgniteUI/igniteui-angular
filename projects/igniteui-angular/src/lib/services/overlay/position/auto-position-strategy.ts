cc454f1e21089b215c607630ec398a2ff517e458
IvayloG <inj.iganchev@gmail.com>
2018-06-15 08:39:46 +0300

    chore(igxOverlay): Remove _wrapperClass.

--- projects/igniteui-angular/src/lib/services/overlay/position/auto-position-strategy.ts
+++ projects/igniteui-angular/src/lib/services/overlay/position/auto-position-strategy.ts
@@ -2,12 +2,10 @@ import { PositionSettings } from './../utilities';
 import { IPositionStrategy } from './IPositionStrategy';
 
 export class AutoPositionStrategy implements IPositionStrategy {
-    public _wrapperClass: string;
     public _settings: PositionSettings;
 
     constructor(settings?: PositionSettings) {
         this._settings = settings ? settings : new PositionSettings();
-        this._wrapperClass = 'auto-show';
     }
 
     position(element): void {
@@ -26,6 +24,5 @@ export class AutoPositionStrategy implements IPositionStrategy {
         console.log(elementheight); // returns 0
         console.log(windowheight);
 
-        element.parentElement.classList.add(this._wrapperClass);
     }
 }
