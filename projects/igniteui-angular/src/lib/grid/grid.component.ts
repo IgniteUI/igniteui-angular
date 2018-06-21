732aad0fe327fd175ce3ce0420604f17eb0ee710
Konstantin Dinev <kdinev@mail.bw.edu>
2018-06-21 15:19:59 +0300

    feat(grid): Exposing filteredSortedData on the grid in 6.0.x #1636

--- projects/igniteui-angular/src/lib/grid/grid.component.ts
+++ projects/igniteui-angular/src/lib/grid/grid.component.ts
@@ -1400,7 +1400,7 @@ export class IgxGridComponent implements OnInit, OnDestroy, AfterContentInit, Af
         return this.lastSearchInfo.matchInfoCache.length;
     }
 
-    private get filteredSortedData(): any[] {
+    get filteredSortedData(): any[] {
         let data: any[] = this.filteredData ? this.filteredData : this.data;
 
         if (this.sortingExpressions &&
