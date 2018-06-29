c84b9b2c40d66af90a888a6a627d8afe97dc4023
Hristo Anastasov <HAnastasov@infragistics.com>
2018-06-29 12:14:54 +0300

    fix(grid): move columns when inside pinned area #1775

--- projects/igniteui-angular/src/lib/grid/grid.component.ts
+++ projects/igniteui-angular/src/lib/grid/grid.component.ts
@@ -1097,12 +1097,10 @@ export class IgxGridComponent implements OnInit, OnDestroy, AfterContentInit, Af
         if (dropTarget.pinned && column.pinned) {
             const pinned = this._pinnedColumns;
             pinned.splice(pinned.indexOf(dropTarget), 0, ...pinned.splice(pinned.indexOf(column), 1));
-            return;
         }
 
         if (dropTarget.pinned && !column.pinned) {
             column.pin(dropTarget.index);
-            return;
         }
 
         if (!dropTarget.pinned && column.pinned) {
