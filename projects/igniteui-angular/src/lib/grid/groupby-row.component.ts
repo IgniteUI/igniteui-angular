cddd6aba08a288d56e0d90590b26c9e437bd9204
AMishev <AMishev@DEV-AMISHEV>
2018-07-19 15:44:36 +0300

    docs(grid-groupby-row): Add API docs for IgxGridGroupByRowComponent

--- projects/igniteui-angular/src/lib/grid/groupby-row.component.ts
+++ projects/igniteui-angular/src/lib/grid/groupby-row.component.ts
@@ -26,52 +26,130 @@ export class IgxGridGroupByRowComponent {
                 public element: ElementRef,
                 public cdr: ChangeDetectorRef) { }
 
+    /**
+     * @hidden
+     */
     protected defaultCssClass = 'igx-grid__group-row';
+
+    /**
+     * @hidden
+     */
     protected paddingIndentationCssClass = 'igx-grid__group-row--padding-level';
+
+    /**
+     * @hidden
+     */
     protected isFocused = false;
 
+    /**
+     * Returns whether the row is focused.
+     * ```
+     * let gridRowFocused = this.grid1.rowList.first.focused;
+     * ```
+     */
     get focused(): boolean {
         return this.isFocused;
     }
+
+    /**
+     * An @Input property that sets the index of the row.
+     * ```html
+     * <igx-grid-groupby-row [gridID]="id" [index]="rowIndex" [groupRow]="rowData" #row></igx-grid-groupby-row>
+     * ```
+     */
     @Input()
     public index: number;
 
+    /**
+     * An @Input property that sets the id of the grid the row belongs to.
+     * ```html
+     * <igx-grid-groupby-row [gridID]="id" [index]="rowIndex" [groupRow]="rowData" #row></igx-grid-groupby-row>
+     * ```
+     */
     @Input()
     public gridID: string;
 
+    /**
+     * An @Input property that sets a group by row.
+     * ```typescript
+     * <igx-grid-groupby-row [gridID]="id" [index]="rowIndex" [groupRow]="rowData" #row></igx-grid-groupby-row>
+     * ```
+     */
     @Input()
     public groupRow: IGroupByRecord;
 
+    /**
+     * Returns a reference of the content of the group.
+     * ```typescript
+     * const groupRowContent = this.grid1.rowList.first.groupContent;
+     * ```
+     */
     @ViewChild('groupContent')
     public groupContent: ElementRef;
 
+    /**
+     * Returns whether the group row is expanded.
+     * ```typescript
+     * const groupRowExpanded = this.grid1.rowList.first.expanded;
+     * ```
+     */
     @HostBinding('attr.aria-expanded')
     get expanded(): boolean {
         return this.grid.isExpandedGroup(this.groupRow);
     }
 
+    /**
+     * @hidden
+     */
     public tabindex = 0;
 
+    /**
+     * Returns the category the row is grouped by.
+     * ```typescript
+     * const groupDesc = this.grid1.rowList.first.describedBy;
+     * ```
+     */
     @HostBinding('attr.aria-describedby')
     get describedBy(): string {
         return this.gridID + '_' + this.groupRow.expression.fieldName;
     }
 
+    /**
+     * Returns the style classes applied to the group rows.
+     * ```typescript
+     * const groupCssStyles = this.grid1.rowList.first.styleClasses;
+     * ```
+     */
     @HostBinding('class')
     get styleClasses(): string {
         return `${this.defaultCssClass} ` + `${this.paddingIndentationCssClass}-` + this.groupRow.level;
     }
 
+    /**
+     * Toggles the group row.
+     * ```typescript
+     * this.grid1.rowList.first.toggle()
+     * ```
+     */
     @HostListener('keydown.enter')
     @HostListener('keydown.space')
     public toggle() {
         this.grid.toggleGroup(this.groupRow);
     }
 
+    /**
+     * Returns a reference to the `IgxGridComponent` the `IgxGridGroupByRowComponent` belongs to.
+     * ```typescript
+     * this.grid1.rowList.first.grid;
+     * ```
+     */
     get grid(): IgxGridComponent {
         return this.gridAPI.get(this.gridID);
     }
 
+    /**
+     * @hidden
+     */
     @HostListener('keydown.arrowdown', ['$event'])
     public onKeydownArrowDown(event) {
         const colIndex = this._getSelectedColIndex() || this._getPrevSelectedColIndex();
@@ -81,6 +159,9 @@ export class IgxGridGroupByRowComponent {
         this.grid.navigateDown(rowIndex, visibleColumnIndex);
     }
 
+    /**
+     * @hidden
+     */
     @HostListener('keydown.arrowup', ['$event'])
     public onKeydownArrowUp(event) {
         const colIndex = this._getSelectedColIndex() || this._getPrevSelectedColIndex();
@@ -93,10 +174,16 @@ export class IgxGridGroupByRowComponent {
         this.grid.navigateUp(rowIndex, visibleColumnIndex);
     }
 
+    /**
+     * @hidden
+     */
     public onFocus() {
         this.isFocused = true;
     }
 
+    /**
+     * @hidden
+     */
     public onBlur() {
         this.isFocused = false;
     }
