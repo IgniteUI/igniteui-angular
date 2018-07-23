e00ceb0d86d40e7158533e207b99ccd360716e66
AMishev <AMishev@DEV-AMISHEV>
2018-07-23 14:05:32 +0300

    chore(chip): Add chip dev sample for moving from one chiparea to another

--- src/app/chips/chips.sample.ts
+++ src/app/chips/chips.sample.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ViewChildren, ChangeDetectorRef } from '@angular/core';
+import { Component, ViewChild, ViewChildren, ChangeDetectorRef, ElementRef } from '@angular/core';
 import { IgxChipsAreaComponent, IgxChipComponent } from 'igniteui-angular';
 
 @Component({
@@ -14,8 +14,32 @@ export class ChipsSampleComponent {
         { id: 'FirstName', text: 'First Name' },
     ];
 
+    public chipListTo = [
+        { id: '1', text: 'Allen' },
+        { id: '2', text: 'George' },
+        { id: '3', text: 'Jason' },
+        { id: '4', text: 'Dean' },
+    ];
+
+    public chipListCc = [];
+
+    public draggingElem = false;
+    public dragEnteredArea = false;
+
     @ViewChild('chipsArea', { read: IgxChipsAreaComponent})
     public chipsArea: IgxChipsAreaComponent;
+
+    @ViewChild('chipsAreaTo', { read: IgxChipsAreaComponent})
+    public chipsAreaTo: IgxChipsAreaComponent;
+
+    @ViewChild('chipsAreaCc', { read: IgxChipsAreaComponent})
+    public chipsAreaCc: IgxChipsAreaComponent;
+
+    @ViewChild('dropTo', { read: ElementRef})
+    public dropTo: ElementRef;
+
+    @ViewChild('dropCc', { read: ElementRef})
+    public dropCc: ElementRef;
 
     constructor(public cdr: ChangeDetectorRef) { }
 
@@ -50,5 +74,89 @@ export class ChipsSampleComponent {
 
     onChipsSelected(event) {
         console.log(event.newSelection);
+    }
+
+    /**
+     * Chip Sample 2
+     */
+
+    chipsOrderChangedTo(event) {
+        const newChipListTo = [];
+        for (let i = 0; i < event.chipsArray.length; i++) {
+            const chipItem = this.chipListTo.filter((item) => {
+                return item.id === event.chipsArray[i].id;
+            })[0];
+            newChipListTo.push(chipItem);
+        }
+        this.chipListTo = newChipListTo;
+        event.isValid = true;
+    }
+
+    chipsOrderChangedCc(event) {
+        const newChipListCc = [];
+        for (let i = 0; i < event.chipsArray.length; i++) {
+            const chipItem = this.chipListCc.filter((item) => {
+                return item.id === event.chipsArray[i].id;
+            })[0];
+            newChipListCc.push(chipItem);
+        }
+        this.chipListCc = newChipListCc;
+        event.isValid = true;
+    }
+
+    public onDragEnterCc() {
+        this.dragEnteredArea = true;
+    }
+
+    public onDragLeaveCc() {
+        this.dragEnteredArea = false;
+    }
+
+    public onDropCc(event){
+        
+        event.cancel = true;
+        event.drag._dragGhost.remove();
+
+        let chipSwapEl = this.chipListTo.find(val=> val.text === event.drag.element.nativeElement.parentElement.children[0].textContent);
+        this.chipListCc.push(chipSwapEl);
+
+        this.chipsAreaCc.cdr.detectChanges();
+
+        this.chipListTo = this.chipListTo.filter(item => {
+            return item.text !== chipSwapEl.text;
+        });
+
+        this.chipsAreaTo.cdr.detectChanges();
+        this.dropCc.nativeElement.style.visibility = "hidden"
+    }
+
+    public onDropTo(event){
+        
+        event.cancel = true;
+        event.drag._dragGhost.remove();
+
+        let chipSwapEl = this.chipListCc.find(val=> val.text === event.drag.element.nativeElement.parentElement.children[0].textContent);
+        this.chipListTo.push(chipSwapEl);
+
+        this.chipsAreaTo.cdr.detectChanges();
+
+        this.chipListCc = this.chipListCc.filter(item => {
+            return item.text !== chipSwapEl.text;
+        });
+
+        this.chipsAreaCc.cdr.detectChanges();
+        this.dropTo.nativeElement.style.visibility = "hidden";
+    }
+
+    onMoveStartTo(){
+        this.dropCc.nativeElement.style.visibility = "visible";
+        this.dropCc.nativeElement.textContent = "You can drop me here!";
+        this.dropTo.nativeElement.style.visibility = "hidden";
+    }
+
+    onMoveStartCc(){
+        this.dropTo.nativeElement.style.visibility = "visible";
+        this.dropTo.nativeElement.textContent = "You can drop me here!";
+        this.dropCc.nativeElement.style.visibility = "hidden";
     }
 }
