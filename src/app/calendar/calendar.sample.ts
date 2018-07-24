45f1a6367676c9825902d1f85a13d603901ae8f0
Simeon Simeonoff <sim.simeonoff@gmail.com>
2018-07-24 18:13:28 +0300

    refactor(demos): udpate demos to reflect typography changes

--- src/app/calendar/calendar.sample.ts
+++ src/app/calendar/calendar.sample.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-calendar-sample',
-    templateUrl: 'calendar.sample.html'
+    templateUrl: 'calendar.sample.html',
+    styleUrls: ['calendar.sample.scss']
 })
-export class CalendarSampleComponent {}
+export class CalendarSampleComponent { }
