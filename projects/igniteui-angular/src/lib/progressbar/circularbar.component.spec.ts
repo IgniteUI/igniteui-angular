fc3c3f72859641aba5a24a7aae084710d92cc131
Aleksandar Kamenov <aleksandyr.kamenov@gmail.com>
2018-07-30 12:22:30 +0300

    test(circular-bar): allow to run all tests
    Closes #2172

--- projects/igniteui-angular/src/lib/progressbar/circularbar.component.spec.ts
+++ projects/igniteui-angular/src/lib/progressbar/circularbar.component.spec.ts
@@ -9,7 +9,7 @@ import {
 import { By } from '@angular/platform-browser';
 import { IgxCircularProgressBarComponent } from './progressbar.component';
 
-fdescribe('IgCircularBar', () => {
+describe('IgCircularBar', () => {
     const tickTime = 2000;
     beforeEach(async(() => {
         TestBed.configureTestingModule({
