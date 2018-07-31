a2978abf4558a6b36b4dd8e37ff068cdd38bb541
DDavidkov <ddavidkov3@gmail.com>
2018-07-31 16:42:12 +0300

    fix(grid): fire change detection when toggleAllGroupRows is called #2200

--- projects/igniteui-angular/src/lib/grid/grid.component.ts
+++ projects/igniteui-angular/src/lib/grid/grid.component.ts
@@ -2766,6 +2766,7 @@ export class IgxGridComponent implements OnInit, OnDestroy, AfterContentInit, Af
     public toggleAllGroupRows() {
         this.groupingExpansionState = [];
         this.groupsExpanded = !this.groupsExpanded;
+        this.cdr.detectChanges();
     }
 
 
