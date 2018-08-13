c74ea5e511dfae71b3aa5fbd93d7d9afda502244
Simeon Simeonoff <sim.simeonoff@gmail.com>
2018-08-13 16:13:09 +0300

    refactor(demos): calendar sample with special and disabled dates

--- src/app/calendar/calendar.sample.ts
+++ src/app/calendar/calendar.sample.ts
@@ -1,7 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IgxCalendarComponent, DateRangeDescriptor, DateRangeType } from '../../../projects/igniteui-angular/src/lib/calendar';
 
 @Component({
     selector: 'app-calendar-sample',
     templateUrl: 'calendar.sample.html'
 })
-export class CalendarSampleComponent {}
+export class CalendarSampleComponent implements OnInit {
+    @ViewChild('calendar') calendar: IgxCalendarComponent;
+
+    ngOnInit() {
+        this.calendar.disabledDates = [new DateRangeDescriptor(DateRangeType.Between, [
+            new Date(2018, 8, 2),
+            new Date(2018, 8, 8)
+        ])];
+        this.calendar.specialDates = [new DateRangeDescriptor(DateRangeType.Specific, [
+            new Date(2018, 7, 4),
+            new Date(2018, 7, 14),
+            new Date(2018, 7, 15),
+            new Date(2018, 8, 14)
+        ])];
+    }
+}
