bbeceeca588796b9b2a6fc67b0a1e62b270df01e
Simeon Simeonoff <sim.simeonoff@gmail.com>
2018-08-21 17:23:58 +0300

    Merge from docthemes

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 'use strict';
 
 const autoprefixer = require('autoprefixer');
+const browserSync = require('browser-sync').create();
+const del = require('del');
 const gulp = require('gulp');
+const concat = require('gulp-concat');
 const sass = require('gulp-sass');
+const shell = require('gulp-shell');
 const sourcemaps = require('gulp-sourcemaps');
 const postcss = require('gulp-postcss');
+const uglify = require('gulp-uglify');
 const process = require('process');
 const fs = require('fs');
 const {
@@ -23,6 +28,19 @@ const STYLES = {
         outputStyle: 'compressed'
     }
 };
+
+const TYPEDOC = {
+    SRC: './extra/docs/themes/typedoc/src/',
+    DIST: './extra/docs/themes/typedoc/bin/',
+    STYLES: {
+        ENTRY: './assets/css/main.sass',
+        OUT: './assets/css',
+        MAPS: './',
+        CONFIG: {
+            outputStyle: 'compressed'
+        }
+    }
+}
 
 gulp.task('build-style', () => {
     const prefixer = postcss([autoprefixer({
@@ -108,8 +126,107 @@ gulp.task('watch', () => {
 
 gulp.task('copy-migrations', () => {
     return gulp.src([
-        './projects/igniteui-angular/migrations/**/*.json',
-        '!**/tsconfig.json'
-    ])
-    .pipe(gulp.dest('./dist/igniteui-angular/migrations'));
+            './projects/igniteui-angular/migrations/**/*.json',
+            '!**/tsconfig.json'
+        ])
+        .pipe(gulp.dest('./dist/igniteui-angular/migrations'));
+});
+
+gulp.task('typedoc-styles', ['typedoc:clean-styles'], () => {
+    const prefixer = postcss([autoprefixer({
+        browsers: ['last 5 versions', '> 3%'],
+        cascade: false,
+        grid: false
+    })]);
+
+    return gulp.src(`${TYPEDOC.SRC}/${TYPEDOC.STYLES.ENTRY}`)
+        .pipe(sourcemaps.init())
+        .pipe(sass.sync(TYPEDOC.STYLES.CONFIG).on('error', sass.logError))
+        .pipe(prefixer)
+        .pipe(sourcemaps.write(TYPEDOC.STYLES.MAPS))
+        .pipe(gulp.dest(`${TYPEDOC.DIST}/${TYPEDOC.STYLES.OUT}`))
+});
+
+gulp.task('typedoc-ts',
+    shell.task('tsc --project ./extra/docs/themes/typedoc/tsconfig.json')
+);
+
+gulp.task('typedoc-js', ['typedoc-ts'], () => {
+    return gulp.src([
+            `${TYPEDOC.SRC}/assets/js/lib/jquery-2.1.1.min.js`,
+            `${TYPEDOC.SRC}/assets/js/lib/underscore-1.6.0.min.js`,
+            `${TYPEDOC.SRC}/assets/js/lib/backbone-1.1.2.min.js`,
+            `${TYPEDOC.SRC}/assets/js/lib/lunr.min.js`,
+            `${TYPEDOC.SRC}/assets/js/main.js`
+        ])
+        .pipe(concat('main.js'))
+        // .pipe(uglify({
+        //     mangle: false
+        // }))
+        .pipe(gulp.dest(`${TYPEDOC.DIST}/assets/js/`));
+});
+
+gulp.task('typedoc-images', ['typedoc:clean-images'], () => {
+    return gulp.src(`${TYPEDOC.SRC}/assets/images/**/*.{png,gif,jpg}`)
+        .pipe(gulp.dest(`${TYPEDOC.DIST}/assets/images`));
+});
+
+gulp.task('typedoc-hbs', ['typedoc:clean-hbs'], () => {
+    return gulp.src([
+            `${TYPEDOC.SRC}/layouts/**/*`,
+            `${TYPEDOC.SRC}/partials/**/*`,
+            `${TYPEDOC.SRC}/templates/**/*`,
+        ], {
+            base: `${TYPEDOC.SRC}`
+        })
+        .pipe(gulp.dest(`${TYPEDOC.DIST}`));
+});
+
+gulp.task('typedoc:clean-js', () => {
+    del.sync(`${TYPEDOC.DIST}/assets/js`);
+});
+
+gulp.task('typedoc:clean-styles', () => {
+    del.sync(`${TYPEDOC.DIST}/assets/css`);
+});
+
+gulp.task('typedoc:clean-images', () => {
+    del.sync(`${TYPEDOC.DIST}/assets/images`);
+});
+
+gulp.task('typedoc:clean-hbs', () => {
+    del.sync([
+        `${TYPEDOC.DIST}/layouts`,
+        `${TYPEDOC.DIST}/partials`,
+        `${TYPEDOC.DIST}/templates`
+    ]);
+});
+
+gulp.task('typedoc-watch', ['typedoc-build:theme'], () => {
+    gulp.watch([
+        `${TYPEDOC.SRC}/assets/js/src/**/*.{ts,js}`,
+        `${TYPEDOC.SRC}/assets/css/**/*.{scss,sass}`,
+        `${TYPEDOC.SRC}/**/*.hbs`,
+        `${TYPEDOC.SRC}/assets/images/**/*.{png,jpg,gif}`,
+    ], ['typedoc-build:theme']);
+});
+
+gulp.task('typedoc-build', [
+    'typedoc-images',
+    'typedoc-hbs',
+    'typedoc-styles',
+    'typedoc-js'
+]);
+
+gulp.task('typedoc-build:theme', ['typedoc-build'],
+    shell.task('typedoc projects/igniteui-angular/src/public_api.ts')
+);
+
+gulp.task('typedoc-serve', ['typedoc-watch'], () => {
+    browserSync.init({
+        server: './dist/igniteui-angular/docs/typescript'
+    });
+
+    gulp.watch('./dist/igniteui-angular/docs/typescript/**/*')
+        .on('change', browserSync.reload);
 });
