0996e4513759c84632dae8c6222b44c674d20184
Tacho <tjelev22@abv.bg>
2018-09-14 10:11:32 +0300

    chore(tooltip): modify tooltip demo #1710

--- src/app/tooltip/tooltip.sample.ts
+++ src/app/tooltip/tooltip.sample.ts
@@ -11,12 +11,12 @@ import {
 })
 export class TooltipSampleComponent implements OnInit, AfterViewInit {
 
-    @ViewChild("button") public tooltipTarget: IgxTooltipTargetDirective;
+    @ViewChild("target") public tooltipTarget: IgxTooltipTargetDirective;
 
     settings: OverlaySettings = {
-        positionStrategy: new AutoPositionStrategy(),
-        closeOnOutsideClick: false,
-        modal: false
+        // positionStrategy: new AutoPositionStrategy(),
+        // closeOnOutsideClick: false,
+        // modal: false
     };
 
     public data: any[];
@@ -27,16 +27,67 @@ export class TooltipSampleComponent implements OnInit, AfterViewInit {
 
     ngOnInit() {
         this.data = [
-            { Name: "John", LastName: "Doe", Age: 29 },
-            { Name: "Jane", LastName: "Doe", Age: 25 },
-            { Name: "Nick", LastName: "Rogers", Age: 54 },
-            { Name: "Samantha", LastName: "Black", Age: 33 },
-            { Name: "Nicole", LastName: "Donovan", Age: 30 }
+            {
+                Brand: "Samsung",
+                Model: "Galaxy Note 9",
+                Spec: {
+                    Launch: "2018, August",
+                    Build: "Front/back glass",
+                    Display: "6.4'', Super AMOLED",
+                    Resolution: "1440 x 2960 px",
+                    OS: "Android 8.1 (Oreo)",
+                    Battery: "Li-Ion 4000 mAh"
+                }
+            },
+            {
+                Brand: "Google", Model: "Pixel 2 XL",
+                Spec: {
+                    Launch: "2017, October",
+                    Build: "Front glass, partial glass back",
+                    Display: "6.0'', P-OLED",
+                    Resolution: "1440 x 2880 px",
+                    OS: "Android 8.0 (Oreo)",
+                    Battery: "Li-Ion 3520 mAh"
+                }
+            },
+            {
+                Brand: "Apple", Model: "iPhone XS Max",
+                Spec: {
+                    Launch: "2018, September",
+                    Build: "Front/back glass",
+                    Display: "6.5'', Super Retina",
+                    Resolution: "1242 x 2688 px",
+                    OS: "iOS 12",
+                    Battery: "N/A"
+                }
+            },
+            {
+                Brand: "LG", Model: "G7 ThinQ",
+                Spec: {
+                    Launch: "2018, May",
+                    Build: "Front/back glass",
+                    Display: "6.1'', IPS LCD",
+                    Resolution: "1440 x 3120 px",
+                    OS: "Android 8.0 (Oreo)",
+                    Battery: "Li-Po 3000 mAh"
+                }
+            },
+            {
+                Brand: "HTC", Model: "U12+",
+                Spec: {
+                    Launch: "2018, May",
+                    Build: "Front/back glass",
+                    Display: "6.0'', Super LCD6",
+                    Resolution: "1440 x 2880 px",
+                    OS: "Android 8.0 (Oreo)",
+                    Battery: "Li-Ion 3500 mAh"
+                }
+            }
         ];
     }
 
     ngAfterViewInit() {
-        this.settings.positionStrategy.settings.target = this.tooltipTarget.nativeElement;
+        // this.settings.positionStrategy.settings.target = this.tooltipTarget.nativeElement;
         // this.settings.positionStrategy.settings.openAnimation = null;
         // this.settings.positionStrategy.settings.closeAnimation = null;
     }
