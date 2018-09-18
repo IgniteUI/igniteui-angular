db7e9b8bc9aaf867f68cfc296fa33011b2429187
Diyan Dimitrov <ddimitrov@infragistics.com>
2018-09-18 17:55:44 +0300

    fix(selection): throw error when the id is invalid #2581

--- projects/igniteui-angular/src/lib/core/selection.spec.ts
+++ projects/igniteui-angular/src/lib/core/selection.spec.ts
@@ -7,14 +7,16 @@ describe('IgxSelectionAPIService', () => {
     });
 
     it('call set method with undefined componentID', () => {
-        service.set(undefined, new Set());
-        expect(service.get(undefined)).toBeUndefined();
+        expect(function() {
+            service.set(undefined, new Set());
+        }).toThrowError('Invalid value for component id!');
     });
 
     it('call add_item method with undefined itemID', () => {
         const componentId = 'id1';
         service.set(componentId, new Set());
-        const set = service.add_item(componentId, undefined);
-        expect(set.size).toEqual(0);
+        expect(function() {
+             service.add_item(componentId, undefined);
+        }).toThrowError('Invalid value for item id!');
     });
 });
