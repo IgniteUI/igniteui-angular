109ac4ef78f8a6fea2b626174ba70a6cf2953e17
Borislav Kulov <bkulov@infragistics.com>
2018-09-28 10:57:45 +0300

    chore(icon): Replace the flag icons with filtering icons (#2613)

--- src/app/icon/icon.sample.ts
+++ src/app/icon/icon.sample.ts
@@ -11,15 +11,12 @@ export class IconSampleComponent implements OnInit {
 
     ngOnInit(): void {
         // register custom svg icons
-        this._iconService.addSvgIcon('aruba', '/assets/svg/country_flags/aruba.svg', 'svg-flags');
-        this._iconService.addSvgIcon('bulgaria', '/assets/svg/country_flags/bulgaria.svg', 'svg-flags');
-        this._iconService.addSvgIcon('cape-verde', '/assets/svg/country_flags/cape-verde.svg', 'svg-flags');
-        this._iconService.addSvgIcon('ceuta', '/assets/svg/country_flags/ceuta.svg', 'svg-flags');
-        this._iconService.addSvgIcon('cocos-island', '/assets/svg/country_flags/cocos-island.svg', 'svg-flags');
-        this._iconService.addSvgIcon('curacao', '/assets/svg/country_flags/curacao.svg', 'svg-flags');
-        this._iconService.addSvgIcon('kiribati', '/assets/svg/country_flags/kiribati.svg', 'svg-flags');
-        this._iconService.addSvgIcon('madagascar', '/assets/svg/country_flags/madagascar.svg', 'svg-flags');
-        this._iconService.addSvgIcon('north-korea', '/assets/svg/country_flags/north-korea.svg', 'svg-flags');
-        this._iconService.addSvgIcon('tokelau', '/assets/svg/country_flags/tokelau.svg', 'svg-flags');
+        this._iconService.addSvgIcon('contains', '/assets/svg/filtering/contains.svg', 'svg-flags');
+        this._iconService.addSvgIcon('does_not_contain', '/assets/svg/filtering/does_not_contain.svg', 'svg-flags');
+        this._iconService.addSvgIcon('does_not_equal', '/assets/svg/filtering/does_not_equal.svg', 'svg-flags');
+        this._iconService.addSvgIcon('ends_with', '/assets/svg/filtering/ends_with.svg', 'svg-flags');
+        this._iconService.addSvgIcon('equals', '/assets/svg/filtering/equals.svg', 'svg-flags');
+        this._iconService.addSvgIcon('is_empty', '/assets/svg/filtering/is_empty.svg', 'svg-flags');
+        this._iconService.addSvgIcon('starts_with', '/assets/svg/filtering/starts_with.svg', 'svg-flags');
     }
 }
