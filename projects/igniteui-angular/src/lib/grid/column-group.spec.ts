dc12be84b46aa343f6b26b46cdcee8b8bee37631
Stamen Stoychev <chronos.stz@gmail.com>
2018-10-15 14:53:25 +0300

    chore(*): removing fdescribe

--- projects/igniteui-angular/src/lib/grid/column-group.spec.ts
+++ projects/igniteui-angular/src/lib/grid/column-group.spec.ts
@@ -15,7 +15,7 @@ const GRID_COL_GROUP_THEAD_TITLE_CLASS = 'igx-grid__thead-title';
 const GRID_COL_GROUP_THEAD_GROUP_CLASS = 'igx-grid__thead-group';
 const GRID_COL_THEAD_CLASS = '.igx-grid__th';
 
-fdescribe('IgxGrid - multi-column headers', () => {
+describe('IgxGrid - multi-column headers', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
