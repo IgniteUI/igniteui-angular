0ab04968d614359af5305d50d39c6d7aaa366702
wnvko <milko.venkov@gmail.com>
2018-10-25 15:08:40 +0300

    docs(igxOverlay): add documentation in source code, #2735

--- projects/igniteui-angular/src/lib/services/overlay/position/IPositionStrategy.ts
+++ projects/igniteui-angular/src/lib/services/overlay/position/IPositionStrategy.ts
@@ -1,8 +1,17 @@
 import { PositionSettings } from './../utilities';
 
+/**
+ * [Documentation](https://www.infragistics.com/products/ignite-ui-angular/angular/components/overlay_position.html)
+ * Position strategies determine where to display the component in the provided IgxOverlayService.
+ */
 export interface IPositionStrategy {
     settings: PositionSettings;
 
-    /** Position the element based on the PositionStrategy implementing this interface */
+    /**
+     * Position the element based on the PositionStrategy implementing this interface.
+     * ```typescript
+     * settings.positionStrategy.position(content, size, Document, true);
+     * ```
+     */
      position(contentElement: HTMLElement, size?: {}, document?: Document, initialCall?: boolean): void;
 }
