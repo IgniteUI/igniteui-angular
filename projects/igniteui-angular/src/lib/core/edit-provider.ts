0aeb3aa812d264d1ada599b744fc1928e5ef59ab
Damyan Petev <damyan.petev@gmail.com>
2018-10-25 17:55:40 +0300

    feat(igxFocus): add igxFocus support to radio, slider, switch #2456

--- projects/igniteui-angular/src/lib/core/edit-provider.ts
+++ projects/igniteui-angular/src/lib/core/edit-provider.ts
@@ -1,4 +1,7 @@
-/** Used for editor control components */
+/**
+ * Used for editor control components
+ * @hidden
+ */
 export interface EditorProvider {
     /** Return the focusable native element */
     getEditElement();
