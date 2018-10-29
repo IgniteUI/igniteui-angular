cd0f29069d038a0bcde0c14170fab3dcd7e69118
Damyan Petev <damyan.petev@gmail.com>
2018-10-29 20:45:51 +0200

    perf(row-edit): prevent needless pipe re-triggers on row edit switch
    Only use _pipeTrigger when updating the actual data
    as there's a transactions.onStateUpdate IF there's an actual update

--- projects/igniteui-angular/src/lib/grids/grid-base.component.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.component.ts
@@ -4309,6 +4309,7 @@ export abstract class IgxGridBaseComponent implements OnInit, OnDestroy, AfterCo
     }
 
     /**
+     * TODO: Refactor
      * @hidden
      */
     private endRowTransaction(commit?: boolean, closeOverlay?: boolean, row?: any, rowObject?: IgxRowComponent<IgxGridBaseComponent>) {
@@ -4320,7 +4321,10 @@ export abstract class IgxGridBaseComponent implements OnInit, OnDestroy, AfterCo
         const lastCommitedValue = // Last commited value (w/o pending)
         this.transactions.getState(rowInEdit.rowID) ? Object.assign({}, this.transactions.getState(rowInEdit.rowID).value) : {};
         // we want pure object, not object reference, as it changes when endPending is called
-        this.transactions.endPending(commit); // End pending
+        if (closeOverlay) { // End pending
+            // TODO: Why is transactions.endPending in closeRowEditingOverlay?
+            this.closeRowEditingOverlay(commit);
+        }
         const rowObj = rowObject ? rowObject : this.getRowByKey(rowInEdit.rowID); // If row obj was pass, use it
         const rowIndex = this.gridAPI.get_row_index_in_data(this.id, rowInEdit.rowID);
         let oldValue = Object.assign({}, this.data[rowIndex]); // Get actual index in data
@@ -4333,13 +4337,11 @@ export abstract class IgxGridBaseComponent implements OnInit, OnDestroy, AfterCo
             oldValue,
             row: rowObj
         });
+
         if (commit && newValue && !this.transactions.enabled) {
             this.data[rowIndex] = newValue; // If no transactions, write to data directly
+            this._pipeTrigger++;
         }
-        if (closeOverlay) {
-            this.closeRowEditingOverlay(commit);
-        }
-        this._pipeTrigger++;
     }
 
     /**
