15a23bfb482b6a6f0cb05c7f52f8f618d26840e6
Damyan Petev <damyan.petev@gmail.com>
2018-11-05 15:16:00 +0200

    docs(typedoc): export expansion panel body component and event args

--- projects/igniteui-angular/src/lib/expansion-panel/index.ts
+++ projects/igniteui-angular/src/lib/expansion-panel/index.ts
@@ -1,3 +1,5 @@
-export {IgxExpansionPanelHeaderComponent} from './expansion-panel-header.component';
-export {IgxExpansionPanelComponent} from './expansion-panel.component';
+
+export { IgxExpansionPanelHeaderComponent, IExpansionPanelEventArgs } from './expansion-panel-header.component';
+export { IgxExpansionPanelBodyComponent } from './expansion-panel-body.component';
+export { IgxExpansionPanelComponent } from './expansion-panel.component';
 export * from './expansion-panel.module';
