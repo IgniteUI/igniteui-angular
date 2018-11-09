215073fba415b1f1af651e8fd1732903fe13e069
ViktorSlavov <viktorslavov91@gmail.com>
2018-11-09 16:29:47 +0200

    fix(igxGrid): exit edit mode on delete, #2977

--- projects/igniteui-angular/src/lib/grids/grid-base.component.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.component.ts
@@ -2807,9 +2807,8 @@ export abstract class IgxGridBaseComponent extends DisplayDensityBase implements
         //  Otherwise just exit - there is nothing to delete
         if (index !== -1 || hasRowInNonDeletedState) {
             const editableCell = this.gridAPI.get_cell_inEditMode(this.id);
-            if (this.transactions.enabled) {
-                this.endEdit(true);
-            }
+            // Always exit edit when row is deleted
+            this.endEdit(true);
             if (editableCell && editableCell.cellID.rowID === rowId) {
                 this.gridAPI.escape_editMode(this.id, editableCell.cellID);
             }
