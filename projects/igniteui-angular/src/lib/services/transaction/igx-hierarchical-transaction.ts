825d54afa41d649765c96ea82cc97c8630b99983
wnvko <milko.venkov@gmail.com>
2018-11-19 10:58:24 +0200

    fix(tree-grid): delete row in flat data source, #2921

--- projects/igniteui-angular/src/lib/services/transaction/igx-hierarchical-transaction.ts
+++ projects/igniteui-angular/src/lib/services/transaction/igx-hierarchical-transaction.ts
@@ -25,19 +25,9 @@ export class IgxHierarchicalTransactionService<T extends HierarchicalTransaction
             currentState.path = transaction.path;
         }
 
-        if (currentState.type === TransactionType.DELETE) {
-            states.forEach((v: S, k: any) => {
-                if (v.path.indexOf(transaction.id) !== -1) {
-                    switch (v.type) {
-                        case TransactionType.ADD:
-                            states.delete(k);
-                            break;
-                        case TransactionType.UPDATE:
-                            states.get(k).type = TransactionType.DELETE;
-                            states.get(k).value = null;
-                    }
-                }
-            });
+        //  if transaction has no path, e.g. flat data source, get out
+        if (!transaction.path) {
+            return;
         }
     }
 
