89638ed7b1155bdebd5e1029a81700d616fe8718
Tacho <tjelev22@abv.bg>
2018-11-26 16:02:11 +0200

    chore(*): refactor MCH failing test #3099

--- projects/igniteui-angular/src/lib/grids/tree-grid/tree-grid-integration.spec.ts
+++ projects/igniteui-angular/src/lib/grids/tree-grid/tree-grid-integration.spec.ts
@@ -601,13 +601,17 @@ describe('IgxTreeGrid - Integration', () => {
             const column = treeGrid.columnList.filter(c => c.header === 'General Information')[0];
             column.movable = true;
             fix.detectChanges();
+            
+            const header = fix.debugElement.queryAll(By.css('.igx-grid__thead-item'))[0].nativeElement;
 
-            const header = TreeGridFunctions.getHeaderCell(fix, 'General Information').nativeElement;
             UIInteractions.simulatePointerEvent('pointerdown', header, 100, 40);
-            UIInteractions.simulatePointerEvent('pointermove', header, 106, 46);
             await wait();
+            UIInteractions.simulatePointerEvent('pointermove', header, 106, 46);
+            await wait(50);
             UIInteractions.simulatePointerEvent('pointermove', header, 700, 40);
+            await wait();
             UIInteractions.simulatePointerEvent('pointerup', header, 700, 40);
+            await wait();
             fix.detectChanges();
 
             TreeGridFunctions.verifyTreeColumnInMultiColHeaders(fix, 'HireDate', 4);
