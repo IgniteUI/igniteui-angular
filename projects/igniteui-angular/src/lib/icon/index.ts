50f34998a30abe42adfebf2eec1f07d2087e8975
Borislav Kulov <bkulov@infragistics.com>
2018-11-28 18:41:31 +0200

    chore(icon): Show deprecation warning for IgxIconModule.forRoot (#3026)

--- projects/igniteui-angular/src/lib/icon/index.ts
+++ projects/igniteui-angular/src/lib/icon/index.ts
@@ -3,13 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { IgxIconComponent } from './icon.component';
 import { HttpClientModule } from '@angular/common/http';
+import { DeprecateMethod } from '../core/deprecateDecorators';
 
 @NgModule({
     declarations: [IgxIconComponent],
     exports: [IgxIconComponent],
     imports: [CommonModule, HttpClientModule]
 })
-export class IgxIconModule {}
+export class IgxIconModule {
+    @DeprecateMethod('IgxIconModule.forRoot method is deprecated. Use IgxIconModule instead.')
+    public static forRoot() {
+        return {
+            ngModule: IgxIconModule
+        };
+    }
+}
 
 export * from './icon.component';
 export * from './icon.service';
