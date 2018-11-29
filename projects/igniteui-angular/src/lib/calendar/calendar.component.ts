ea918fa52c54a21557c7591df6dc2bc21737ce99
Nadia Robakova <nrobakova@infragistics.com>
2018-11-29 09:01:43 +0200

    fix(calendar): Set rangeStarted to false when deselect date #3172

--- projects/igniteui-angular/src/lib/calendar/calendar.component.ts
+++ projects/igniteui-angular/src/lib/calendar/calendar.component.ts
@@ -1280,6 +1280,7 @@ export class IgxCalendarComponent implements OnInit, ControlValueAccessor {
 
         if (!(valueEnd < selectedDatesStart) && !(valueStart > selectedDatesEnd)) {
             this.selectedDates = [];
+            this.rangeStarted = false;
             this._onChangeCallback(this.selectedDates);
         }
     }
