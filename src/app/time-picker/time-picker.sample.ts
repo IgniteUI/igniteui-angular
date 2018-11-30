6f1b64468ae08b13476a9c37a88de60583e10326
SAndreeva <sandreeva@infragistics.com>
2018-11-30 19:17:37 +0200

    feat(time picker): fix broken sample #2337

--- src/app/time-picker/time-picker.sample.ts
+++ src/app/time-picker/time-picker.sample.ts
@@ -17,6 +17,10 @@ export class TimePickerSampleComponent {
 
     date = new Date(2018, 10, 27, 17, 45, 0, 0);
 
+    showDate() {
+        return this.date ? this.date.toLocaleString() : 'Value is null.';
+    }
+
     @ViewChild('tp', { read: IgxTimePickerComponent })
     public tp: IgxTimePickerComponent;
 }
