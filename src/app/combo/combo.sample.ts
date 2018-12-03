58feeb62ee9642e2c8941d1f8162f9361d056e4a
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2018-12-03 17:02:16 +0200

    refactor(displayDensity): Code cleanup in display density base class
    Cleaned host bindings in child components.
    The displayDensity getter cannot retrun undefined anymore.

--- src/app/combo/combo.sample.ts
+++ src/app/combo/combo.sample.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, OnInit, TemplateRef } from '@angular/core';
-import { IgxComboComponent } from 'igniteui-angular';
+import { IgxComboComponent, DisplayDensity } from 'igniteui-angular';
 import { take } from 'rxjs/operators';
 import { NgModule } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -122,7 +122,7 @@ export class ComboSampleComponent implements OnInit {
         this.initialItemTemplate = comboTemplate;
     }
 
-    setDensity(density: string) {
+    setDensity(density: DisplayDensity) {
         this.igxCombo.displayDensity = density;
     }
 }
