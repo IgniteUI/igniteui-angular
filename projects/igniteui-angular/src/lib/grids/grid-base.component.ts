549d08a2327ad8d391f3c590972bb4fc7fff8118
SAndreeva <sandreeva@infragistics.com>
2018-12-05 15:21:26 +0200

    chore(*): fix lint errors #3071

--- projects/igniteui-angular/src/lib/grids/grid-base.component.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.component.ts
@@ -2488,7 +2488,7 @@ export abstract class IgxGridBaseComponent extends DisplayDensityBase implements
             return minWidth.toString();
         }
 
-        return column.width
+        return column.width;
     }
 
     /**
