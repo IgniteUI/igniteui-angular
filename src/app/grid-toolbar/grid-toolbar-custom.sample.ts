bf6db4f9a10fa0682c1bc239b0ada7213c99ba57
Radko Kolev <rkolev@infragistics.com>
2018-12-12 14:55:54 +0200

    chore(toolbar): removing unneeded variable #3397

--- src/app/grid-toolbar/grid-toolbar-custom.sample.ts
+++ src/app/grid-toolbar/grid-toolbar-custom.sample.ts
@@ -1,6 +1,6 @@
 
-import { Component, ViewChild } from '@angular/core';
-import { IgxColumnComponent, IgxGridComponent } from 'igniteui-angular';
+import { Component } from '@angular/core';
+import { IgxColumnComponent } from 'igniteui-angular';
 
 @Component({
     selector: 'app-grid-toolbar-custom-sample',
@@ -8,9 +8,6 @@ import { IgxColumnComponent, IgxGridComponent } from 'igniteui-angular';
     templateUrl: 'grid-toolbar-custom.sample.html'
 })
 export class GridToolbarCustomSampleComponent {
-
-    @ViewChild('grid1', { read: IgxGridComponent })
-    public igxGrid1: IgxGridComponent;
 
     data = [
         {
