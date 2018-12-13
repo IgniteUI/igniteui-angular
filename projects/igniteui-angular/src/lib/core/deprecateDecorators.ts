5c44188d939ef7b52b88238ae96429a7f82a4fad
Borislav Kulov <bkulov@infragistics.com>
2018-12-13 15:12:10 +0200

    chore(*): Rewrite deprecation decorator for properties and methods (#2915)
    Remove DeprecateClass util method

--- projects/igniteui-angular/src/lib/core/deprecateDecorators.ts
+++ projects/igniteui-angular/src/lib/core/deprecateDecorators.ts
@@ -1,19 +1,28 @@
-
-/**
- * @hidden
- */
-export function DeprecateClass(message: string): ClassDecorator {
-    return (constructor: any) => {
-        console.warn(constructor.name + ': ' + message);
-    };
-}
+import { isDevMode } from '@angular/core';
 
 /**
  * @hidden
  */
 export function DeprecateMethod(message: string): MethodDecorator {
-    return (constructor: any) => {
-        console.warn(constructor.constructor.name + ': ' + message);
+    let isMessageShown = false;
+
+    return function (target: any, key: string, descriptor: PropertyDescriptor) {
+        if (descriptor && descriptor.value) {
+            const originalMethod = descriptor.value;
+
+            descriptor.value = function () {
+                if (!isMessageShown && isDevMode()) {
+                    const targetName = typeof target === 'function' ? target.name : target.constructor.name;
+                    isMessageShown = true;
+
+                    console.warn(`${targetName}.${key}: ${message}`);
+                }
+
+                return originalMethod.apply(this, arguments);
+            };
+
+            return descriptor;
+        }
     };
 }
 
@@ -21,7 +30,36 @@ export function DeprecateMethod(message: string): MethodDecorator {
  * @hidden
  */
 export function DeprecateProperty(message: string): PropertyDecorator {
-    return (constructor: any) => {
-        console.warn(constructor.constructor.name + ': ' + message);
+    return function(target: any, key: string) {
+        let isMessageShown = false;
+
+        // use backing field to set/get the value of the property to ensure there won't be infinite recursive calls
+        const newKey = generateUniqueKey(target, key);
+
+        Object.defineProperty(target, key, {
+            set(value) {
+                this[newKey] = value;
+            },
+            get() {
+                if (!isMessageShown && isDevMode()) {
+                    isMessageShown = true;
+                    console.warn(`${target.constructor.name}.${key}: ${message}`);
+                }
+
+                return this[newKey];
+            }
+        });
     };
+}
+
+/**
+ * @hidden
+ */
+function generateUniqueKey(target: any, key: string): string {
+    let newKey = '_' + key;
+    while (target.hasOwnProperty(newKey)) {
+        newKey = '_' + newKey;
+    }
+
+    return newKey;
 }
