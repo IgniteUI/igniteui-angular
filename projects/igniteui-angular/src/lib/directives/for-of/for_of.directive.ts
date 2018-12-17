9d3b988f84612d9aeb49602a58fdc031794be640
MartinKamenov <martinkamenov10@gmail.com>
2018-12-17 16:58:07 +0200

    chore(*): Added test for itemSize.

--- projects/igniteui-angular/src/lib/directives/for-of/for_of.directive.ts
+++ projects/igniteui-angular/src/lib/directives/for-of/for_of.directive.ts
@@ -1110,7 +1110,8 @@ export class IgxForOfDirective<T> implements OnInit, OnChanges, DoCheck, OnDestr
     }
 
     private _getItemSize(item, dimension: string): number {
-        return parseInt(item[dimension], 10) || this.igxForItemSize;
+        const hasDimension = (item[dimension] !== null && item[dimension] !== undefined);
+        return hasDimension ? parseInt(item[dimension], 10) : this.igxForItemSize;
     }
 }
 
