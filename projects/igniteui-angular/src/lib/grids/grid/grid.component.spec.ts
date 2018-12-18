18b62b1859768ebce3f07769fa924132f4bbc389
ddincheva <desislava9dincheva@gmail.com>
2018-12-18 14:46:21 +0200

    fix(IgxGridCell): focus input when enter cell in edit mode #2801

--- projects/igniteui-angular/src/lib/grids/grid/grid.component.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid.component.spec.ts
@@ -1793,11 +1793,12 @@ describe('IgxGrid Component Tests', () => {
 
                 const targetCell = grid.getCellByColumn(0, 'ProductName');
                 targetCell.onFocus({});
-                tick();
+                tick(100);
                 fixture.detectChanges();
                 expect(grid.endRowTransaction).toHaveBeenCalledTimes(1);
                 expect(targetCell.focused).toBeTruthy();
-                expect(firstCell.focused).toBeFalsy();
+                expect(targetCell.selected).toBeTruthy();
+                expect(firstCell.selected).toBeFalsy();
             }));
         });
 
