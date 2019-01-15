71ddae074fc0ecd8f41e36c5b278eb47b69eeada
SAndreeva <sandreeva@infragistics.com>
2019-01-15 13:07:44 +0200

    fix(filtering): chip creation while typing in JP on Edge #3599

--- projects/igniteui-angular/src/lib/grids/filtering/grid-filtering-row.component.ts
+++ projects/igniteui-angular/src/lib/grids/filtering/grid-filtering-row.component.ts
@@ -24,7 +24,7 @@ import { IChipSelectEventArgs, IBaseChipEventArgs, IgxChipsAreaComponent, IgxChi
 import { ExpressionUI } from './grid-filtering.service';
 import { IgxDropDownItemComponent } from '../../drop-down/drop-down-item.component';
 import { IgxFilteringService } from './grid-filtering.service';
-import { KEYS } from '../../core/utils';
+import { KEYS, isIE } from '../../core/utils';
 import { AbsoluteScrollStrategy } from '../../services/overlay/scroll';
 
 /**
@@ -286,9 +286,12 @@ export class IgxGridFilteringRowComponent implements AfterViewInit {
     public onInput(eventArgs) {
         // The 'iskeyPressed' flag is needed for a case in IE, because the input event is fired on focus and for some reason,
         // when you have a japanese character as a placeholder, on init the value here is empty string .
-        if (this.isKeyPressed) {
+        if (isIE() && this.isKeyPressed) {
             this.value = eventArgs.target.value;
+            return;
         }
+
+        this.value = eventArgs.target.value;
     }
 
     /**
