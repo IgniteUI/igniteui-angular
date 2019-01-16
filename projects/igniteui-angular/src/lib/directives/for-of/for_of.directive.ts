e59521d9b4210a1c15e3e6534acbd282d10019d5
MKirova <MKirova@DEV-MKIROVA>
2019-01-16 19:55:03 +0200

    fix(TreeGrid): Removing request animation frame because TreeGrid also uses it when moving focus in cell  after toggle and it causes timing issues. Re-working test so that it checks if rows fill the available space.

--- projects/igniteui-angular/src/lib/directives/for-of/for_of.directive.ts
+++ projects/igniteui-angular/src/lib/directives/for-of/for_of.directive.ts
@@ -1269,11 +1269,9 @@ export class IgxGridForOfDirective<T> extends IgxForOfDirective<T> implements On
         // if data has been changed while container is scrolled
         // should update scroll top/left according to change so that same startIndex is in view
         if (Math.abs(diff) > 0 && scr.scrollTop > 0) {
-            requestAnimationFrame(() => {
                 this.recalcUpdateSizes();
                 const offset = parseInt(this.dc.instance._viewContainer.element.nativeElement.style.top, 10);
                 scr.scrollTop = this.sizesCache[this.state.startIndex] - offset;
-            });
         }
     }
 
