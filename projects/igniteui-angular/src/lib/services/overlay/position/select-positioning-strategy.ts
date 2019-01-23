78af0ba2a3b48fbfe0564af548816cae379be368
ViktorSlavov <viktorslavov91@gmail.com>
2019-01-23 10:47:05 +0200

    chore(*): refactor pseudocode for positioning strat

--- projects/igniteui-angular/src/lib/services/overlay/position/select-positioning-strategy.ts
+++ projects/igniteui-angular/src/lib/services/overlay/position/select-positioning-strategy.ts
@@ -39,41 +39,50 @@ export class SelectPositioningStrategy extends ConnectedPositioningStrategy impl
         // OR item is just focused
         /**
          * // Get drop-down height
-         * const listHeight = contentElement.getBoundingClientRect().height
+         * const LIST_HEIGHT = contentElement.getBoundingClientRect().height;
          * // Get how much space there is between input and window bounds (top / bottom)
          * // Container position
-         * const containerTop = startPoint.y;
+         * const CONTAINER: {
+         *  TOP: number,
+         *  BOTTOM: number
+         * } = {
+         *  TOP: startPoint.y;
+         *  BOTTOM: select.inputGroup.element.nativeElement.getBoundingClientRect().bottom;
+         * }
+         * const CONTAINER.TOP = startPoint.y;
          * const containerBottom = select.inputGroup.element.nativeElement.getBoundingClientRect().bottom;
          * // Document bounds
-         * const bounds = {
-         *  top: document.documentElement.top,
-         *  bottom: document.documentElement.bottom
+         * const BOUNDS: {
+         *  TOP: number,
+         *  BOTTOM: number
+         * } = {
+         *  TOP: document.documentElement.top,
+         *  BOTTOM: document.documentElement.bottom
          * }
-         * const remainingTop = bounds.top - startPoint.y;
-         * const remainingBottom = bounds.bottom - containerBottom;
+         * const REMAINING_TOP = CONTAINER.TOP - BOUNDS.TOP;
+         * const remainingBottom = BOUNDS.BOTTOM - CONTAINER.BOTTOM;
          */
         // The current dropdown state will be - item is scrolled to the middle (if scrollbar)
         // OR item is just focused
         // ASSUME SAME ITEM HEIGHT
         // ASSUME MAX 5 ITEMS PER DD "SCREEN"
         /**
-        * // Position item to be on top of the input (startY -= index_of_item * item_height)
+        * // Position item to be on top of the input (START.Y -= ITEM_VISIBLE_INDEX * ITEM_HEIGHT)
         * // This should maybe be recursive?
         * let CURRENT_POSITION_Y = START.Y - ITEM_VISIBLE_INDEX * ITEM_HEIGHT;
-        * const OUT_OF_BOUNDS = is list out of bonds either top OR bottom; IF NOT => NULL
+        * const OUT_OF_BOUNDS: {
+        *   DIRECTION: ENUM_DIRECTION,
+        *   AMOUNT: number
+        * } = is list out of bounds either top OR bottom; IF NOT => NULL
         * IF (OUT_OF_BOUNDS) {
         *   let CONTAINER_CAN_BE_SCROLLED = 0;
-        *   const NUMBER_OF_ITEMS_NEEDED = OUT_OF_BOUNDS.AMMOUNT / ITEM_HEIGHT;
-        *   IF (OUT_OF_BOUNDS.DIRECTION === 'TOP') {
-        *       const NUMBER_OF_ITEMS_OUT_OF_VIEW = remaining items until the end of the list;
-        *       IF (NUMBER_OF_ITEMS_OUT_OF_VIEW >= NUMBER_OF_ITEMS_NEEDED) {
-        *           CONTAINER_CAN_BE_SCROLLED = -1 * NUBMER_OF_ITEMS_NEEDED * ITEM_HEIGHT;
-        *       }
-        *   } ELSE IF (OUT_OF_BOUNDS.DIRECTION === 'BOTTOM') {
-        *      const NUMBER_OF_ITEMS_OUT_OF_VIEW = remaining items until the start of the list;
-        *       IF (NUMBER_OF_ITEMS_OUT_OF_VIEW >= NUMBER_OF_ITEMS_NEEDED) {
-        *           CONTAINER_CAN_BE_SCROLLED = 1 * NUBMER_OF_ITEMS_NEEDED * ITEM_HEIGHT;
-        *       }
+        *   const NUMBER_OF_ITEMS_NEEDED = OUT_OF_BOUNDS.AMOUNT / ITEM_HEIGHT;
+        *   const NUMBER_OF_ITEMS_OUT_OF_VIEW: {
+        *       TOP: number,
+        *       BOTTOM: number
+        *   } = remaining items before and after the current view
+        *   IF (NUMBER_OF_ITEMS_OUT_OF_VIEW[OUT_OF_BOUNDS.DIRECTION] >= NUMBER_OF_ITEMS_NEEDED) {
+        *        CONTAINER_CAN_BE_SCROLLED = (OUT_OF_BOUNDS.DIRECTION === 'TOP' ? -1 : 1) * NUBMER_OF_ITEMS_NEEDED * ITEM_HEIGHT;
         *   }
         *   IF (CONTAINER_CAN_BE_SCROLLED) {
         *       CONTAINER_SCROLL_TOP += CONTAINER_CAN_BE_SCROLLED;
