69ecf47399d6e4ad953211bf24a6b22ef4d3040c
gedinakova <gedinakova@infragistics.com>
2019-02-01 19:43:44 +0200

    test(Overlay): #3730 Fixing failing tests.

--- projects/igniteui-angular/src/lib/services/overlay/overlay.spec.ts
+++ projects/igniteui-angular/src/lib/services/overlay/overlay.spec.ts
@@ -1314,7 +1314,6 @@ describe('igxOverlay', () => {
             const overlay = fixture.componentInstance.overlay;
             overlay.show(SimpleDynamicComponent, overlaySettings);
             tick();
-
             expect(document.documentElement.scrollTop).toEqual(0);
             let overlayElement = document.getElementsByClassName(CLASS_OVERLAY_CONTENT)[0] as HTMLElement;
             let overlayChildPosition: DOMRect = overlayElement.lastElementChild.getBoundingClientRect() as DOMRect;
@@ -2363,7 +2362,7 @@ describe('igxOverlay', () => {
             tick();
             const styles = css(overlayWrapper);
             const expectedBackgroundColor = 'background-color: rgba(0, 0, 0, 0.38)';
-            const appliedBackgroundStyles = styles[3];
+            const appliedBackgroundStyles = styles[2];
             expect(appliedBackgroundStyles).toContain(expectedBackgroundColor);
         }));
 
