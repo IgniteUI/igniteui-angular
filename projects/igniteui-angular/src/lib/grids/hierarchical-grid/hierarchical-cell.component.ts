b9d0b31f59635923d626b1141ba5a09e956b5451
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2019-02-15 15:19:17 +0200

    feat(grid): Hierarchical navigation and selection
    Closes #3915

--- projects/igniteui-angular/src/lib/grids/hierarchical-grid/hierarchical-cell.component.ts
+++ projects/igniteui-angular/src/lib/grids/hierarchical-grid/hierarchical-cell.component.ts
@@ -1,21 +1,22 @@
 import { IgxGridCellComponent } from '../cell.component';
 import { GridBaseAPIService } from '../api.service';
-import { IgxHierarchicalGridAPIService } from './hierarchical-grid-api.service';
 import { ChangeDetectorRef, ElementRef, ChangeDetectionStrategy, Component,
-     OnInit, AfterViewInit, forwardRef, HostListener, NgZone } from '@angular/core';
+     OnInit, HostListener, NgZone, Input } from '@angular/core';
 import { IgxHierarchicalGridComponent } from './hierarchical-grid.component';
 import { IgxHierarchicalSelectionAPIService } from './selection';
 import { IgxGridSelectionService, IgxGridCRUDService } from '../../core/grid-selection';
 
 @Component({
-    changeDetection: ChangeDetectionStrategy.Default,
+    changeDetection: ChangeDetectionStrategy.OnPush,
     preserveWhitespaces: false,
     selector: 'igx-hierarchical-grid-cell',
     templateUrl: './../cell.component.html'
 })
 export class IgxHierarchicalGridCellComponent extends IgxGridCellComponent implements OnInit {
+
     protected hSelection;
     protected _rootGrid;
+
     constructor(
         protected selectionService: IgxGridSelectionService,
         protected crudService: IgxGridCRUDService,
@@ -33,6 +34,7 @@ export class IgxHierarchicalGridCellComponent extends IgxGridCellComponent imple
         super.ngOnInit();
         this._rootGrid = this._getRootGrid();
     }
+
     private _getRootGrid() {
         let currGrid = this.grid;
         while (currGrid.parent) {
@@ -40,75 +42,110 @@ export class IgxHierarchicalGridCellComponent extends IgxGridCellComponent imple
         }
         return currGrid;
     }
-    protected _saveCellSelection(newSelection?: Set<any>) {
-        if (!newSelection) {
-            this.hSelection.add_sub_item(this._rootGrid.id, this.grid.id, this);
 
-            const currentElement = this.grid.nativeElement;
-            let parentGrid = this.grid;
-            let childGrid;
-            // add highligh to the current grid
-            if (this._rootGrid.id !== currentElement.id) {
-                currentElement.classList.add('igx-grid__tr--highlighted');
-            }
+    // TODO: Extend the new selection service to avoid complete traversal
+    _clearAllHighlights() {
+        [this._rootGrid, ...this._rootGrid.getChildGrids(true)].forEach(grid => {
+            grid.selectionService.clear();
+            grid.nativeElement.classList.remove('igx-grid__tr--highlighted');
+            grid.highlightedRowID = null;
+            grid.cdr.markForCheck();
+        });
+    }
 
-            // add highligh to the current grid
-            while (this._rootGrid.id !== parentGrid.id) {
-                childGrid = parentGrid;
-                parentGrid = parentGrid.parent;
-
-                const parentRowID = parentGrid.hgridAPI.getParentRowId(childGrid);
-                parentGrid.highlightedRowID = parentRowID;
-            }
+    _updateCellSelectionStatus() {
+        this._clearAllHighlights();
+        const currentElement = this.grid.nativeElement;
+        let parentGrid = this.grid;
+        let childGrid;
+        // add highligh to the current grid
+        if (this._rootGrid.id !== currentElement.id) {
+            currentElement.classList.add('igx-grid__tr--highlighted');
         }
+
+        // add highligh to the current grid
+        while (this._rootGrid.id !== parentGrid.id) {
+            childGrid = parentGrid;
+            parentGrid = parentGrid.parent;
+
+            const parentRowID = parentGrid.hgridAPI.getParentRowId(childGrid);
+            parentGrid.highlightedRowID = parentRowID;
+        }
+        super._updateCellSelectionStatus();
     }
 
-    public isCellSelected() {
-        const selection = this.hSelection.get_sub_item(this._rootGrid.id);
-        const isSelected = selection ? selection.gridID === this.grid.id : false;
-        return  super.isCellSelected() && isSelected;
-    }
+    // TODO: Remove
+    // protected _saveCellSelection(newSelection?: Set<any>) {
+    //     if (!newSelection) {
+    //         this.hSelection.add_sub_item(this._rootGrid.id, this.grid.id, this);
+
+    //         const currentElement = this.grid.nativeElement;
+    //         let parentGrid = this.grid;
+    //         let childGrid;
+    //         // add highligh to the current grid
+    //         if (this._rootGrid.id !== currentElement.id) {
+    //             currentElement.classList.add('igx-grid__tr--highlighted');
+    //         }
+
+    //         // add highligh to the current grid
+    //         while (this._rootGrid.id !== parentGrid.id) {
+    //             childGrid = parentGrid;
+    //             parentGrid = parentGrid.parent;
+
+    //             const parentRowID = parentGrid.hgridAPI.getParentRowId(childGrid);
+    //             parentGrid.highlightedRowID = parentRowID;
+    //         }
+    //     }
+    // }
+
+    // TODO: Remove
+    // public isCellSelected() {
+    //     const selection = this.hSelection.get_sub_item(this._rootGrid.id);
+    //     const isSelected = selection ? selection.gridID === this.grid.id : false;
+    //     return  super.isCellSelected() && isSelected;
+    // }
 
     // TODO: Refactor
     @HostListener('keydown', ['$event'])
     dispatchEvent(event: KeyboardEvent) {
-        // const key = event.key.toLowerCase();
-        // if (event.altKey) {
-        //     const grid = this.gridAPI.get(this.grid.id);
-        //     const state = this.gridAPI.get(this.grid.id).hierarchicalState;
-        //     const collapse = this.row.expanded && (key === 'left' || key === 'arrowleft' || key === 'up' || key === 'arrowup');
-        //     const expand = !this.row.expanded && (key === 'right' || key === 'arrowright' || key === 'down' || key === 'arrowdown');
-        //     if (collapse) {
-        //         grid.hierarchicalState = state.filter(v => {
-        //             return v.rowID !== this.row.rowID;
-        //         });
-        //     } else if (expand) {
-        //         state.push({ rowID: this.row.rowID });
-        //         grid.hierarchicalState = [...state];
-        //     }
-        //     return;
-        // }
+        const key = event.key.toLowerCase();
+        if (event.altKey) {
+            const grid = this.gridAPI.grid;
+            const state = this.gridAPI.grid.hierarchicalState;
+            const collapse = this.row.expanded && (key === 'left' || key === 'arrowleft' || key === 'up' || key === 'arrowup');
+            const expand = !this.row.expanded && (key === 'right' || key === 'arrowright' || key === 'down' || key === 'arrowdown');
+            if (collapse) {
+                grid.hierarchicalState = state.filter(v => {
+                    return v.rowID !== this.row.rowID;
+                });
+            } else if (expand) {
+                state.push({ rowID: this.row.rowID });
+                grid.hierarchicalState = [...state];
+            }
+            return;
+        }
         super.dispatchEvent(event);
     }
 
-    protected _clearCellSelection() {
-        const sel = this.hSelection.get_sub_item(this._rootGrid.id);
-        if (sel) {
-            let selectedGrid = sel.cell.grid;
-            const currentElement = selectedGrid.nativeElement;
-            // remove highligh from the current grid
-            if (this._rootGrid.id !== currentElement.id) {
-                currentElement.classList.remove('igx-grid__tr--highlighted');
-            }
+    // TODO: Remove
+    // protected _clearCellSelection() {
+    //     const sel = this.hSelection.get_sub_item(this._rootGrid.id);
+    //     if (sel) {
+    //         let selectedGrid = sel.cell.grid;
+    //         const currentElement = selectedGrid.nativeElement;
+    //         // remove highligh from the current grid
+    //         if (this._rootGrid.id !== currentElement.id) {
+    //             currentElement.classList.remove('igx-grid__tr--highlighted');
+    //         }
 
-            // remove highligh from all the parent rows
-            while (this._rootGrid.id !== selectedGrid.id) {
-                selectedGrid = selectedGrid.parent;
-                selectedGrid.highlightedRowID = null;
-            }
+    //         // remove highligh from all the parent rows
+    //         while (this._rootGrid.id !== selectedGrid.id) {
+    //             selectedGrid = selectedGrid.parent;
+    //             selectedGrid.highlightedRowID = null;
+    //         }
 
-            this.hSelection.clear_sub_item(this._rootGrid.id);
-            sel.cell.cdr.markForCheck();
-        }
-    }
+    //         this.hSelection.clear_sub_item(this._rootGrid.id);
+    //         sel.cell.cdr.markForCheck();
+    //     }
+    // }
 }
