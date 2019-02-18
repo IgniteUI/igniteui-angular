c014ca9feccabb8d4ddeab16f309b78565d5cbb9
Galina Edinakova <gedinakova@infragistics.com>
2019-02-18 17:35:43 +0200

    fix(Input): #3641 Remove the '' from input value setter. (#3860)

--- projects/igniteui-angular/src/lib/date-picker/date-picker.component.ts
+++ projects/igniteui-angular/src/lib/date-picker/date-picker.component.ts
@@ -381,6 +381,7 @@ export class IgxDatePickerComponent implements ControlValueAccessor, EditorProvi
         if (this.value) {
             return this._customFormatChecker(this.formatter, this.value);
         }
+        return '';
     }
 
     /**
