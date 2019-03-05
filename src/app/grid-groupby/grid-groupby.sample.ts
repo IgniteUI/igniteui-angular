7d520d66c565583e0c50ac05dbaedc6c788b7b0c
Konstantin Dinev <kdinev@mail.bw.edu>
2019-03-05 17:07:26 +0200

    chore(*): Changing back the import for the groupby sample

--- src/app/grid-groupby/grid-groupby.sample.ts
+++ src/app/grid-groupby/grid-groupby.sample.ts
@@ -1,9 +1,11 @@
 import { Component, ViewChild, OnInit, Inject } from '@angular/core';
 
+import { IgxGridComponent,
+    SortingDirection,
+    ISortingExpression,
+    IGridFocusChangeEventArgs,
+    DefaultSortingStrategy } from 'igniteui-angular';
 import { DisplayDensity, IDisplayDensityOptions, DisplayDensityToken } from 'projects/igniteui-angular/src/lib/core/density';
-import { IgxGridComponent, IGridFocusChangeEventArgs } from 'projects/igniteui-angular/src/lib/grids/grid/grid.component';
-import { ISortingExpression, SortingDirection } from 'projects/igniteui-angular/src/lib/data-operations/sorting-expression.interface';
-import { DefaultSortingStrategy } from 'projects/igniteui-angular/src/lib/data-operations/sorting-strategy';
 @Component({
     providers: [{ provide: DisplayDensityToken, useValue: { displayDensity: DisplayDensity.compact} }],
     selector: 'app-grid-sample',
