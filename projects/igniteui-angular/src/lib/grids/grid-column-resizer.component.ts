d841f50eb0a915d69a7c75f4c258ae34e19726dc
Stefana Andreeva <SAndreeva@infragistics.com>
2019-03-14 17:33:34 +0200

    Refactor grid resize line positioning and display (#4231)
    * refactor(resizing): resize line positioning #3908
    
    * chore(*): fix failing tests #3908
    
    * chore(*): prevent text selection on drag in IE and Edge #3908
    
    * chore(*): code cleanup, small fixes #4231

---
+++ projects/igniteui-angular/src/lib/grids/grid-column-resizer.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+import { IgxColumnResizingService } from './grid-column-resizing.service';
+import { IgxColumnResizerDirective } from './grid.common';
+
+@Component({
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    preserveWhitespaces: false,
+    selector: 'igx-grid-column-resizer',
+    templateUrl: './grid-column-resizer.component.html'
+})
+export class IgxGridColumnResizerComponent {
+    constructor(public colResizingService: IgxColumnResizingService) { }
+
+    @ViewChild(IgxColumnResizerDirective)
+    public resizer: IgxColumnResizerDirective;
+}
