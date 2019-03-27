0e7bea149938c36e94282fd9362bfdf6dc8939b7
Stamen Stoychev <chronos.stz@gmail.com>
2019-03-27 18:40:51 +0200

    chore(*): pushing down DI to rows for MCH scenario

--- projects/igniteui-angular/src/lib/grids/grid/grid-row.component.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid-row.component.ts
@@ -3,13 +3,14 @@ import { IgxGridComponent } from './grid.component';
 import { IgxRowComponent } from '../row.component';
 import { GridBaseAPIService } from '../api.service';
 import { IgxSelectionAPIService } from '../../core/selection';
+import { IgxForOfSyncService } from '../../directives/for-of/for_of.sync.service';
 
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
     preserveWhitespaces: false,
     selector: 'igx-grid-row',
     templateUrl: './grid-row.component.html',
-    providers: [{provide: IgxRowComponent, useExisting: forwardRef(() => IgxGridRowComponent)}]
+    providers: [{provide: IgxRowComponent, useExisting: forwardRef(() => IgxGridRowComponent)}, IgxForOfSyncService]
 })
 export class IgxGridRowComponent extends IgxRowComponent<IgxGridComponent> {
     constructor(
