c08ec8ee2d820ba3878d6670dec7bbc28f94a5d5
gedinakova <gedinakova@infragistics.com>
2019-04-03 17:26:31 +0300

    fix(Filter): #4434 Fixed Date pipe error in IE11.

--- projects/igniteui-angular/src/lib/date-picker/date-picker.component.spec.ts
+++ projects/igniteui-angular/src/lib/date-picker/date-picker.component.spec.ts
@@ -47,6 +47,7 @@ describe('IgxDatePicker', () => {
         it('Initialize a datepicker component', () => {
              expect(fixture.componentInstance).toBeDefined();
             expect(datePicker.displayData).toBe('');
+            expect(datePicker.displayDataISO).toBe('');
         });
 
         it('Initialize a datepicker component with id', () => {
@@ -245,6 +246,7 @@ describe('IgxDatePicker', () => {
         const dateConvertedToDeLocale = fixture.componentInstance.date.toLocaleDateString('de-DE');
 
         expect(datePicker.displayData).toBe(dateConvertedToDeLocale);
+        expect(datePicker.displayDataISO).toBe(fixture.componentInstance.date.toISOString());
     });
 
     it('Datepicker custom formatter', () => {
@@ -259,6 +261,7 @@ describe('IgxDatePicker', () => {
         const formattedDate = compInstance.customFormatter(date);
 
         expect(inputTarget.value).toEqual(formattedDate);
+        expect(datePicker.displayDataISO).toBe(date.toISOString());
     });
 
     it('Value should respond when is bound through ngModel and selection through selectDate method is made.', fakeAsync(() => {
@@ -345,7 +348,7 @@ describe('IgxDatePicker', () => {
 
         target.nativeElement.dispatchEvent(new Event('click', { bubbles: true }));
         fixture.detectChanges();
-        await wait();
+        await wait(16);
 
         const todayDate = datePicker.calendar.dates.find(d => d.isToday);
         expect(document.activeElement).toEqual(todayDate.nativeElement);
