a9681f9bd13c1938e16959b738f724f63645372e
Simeon Simeonoff <sim.simeonoff@gmail.com>
2019-04-05 17:52:11 +0300

    test(card): add initial card tests

--- projects/igniteui-angular/src/lib/card/card.spec.ts
+++ projects/igniteui-angular/src/lib/card/card.spec.ts
@@ -13,12 +13,13 @@ import {
     IgxCardHeaderTitleDirective,
     IgxCardHeaderSubtitleDirective,
     IgxCardActionsComponent,
+    IgxCardMediaDirective,
 } from './card.component';
 
 import { IgxButtonModule } from '../directives/button/button.directive';
 import { IgxIconModule } from '../icon/index';
 
-fdescribe('Card', () => {
+describe('Card', () => {
     const baseClass = 'igx-card';
 
     const classes = {
@@ -50,13 +51,17 @@ fdescribe('Card', () => {
             get justify() {
                 return `${this.base}--justify`;
             },
+            get reverse() {
+                return `${this.base}--reverse`;
+            },
             get icons() {
                 return `${this.base}__icons`;
             },
             get buttons() {
                 return `${this.base}__buttons`;
             }
-        }
+        },
+        media: `${baseClass}__media`
     };
 
     beforeEach(async(() => {
@@ -64,9 +69,9 @@ fdescribe('Card', () => {
             declarations: [
                 InitCardComponent,
                 InitOutlinedCardComponent,
-                InitHorizontalCardComponent,
                 CardWithHeaderComponent,
-                FullCardComponent
+                VerticalCardComponent,
+                HorizontalCardComponent
             ],
             imports: [
                 IgxCardModule,
@@ -85,9 +90,9 @@ fdescribe('Card', () => {
         expect(card).toBeDefined();
         expect(card.getAttribute('role')).toEqual('group');
 
-        expect(card.classList).toContain(`${baseClass}`);
-        expect(card.classList).not.toContain(classes.outlined);
-        expect(card.classList).not.toContain(classes.horizontal);
+        expect(card).toHaveClass(`${baseClass}`);
+        expect(card).not.toHaveClass(classes.outlined);
+        expect(card).not.toHaveClass(classes.horizontal);
         expect(card.id).toContain(`${baseClass}-`);
     });
 
@@ -99,19 +104,19 @@ fdescribe('Card', () => {
         const card = fixture.debugElement.query(By.css(baseClass)).nativeElement;
 
         expect(instance.type).toEqual(IgxCardType.OUTLINED);
-        expect(card.classList).toContain(classes.outlined);
-        expect(card.classList).not.toContain(classes.horizontal);
+        expect(card).toHaveClass(classes.outlined);
+        expect(card).not.toHaveClass(classes.horizontal);
     });
 
     it('Initializes horizontal card', () => {
-        const fixture = TestBed.createComponent(InitHorizontalCardComponent);
+        const fixture = TestBed.createComponent(HorizontalCardComponent);
         fixture.detectChanges();
 
         const instance = fixture.componentInstance.card;
         const card = fixture.debugElement.query(By.css(baseClass)).nativeElement;
 
         expect(instance.horizontal).toEqual(true);
-        expect(card.classList).toContain(classes.horizontal);
+        expect(card).toHaveClass(classes.horizontal);
     });
 
     it('Initializes card header', () => {
@@ -123,20 +128,20 @@ fdescribe('Card', () => {
         expect(header).toBeDefined();
         expect(header.getAttribute('role')).toEqual('header');
 
-        expect(header.classList).toContain(classes.header.base);
-        expect(header.classList).not.toContain(classes.header.vertical);
+        expect(header).toHaveClass(classes.header.base);
+        expect(header).not.toHaveClass(classes.header.vertical);
     });
 
     it('Initializes vertical card header', () => {
-        const fixture = TestBed.createComponent(FullCardComponent);
+        const fixture = TestBed.createComponent(VerticalCardComponent);
         fixture.detectChanges();
 
         const header = fixture.debugElement.query(By.css('igx-card-header')).nativeElement;
-        expect(header.classList).toContain(classes.header.vertical);
+        expect(header).toHaveClass(classes.header.vertical);
     });
 
     it('Initializes title, subtitle, and thumb in header', () => {
-        const fixture = TestBed.createComponent(FullCardComponent);
+        const fixture = TestBed.createComponent(VerticalCardComponent);
         fixture.detectChanges();
 
         const thumb = fixture.debugElement.query(By.directive(IgxCardThumbnailDirective));
@@ -144,15 +149,15 @@ fdescribe('Card', () => {
         const subtitle = fixture.debugElement.query(By.directive(IgxCardHeaderSubtitleDirective));
 
         // Check to see if thumbnail has been inserted in the thumbnail section;
-        expect(thumb.parent.nativeElement.classList).toContain(classes.header.thumb);
+        expect(thumb.parent.nativeElement).toHaveClass(classes.header.thumb);
 
         // Check to see if the title and subtitle have been
         // inserted in the titles section;
-        expect(title.parent.nativeElement.classList).toContain(classes.header.titles);
-        expect(subtitle.parent.nativeElement.classList).toContain(classes.header.titles);
+        expect(title.parent.nativeElement).toHaveClass(classes.header.titles);
+        expect(subtitle.parent.nativeElement).toHaveClass(classes.header.titles);
 
-        expect(title.nativeElement.classList).toContain(classes.header.title);
-        expect(subtitle.nativeElement.classList).toContain(classes.header.subtitle);
+        expect(title.nativeElement).toHaveClass(classes.header.title);
+        expect(subtitle.nativeElement).toHaveClass(classes.header.subtitle);
 
         // Validate Content
         expect(thumb.nativeElement.textContent).toEqual('Thumb');
@@ -161,7 +166,7 @@ fdescribe('Card', () => {
     });
 
     it('Initializes content', () => {
-        const fixture = TestBed.createComponent(FullCardComponent);
+        const fixture = TestBed.createComponent(VerticalCardComponent);
         fixture.detectChanges();
 
         const content = fixture.debugElement.query(By.css('igx-card-content')).nativeElement;
@@ -170,32 +175,89 @@ fdescribe('Card', () => {
         expect(content.textContent).toEqual('Test Content');
     });
 
+    it('Initializes card media', () => {
+        const fixture = TestBed.createComponent(VerticalCardComponent);
+        fixture.detectChanges();
+
+        const media = fixture.debugElement.query(By.css('igx-card-media'));
+        const mediaContent = media.query(By.css('div')).nativeElement;
+
+        expect(media).toBeDefined();
+        expect(mediaContent.textContent).toEqual('media');
+        expect(media.nativeElement).toHaveClass(classes.media);
+    });
+
     it('Initializes actions with buttons', () => {
-        const fixture = TestBed.createComponent(FullCardComponent);
+        const fixture = TestBed.createComponent(VerticalCardComponent);
         fixture.detectChanges();
 
         const actions = fixture.debugElement.query(By.css('igx-card-actions')).nativeElement;
 
         expect(actions).toBeDefined();
-        expect(actions.classList).toContain(classes.actions.base);
-        expect(actions.classList).not.toContain(classes.actions.justify);
-        expect(actions.classList).not.toContain(classes.actions.vertical);
+        expect(actions).toHaveClass(classes.actions.base);
+        expect(actions).not.toHaveClass(classes.actions.justify);
+        expect(actions).not.toHaveClass(classes.actions.vertical);
     });
 
-    it('Should align actions buttons vertically when card uses horizontal layout', () => {
-        const fixture = TestBed.createComponent(FullCardComponent);
+    it('Should automatically align actions vertically when in horizontal layout', () => {
+        const fixture = TestBed.createComponent(HorizontalCardComponent);
         fixture.detectChanges();
 
-        const cardInstance = fixture.componentInstance.card;
         const actionsInstance = fixture.componentInstance.actions;
         const actionsElement = fixture.debugElement.query(By.css('igx-card-actions')).nativeElement;
 
-        cardInstance.horizontal = true;
+        expect(actionsInstance.vertical).toEqual(true);
+        expect(actionsElement).toHaveClass(classes.actions.vertical);
+    });
+
+    it('Should align actions horizontally and vertically when explicitly set', () => {
+        const fixture = TestBed.createComponent(HorizontalCardComponent);
         fixture.detectChanges();
 
-        expect(cardInstance.horizontal).toEqual(true);
-        // expect(actionsInstance.vertical).toEqual(true);
-        expect(actionsElement.classList).toContain(classes.actions.vertical);
+        const actionsInstance = fixture.componentInstance.actions;
+        const actionsElement = fixture.debugElement.query(By.css('igx-card-actions')).nativeElement;
+
+        actionsInstance.vertical = false;
+        fixture.detectChanges();
+
+        expect(actionsInstance.vertical).toEqual(false);
+        expect(actionsElement).not.toHaveClass(classes.actions.vertical);
+    });
+
+    it('Should display icon buttons after regular buttons by default', () => {
+        const fixture = TestBed.createComponent(HorizontalCardComponent);
+        fixture.detectChanges();
+
+        const actionsElement = fixture.debugElement.query(By.css('igx-card-actions'));
+
+        const buttons = actionsElement.query(By.css(`.${classes.actions.buttons}`)).nativeElement;
+        const icons = actionsElement.query(By.css(`.${classes.actions.icons}`)).nativeElement;
+
+        const buttonsOrder = window.getComputedStyle(buttons).getPropertyValue('order');
+        const iconsOrder = window.getComputedStyle(icons).getPropertyValue('order');
+
+        expect(actionsElement.nativeElement).not.toHaveClass(classes.actions.reverse);
+        expect(parseInt(buttonsOrder, 10)).toBeLessThan(parseInt(iconsOrder, 10));
+    });
+
+    it('Actions should display icon buttons before regular buttons with reverse set to true', () => {
+        const fixture = TestBed.createComponent(HorizontalCardComponent);
+        fixture.detectChanges();
+
+        const actionsInstance = fixture.componentInstance.actions;
+        const actionsElement = fixture.debugElement.query(By.css('igx-card-actions'));
+
+        const buttons = actionsElement.query(By.css(`.${classes.actions.buttons}`)).nativeElement;
+        const icons = actionsElement.query(By.css(`.${classes.actions.icons}`)).nativeElement;
+
+        actionsInstance.reverse = true;
+        fixture.detectChanges();
+
+        const buttonsOrder = window.getComputedStyle(buttons).getPropertyValue('order');
+        const iconsOrder = window.getComputedStyle(icons).getPropertyValue('order');
+
+        expect(actionsElement.nativeElement).toHaveClass(classes.actions.reverse);
+        expect(parseInt(buttonsOrder, 10)).toBeGreaterThan(parseInt(iconsOrder, 10));
     });
 });
 
@@ -213,14 +275,6 @@ class InitOutlinedCardComponent {
 }
 
 @Component({
-    template: `<igx-card [horizontal]="true"></igx-card>`
-})
-class InitHorizontalCardComponent {
-    @ViewChild(IgxCardComponent)
-    public card: IgxCardComponent;
-}
-
-@Component({
     template: `<igx-card>
         <igx-card-header></igx-card-header>
     <igx-card>`
@@ -228,7 +282,7 @@ class InitHorizontalCardComponent {
 class CardWithHeaderComponent { }
 
 @Component({
-    template: `<igx-card #card>
+    template: `<igx-card>
         <igx-card-media width="200px" height="50%">
             <div>media</div>
         <igx-card-media>
@@ -241,7 +295,7 @@ class CardWithHeaderComponent { }
 
         <igx-card-content>Test Content</igx-card-content>
 
-        <igx-card-actions #actions>
+        <igx-card-actions>
             <button igxButton>Test</button>
             <button igxButton="icon">
                 <igx-icon>home</igx-icon>
@@ -249,7 +303,21 @@ class CardWithHeaderComponent { }
         </igx-card-actions>
     <igx-card>`
 })
-class FullCardComponent {
-    @ViewChild('card') public card: IgxCardComponent;
-    @ViewChild('actions') public actions: IgxCardActionsComponent;
+class VerticalCardComponent {
+    @ViewChild(IgxCardMediaDirective) public media: IgxCardMediaDirective;
+}
+
+@Component({
+    template: `<igx-card [horizontal]="true">
+        <igx-card-actions>
+            <button igxButton>Test</button>
+            <button igxButton="icon">
+                <igx-icon>home</igx-icon>
+            </button>
+        </igx-card-actions>
+    </igx-card>`
+})
+class HorizontalCardComponent {
+    @ViewChild(IgxCardComponent) public card: IgxCardComponent;
+    @ViewChild(IgxCardActionsComponent) public actions: IgxCardActionsComponent;
 }
