042806f5d6eb22ea73c44b7dd5d6e22054c0215d
MKirova <MKirova@DEV-MKIROVA>
2019-04-15 18:37:29 +0300

    chore(*): Adding a few more test scenarios.

--- projects/igniteui-angular/src/lib/grids/grid/grid.multi-row-layout.integration.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid.multi-row-layout.integration.spec.ts
@@ -7,6 +7,8 @@ import { IgxGridModule } from './grid.module';
 import { SampleTestData } from '../../test-utils/sample-test-data.spec';
 import { ViewChild, Component } from '@angular/core';
 import { verifyLayoutHeadersAreAligned, verifyDOMMatchesLayoutSettings } from '../../test-utils/helper-utils.spec';
+import { IgxColumnLayoutComponent } from './../column.component';
+import { wait } from '../../test-utils/ui-interactions.spec';
 
 describe('IgxGrid - multi-row-layout Integration - ', () => {
     configureTestSuite();
@@ -41,7 +43,7 @@ describe('IgxGrid - multi-row-layout Integration - ', () => {
             expect(grid.getColumnByName('ID').pinned).toBeFalsy();
             expect(grid.getColumnByName('CompanyName').pinned).toBeFalsy();
             expect(grid.getColumnByName('ContactName').pinned).toBeFalsy();
-            expect(grid.getColumnByName('ContactName').pinned).toBeFalsy();
+            expect(grid.getColumnByName('ContactTitle').pinned).toBeFalsy();
 
             const gridFirstRow = grid.rowList.first;
             const firstRowCells = gridFirstRow.cells.toArray();
@@ -50,20 +52,250 @@ describe('IgxGrid - multi-row-layout Integration - ', () => {
             // headers are aligned to cells
             verifyLayoutHeadersAreAligned(headerCells, firstRowCells);
 
-            // verifyDOMMatchesLayoutSettings(gridFirstRow, fixture.componentInstance.colGroups);
+            verifyDOMMatchesLayoutSettings(gridFirstRow, fixture.componentInstance.colGroups);
+
+            // unpin group
+            fixture.componentInstance.colGroups[0].pinned = false;
+            fixture.detectChanges();
+
+            expect(grid.getColumnByName('PostalCode').pinned).toBeFalsy();
+            expect(grid.getColumnByName('City').pinned).toBeFalsy();
+            expect(grid.getColumnByName('Country').pinned).toBeFalsy();
+            expect(grid.getColumnByName('Address').pinned).toBeFalsy();
+
+            expect(grid.getColumnByName('ID').pinned).toBeFalsy();
+            expect(grid.getColumnByName('CompanyName').pinned).toBeFalsy();
+            expect(grid.getColumnByName('ContactName').pinned).toBeFalsy();
+            expect(grid.getColumnByName('ContactTitle').pinned).toBeFalsy();
+
+            // headers are aligned to cells
+            verifyLayoutHeadersAreAligned(headerCells, firstRowCells);
+
+            verifyDOMMatchesLayoutSettings(gridFirstRow, fixture.componentInstance.colGroups);
+
+            // pin the other group
+            fixture.componentInstance.colGroups[1].pinned = true;
+            fixture.detectChanges();
+
+            expect(grid.getColumnByName('PostalCode').pinned).toBeFalsy();
+            expect(grid.getColumnByName('City').pinned).toBeFalsy();
+            expect(grid.getColumnByName('Country').pinned).toBeFalsy();
+            expect(grid.getColumnByName('Address').pinned).toBeFalsy();
+
+            expect(grid.getColumnByName('ID').pinned).toBeTruthy();
+            expect(grid.getColumnByName('CompanyName').pinned).toBeTruthy();
+            expect(grid.getColumnByName('ContactName').pinned).toBeTruthy();
+            expect(grid.getColumnByName('ContactTitle').pinned).toBeTruthy();
+
         });
-        it('should pin/unpin whole group if a single child column is pinned/unpinned.', () => {});
-        it('should not allow pinning if group width exceeds max allowed.', () => {});
-        it('should work with horizontal virtualization on the unpinned groups.', () => {});
+
+        it('should pin/unpin whole group if a single child column is pinned/unpinned.', () => {
+            // group 1 should be pinned - all child columns should be pinned
+            expect(grid.getColumnByName('PostalCode').pinned).toBeTruthy();
+            expect(grid.getColumnByName('City').pinned).toBeTruthy();
+            expect(grid.getColumnByName('Country').pinned).toBeTruthy();
+            expect(grid.getColumnByName('Address').pinned).toBeTruthy();
+
+            expect(grid.getColumnByName('ID').pinned).toBeFalsy();
+            expect(grid.getColumnByName('CompanyName').pinned).toBeFalsy();
+            expect(grid.getColumnByName('ContactName').pinned).toBeFalsy();
+            expect(grid.getColumnByName('ContactTitle').pinned).toBeFalsy();
+
+
+            grid.unpinColumn('City');
+            fixture.detectChanges();
+
+            expect(grid.getColumnByName('PostalCode').pinned).toBeFalsy();
+            expect(grid.getColumnByName('City').pinned).toBeFalsy();
+            expect(grid.getColumnByName('Country').pinned).toBeFalsy();
+            expect(grid.getColumnByName('Address').pinned).toBeFalsy();
+
+            expect(grid.getColumnByName('ID').pinned).toBeFalsy();
+            expect(grid.getColumnByName('CompanyName').pinned).toBeFalsy();
+            expect(grid.getColumnByName('ContactName').pinned).toBeFalsy();
+            expect(grid.getColumnByName('ContactTitle').pinned).toBeFalsy();
+
+            grid.pinColumn('ContactName');
+            fixture.detectChanges();
+
+            expect(grid.getColumnByName('PostalCode').pinned).toBeFalsy();
+            expect(grid.getColumnByName('City').pinned).toBeFalsy();
+            expect(grid.getColumnByName('Country').pinned).toBeFalsy();
+            expect(grid.getColumnByName('Address').pinned).toBeFalsy();
+
+            expect(grid.getColumnByName('ID').pinned).toBeTruthy();
+            expect(grid.getColumnByName('CompanyName').pinned).toBeTruthy();
+            expect(grid.getColumnByName('ContactName').pinned).toBeTruthy();
+            expect(grid.getColumnByName('ContactTitle').pinned).toBeTruthy();
+        });
+
+        it('should not allow pinning if group width exceeds max allowed.', () => {
+            // pin the other group
+            fixture.componentInstance.colGroups[1].pinned = true;
+            fixture.detectChanges();
+
+            // group 1 should still be pinned - all child columns should be pinned
+            expect(grid.getColumnByName('PostalCode').pinned).toBeTruthy();
+            expect(grid.getColumnByName('City').pinned).toBeTruthy();
+            expect(grid.getColumnByName('Country').pinned).toBeTruthy();
+            expect(grid.getColumnByName('Address').pinned).toBeTruthy();
+            // group 2 should not be pinned as it will exceed unpinnedAreaMinWidth
+            expect(grid.getColumnByName('ID').pinned).toBeFalsy();
+            expect(grid.getColumnByName('CompanyName').pinned).toBeFalsy();
+            expect(grid.getColumnByName('ContactName').pinned).toBeFalsy();
+            expect(grid.getColumnByName('ContactTitle').pinned).toBeFalsy();
+        });
+
+        it('should emit onColumnPinning event with correct parameters', () => {
+            let allArgs = [];
+            grid.onColumnPinning.subscribe((args) => {
+                allArgs.push(args);
+            });
+
+            grid.unpinColumn('City');
+            fixture.detectChanges();
+            // should unpin parent and all child cols - 4 child + 1 parent
+            expect(allArgs.length).toBe(5);
+
+            expect(allArgs[0].column.field).toBe('PostalCode');
+            expect(allArgs[0].isPinned).toBeFalsy();
+
+            expect(allArgs[1].column.field).toBe('City');
+            expect(allArgs[1].isPinned).toBeFalsy();
+
+            expect(allArgs[2].column.field).toBe('Country');
+            expect(allArgs[2].isPinned).toBeFalsy();
+
+            expect(allArgs[3].column.field).toBe('Address');
+            expect(allArgs[3].isPinned).toBeFalsy();
+
+            expect(allArgs[4].column instanceof IgxColumnLayoutComponent).toBeTruthy();
+            expect(allArgs[4].isPinned).toBeFalsy();
+
+            allArgs = [];
+            grid.pinColumn('ID');
+            fixture.detectChanges();
+            // should pin parent and all child cols - 4 child + 1 parent
+            expect(allArgs.length).toBe(5);
+
+            expect(allArgs[0].column instanceof IgxColumnLayoutComponent).toBeTruthy();
+            expect(allArgs[0].isPinned).toBeTruthy();
+
+            expect(allArgs[1].column.field).toBe('ID');
+            expect(allArgs[1].isPinned).toBeTruthy();
+
+            expect(allArgs[2].column.field).toBe('CompanyName');
+            expect(allArgs[2].isPinned).toBeTruthy();
+
+            expect(allArgs[3].column.field).toBe('ContactName');
+            expect(allArgs[3].isPinned).toBeTruthy();
+
+            expect(allArgs[4].column.field).toBe('ContactTitle');
+            expect(allArgs[4].isPinned).toBeTruthy();
+
+        });
+
+        it('should work with horizontal virtualization on the unpinned groups.', async() => {
+            const uniqueGroups = [
+                {
+                group: 'group1',
+                // total colspan 3
+                columns: [
+                    { field: 'Address', rowStart: 1, colStart: 1, colEnd : 'span 3', rowEnd: 'span 2'},
+                    { field: 'County', rowStart: 3, colStart: 1},
+                    { field: 'Region', rowStart: 3, colStart: 2},
+                    { field: 'City', rowStart: 3, colStart: 3}
+                ]
+            },
+            {
+                group: 'group2',
+                  // total colspan 2
+                columns: [
+                    { field: 'CompanyName', rowStart: 1, colStart: 1},
+                    { field: 'Address', rowStart: 1, colStart: 2},
+                    { field: 'ContactName', rowStart: 2, colStart: 1, colEnd : 'span 2', rowEnd: 'span 2'}
+                ]
+            },
+            {
+                group: 'group3',
+                // total colspan 1
+                columns: [
+                    { field: 'Phone', rowStart: 1, colStart: 1},
+                    { field: 'Fax', rowStart: 2, colStart: 1, rowEnd: 'span 2'}
+                ]
+            },
+            {
+                group: 'group4',
+                // total colspan 4
+                columns: [
+                    { field: 'CompanyName', rowStart: 1, colStart: 1, colEnd: 'span 2'},
+                    { field: 'Phone', rowStart: 1, colStart: 3, rowEnd: 'span 2'},
+                    { field: 'Address', rowStart: 1, colStart: 4, rowEnd: 'span 3'},
+                    { field: 'Region', rowStart: 2, colStart: 1},
+                    { field: 'City', rowStart: 2, colStart: 2},
+                    { field: 'ContactName', rowStart: 3, colStart: 1, colEnd: 'span 3'},
+                ]
+            }
+            ];
+            fixture.componentInstance.colGroups = uniqueGroups;
+            grid.columnWidth = '200px';
+            fixture.componentInstance.grid.width = '600px';
+            fixture.detectChanges();
+            // pin group3
+            grid.pinColumn('group3');
+            fixture.detectChanges();
+            // check group 3 is pinned
+            expect(grid.getColumnByName('group3').pinned).toBeTruthy();
+            expect(grid.getColumnByName('Fax').pinned).toBeTruthy();
+            expect(grid.getColumnByName('Phone').pinned).toBeTruthy();
+            const gridFirstRow = grid.rowList.first;
+            const firstRowCells = gridFirstRow.cells.toArray();
+            const headerCells = grid.headerGroups.first.children.toArray();
+
+            verifyDOMMatchesLayoutSettings(gridFirstRow, fixture.componentInstance.colGroups.slice(2, 3));
+             // headers are aligned to cells
+             verifyLayoutHeadersAreAligned(headerCells, firstRowCells);
+
+            // check virtualization state
+            // 4 groups in total - 1 is pinned
+            const horizontalVirtualization = grid.rowList.first.virtDirRow;
+            expect(grid.hasHorizontalScroll()).toBeTruthy();
+            expect(horizontalVirtualization.igxForOf.length).toBe(3);
+            // check order is correct
+            expect(horizontalVirtualization.igxForOf[0]).toBe(grid.getColumnByName('group1'));
+            expect(horizontalVirtualization.igxForOf[1]).toBe(grid.getColumnByName('group2'));
+            expect(horizontalVirtualization.igxForOf[2]).toBe(grid.getColumnByName('group4'));
+            // check their sizes are correct
+            expect(horizontalVirtualization.getSizeAt(0)).toBe(3 * 200);
+            expect(horizontalVirtualization.getSizeAt(1)).toBe(2 * 200);
+            expect(horizontalVirtualization.getSizeAt(2)).toBe(4 * 200);
+
+            // check total widths sum
+            const horizonatalScrElem = horizontalVirtualization.getHorizontalScroll();
+            // 9 column span in total
+            const totalExpected = 9 * 200;
+            expect(parseInt(horizonatalScrElem.children[0].style.width, 10)).toBe(totalExpected);
+
+            // check last column group can be scrolled in view
+            horizontalVirtualization.scrollTo(2);
+            await wait(100);
+            fixture.detectChanges();
+
+            const lastCell = grid.rowList.first.cells.toArray()[4];
+            expect(lastCell.column.field).toBe('Address');
+            expect(lastCell.column.parent.field).toBe('group4');
+            expect(lastCell.nativeElement.getBoundingClientRect().right - 1)
+            .toEqual(grid.tbody.nativeElement.getBoundingClientRect().right);
+        });
     });
 });
 
 @Component({
     template: `
     <igx-grid #grid [data]="data" height="500px">
-        <igx-column-layout *ngFor='let group of colGroups' [pinned]='group.pinned'>
+        <igx-column-layout *ngFor='let group of colGroups' [field]='group.group' [pinned]='group.pinned'>
             <igx-column *ngFor='let col of group.columns'
-            [rowStart]="col.rowStart" [colStart]="col.colStart" [width]='col.width' [pinned]='col.pinned'
+            [rowStart]="col.rowStart" [colStart]="col.colStart" [width]='col.width'
             [colEnd]="col.colEnd" [rowEnd]="col.rowEnd" [field]='col.field'></igx-column>
         </igx-column-layout>
     </igx-grid>
@@ -87,13 +319,13 @@ export class ColumnLayoutPinningTestComponent {
     colGroups = [
         {
             group: 'group1',
-            pinned: false,
-            columns: this.cols1
+            pinned: true,
+            columns: this.cols2
         },
         {
             group: 'group2',
-            pinned: true,
-            columns: this.cols2
+            pinned: false,
+            columns: this.cols1
         }
     ];
     data = SampleTestData.contactInfoDataFull();
