4625fcb812b177df1582bc1d91739ea85c383877
AMarinov <amarinov@infragistics.com>
2019-04-16 17:01:39 +0300

    feat(grid): Canceling the drag by pressing ESC key #3993

--- projects/igniteui-angular/src/lib/grids/row-drag.directive.ts
+++ projects/igniteui-angular/src/lib/grids/row-drag.directive.ts
@@ -1,6 +1,8 @@
 import { Directive, Input, OnDestroy, NgModule, Output, EventEmitter } from '@angular/core';
 import { IgxDragDirective } from '../directives/dragdrop/dragdrop.directive';
 import { IRowDragStartEventArgs } from './grid-base.component';
+import { KEYS } from '../core/utils';
+import { fromEvent, Subscription } from 'rxjs';
 
 /**
  * @hidden
@@ -10,6 +12,7 @@ import { IRowDragStartEventArgs } from './grid-base.component';
 })
 export class IgxRowDragDirective extends IgxDragDirective implements OnDestroy {
     private _row;
+    private subscription$: Subscription;
 
     @Input('igxRowDrag')
     set data(val) {
@@ -29,6 +32,12 @@ export class IgxRowDragDirective extends IgxDragDirective implements OnDestroy {
         };
 
         this.row.grid.onDragStart.emit(args);
+
+        this.subscription$ = fromEvent(this.row.grid.document.defaultView, 'keydown').subscribe((ev: KeyboardEvent) => {
+            if (ev.key === KEYS.ESCAPE || ev.key === KEYS.ESCAPE_IE) {
+                this.onPointerUp(event);
+            }
+        });
     }
 
     protected createDragGhost(event) {
