2951e992708c9f6f12c9a16914f449c06dba4246
wnvko <milko.venkov@gmail.com>
2019-04-23 17:44:12 +0300

    refactor(igxOverlay): refactor fit strategies, WIP, #4564

--- projects/igniteui-angular/src/lib/services/overlay/position/base-fit-position-strategy.ts
+++ projects/igniteui-angular/src/lib/services/overlay/position/base-fit-position-strategy.ts
@@ -1,6 +1,6 @@
 import { ConnectedPositioningStrategy } from './connected-positioning-strategy';
 import { IPositionStrategy } from './IPositionStrategy';
-import { HorizontalAlignment, VerticalAlignment, PositionSettings, Size, getViewportRect } from '../utilities';
+import { HorizontalAlignment, VerticalAlignment, PositionSettings, Size, getViewportRect, Point } from '../utilities';
 
 export abstract class BaseFitPositionStrategy extends ConnectedPositioningStrategy {
     protected _initialSettings: PositionSettings;
@@ -8,42 +8,41 @@ export abstract class BaseFitPositionStrategy extends ConnectedPositioningStrate
 
     position(contentElement: HTMLElement, size: Size, document?: Document, initialCall?: boolean): void {
         super.position(contentElement, size);
-        if (!initialCall) {
-            return;
-        }
         this._initialSettings = this._initialSettings || Object.assign({}, this._initialSettings, this.settings);
         this.settings = Object.assign({}, this.settings, this._initialSettings);
-        const elementRect: ClientRect = contentElement.getBoundingClientRect();
-        const viewPort: ClientRect = getViewportRect(document);
-        if (this.shouldFitHorizontal(elementRect, viewPort)) {
-            this.fitHorizontal(contentElement, this.settings, elementRect, viewPort, this.settings.minSize);
-        }
-
-        if (this.shouldFitVertical(elementRect, viewPort)) {
-            this.fitVertical(contentElement, this.settings, elementRect, viewPort, this.settings.minSize);
+        const connectedFit: ConnectedFit = {}
+        connectedFit.elementRect = contentElement.getBoundingClientRect();
+        connectedFit.viewPortRect = getViewportRect(document);
+        this.canFitInViewPort(connectedFit);
+        if (!connectedFit.fitHorizontal || !connectedFit.fitVertical) {
+            this.fitInViewPort(contentElement, this.settings, connectedFit, initialCall);
         }
     }
 
-    protected shouldFitHorizontal(innerRect: ClientRect, outerRect: ClientRect): boolean {
-        return innerRect.left < outerRect.left || outerRect.right < innerRect.right;
+    protected canFitInViewPort(connectedFit: ConnectedFit) {
+        connectedFit.fitHorizontal =
+            connectedFit.viewPortRect.left < connectedFit.elementRect.left &&
+            connectedFit.elementRect.right < connectedFit.viewPortRect.right;
+        connectedFit.fitVertical =
+            connectedFit.viewPortRect.top < connectedFit.elementRect.top &&
+            connectedFit.elementRect.bottom < connectedFit.viewPortRect.bottom;
     }
 
-    protected shouldFitVertical(innerRect: ClientRect, outerRect: ClientRect): boolean {
-        return innerRect.top < outerRect.top || outerRect.bottom < innerRect.bottom;
+    protected joinStringNoTrailingSpaces(input: string[]): string {
+        return input.join(' ').trim();
     }
 
-    protected abstract fitHorizontal(
+    protected abstract fitInViewPort(
         element: HTMLElement,
         settings: PositionSettings,
-        innerRect: ClientRect,
-        outerRect: ClientRect,
-        minSize: Size);
+        connectedFit: ConnectedFit,
+        initialCall?: boolean);
+}
 
-
-    protected abstract fitVertical(
-        element: HTMLElement,
-        settings: PositionSettings,
-        innerRect: ClientRect,
-        outerRect: ClientRect,
-        minSize: Size);
+export interface ConnectedFit {
+    targetRect?: ClientRect;
+    elementRect?: ClientRect;
+    viewPortRect?: ClientRect;
+    fitHorizontal?: boolean;
+    fitVertical?: boolean;
 }
