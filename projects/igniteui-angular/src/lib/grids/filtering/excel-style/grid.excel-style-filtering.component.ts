c9b0161dfb5ddab629750adb90d0998e35277c87
astaev <astaev@infragistics.com>
2019-05-10 16:38:41 +0300

    fix(esf): Do not format the SelectAll checkbox #4737

--- projects/igniteui-angular/src/lib/grids/filtering/excel-style/grid.excel-style-filtering.component.ts
+++ projects/igniteui-angular/src/lib/grids/filtering/excel-style/grid.excel-style-filtering.component.ts
@@ -47,6 +47,7 @@ export class FilterListItem {
     public label: any;
     public isSelected: boolean;
     public indeterminate: boolean;
+    public isSpecial = false;
 }
 
 @Directive({
@@ -427,6 +428,7 @@ export class IgxGridExcelStyleFilteringComponent implements OnDestroy, AfterView
         selectAll.value = this.grid.resourceStrings.igx_grid_excel_select_all;
         selectAll.label = this.grid.resourceStrings.igx_grid_excel_select_all;
         selectAll.indeterminate = this.selectAllIndeterminate;
+        selectAll.isSpecial = true;
         this.listData.unshift(selectAll);
     }
 
