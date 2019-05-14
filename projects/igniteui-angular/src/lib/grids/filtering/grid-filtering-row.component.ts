335932cfb2453e780aa9a3d851ab085b45e02e38
Tacho <tachojelev@gmail.com>
2019-05-14 12:33:02 +0300

    fix(filterRow): update arrows cond - 7.2.x #4700

--- projects/igniteui-angular/src/lib/grids/filtering/grid-filtering-row.component.ts
+++ projects/igniteui-angular/src/lib/grids/filtering/grid-filtering-row.component.ts
@@ -631,7 +631,7 @@ export class IgxGridFilteringRowComponent implements AfterViewInit {
             const containerWidth = this.container.nativeElement.getBoundingClientRect().width;
             this.chipsAreaWidth = this.chipsArea.element.nativeElement.getBoundingClientRect().width;
 
-            this.showArrows = this.chipsAreaWidth >= containerWidth;
+            this.showArrows = this.chipsAreaWidth >= containerWidth && this.isColumnFiltered;
 
             // TODO: revise the cdr.detectChanges() usage here
             this.cdr.detectChanges();
