de42b5eee25ef497693babe5d51da1662959505a
Stamen Stoychev <chronos.stz@gmail.com>
2019-05-21 17:16:35 +0300

    chore(hgrid): removing commented out leftover

--- projects/igniteui-angular/src/lib/grids/hierarchical-grid/hierarchical-grid.component.ts
+++ projects/igniteui-angular/src/lib/grids/hierarchical-grid/hierarchical-grid.component.ts
@@ -262,14 +262,6 @@ export class IgxHierarchicalGridComponent extends IgxHierarchicalGridBaseCompone
 
     /**
      * @hidden
-
-    protected get rowBasedHeight() {
-        const rbh = this.dataLength * this.rowHeight;
-        return this.parent ? rbh || null : rbh;
-    } */
-
-    /**
-     * @hidden
      */
     public childLayoutKeys = [];
 
