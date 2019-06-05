8c98a15b7b72cdf675f6674af6dce6e3bfac8587
ddincheva <desislava9dincheva@gmail.com>
2019-06-05 16:35:18 +0300

    refactor(IgxGrid): remove deprecated methods related with summary  recalculation

--- projects/igniteui-angular/src/lib/grids/grid-base.component.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.component.ts
@@ -3713,13 +3713,6 @@ export abstract class IgxGridBaseComponent extends DisplayDensityBase implements
     /**
      * @hidden
      */
-    @DeprecateMethod('There is no need to call clearSummaryCache method.The summary cache is cleared automatically when needed.')
-    public clearSummaryCache(args?) {
-    }
-
-    /**
-     * @hidden
-     */
     public refreshGridState(args?) {
         this.endEdit(true);
         this.summaryService.clearSummaryCache(args);
@@ -3765,13 +3758,6 @@ export abstract class IgxGridBaseComponent extends DisplayDensityBase implements
      */
     public reflow() {
         this.calculateGridSizes();
-    }
-
-    /**
-     * @hidden
-     */
-    @DeprecateMethod('There is no need to call recalculateSummaries method. The summaries are recalculated automatically when needed.')
-    public recalculateSummaries() {
     }
 
     /**
