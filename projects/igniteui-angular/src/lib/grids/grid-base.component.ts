76327b4dcecc872a31a53b5ac3ccffa213f9564c
Vasil Mihalkov <VMihalkov@infragistics.com>
2019-06-05 14:17:14 +0300

    fix(grid): Removed trailing whitespace #4905

--- projects/igniteui-angular/src/lib/grids/grid-base.component.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.component.ts
@@ -4503,7 +4503,6 @@ export abstract class IgxGridBaseComponent extends DisplayDensityBase implements
      */
     protected reinitPinStates() {
         this._pinnedColumns = this.columnList.filter((c) => c.pinned);
-        
         this._unpinnedColumns = this.columnList.filter((c) => !c.pinned);
     }
 
