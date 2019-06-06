5ce6093942c6733a767e2415e8d4a7fb03638027
Tacho <tachojelev@gmail.com>
2019-06-06 13:38:30 +0300

    fix(filter): update filterGlobal logic #4897

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,7 @@ All notable changes for each version of this project will be documented in this 
 
 ## 7.2.11
 - `IgxGrid`, `IgxTreeGrid`, `IgxHierarchicalGrid`
-    - **Breaking Change** The **condition** parameter of the `filterGlobal` method is no longer optional. When the filterGlobal method is called with an invalid condition, it will now throw an error and will not clear the existing filters for all columns.
+    - **Breaking Change** The **condition** parameter of the `filterGlobal` method is no longer optional. When the filterGlobal method is called with an invalid condition, it will not clear the existing filters for all columns.
 
 ## 7.2.9
 - `Pager`
--- projects/igniteui-angular/src/lib/grids/grid/grid-filtering.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid-filtering.spec.ts
@@ -605,11 +605,18 @@ describe('IgxGrid - Filtering actions', () => {
         }).toThrowError('Invalid condition or Expression Tree!');
     }));
 
-    it('Should throw descriptive error when filterGlobal() is called without condition', fakeAsync(() => {
-        expect(() => {
-            grid.filterGlobal(100);
-            fix.detectChanges();
-        }).toThrowError('Invalid condition!');
+    it('Should not clear previous filtering when filterGlobal() is called with invalid condition', fakeAsync(() => {
+        grid.filter('Downloads', 100, IgxNumberFilteringOperand.instance().condition('greaterThan'), true);
+        fix.detectChanges();
+        expect(grid.rowList.length).toEqual(4);
+        expect(grid.getCellByColumn(0, 'Downloads').value).toEqual(254);
+
+        // Execute global filtering with invalid condition.
+        grid.filterGlobal(1000, null);
+        fix.detectChanges();
+
+        expect(grid.rowList.length).toEqual(4);
+        expect(grid.getCellByColumn(0, 'Downloads').value).toEqual(254);
     }));
 });
 
