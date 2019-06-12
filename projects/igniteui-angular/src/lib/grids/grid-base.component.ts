fae1245e962b53ad3210ac49fcba004dff6a0de5
ViktorSlavov <viktorslavov91@gmail.com>
2019-06-12 13:02:23 +0300

    fix(grid): fix row editing warning to show when !primaryKey, #5010

--- projects/igniteui-angular/src/lib/grids/grid-base.component.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.component.ts
@@ -208,7 +208,7 @@ export interface IRowDragEndEventArgs {
 export interface IRowDragStartEventArgs extends CancelableEventArgs {
     owner: IgxDragDirective;
     dragData: IgxRowComponent<IgxGridBaseComponent & IGridDataBindable>;
- }
+}
 
 export enum GridSummaryPosition {
     top = 'top',
@@ -247,6 +247,7 @@ export abstract class IgxGridBaseComponent extends DisplayDensityBase implements
     private _locale = null;
     private _observer: MutationObserver;
     private _destroyed = false;
+    private _primaryKey = null;
     private overlayIDs = [];
     /**
      * An accessor that sets the resource strings.
@@ -600,11 +601,9 @@ export abstract class IgxGridBaseComponent extends DisplayDensityBase implements
     * @memberof IgxGridBaseComponent
     */
     set rowEditable(val: boolean) {
-        if (val && (this.primaryKey === undefined || this.primaryKey === null)) {
-            console.warn('The grid must have a `primaryKey` specified when using `rowEditable`!');
-        }
         this._rowEditable = val;
         if (this.gridAPI.grid) {
+            this.checkEditKey(val);
             this.refreshGridState();
         }
     }
@@ -758,8 +757,16 @@ export abstract class IgxGridBaseComponent extends DisplayDensityBase implements
      */
     @WatchChanges()
     @Input()
-    public primaryKey;
+    public set primaryKey(val: any) {
+        this._primaryKey = val;
+        if (this.gridAPI.grid) {
+            this.checkEditKey(this._rowEditable);
+        }
+    }
 
+    public get primaryKey(): any {
+        return this._primaryKey;
+    }
     /**
      * An @Input property that sets the message displayed when there are no records.
      * ```html
@@ -2586,6 +2593,12 @@ export abstract class IgxGridBaseComponent extends DisplayDensityBase implements
         this.hideOverlays();
     }
 
+    private checkEditKey(val: boolean): void {
+        if (val && (this.primaryKey === undefined || this.primaryKey === null)) {
+            console.warn('The grid must have a `primaryKey` specified when using `rowEditable`!');
+        }
+    }
+
     /**
     * @hidden
     * @internal
@@ -2701,6 +2714,7 @@ export abstract class IgxGridBaseComponent extends DisplayDensityBase implements
         this.calcWidth = this._width && this._width.indexOf('%') === -1 ? parseInt(this._width, 10) : 0;
         this.shouldGenerate = this.autoGenerate;
         this._scrollWidth = this.getScrollWidth();
+        this.checkEditKey(this._rowEditable);
     }
 
     protected setupColumns() {
@@ -3395,7 +3409,7 @@ export abstract class IgxGridBaseComponent extends DisplayDensityBase implements
         this._moveColumns(column, dropTarget, position);
         this.cdr.detectChanges();
         if (this.hasColumnLayouts) {
-            this.columns.filter(x => x.columnLayout).forEach( x => x.populateVisibleIndexes());
+            this.columns.filter(x => x.columnLayout).forEach(x => x.populateVisibleIndexes());
         }
 
         const args = {
@@ -4887,13 +4901,13 @@ export abstract class IgxGridBaseComponent extends DisplayDensityBase implements
             && this.navigation.isColumnLeftFullyVisible(visibleColIndex))) {
             if (this.navigation.shouldPerformVerticalScroll(rowIndex, visibleColIndex)) {
                 this.navigation.performVerticalScrollToCell(rowIndex, visibleColIndex,
-                     () => { this.executeCallback(rowIndex, visibleColIndex, cb); } );
+                    () => { this.executeCallback(rowIndex, visibleColIndex, cb); });
             } else {
                 this.executeCallback(rowIndex, visibleColIndex, cb);
             }
         } else {
             this.navigation.performHorizontalScrollToCell(rowIndex, visibleColIndex, false,
-                 () => { this.executeCallback(rowIndex, visibleColIndex, cb); });
+                () => { this.executeCallback(rowIndex, visibleColIndex, cb); });
         }
     }
 
