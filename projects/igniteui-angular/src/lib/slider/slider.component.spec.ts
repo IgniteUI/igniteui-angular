e6c06ee32e6d33aac388eee01f6f8dfb57b285c2
Aleksandar Kamenov <aleksandyr.kamenov@gmail.com>
2019-06-17 13:43:18 +0300

    fix(slider): make test to import from slider common file
    Closes #5075

--- projects/igniteui-angular/src/lib/slider/slider.component.spec.ts
+++ projects/igniteui-angular/src/lib/slider/slider.component.spec.ts
@@ -1,14 +1,15 @@
 import { Component, ViewChild } from '@angular/core';
 import { async, TestBed, ComponentFixture, fakeAsync, tick, flush } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { IgxSliderComponent, IgxSliderModule, IRangeSliderValue, SliderType } from './slider.component';
+import { IgxSliderComponent, IgxSliderModule } from './slider.component';
 import { UIInteractions, wait } from '../test-utils/ui-interactions.spec';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { configureTestSuite } from '../test-utils/configure-suite';
+import { SliderType, IRangeSliderValue } from './slider.common';
 
 declare var Simulator: any;
 
-describe('IgxSlider', () => {
+fdescribe('IgxSlider', () => {
     configureTestSuite();
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -724,7 +725,7 @@ export class SliderMinMaxComponent {
                     </igx-slider>
                 </div>`
 })
-class SliderTestComponent {
+export class SliderTestComponent {
     @ViewChild(IgxSliderComponent) public slider: IgxSliderComponent;
 
     minValue = 0;
