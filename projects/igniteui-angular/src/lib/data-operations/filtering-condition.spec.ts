3cc14b72f70a42b6618278b763b1672034b3c611
Konstantin Dinev <kdinev@mail.bw.edu>
2019-06-18 16:10:10 +0300

    chore(*): Removing the fdescribe

--- projects/igniteui-angular/src/lib/data-operations/filtering-condition.spec.ts
+++ projects/igniteui-angular/src/lib/data-operations/filtering-condition.spec.ts
@@ -4,7 +4,7 @@ import { IgxStringFilteringOperand,
     IgxBooleanFilteringOperand, 
     IgxFilteringOperand} from './filtering-condition';
 
-fdescribe('Unit testing FilteringCondition', () => {
+describe('Unit testing FilteringCondition', () => {
     it('tests string conditions', () => {
         const fc = IgxStringFilteringOperand.instance();
         // contains
