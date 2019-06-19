a14bacd954fb1d05dc7f2a09d4ed30719daa44f8
Nadia Robakova <nrobakova@infragistics.com>
2019-06-19 15:37:47 +0300

    fix(grid): Reset maxLevelHeaderDepth on initColumns #5100

--- projects/igniteui-angular/src/lib/grids/grid-base.component.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.component.ts
@@ -4470,6 +4470,7 @@ export abstract class IgxGridBaseComponent extends DisplayDensityBase implements
             const columnLayoutColumns = this.columnList.filter((col) => col.columnLayout || col.columnLayoutChild);
             this.columnList.reset(columnLayoutColumns);
         }
+        this._maxLevelHeaderDepth = null;
         this._columns = this.columnList.toArray();
         collection.forEach((column: IgxColumnComponent) => {
             column.grid = this;
