41aa36cf4c5a67b06845888ffd7fec8cf2145dd8
Stamen Stoychev <chronos.stz@gmail.com>
2019-06-25 11:51:44 +0300

    chore(*): swapping back wrongly updated code line

--- projects/igniteui-angular/src/lib/directives/for-of/for_of.directive.ts
+++ projects/igniteui-angular/src/lib/directives/for-of/for_of.directive.ts
@@ -701,7 +701,7 @@ export class IgxForOfDirective<T> implements OnInit, OnChanges, DoCheck, OnDestr
             const view = this._embeddedViews[i];
             const rNode = view.rootNodes.find((node) => node.nodeType === Node.ELEMENT_NODE);
             if (rNode) {
-                const h = rNode.offsetHeight ? rNode.offsetHeight : parseInt(this.igxForItemSize, 10);
+                const h = Math.max(rNode.offsetHeight, parseInt(this.igxForItemSize, 10));
                 const index = this.state.startIndex + i;
                 if (!this.isRemote && !this.igxForOf[index]) {
                     continue;
