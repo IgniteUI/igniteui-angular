43cbe2686cb1a1db8bffe2f34b74dc2f144b3ed9
MPopov <desig9stein@gmail.com>
2019-06-26 13:36:37 +0300

    style(toolbar): Fix tests

--- projects/igniteui-angular/src/lib/grids/grid/grid-toolbar.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid-toolbar.spec.ts
@@ -427,7 +427,7 @@ describe('IgxGrid - Grid Toolbar', () => {
         const toolbar = getToolbar(fixture).nativeElement;
         expect(grid.toolbar.displayDensity).toEqual(DisplayDensity.comfortable);
         expect(toolbar.classList[0]).toBe('igx-grid-toolbar');
-        expect(parseFloat(toolbar.offsetHeight) > 55).toBe(true);
+        expect(parseFloat(toolbar.offsetHeight) >= 56).toBe(true);
         verifyButtonsDisplayDensity(getToolbar(fixture), DisplayDensity.comfortable);
 
         grid.displayDensity = DisplayDensity.compact;
@@ -436,7 +436,7 @@ describe('IgxGrid - Grid Toolbar', () => {
 
         expect(grid.toolbar.displayDensity).toBe(DisplayDensity.compact);
         expect(toolbar.classList[0]).toBe('igx-grid-toolbar--compact');
-        expect(parseFloat(toolbar.offsetHeight) < 50).toBe(true);
+        expect(parseFloat(toolbar.offsetHeight) <= 48).toBe(true);
         verifyButtonsDisplayDensity(getToolbar(fixture), DisplayDensity.compact);
 
         grid.displayDensity = DisplayDensity.cosy;
@@ -445,7 +445,7 @@ describe('IgxGrid - Grid Toolbar', () => {
 
         expect(grid.toolbar.displayDensity).toBe(DisplayDensity.cosy);
         expect(toolbar.classList[0]).toBe('igx-grid-toolbar--cosy');
-        expect(parseFloat(toolbar.offsetHeight) < 50).toBe(true);
+        expect(parseFloat(toolbar.offsetHeight) <= 48).toBe(true);
         verifyButtonsDisplayDensity(getToolbar(fixture), DisplayDensity.cosy);
     }));
 
