065e10951421c3d1d09edaad7e024b4cc313b67c
ddincheva <desislava9dincheva@gmail.com>
2019-06-28 16:25:59 +0300

    chore(*): fix linting error

--- projects/igniteui-angular/src/lib/grids/hierarchical-grid/hierarchical-grid-navigation.service.ts
+++ projects/igniteui-angular/src/lib/grids/hierarchical-grid/hierarchical-grid-navigation.service.ts
@@ -265,8 +265,8 @@ export class IgxHierarchicalGridNavigationService extends IgxGridNavigationServi
         const virt = this.grid.verticalScrollContainer;
         const isNextChild = nextIndex <= virt.igxForOf.length - 1 &&
             this.grid.isChildGridRecord(virt.igxForOf[nextIndex]);
-        const row = this.grid.getRowByIndex(rowIndex);
-        if (row && row.inEditMode) {
+        const currentRow = this.grid.getRowByIndex(rowIndex);
+        if (currentRow && currentRow.inEditMode) {
             super.performTab(currentRowEl, selectedNode);
             return;
         }
