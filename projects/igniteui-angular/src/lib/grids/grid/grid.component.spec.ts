dfdb149c7ef4faeda40224933c38ac5aaa4e88ae
MKirova <MKirova@DEV-MKIROVA>
2019-07-02 14:33:21 +0300

    chore(*): Fixing merge issue.

--- projects/igniteui-angular/src/lib/grids/grid/grid.component.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid.component.spec.ts
@@ -4618,7 +4618,7 @@ export class IgxGridInsideIgxTabsComponent {
     public grid5: IgxGridComponent;
     @ViewChild('grid6', { read: IgxGridComponent })
     public grid6: IgxGridComponent;
-    @ViewChild(IgxTabsComponent, { read: IgxTabsComponen })
+    @ViewChild(IgxTabsComponent, { read: IgxTabsComponent })
     public tabs: IgxTabsComponent;
 
     public columns = [
