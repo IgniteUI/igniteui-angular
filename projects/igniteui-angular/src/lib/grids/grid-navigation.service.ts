6a6090ee18274aa3c7f923d16d8c0c281d35d531
MKirova <MKirova@DEV-MKIROVA>
2019-07-04 16:40:09 +0300

    chore(*): Fixing more merge issues.

--- projects/igniteui-angular/src/lib/grids/grid-navigation.service.ts
+++ projects/igniteui-angular/src/lib/grids/grid-navigation.service.ts
@@ -159,7 +159,6 @@ export class IgxGridNavigationService {
         if (!element.previousElementSibling && this.grid.pinnedColumns.length && index === - 1) {
             element.parentNode.previousElementSibling.focus({ preventScroll: true });
         } else if (!this.isColumnLeftEdgeVisible(visibleColumnIndex - 1)) {
-        } else if (!this.isColumnLeftFullyVisible(visibleColumnIndex - 1)) {
             this.getFocusableGrid().nativeElement.focus({ preventScroll: true });
             this.performHorizontalScrollToCell(rowIndex, visibleColumnIndex - 1, isSummary);
         } else {
