0f2acf21fec5a7a62e89440b5723b6eb3a323b89
wnvko <milko.venkov@gmail.com>
2019-07-08 22:06:00 +0300

    fix(grid): convert column editable to property, #5022
    Column editable field is undefined by default. If set by user to
    anything other than undefined, the getter will return the value
    set by user. Otherwise will return the value set to rowEditable.

--- projects/igniteui-angular/src/lib/grids/grid-base.component.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.component.ts
@@ -4521,7 +4521,6 @@ export abstract class IgxGridBaseComponent extends DisplayDensityBase implements
         collection.forEach((column: IgxColumnComponent) => {
             column.grid = this;
             column.defaultWidth = this.columnWidth;
-            this.setColumnEditState(column);
 
             if (cb) {
                 cb(column);
@@ -4534,14 +4533,6 @@ export abstract class IgxGridBaseComponent extends DisplayDensityBase implements
             collection.forEach((column: IgxColumnComponent) => {
                 column.populateVisibleIndexes();
             });
-        }
-    }
-
-    private setColumnEditState(column: IgxColumnComponent) {
-        // When rowEditable is true, then all columns, with defined field, excluding priamaryKey, are set to editable by default.
-        if (this.rowEditable && column.editable === null &&
-            column.field && column.field !== this.primaryKey) {
-            column.editable = this.rowEditable;
         }
     }
 
--- projects/igniteui-angular/src/lib/grids/grid/grid.component.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid.component.spec.ts
@@ -3496,7 +3496,7 @@ describe('IgxGrid Component Tests', () => {
         });
 
         describe('Row Editing - Column editable property', () => {
-            fit('Default column editable value is correct, when row editing is enabled', fakeAsync(() => {
+            it('Default column editable value is correct, when row editing is enabled', fakeAsync(() => {
                 const fixture = TestBed.createComponent(IgxGridRowEditingWithoutEditableColumnsComponent);
                 fixture.detectChanges();
                 tick();
@@ -3527,7 +3527,7 @@ describe('IgxGrid Component Tests', () => {
                 expect(columns[4].editable).toBeFalsy();  // column.editable set to false
             }));
 
-            fit(`Default column editable value is correct, when row edititng is disabled`, fakeAsync(() => {
+            it(`Default column editable value is correct, when row edititng is disabled`, fakeAsync(() => {
                 const fixture = TestBed.createComponent(IgxGridTestComponent);
                 fixture.componentInstance.columns.push({ field: 'ID', header: 'ID', dataType: 'number', width: null, hasSummary: false });
                 fixture.componentInstance.data = [
