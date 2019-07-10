adeb94e3c615935a279530b83a1ebaeb274db197
MKirova <MKirova@DEV-MKIROVA>
2019-07-10 19:15:20 +0300

    fix(ixgForOf): Make sure igxForItemSize is parsed to int when calculating item sizes.

--- src/app/virtual-for-directive/virtual-for.sample.ts
+++ src/app/virtual-for-directive/virtual-for.sample.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild, Injectable, OnInit, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, Injectable, OnInit, AfterViewInit, ViewEncapsulation } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Http } from '@angular/http';
@@ -8,7 +8,9 @@ import { RemoteService } from '../shared/remote.service';
 
 @Component({
     selector: 'app-virt-for-sample',
-    templateUrl: 'virtual-for.sample.html'
+    templateUrl: 'virtual-for.sample.html',
+    styleUrls: ['virtual-for.sample.css'],
+    encapsulation: ViewEncapsulation.None
 })
 export class VirtualForSampleComponent implements OnInit, AfterViewInit {
     search1: string;
@@ -52,7 +54,6 @@ export class VirtualForSampleComponent implements OnInit, AfterViewInit {
             link: '#',
             phone: '770-504-2217',
             text: 'Terrance Orta',
-            width: 100,
             height: 100
         }, {
             key: 2,
@@ -61,7 +62,6 @@ export class VirtualForSampleComponent implements OnInit, AfterViewInit {
             link: '#',
             phone: '423-676-2869',
             text: 'Richard Mahoney',
-            width: 200,
             height: 200
         }, {
             key: 3,
@@ -70,7 +70,6 @@ export class VirtualForSampleComponent implements OnInit, AfterViewInit {
             link: '#',
             phone: '859-496-2817',
             text: 'Donna Price',
-            width: 300,
             height: 300
         }, {
             key: 4,
@@ -79,7 +78,6 @@ export class VirtualForSampleComponent implements OnInit, AfterViewInit {
             link: '#',
             phone: '901-747-3428',
             text: 'Lisa Landers',
-            width: 200,
             height: 200
         }, {
             key: 5,
@@ -88,7 +86,6 @@ export class VirtualForSampleComponent implements OnInit, AfterViewInit {
             link: '#',
             phone: '573-394-9254',
             text: 'Dorothy H. Spencer',
-            width: 200,
             height: 200
         }, {
             key: 6,
@@ -97,7 +94,6 @@ export class VirtualForSampleComponent implements OnInit, AfterViewInit {
             link: '#',
             phone: '323-668-1482',
             text: 'Stephanie May',
-            width: 100,
             height: 100
         }, {
             key: 7,
@@ -106,7 +102,6 @@ export class VirtualForSampleComponent implements OnInit, AfterViewInit {
             link: '#',
             phone: '401-661-3742',
             text: 'Marianne Taylor',
-            width: 100,
             height: 100
         }, {
             key: 8,
@@ -115,7 +110,6 @@ export class VirtualForSampleComponent implements OnInit, AfterViewInit {
             link: '#',
             phone: '662-374-2920',
             text: 'Tammie Alvarez',
-            width: 300,
             height: 300
         }, {
             key: 9,
@@ -124,7 +118,6 @@ export class VirtualForSampleComponent implements OnInit, AfterViewInit {
             link: '#',
             phone: '240-455-2267',
             text: 'Charlotte Flores',
-            width: 200,
             height: 200
         }, {
             key: 10,
@@ -133,7 +126,6 @@ export class VirtualForSampleComponent implements OnInit, AfterViewInit {
             link: '#',
             phone: '724-742-0979',
             text: 'Ward Riley',
-            width: 100,
             height: 100
         }];
         for (let i = 10; i < 1e5; i++) {
