17c67135f292b8946fc51370de2eae5853773969
ddincheva <desislava9dincheva@gmail.com>
2019-07-12 14:50:48 +0300

    chore(*): when there is no visible cols hide paging

--- projects/igniteui-angular/src/lib/grids/grid-base.component.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.component.ts
@@ -3247,7 +3247,7 @@ export abstract class IgxGridBaseComponent extends DisplayDensityBase implements
     }
 
     get showRowCheckboxes(): boolean {
-        return this.rowSelectable && this.columns.length > this.hiddenColumnsCount;
+        return this.rowSelectable && this.hasVisibleColumns;
     }
 
     /**
