f2b0e6dce11bf64a1eaacb96414c138711442b8a
MKirova <MKirova@DEV-MKIROVA>
2019-07-12 18:00:05 +0300

    fix(igxHierarchicalGrid): Fixing navigation when parent has summary but child does not.

--- projects/igniteui-angular/src/lib/grids/hierarchical-grid/hierarchical-grid-navigation.service.ts
+++ projects/igniteui-angular/src/lib/grids/hierarchical-grid/hierarchical-grid-navigation.service.ts
@@ -297,33 +297,39 @@ export class IgxHierarchicalGridNavigationService extends IgxGridNavigationServi
                 } else {
                     this.focusNextChildDOMElem(currentRowEl, this.grid);
                 }
+            } else if (this.grid.parent && this.grid.parent.summariesRowList.toArray().length > 0) {
+               this._navigateToNextParentRow(currentRowEl);
             } else {
                 this.navigateDown(currentRowEl, { row:  rowIndex, column: 0});
             }
         } else if (isLastSummaryRow && isLastColumn && this.grid.parent) {
-            // navigating in child summary, next is parent summary or next parent row
-            const parent = this.grid.parent;
-            const parentHasSummary = parent.summariesRowList.toArray().length > 0;
-            const parentRowIndex = parseInt(
-                this.getClosestElemByTag(currentRowEl, 'igx-child-grid-row').parentNode.getAttribute('data-rowindex'), 10);
-            const isLastRowInParent = parent.verticalScrollContainer.igxForOf.length - 1 === parentRowIndex;
-            // check if next is sibling
-            const childRowContainer = this.getChildGridRowContainer(this.grid);
-            const nextIsSiblingChild = !!childRowContainer.nextElementSibling;
-            if (isLastRowInParent && parentHasSummary && !nextIsSiblingChild) {
-                // next is parent summary
-                const parentSummary = parent.summariesRowList.toArray()[0].nativeElement;
-                parent.navigation.focusNextRow(parentSummary, 0, this.grid.rootGrid, true);
-            } else {
-                // next is sibling or parent
-                this.focusNext(0);
-            }
+            this._navigateToNextParentRow(currentRowEl);
         } else  if (isLastDataRow && hasSummaries && isLastColumn && this.grid.parent) {
             // navigating in child rows, next is child grid's summary row
            this.focusNextRow(summaryRows[0].nativeElement, 0, this.grid.parent, true);
         } else {
-            super.performTab(currentRowEl, selectedNode);
+            super.performTab(currentRowEl, rowIndex, visibleColumnIndex, isSummaryRow);
         }
+    }
+
+    private _navigateToNextParentRow(currentRowEl) {
+         // next is parent summary or next parent row
+         const parent = this.grid.parent;
+         const parentHasSummary = parent.summariesRowList.toArray().length > 0;
+         const parentRowIndex = parseInt(
+             this.getClosestElemByTag(currentRowEl, 'igx-child-grid-row').parentNode.getAttribute('data-rowindex'), 10);
+         const isLastRowInParent = parent.verticalScrollContainer.igxForOf.length - 1 === parentRowIndex;
+         // check if next is sibling
+         const childRowContainer = this.getChildGridRowContainer(this.grid);
+         const nextIsSiblingChild = !!childRowContainer.nextElementSibling;
+         if (isLastRowInParent && parentHasSummary && !nextIsSiblingChild) {
+             // next is parent summary
+             const parentSummary = parent.summariesRowList.toArray()[0].nativeElement;
+             parent.navigation.focusNextRow(parentSummary, 0, this.grid.rootGrid, true);
+         } else {
+             // next is sibling or parent
+             this.focusNext(0);
+         }
     }
 
     private focusNextChildDOMElem(currentRowEl, grid) {
@@ -460,8 +466,8 @@ export class IgxHierarchicalGridNavigationService extends IgxGridNavigationServi
                  // move to filter cell
                 childGrid.navigation.moveFocusToFilterCell();
             } else {
-                // move to next cell
-                this.navigateUp(currentRowEl, { row : rowIndex, column: lastIndex});
+                // move to prev cell
+                childGrid.navigation.goToLastCell();
             }
     }
 
