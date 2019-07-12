b9eb907f418bca63113beb4883e1e67d78035b8b
wnvko <milko.venkov@gmail.com>
2019-07-12 17:59:12 +0300

    chore(grid): remove leftovers, #5020

--- projects/igniteui-angular/src/lib/grids/grid/grid.component.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid.component.spec.ts
@@ -2658,8 +2658,7 @@ describe('IgxGrid Component Tests', () => {
                 expect(gridAPI.submit_value).not.toHaveBeenCalled();
                 expect(gridAPI.escape_editMode).toHaveBeenCalled();
             }));
-for (let index = 0; index < 100; index++) {
-            fit(`Should exit row editing when clicking on a cell from a deleted row`, fakeAsync(() => {
+            it(`Should exit row editing when clicking on a cell from a deleted row`, fakeAsync(() => {
                 console.log(`test ${index+1}/100`);
                 const fixture = TestBed.createComponent(IgxGridRowEditingTransactionComponent);
                 fixture.detectChanges();
@@ -2685,7 +2684,6 @@ for (let index = 0; index < 100; index++) {
                 expect(targetCell.selected).toBeTruthy();
                 expect(firstCell.selected).toBeFalsy();
             }));
-        }
         });
 
         describe('Row Editing - Paging', () => {
