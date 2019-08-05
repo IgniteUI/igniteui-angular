623f4220c4ac90e559410756a6af5b84a112ad3c
Zdravko Kolev <ZKolev@infragistics.com>
2019-08-05 15:24:09 +0300

    fix(dialog): Expose positionSettings Input #5576

--- src/app/dialog/dialog.sample.ts
+++ src/app/dialog/dialog.sample.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { IgxDialogComponent, slideOutBottom, slideInTop,
+    PositionSettings, HorizontalAlignment, VerticalAlignment } from 'igniteui-angular';
+import { useAnimation } from '@angular/animations';
 
 @Component({
     selector: 'app-dialog-sample',
     styleUrls: ['dialog.sample.css'],
     templateUrl: 'dialog.sample.html'
 })
-export class DialogSampleComponent {
+export class DialogSampleComponent implements OnInit {
+
+    @ViewChild('alert', { static: true }) public alert: IgxDialogComponent;
+
+    public positionSettings: PositionSettings = {
+        openAnimation: useAnimation(slideInTop, { params: { duration: '2000ms' } }),
+        closeAnimation: useAnimation(slideOutBottom, { params: { duration: '2000ms'} }),
+        horizontalDirection: HorizontalAlignment.Left,
+        verticalDirection: VerticalAlignment.Middle,
+        horizontalStartPoint: HorizontalAlignment.Left,
+        verticalStartPoint: VerticalAlignment.Middle,
+        minSize: { height: 100, width: 100 }
+    };
+
+    public positionSettings2: PositionSettings = {
+        openAnimation: useAnimation(slideInTop),
+        closeAnimation: useAnimation(slideOutBottom)
+    };
+
+    public ngOnInit() {
+        // Set position settings on ngOnInit
+        // this.alert.positionSettings = this.positionSettings2;
+
+        console.log(this.alert.positionSettings);
+    }
 
     onDialogOKSelected(args) {
         // args.event - event
