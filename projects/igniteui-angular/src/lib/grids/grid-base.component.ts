ef365d66b2a2b5fd556d9b8a28e9f5b4292913c7
MKirova <MKirova@DEV-MKIROVA>
2019-08-08 11:21:29 +0300

    chore(*): Fixing lint.

--- projects/igniteui-angular/src/lib/grids/grid-base.component.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.component.ts
@@ -4015,9 +4015,8 @@ export abstract class IgxGridBaseComponent extends DisplayDensityBase implements
         // after grid initial rendering
         // if (!this.columnWidthSetByUser) {
             if (!this.columnWidthSetByUser) {
-                this._columnWidth = this.width !== null ? this.getPossibleColumnWidth() : MINIMUM_COLUMN_WIDTH + 'px'; 
-             }
-            
+                this._columnWidth = this.width !== null ? this.getPossibleColumnWidth() : MINIMUM_COLUMN_WIDTH + 'px';
+            }
             this.columnList.forEach((column: IgxColumnComponent) => {
                 if (this.hasColumnLayouts && parseInt(this._columnWidth, 10)) {
                     const columnWidthCombined = parseInt(this._columnWidth, 10) * (column.colEnd ? column.colEnd - column.colStart : 1);
