0099c37a955f644a224f1b08cf9f77f4b0e87fe9
Deyan Kamburov <dkamburov@infragistics.com>
2019-08-14 19:56:25 +0300

    test(igxChip): Fix tests in group by - use click instead pointerdown/up #5319

--- projects/igniteui-angular/src/lib/grids/grid/grid.groupby.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid.groupby.spec.ts
@@ -1673,15 +1673,11 @@ describe('IgxGrid - GroupBy', () => {
         expect(chips[1].querySelectorAll(CHIP_REMOVE_ICON).length).toEqual(1);
 
         // check click does not allow changing sort dir
-        chips[0].children[0].dispatchEvent(new PointerEvent('pointerdown', { pointerId: 1 }));
-        tick();
-        chips[0].children[0].dispatchEvent(new PointerEvent('pointerup'));
+        chips[0].children[0].click();
         tick();
         fix.detectChanges();
 
-        chips[1].children[0].dispatchEvent(new PointerEvent('pointerdown', { pointerId: 1 }));
-        tick();
-        chips[1].children[0].dispatchEvent(new PointerEvent('pointerup'));
+        chips[1].children[0].click();
         tick();
 
         fix.detectChanges();
