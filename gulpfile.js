7ae24450cff3a5037e791a7b7e3d1861f0501029
Aleksandar Kamenov <aleksandyr.kamenov@gmail.com>
2019-08-23 17:58:58 +0300

    refactor(gulp): exposing typedoc sassdoc new build tasks
    Closes #5614

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,19 @@ const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync').create();
 const del = require('del');
 const gulp = require('gulp');
-const concat = require('gulp-concat');
 const sass = require('gulp-sass');
-const shell = require('gulp-shell');
 const sourcemaps = require('gulp-sourcemaps');
 const postcss = require('gulp-postcss');
 const process = require('process');
 const fs = require('fs');
 const argv = require('yargs').argv;
 const sassdoc = require('sassdoc');
-const ts = require('gulp-typescript');
 const path = require('path');
 const EventEmitter = require('events').EventEmitter;
 const typedocGulp = require('igniteui-typedoc-theme/gulpfile');
 const sassdocGulp = require('igniteui-sassdoc-theme/gulpfile');
-const { series, parallel } = require('gulp');
-const {execSync, spawnSync} = require('child_process');
+const { series } = require('gulp');
+const {spawnSync} = require('child_process');
 const slash = require('slash');
 
 sass.compiler = require('sass');
@@ -37,21 +34,12 @@ const STYLES = {
     }
 };
 
-const TYPEDOC_THEME = {
-    // SRC: `${path.join(__dirname, "node_modules", "igniteui_typedoc_theme", "typedoc", "src")}`,
-    // SRC: `${path.join(__dirname, 'extras', 'docs', 'themes', 'typedoc', 'src')}`,
-    SRC: slash(path.join(__dirname, 'node_modules', 'igniteui-typedoc-theme', 'typedoc', 'src'))
+const DOCS_OUTPUT_PATH = slash(path.join(__dirname, 'dist', 'igniteui-angular', 'docs'));
 
-    // DIST: './extras/docs/themes/typedoc/bin/',
-    // STYLES: {
-    //     ENTRY: './assets/css/main.sass',
-    //     OUT: './assets/css',
-    //     MAPS: './',
-    //     CONFIG: {
-    //         outputStyle: 'compressed'
-    //     }
-    // }
-}
+const TYPEDOC_THEME = {
+    SRC: slash(path.join(__dirname, 'node_modules', 'igniteui-typedoc-theme', 'typedoc', 'src')),
+    OUTPUT: slash(path.join(DOCS_OUTPUT_PATH, 'typescript'))
+};
 
 module.exports.buildStyle =  (cb) => {
     const prefixer = postcss([autoprefixer({
@@ -149,22 +137,22 @@ module.exports.copySchematics = (cb) => {
     cb();
 };
 
-const typedocBuildTheme = (cb) => {
+const typedocBuildTheme = series(typedocGulp.typedocBuild, (cb) => {
     spawnSync(`typedoc`, [TYPEDOC.PROJECT_PATH], { stdio: 'inherit', shell: true });
     cb();
-}
+});
 typedocBuildTheme.displayName = 'typedoc-build:theme';
 
 const browserReload = (cb) => {
     browserSync.reload();
 
     cb();
-}
+};
 
 const typedocServe = (cb) => {
     const config = {
         server: {
-            baseDir: './dist/igniteui-angular/docs/typescript'
+            baseDir: TYPEDOC_THEME.OUTPUT
         },
         port: 3000
     }
@@ -172,7 +160,7 @@ const typedocServe = (cb) => {
     browserSync.init(config);
 
     cb();
-}
+};
 
 function typedocWatchFunc(cb) {
     gulp.watch([
@@ -184,66 +172,72 @@ function typedocWatchFunc(cb) {
 
       cb();
 }
-// typedocWatch.displayName = 'typedoc-watch';
 
-module.exports.typedocServe = series(
-    typedocGulp.typedocBuild,
-    typedocBuildTheme,
-    typedocWatchFunc,
-    typedocServe);
-
-gulp.task('sassdoc-build', sassdocGulp.sassdocBuild);
-
-// const DOCS_OUTPUT_PATH = path.join(__dirname, 'dist', 'igniteui-angular', 'docs');
 
 const TYPEDOC = {
-    EXPORT_JSON_PATH: path.join('dist', 'igniteui-angular', 'docs', 'typescript-exported'),
-    PROJECT_PATH: path.join(__dirname, 'projects', 'igniteui-angular', 'src'),
-    TEMPLATE_STRINGS_PATH: path.join(__dirname, 'extras', 'template', 'strings', 'shell-strings.json')
+    EXPORT_JSON_PATH: slash(path.join(DOCS_OUTPUT_PATH, 'typescript-exported')),
+    PROJECT_PATH: slash(path.join(__dirname, 'projects', 'igniteui-angular', 'src')),
+    TEMPLATE_STRINGS_PATH: slash(path.join(__dirname, 'extras', 'template', 'strings', 'shell-strings.json'))
+};
+
+function typedocBuildExportFn(cb) {
+    spawnSync('typedoc', [TYPEDOC.PROJECT_PATH, "--generate-json", TYPEDOC.EXPORT_JSON_PATH, "--tags", "--params"],
+        { stdio: 'inherit', shell: true });
+    cb();
 }
 
-// gulp.task('typedoc-build:theme', ['typedoc-build'],
-//     shell.task(`typedoc ${TYPEDOC.PROJECT_PATH}`)
-// );
+function typedocImportJsonFn(cb) {
+    spawnSync('typedoc', [TYPEDOC.PROJECT_PATH, "--generate-from-json", TYPEDOC.EXPORT_JSON_PATH],
+        { stdio: 'inherit', shell: true});
+    cb();
+}
 
-// gulp.task('typedoc-build:export',
-//     shell.task(`typedoc ${TYPEDOC.PROJECT_PATH} --generate-json ${TYPEDOC.EXPORT_JSON_PATH} --tags --params`)
-// );
+function createDocsOutputDirFn(cb) {
+    !fs.existsSync(DOCS_OUTPUT_PATH) && fs.mkdirSync(DOCS_OUTPUT_PATH);
+    cb();
+}
 
-// gulp.task('typedoc-build:import', ['typedoc-build'],
-//     shell.task(`typedoc ${TYPEDOC.PROJECT_PATH} --generate-from-json ${TYPEDOC.EXPORT_JSON_PATH}`)
-// );
+function cleanTypedocOutputDirFn(cb) {
+    del.sync(slash(path.join(DOCS_OUTPUT_PATH, 'typescript')));
+    cb();
+}
 
-// gulp.task('create:docs-output-path', () => {
-//     !fs.existsSync(DOCS_OUTPUT_PATH) && fs.mkdirSync(DOCS_OUTPUT_PATH);
-// });
+function typedocBuildDocsJA (cb) {
+        spawnSync('typedoc',[
+            TYPEDOC.PROJECT_PATH,
+            '--generate-from-json',
+            slash(path.join(__dirname, 'i18nRepo', 'typedoc', 'ja')),
+            '--templateStrings',
+            TYPEDOC.TEMPLATE_STRINGS_PATH,
+            '--localize',
+            'jp'], { stdio: 'inherit', shell: true });
 
-// gulp.task('typedoc:clean-docs-dir', () => {
-//     del.sync(`${DOCS_OUTPUT_PATH}typescript`)
-// });
+        cb();
+}
 
-// gulp.task('typedoc-build:doc:ja:localization', ['typedoc-build', 'create:docs-output-path', 'typedoc:clean-docs-dir'],
-//     shell.task(`typedoc ${TYPEDOC.PROJECT_PATH} --generate-from-json ${path.join(__dirname, 'i18nRepo', 'typedoc', 'ja')} --templateStrings ${TYPEDOC.TEMPLATE_STRINGS_PATH} --localize jp`)
-// );
+function typedocBuildDocsEN (cb) {
+        spawnSync('typedoc', [
+            TYPEDOC.PROJECT_PATH,
+            '--localize',
+            'en'], { stdio: 'inherit', shell: true});
 
-// gulp.task('typedoc-build:doc:en:localization', ['typedoc-build', 'create:docs-output-path', 'typedoc:clean-docs-dir'],
-//     shell.task(`typedoc ${TYPEDOC.PROJECT_PATH} --localize en`)
-// );
+        cb();
+}
 
 const SASSDOC = {
     PROJECT_PATH: path.join(__dirname, 'projects', 'igniteui-angular', 'src', 'lib', 'core', 'styles'),
-    DEST: path.join(__dirname, 'dist', 'igniteui-angular', 'docs', 'sass'),
+    DEST: path.join(DOCS_OUTPUT_PATH, 'sass'),
     OPTIONS: path.join(__dirname, '.sassdocrc'),
-}
+};
 
 const sassdocCleanOutputDir = (cb) => {
     del.sync(SASSDOC.DEST);
     cb();
-};
+}
 
 module.exports.sassdocDevMode = sassdocGulp.develop;
 
-module.exports.sassdocBuildJson = (cb) => {
+function sassdocBuildJson(cb) {
     const options = JSON.parse(fs.readFileSync(SASSDOC.OPTIONS, 'utf8'));
 
     const { convert, exportDir } = argv;
@@ -258,7 +252,7 @@ module.exports.sassdocBuildJson = (cb) => {
 
 }
 
-module.exports.sassdocImportJson = (cb) => {
+function sassdocImportJson(cb) {
     const options = JSON.parse(fs.readFileSync(SASSDOC.OPTIONS, 'utf8'));
 
     const {render, importDir} = argv;
@@ -273,7 +267,7 @@ module.exports.sassdocImportJson = (cb) => {
     cb();
 }
 
-module.exports.sassdocBuildJA = series(sassdocCleanOutputDir, sassdocGulp.sassdocBuild, (cb) => {
+function sassdocBuildJA (cb) {
     const pathTranslations = path.join(__dirname, 'i18nRepo', 'sassdoc', 'ja');
     const options = JSON.parse(fs.readFileSync(SASSDOC.OPTIONS, 'utf8'));
 
@@ -286,9 +280,9 @@ module.exports.sassdocBuildJA = series(sassdocCleanOutputDir, sassdocGulp.sassdo
         .pipe(sassdoc(options));
 
     cb();
-});
+}
 
-module.exports.sassdocBuildEN = series(sassdocCleanOutputDir, sassdocGulp.sassdocBuild, (cb) => {
+function sassdocBuildEN (cb) {
     const options = JSON.parse(fs.readFileSync(SASSDOC.OPTIONS, 'utf8'));
 
     options.lang = 'en';
@@ -298,13 +292,38 @@ module.exports.sassdocBuildEN = series(sassdocCleanOutputDir, sassdocGulp.sassdo
         .pipe(sassdoc(options));
 
     cb();
-});
+}
 
-// gulp.task('typedoc-serve', ['typedoc-watch'], () => {
-//     browserSync.init({
-//         server: './dist/igniteui-angular/docs/typescript'
-//     });
+module.exports.createDocsOutputDir = createDocsOutputDirFn;
 
-//     gulp.watch('./dist/igniteui-angular/docs/typescript/**/*')
-//         .on('change', browserSync.reload);
-// });
+/**
+ * Typedoc build tasks
+ */
+module.exports.exportTypedocJson = typedocBuildExportFn;
+module.exports.importTypedocJson = series(typedocGulp.typedocBuild, typedocImportJsonFn);
+module.exports.cleanTypedocOutputDir = cleanTypedocOutputDirFn;
+module.exports.typedocServe = series(
+    typedocBuildTheme,
+    typedocWatchFunc,
+    typedocServe
+);
+module.exports.typedocBuildDocsJA = series(
+    typedocGulp.typedocBuild,
+    this.createDocsOutputDir,
+    this.cleanTypedocOutputDir,
+    typedocBuildDocsJA
+);
+module.exports.typedocBuildDocsEN = series(
+    typedocGulp.typedocBuild,
+    this.createDocsOutputDir,
+    this.cleanTypedocOutputDir,
+    typedocBuildDocsEN
+);
+
+/**
+ * Sassdoc build tasks
+ */
+module.exports.sassdocImportJson = sassdocImportJson;
+module.exports.sassdocBuildJson = sassdocBuildJson;
+module.exports.sassdocBuildJA = series(sassdocCleanOutputDir, sassdocGulp.sassdocBuild, sassdocBuildJA);
+module.exports.sassdocBuildEN = series(sassdocCleanOutputDir, sassdocGulp.sassdocBuild, sassdocBuildEN);
