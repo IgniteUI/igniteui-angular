30fa0e690369856d5e8462c1d959d164b01b617c
Aleksandar Kamenov <aleksandyr.kamenov@gmail.com>
2019-08-29 17:19:02 +0300

    feat(slider): incorrectly positioned label ie11
    Closes #5210

---
+++ projects/igniteui-angular/src/lib/slider/label/thumb-label.component.ts
@@ -0,0 +1,63 @@
+import { Component, NgModule, Input, TemplateRef, HostBinding, ElementRef } from '@angular/core';
+import { SliderHandle } from '../slider.common';
+
+@Component({
+    selector: 'igx-thumb-label',
+    templateUrl: 'thumb-label.component.html'
+})
+export class IgxThumbLabelComponent {
+    private _active: boolean;
+
+    @Input()
+    public value: number;
+
+    @Input()
+    public templateRef: TemplateRef<any>;
+
+    @Input()
+    public context: any;
+
+    @Input()
+    public type: SliderHandle;
+
+    @Input()
+    public continuous: boolean;
+
+    @HostBinding('class.igx-slider__thumb-from')
+    public get thumbFromClass() {
+        return this.type === SliderHandle.FROM;
+    }
+
+    @HostBinding('class.igx-slider__thumb-to')
+    public get thumbToClass() {
+        return this.type === SliderHandle.TO;
+    }
+
+    @HostBinding('class.igx-slider__thumb-from--active')
+    public get thumbFromActiveClass() {
+        return this.type === SliderHandle.FROM && this.active;
+    }
+
+    @HostBinding('class.igx-slider__thumb-to--active')
+    public get thumbToActiveClass() {
+        return this.type === SliderHandle.TO && this.active;
+    }
+
+    constructor(private _elementRef: ElementRef) { }
+
+    public get nativeElement() {
+        return this._elementRef.nativeElement;
+    }
+
+    public get active() {
+        return this._active;
+    }
+
+    public set active(val: boolean) {
+        if (this.continuous) {
+            return;
+        }
+
+        this._active = val;
+    }
+}
