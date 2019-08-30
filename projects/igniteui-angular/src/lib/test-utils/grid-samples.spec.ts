84b0b472ea9f1433bc38b8c0ad1c0152652b765c
Diyan Dimitrov <ddimitrov@infragistics.com>
2019-08-30 18:24:10 +0300

    test(adv-filtering): update test component #5496

--- projects/igniteui-angular/src/lib/test-utils/grid-samples.spec.ts
+++ projects/igniteui-angular/src/lib/test-utils/grid-samples.spec.ts
@@ -1134,7 +1134,7 @@ export class IgxTestExcelFilteringDatePickerComponent extends IgxGridFilteringCo
 }
 
 @Component({
-    template: `<igx-grid [data]="data" height="500px" [allowFiltering]="true" [showToolbar]="true">
+    template: `<igx-grid [data]="data" height="500px" [allowAdvancedFiltering]="true" [showToolbar]="true">
         <igx-column width="100px" [field]="'ID'" [header]="'ID'" [hasSummary]="true"></igx-column>
         <igx-column width="100px" [field]="'ProductName'" dataType="string"></igx-column>
         <igx-column width="100px" [field]="'Downloads'" dataType="number" [hasSummary]="true"></igx-column>
