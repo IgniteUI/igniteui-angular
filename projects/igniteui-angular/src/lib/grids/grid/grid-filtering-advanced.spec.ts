ce4e913a2ce3f8ed15da1542e0e631fa1f9c053d
Tacho <tachojelev@gmail.com>
2019-09-05 16:53:18 +0300

    chore(*): remove unused test imports #5496

--- projects/igniteui-angular/src/lib/grids/grid/grid-filtering-advanced.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid-filtering-advanced.spec.ts
@@ -1,33 +1,21 @@
-import { DebugElement } from '@angular/core';
-import { async, fakeAsync, TestBed, tick, flush } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
+import { async, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { IgxInputDirective } from '../../directives/input/input.directive';
 import { IgxGridComponent } from './grid.component';
 import { IgxGridModule } from './index';
-import { IgxButtonDirective } from '../../directives/button/button.directive';
 import { UIInteractions, wait } from '../../test-utils/ui-interactions.spec';
 import { configureTestSuite } from '../../test-utils/configure-suite';
 import {
     IgxNumberFilteringOperand,
-    IgxDateFilteringOperand,
-    IgxBooleanFilteringOperand,
     IgxStringFilteringOperand
 } from '../../data-operations/filtering-condition';
-import { IgxDatePickerComponent } from '../../date-picker/date-picker.component';
 import { GridFunctions } from '../../test-utils/grid-functions.spec';
 import { changei18n, getCurrentResourceStrings } from '../../core/i18n/resources';
-import { registerLocaleData } from '@angular/common';
-import localeDE from '@angular/common/locales/de';
 import { FilteringExpressionsTree } from '../../data-operations/filtering-expressions-tree';
-import { FilteringLogic, IFilteringExpression } from '../../data-operations/filtering-expression.interface';
-import { IgxChipComponent } from '../../chips/chip.component';
-import { DisplayDensity } from '../../core/density';
-import { SampleTestData } from '../../test-utils/sample-test-data.spec';
+import { FilteringLogic } from '../../data-operations/filtering-expression.interface';
 import {
-    IgxGridFilteringComponent, IgxGridAdvancedFilteringComponent
+    IgxGridAdvancedFilteringComponent
 } from '../../test-utils/grid-samples.spec';
-import { HelperUtils, resizeObserverIgnoreError } from '../../test-utils/helper-utils.spec';
+import { resizeObserverIgnoreError } from '../../test-utils/helper-utils.spec';
 
 const ADVANCED_FILTERING_OPERATOR_LINE_AND_CSS_CLASS = 'igx-filter-tree__line--and';
 const ADVANCED_FILTERING_OPERATOR_LINE_OR_CSS_CLASS = 'igx-filter-tree__line--or';
