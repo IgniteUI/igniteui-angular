31bc0d04feec63e589bfab257bf9118c6963d31c
MKirova <MKirova@DEV-MKIROVA>
2019-10-01 13:51:31 +0300

    chore(*): Fixing tests.

--- projects/igniteui-angular/src/lib/directives/for-of/for_of.directive.spec.ts
+++ projects/igniteui-angular/src/lib/directives/for-of/for_of.directive.spec.ts
@@ -24,6 +24,7 @@ import { take } from 'rxjs/operators';
 import { UIInteractions, wait } from '../../test-utils/ui-interactions.spec';
 
 import { configureTestSuite } from '../../test-utils/configure-suite';
+import { IgxForOfSyncService } from './for_of.sync.service';
 import { resizeObserverIgnoreError } from '../../test-utils/helper-utils.spec';
 
 describe('IgxForOf directive -', () => {
@@ -1218,8 +1219,9 @@ export class TestIgxForOfDirective<T> extends IgxForOfDirective<T> {
         public differs: IterableDiffers,
         public fResolver: ComponentFactoryResolver,
         public changeDet: ChangeDetectorRef,
-        public zone: NgZone) {
-        super(viewContainer, template, differs, fResolver, changeDet, zone);
+        public zone: NgZone,
+        protected syncService: IgxForOfSyncService) {
+        super(viewContainer, template, differs, fResolver, changeDet, zone, syncService);
     }
     public scrStepArray = [];
     public scrTopArray = [];
