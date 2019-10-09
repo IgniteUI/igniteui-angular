8829ca75195bd2b0e8411b63973d3b8065786a29
Tacho <tachojelev@gmail.com>
2019-10-09 15:02:02 +0300

    test(adv-filtering): add test for col-group #5886

--- projects/igniteui-angular/src/lib/grids/grid/grid-filtering-advanced.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid-filtering-advanced.spec.ts
@@ -13,6 +13,7 @@ import { changei18n, getCurrentResourceStrings } from '../../core/i18n/resources
 import { FilteringExpressionsTree } from '../../data-operations/filtering-expressions-tree';
 import { FilteringLogic } from '../../data-operations/filtering-expression.interface';
 import {
+    IgxGridAdvancedFilteringColumnGroupComponent,
     IgxGridAdvancedFilteringComponent
 } from '../../test-utils/grid-samples.spec';
 import { resizeObserverIgnoreError } from '../../test-utils/helper-utils.spec';
@@ -27,6 +28,7 @@ describe('IgxGrid - Advanced Filtering', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
+                IgxGridAdvancedFilteringColumnGroupComponent,
                 IgxGridAdvancedFilteringComponent
             ],
             imports: [
@@ -2661,6 +2663,35 @@ describe('IgxGrid - Advanced Filtering', () => {
                 });
             }));
         });
+    });
+
+    describe('', () => {
+        let fix, grid: IgxGridComponent;
+        beforeEach(fakeAsync(() => {
+            resizeObserverIgnoreError();
+            fix = TestBed.createComponent(IgxGridAdvancedFilteringColumnGroupComponent);
+            grid = fix.componentInstance.grid;
+            fix.detectChanges();
+        }));
+
+        it('Should not display column groups in advanced filtering dialog.', fakeAsync(() => {
+            // Open dialog through API.
+            grid.openAdvancedFilteringDialog();
+            fix.detectChanges();
+
+            // Click the initial 'Add And Group' button.
+            const addAndGroupButton = GridFunctions.getAdvancedFilteringInitialAddGroupButtons(fix)[0];
+            addAndGroupButton.click();
+            tick(100);
+            fix.detectChanges();
+
+            // Open column dropdown and verify that there are no column groups present.
+            GridFunctions.clickAdvancedFilteringColumnSelect(fix);
+            fix.detectChanges();
+            const dropdownValues = GridFunctions.getAdvancedFilteringSelectDropdownItems(fix).map((x: any) => x.innerText);
+            const expectedValues = ['ID', 'ProductName', 'Downloads', 'Released', 'ReleaseDate', 'Another Field'];
+            verifyEqualArrays(dropdownValues, expectedValues);
+        }));
     });
 });
 
