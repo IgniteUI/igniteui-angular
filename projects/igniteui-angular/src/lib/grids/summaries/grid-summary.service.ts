7eff3589413819730310bf6a157285fe24225efb
ddincheva <desislava9dincheva@gmail.com>
2019-10-11 11:44:16 +0300

    chore(*): remove unnecessary code

--- projects/igniteui-angular/src/lib/grids/summaries/grid-summary.service.ts
+++ projects/igniteui-angular/src/lib/grids/summaries/grid-summary.service.ts
@@ -105,11 +105,10 @@ export class IgxGridSummaryService {
             this.summaryCacheMap.set(rowID, rowSummaries);
         }
         if (!this.hasSummarizedColumns || !data) {return rowSummaries; }
-        const allData = data;
         this.grid.columnList.filter(col => col.hasSummary).forEach((column) => {
             if (!rowSummaries.get(column.field)) {
                 rowSummaries.set(column.field,
-                    column.summaries.operate(data.map(r => r[column.field]), allData, column.field));
+                    column.summaries.operate(data.map(r => r[column.field]), data, column.field));
             }
         });
         return rowSummaries;
