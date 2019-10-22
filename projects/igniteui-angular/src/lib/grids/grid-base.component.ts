a143874db2f025c749859a1bc6a5e0ccbf6b80e1
Tacho <tachojelev@gmail.com>
2019-10-22 11:20:29 +0300

    fix(filter-row): use reflow #6005

--- projects/igniteui-angular/src/lib/grids/grid-base.component.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.component.ts
@@ -1077,7 +1077,7 @@ export abstract class IgxGridBaseComponent extends DisplayDensityBase implements
         if (this.filteringService.isFilterRowVisible) {
             this.filteringRow.close();
         }
-        this.notifyChanges(true);
+        this.reflow();
     }
 
     /**
