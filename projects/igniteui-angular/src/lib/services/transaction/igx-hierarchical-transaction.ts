d4ef4943a6ade9dbe79755c696d437b262ac4afd
Vasil Mihalkov <VMihalkov@infragistics.com>
2019-10-28 13:46:02 +0200

    fix(HGrid Transactions): fix transactions #5500

--- projects/igniteui-angular/src/lib/services/transaction/igx-hierarchical-transaction.ts
+++ projects/igniteui-angular/src/lib/services/transaction/igx-hierarchical-transaction.ts
@@ -65,10 +65,18 @@ export class IgxHierarchicalTransactionService<T extends HierarchicalTransaction
                 transactions = transactions.filter(t => t.id === id);
             }
             DataUtil.mergeHierarchicalTransactions(data, transactions, childDataKey, primaryKey, true);
+            this.clear(id);
         } else {
-            super.commit(data, id);
+            if (id !== undefined) {
+                super.commit(data, id);
+            } else {
+                this._states.forEach((s: S, key: any) => {
+                    if (this.updateRecord(data, s)) {
+                        super.clear(key);
+                    }
+                });
+            }
         }
-        this.clear(id);
     }
 
     //  TODO: remove this method. Force cloning to strip child arrays when needed instead
