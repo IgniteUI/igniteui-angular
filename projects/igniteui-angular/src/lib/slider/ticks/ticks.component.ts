a6f7af9f253372501f004b61c329228297063e74
Aleksandar Kamenov <aleksandyr.kamenov@gmail.com>
2019-11-11 11:29:57 +0200

    feat(slider): drop svg and change template structure
    Closes #4594

--- projects/igniteui-angular/src/lib/slider/ticks/ticks.component.ts
+++ projects/igniteui-angular/src/lib/slider/ticks/ticks.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ElementRef, AfterViewInit, TemplateRef } from '@angular/core';
+import { Component, Input, ElementRef, AfterViewInit, TemplateRef, HostBinding } from '@angular/core';
 import { TicksOrientation, TickLabelsOrientation } from '../slider.common';
 
 @Component({
@@ -53,6 +53,28 @@ export class IgxTicksComponent {
     public tickLabelTemplateRef: TemplateRef<any>;
 
     /**
+     * @hidden
+     */
+    @HostBinding('class.igx-ticks')
+    public ticksClass = true;
+
+    /**
+     * @hidden
+     */
+    @HostBinding('class.igx-ticks__top')
+    public get ticksTopClass() {
+        return this.ticksOrientation === TicksOrientation.top;
+    }
+
+    /**
+     * @hidden
+     */
+    @HostBinding('class.igx-ticks__has-primary')
+    public get hasPrimaryClass() {
+        return this.primaryTicks > 0;
+    }
+
+    /**
      * Returns the template context corresponding to
      * {@link IgxTickLabelTemplateDirective}
      *
@@ -76,37 +98,37 @@ export class IgxTicksComponent {
         };
     }
 
-    /**
-     * @hidden
-     */
-    public get ticksStep() {
-        const ticksStep = this.ticksContainer / this.ticksLength;
-        const stepUnit = ticksStep / (this.ticksLength - 1);
+    // /**
+    //  * @hidden
+    //  */
+    // public get ticksStep() {
+    //     const ticksStep = this.ticksContainer / this.ticksLength;
+    //     const stepUnit = ticksStep / (this.ticksLength - 1);
 
-        return this.ticksContainer ?
-                ticksStep + stepUnit : 0;
-    }
+    //     return this.ticksContainer ?
+    //             ticksStep + stepUnit : 0;
+    // }
 
-    /**
-     * @hidden
-     */
-    public get horizontalLabel() {
-        return this.tickLabelsOrientation === TickLabelsOrientation.horizontal;
-    }
+    // /**
+    //  * @hidden
+    //  */
+    // public get horizontalLabel() {
+    //     return this.tickLabelsOrientation === TickLabelsOrientation.horizontal;
+    // }
 
-    /**
-     * @hidden
-     */
-    public get topToBottomLabel() {
-        return this.tickLabelsOrientation === TickLabelsOrientation.toptobottom;
-    }
+    // /**
+    //  * @hidden
+    //  */
+    // public get topToBottomLabel() {
+    //     return this.tickLabelsOrientation === TickLabelsOrientation.toptobottom;
+    // }
 
-    /**
-     * @hidden
-     */
-    public get tickLabelOrientation() {
-        return `rotate(${this.horizontalLabel ? 0 : this.topToBottomLabel ? 90 : -90})`;
-    }
+    // /**
+    //  * @hidden
+    //  */
+    // public get tickLabelOrientation() {
+    //     return `rotate(${this.horizontalLabel ? 0 : this.topToBottomLabel ? 90 : -90})`;
+    // }
 
     /**
      * @hidden
@@ -117,28 +139,28 @@ export class IgxTicksComponent {
                 this.secondaryTicks > 0 ? this.secondaryTicks + 1 : 0;
     }
 
-        /**
-     * @hidden
-     */
-    public tickXOffset(idx: number) {
-        return idx * this.ticksStep - 1;
-    }
+    // /**
+    //  * @hidden
+    //  */
+    // public tickXOffset(idx: number) {
+    //     return idx * this.ticksStep - 1;
+    // }
 
-    /**
-     * @hidden
-     */
-    public tickYOffset(idx: number) {
-        const trackHeight = this.track.nativeElement.offsetHeight;
-        const primaryTickOffset = this._primaryTicksWidth / 2 + this._defaultTickYOffset + trackHeight;
-        const secondaryTickOffset = this._secondaryTicksWidth / 2 + this._defaultTickYOffset + trackHeight;
-        const yOffset = this.isPrimary(idx) ? primaryTickOffset : secondaryTickOffset;
+    // /**
+    //  * @hidden
+    //  */
+    // public tickYOffset(idx: number) {
+    //     const trackHeight = this.track.nativeElement.offsetHeight;
+    //     const primaryTickOffset = this._primaryTicksWidth / 2 + this._defaultTickYOffset + trackHeight;
+    //     const secondaryTickOffset = this._secondaryTicksWidth / 2 + this._defaultTickYOffset + trackHeight;
+    //     const yOffset = this.isPrimary(idx) ? primaryTickOffset : secondaryTickOffset;
 
-        return this.ticksOrientation === TicksOrientation.top ? - (yOffset - trackHeight) : yOffset;
-    }
+    //     return this.ticksOrientation === TicksOrientation.top ? - (yOffset - trackHeight) : yOffset;
+    // }
 
-    public hiddenTickLabels(idx: number) {
-        return this.isPrimary(idx) ? this.primaryTickLabels : this.secondaryTickLabels;
-    }
+    // public hiddenTickLabels(idx: number) {
+    //     return this.isPrimary(idx) ? this.primaryTickLabels : this.secondaryTickLabels;
+    // }
 
     /**
      * @hidden
@@ -148,12 +170,12 @@ export class IgxTicksComponent {
             idx % (this.secondaryTicks + 1) === 0;
     }
 
-    /**
-     * @hidden
-     */
-    public strokeWidth(idx: number) {
-        return this.isPrimary(idx) ? this._primaryTicksWidth : this._secondaryTicksWidth;
-    }
+    // /**
+    //  * @hidden
+    //  */
+    // public strokeWidth(idx: number) {
+    //     return this.isPrimary(idx) ? this._primaryTicksWidth : this._secondaryTicksWidth;
+    // }
 
     /**
      * @hidden
@@ -169,63 +191,63 @@ export class IgxTicksComponent {
         return labelVal.toFixed(2);
     }
 
-    /**
-     * @hidden
-     */
-    public tickLabelXOffset(index: number) {
-        return this.ticksOrientation === TicksOrientation.top && !this.horizontalLabel ?
-            this.labelXOffset(index) * -1 :
-            this.labelXOffset(index);
-    }
+    // /**
+    //  * @hidden
+    //  */
+    // public tickLabelXOffset(index: number) {
+    //     return this.ticksOrientation === TicksOrientation.top && !this.horizontalLabel ?
+    //         this.labelXOffset(index) * -1 :
+    //         this.labelXOffset(index);
+    // }
 
-    /**
-     * @hidden
-     */
-    public tickLabelYOffset(index: number) {
-        return this.ticksOrientation === TicksOrientation.top && this.horizontalLabel ?
-            this.labelYOffset(index) * -1 :
-            this.labelYOffset(index);
-    }
+    // /**
+    //  * @hidden
+    //  */
+    // public tickLabelYOffset(index: number) {
+    //     return this.ticksOrientation === TicksOrientation.top && this.horizontalLabel ?
+    //         this.labelYOffset(index) * -1 :
+    //         this.labelYOffset(index);
+    // }
 
-    /**
-     * @hidden
-     */
-    public textAnchor() {
-        if (this.horizontalLabel) {
-            return 'middle';
-        }
+    // /**
+    //  * @hidden
+    //  */
+    // public textAnchor() {
+    //     if (this.horizontalLabel) {
+    //         return 'middle';
+    //     }
 
-        if (this.ticksOrientation === TicksOrientation.top) {
-            return this.topToBottomLabel ? 'end' : 'start';
-        }
+    //     if (this.ticksOrientation === TicksOrientation.top) {
+    //         return this.topToBottomLabel ? 'end' : 'start';
+    //     }
 
-        if (this.ticksOrientation === TicksOrientation.bottom) {
-            return this.topToBottomLabel ? 'start' : 'end';
-        }
-    }
+    //     if (this.ticksOrientation === TicksOrientation.bottom) {
+    //         return this.topToBottomLabel ? 'start' : 'end';
+    //     }
+    // }
 
-    /**
-     * @hidden
-     */
-    public dominantBaseline() {
-        return this.ticksOrientation === TicksOrientation.top && this.horizontalLabel ? 'baseline' :
-            this.horizontalLabel ? 'hanging' : !this.topToBottomLabel ? 'central' : 'middle';
-    }
+    // /**
+    //  * @hidden
+    //  */
+    // public dominantBaseline() {
+    //     return this.ticksOrientation === TicksOrientation.top && this.horizontalLabel ? 'baseline' :
+    //         this.horizontalLabel ? 'hanging' : !this.topToBottomLabel ? 'central' : 'middle';
+    // }
 
-    private labelXOffset(index: number) {
-        const diffTicksOffset = index % (this.secondaryTicks + 1) === 0 ? 0 : this._defaultTickYOffset;
-        const verticalDir = this.topToBottomLabel ? 1 : -1;
-        return this.horizontalLabel ?
-            this.tickXOffset(index) :
-            (Math.abs(this.tickYOffset(index)) + this._primaryTicksWidth + diffTicksOffset) * verticalDir;
-    }
+    // private labelXOffset(index: number) {
+    //     const diffTicksOffset = index % (this.secondaryTicks + 1) === 0 ? 0 : this._defaultTickYOffset;
+    //     const verticalDir = this.topToBottomLabel ? 1 : -1;
+    //     return this.horizontalLabel ?
+    //         this.tickXOffset(index) :
+    //         (Math.abs(this.tickYOffset(index)) + this._primaryTicksWidth + diffTicksOffset) * verticalDir;
+    // }
 
-    private labelYOffset(index: number) {
-        const diffTicksOffset = index % (this.secondaryTicks + 1) === 0 ? 0 : this._defaultTickYOffset;
-        const verticalDir = this.topToBottomLabel ? 1 : -1;
-        return this.horizontalLabel ?
-            Math.abs(this.tickYOffset(index)) + this._primaryTicksWidth + diffTicksOffset :
-            (-this.tickXOffset(index)) * verticalDir;
+    // private labelYOffset(index: number) {
+    //     const diffTicksOffset = index % (this.secondaryTicks + 1) === 0 ? 0 : this._defaultTickYOffset;
+    //     const verticalDir = this.topToBottomLabel ? 1 : -1;
+    //     return this.horizontalLabel ?
+    //         Math.abs(this.tickYOffset(index)) + this._primaryTicksWidth + diffTicksOffset :
+    //         (-this.tickXOffset(index)) * verticalDir;
 
-    }
+    // }
 }
