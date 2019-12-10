41a5f53f4bbce4b36de029debef9d23d6dfb379d
Simeon Simeonoff <sim.simeonoff@gmail.com>
2019-12-10 11:36:07 +0200

    feat(progressbar): use gradients in circular progress

--- projects/igniteui-angular/src/lib/progressbar/progressbar.common.ts
+++ projects/igniteui-angular/src/lib/progressbar/progressbar.common.ts
@@ -1,10 +1,16 @@
 import { Directive, TemplateRef } from '@angular/core';
 
-
 @Directive({
     selector: '[igxProcessBarText]'
 })
 export class IgxProcessBarTextTemplateDirective {
+    constructor(public template: TemplateRef<any>) { }
+}
+
+@Directive({
+    selector: '[igxProgressBarGradient]'
+})
+export class IgxProgressBarGradientTemplateDirective {
     constructor(public template: TemplateRef<any>) { }
 }
 
