2f854bc1c631e168702da6661d903954334bfa74
Nadia Robakova <nrobakova@infragistics.com>
2019-12-19 13:43:31 +0200

    chore(*): rever fix for bug 4728

--- projects/igniteui-angular/src/lib/grids/hierarchical-grid/hierarchical-grid.component.ts
+++ projects/igniteui-angular/src/lib/grids/hierarchical-grid/hierarchical-grid.component.ts
@@ -398,6 +398,13 @@ export class IgxHierarchicalGridComponent extends IgxHierarchicalGridBaseDirecti
         return this.rootGrid._outletDirective;
     }
 
+     /**
+     * @hidden
+     */
+    public get parentRowOutletDirective() {
+        return this === this.rootGrid ? null : this.rootGrid.rowEditingOutletDirective;
+    }
+
     /**
      * @hidden
      */
