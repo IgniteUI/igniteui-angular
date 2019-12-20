60ec1df5eb33fd11dd566972ae1374bc88a425ad
Plamena Miteva <plamenamiteva@abv.bg>
2019-12-20 13:40:21 +0200

    fix(grid): аdd contentChild decorator to dragIndicatorIconTemplate in HGrid #4769 (#6371)

--- projects/igniteui-angular/src/lib/grids/hierarchical-grid/hierarchical-grid-base.component.ts
+++ projects/igniteui-angular/src/lib/grids/hierarchical-grid/hierarchical-grid-base.component.ts
@@ -94,27 +94,6 @@ export abstract class IgxHierarchicalGridBaseComponent extends IgxGridBaseCompon
     @ViewChild('dragIndicatorIconBase', { read: TemplateRef, static: true })
     public dragIndicatorIconBase: TemplateRef<any>;
 
-
-    /**
-     * The custom template, if any, that should be used when rendering the row drag indicator icon
-     *
-     * ```typescript
-     * // Set in typescript
-     * const myCustomTemplate: TemplateRef<any> = myComponent.customTemplate;
-     * myComponent.dragIndicatorIconTemplate = myCustomTemplate;
-     * ```
-     * ```html
-     * <!-- Set in markup -->
-     *  <igx-grid #grid>
-     *      ...
-     *      <ng-template igxDragIndicatorIcon>
-     *          <igx-icon fontSet="material">info</igx-icon>
-     *      </ng-template>
-     *  </igx-grid>
-     * ```
-     */
-    public dragIndicatorIconTemplate: TemplateRef<any> = null;
-
     constructor(
         public selectionService: IgxGridSelectionService,
         crudService: IgxGridCRUDService,
