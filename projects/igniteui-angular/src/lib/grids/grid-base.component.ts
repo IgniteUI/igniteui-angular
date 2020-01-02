036e60d352f324d592cf9beb5a970e959b849471
Zdravko Kolev <ZKolev@infragistics.com>
2020-01-02 13:48:37 +0200

    fix(revert): revert change of PR #6376

--- projects/igniteui-angular/src/lib/grids/grid-base.component.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.component.ts
@@ -2072,7 +2072,7 @@ export abstract class IgxGridBaseComponent extends DisplayDensityBase implements
      * @hidden
      */
     public get parentRowOutletDirective() {
-        return null;
+        return this.outletDirective;
     }
 
     /**
