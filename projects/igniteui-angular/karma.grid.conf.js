e24bee023045d69510a111ec551257d3cad9c228
Nadia Robakova <nrobakova@infragistics.com>
2020-01-08 16:56:35 +0200

    chore(*): use old version of chrome

--- projects/igniteui-angular/karma.grid.conf.js
+++ projects/igniteui-angular/karma.grid.conf.js
@@ -42,7 +42,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['Chrome'],
     singleRun: false
   });
 };
