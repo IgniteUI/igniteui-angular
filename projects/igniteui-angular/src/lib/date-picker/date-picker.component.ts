96da06b3f11469126e31fdd6dbaca1ac91a9fe7d
Nadia Robakova <nrobakova@infragistics.com>
2020-01-08 15:38:11 +0200

    chore(*): reverting fix for 6088

--- projects/igniteui-angular/src/lib/date-picker/date-picker.component.ts
+++ projects/igniteui-angular/src/lib/date-picker/date-picker.component.ts
@@ -892,15 +892,15 @@ export class IgxDatePickerComponent implements IDatePicker, ControlValueAccessor
             filter(overlay => overlay.id === this._componentID),
             takeUntil(this._destroy$)).subscribe((event) => {
                 this.onClosing.emit(event);
-                // // If canceled in a user onClosing handler
-                // if (event.cancel) {
-                //     return;
-                // }
-                // // Do not focus the input if clicking outside in dropdown mode
-                // const input = this.getEditElement();
-                // if (input && !(event.event && this.mode === InteractionMode.DropDown)) {
-                //     input.focus();
-                // }
+                // If canceled in a user onClosing handler
+                if (event.cancel) {
+                    return;
+                }
+                // Do not focus the input if clicking outside in dropdown mode
+                const input = this.getEditElement();
+                if (input && !(event.event && this.mode === InteractionMode.DropDown)) {
+                    input.focus();
+                }
             });
 
         if (this.mode === InteractionMode.DropDown) {
@@ -910,7 +910,6 @@ export class IgxDatePickerComponent implements IDatePicker, ControlValueAccessor
             }
             this.inputMask = DatePickerUtil.getInputMask(this.dateFormatParts);
         }
-
     }
 
     ngAfterViewInit() {
@@ -1278,10 +1277,6 @@ export class IgxDatePickerComponent implements IDatePicker, ControlValueAccessor
 
         // TODO: remove this line after deprecating 'onClose'
         this.onClose.emit(this);
-
-        if (this.getEditElement()) {
-            this.getEditElement().focus();
-        }
     }
 
     private _initializeCalendarContainer(componentInstance: IgxCalendarContainerComponent) {
