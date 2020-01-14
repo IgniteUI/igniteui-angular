e55146d23703ad62ca30cf2c6b4dab3fa31e9737
Martin Dragnev <MDragnev@infragistics.com>
2020-01-14 17:42:23 +0200

    chore(*): code refactoring

--- projects/igniteui-angular/src/lib/grids/hierarchical-grid/hierarchical-grid-navigation.service.ts
+++ projects/igniteui-angular/src/lib/grids/hierarchical-grid/hierarchical-grid-navigation.service.ts
@@ -617,9 +617,7 @@ export class IgxHierarchicalGridNavigationService extends IgxGridNavigationServi
                 this.focusNextChild(next, visibleColumnIndex, nextParentGrid);
             } else {
                 const nextGrid = grid || nextParentGrid;
-                this.focusNextRow(next, visibleColumnIndex > nextGrid.columnList.length
-                     ? nextParentGrid.columnList.length - 1
-                     : visibleColumnIndex, nextGrid);
+                this.focusNextRow(next, visibleColumnIndex, grid || nextParentGrid);
             }
         } else if (verticalScroll.scrollTop !==
             verticalScroll.scrollHeight - nextParentGrid.verticalScrollContainer.igxForContainerSize && !atLastChunk) {
@@ -678,6 +676,8 @@ export class IgxHierarchicalGridNavigationService extends IgxGridNavigationServi
     }
 
     private focusNextRow(elem, visibleColumnIndex, grid, isSummary?) {
+        const lastCellIndex = grid.unpinnedColumns[grid.unpinnedColumns.length - 1].visibleIndex;
+        visibleColumnIndex = Math.min(lastCellIndex, visibleColumnIndex);
         const cellSelector = this.getCellSelector(visibleColumnIndex, isSummary);
         if (grid.navigation.isColumnFullyVisible(visibleColumnIndex) || grid.rowList.length === 0) {
             const cell =
@@ -704,6 +704,8 @@ export class IgxHierarchicalGridNavigationService extends IgxGridNavigationServi
     }
 
     private focusPrevRow(elem, visibleColumnIndex, grid, inChild?, isSummary?) {
+        const lastCellIndex = grid.unpinnedColumns[grid.unpinnedColumns.length - 1].visibleIndex;
+        visibleColumnIndex = Math.min(lastCellIndex, visibleColumnIndex);
         if (grid.navigation.isColumnFullyVisible(visibleColumnIndex)) {
             const cellSelector = this.getCellSelector(visibleColumnIndex, isSummary);
             const cells = elem.querySelectorAll(`${cellSelector}[data-visibleIndex="${visibleColumnIndex}"]`);
