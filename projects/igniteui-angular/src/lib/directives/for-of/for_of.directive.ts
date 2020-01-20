b9f26dc42703dcd62bc737b615d2240429df513f
MKirova <MKirova@DEV-MKIROVA>
2020-01-20 11:59:36 +0200

    fix(igxFoOf): Make sure last throttled value is emitted to ensure final state is valid. Update tests for grid that have variable heights to await final throttle before starting interactions.

--- projects/igniteui-angular/src/lib/directives/for-of/for_of.directive.ts
+++ projects/igniteui-angular/src/lib/directives/for-of/for_of.directive.ts
@@ -404,7 +404,8 @@ export class IgxForOfDirective<T> implements OnInit, OnChanges, DoCheck, OnDestr
             });
             const destructor = takeUntil<any>(this.destroy$);
             this.contentResizeNotify.pipe(destructor,
-            filter(() => this.igxForContainerSize && this.igxForOf && this.igxForOf.length > 0), throttleTime(40))
+            filter(() => this.igxForContainerSize && this.igxForOf && this.igxForOf.length > 0),
+            throttleTime(40, undefined, {leading: true, trailing: true}))
             .subscribe(() => {
                 this._zone.runTask(() => {
                     this.updateSizes();
