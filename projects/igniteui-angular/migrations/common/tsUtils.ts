a1668ba01eb9d7ead17a83c65f36cda37f39138a
Damyan Petev <damyan.petev@gmail.com>
2020-02-04 20:07:44 +0200

    refactor(migrations): limit identifier rename from package imports
    Resolve initial identifier match only in our import statements -
    was previously using a general identifier search w/o context.
    Move to TypeScript's Language service to search for rename positions.

--- projects/igniteui-angular/migrations/common/tsUtils.ts
+++ projects/igniteui-angular/migrations/common/tsUtils.ts
@@ -1,5 +1,8 @@
 // tslint:disable-next-line:no-implicit-dependencies
 import * as ts from 'typescript';
+import { Tree } from '@angular-devkit/schematics';
+
+export const PACKAGE_IMPORT = 'igniteui-angular';
 
 /** Returns an source file */
 // export function getFileSource(sourceText: string): ts.SourceFile {
@@ -57,3 +60,99 @@ export function getImportModulePositions(sourceText: string, startsWith: string)
     }
     return positions;
 }
+
+/** Filters out statements to named imports (e.g. `import {x, y}`) from PACKAGE_IMPORT */
+const namedImportFilter = (statement: ts.Statement) => {
+    if (statement.kind === ts.SyntaxKind.ImportDeclaration &&
+        ((statement as ts.ImportDeclaration).moduleSpecifier as ts.StringLiteral).text === PACKAGE_IMPORT) {
+
+        const clause = (statement as ts.ImportDeclaration).importClause;
+        return clause && clause.namedBindings && clause.namedBindings.kind === ts.SyntaxKind.NamedImports;
+    }
+    return false;
+};
+
+export function getRenamePositions(sourcePath: string, name: string, service: ts.LanguageService):
+        Array<{start: number, end: number}> {
+
+    const source = service.getProgram().getSourceFile(sourcePath);
+    const positions = [];
+
+    const imports = source.statements.filter(<(a: ts.Statement) => a is ts.ImportDeclaration>namedImportFilter);
+    if (!imports.length) {
+        return positions;
+    }
+    const elements: ts.NodeArray<ts.ImportSpecifier> =
+        imports
+        .map(x => (x.importClause.namedBindings as ts.NamedImports).elements)
+        .reduce((prev, current) => prev.concat(current) as unknown as ts.NodeArray<ts.ImportSpecifier>);
+
+    for (const elem of elements) {
+        if (elem.propertyName && elem.propertyName.text === name) {
+            // alias imports `igxClass as smth` -> <propertyName> as <name>
+            // other references are only for the name portion
+            positions.push({ start: elem.propertyName.getStart(), end: elem.propertyName.getEnd() });
+            break;
+        }
+
+        if (!elem.propertyName && elem.name.text === name) {
+            const renames = service.findRenameLocations(sourcePath, elem.name.getStart(), false, false, false);
+            if (renames) {
+                const renamesPos = renames.map(x => ({ start: x.textSpan.start, end: x.textSpan.start + x.textSpan.length}) );
+                positions.push(...renamesPos);
+            }
+        }
+    }
+
+    return positions;
+}
+
+//#region Language Service
+
+/**
+ * Create a TypeScript language service
+ * @param filePaths Paths for files to include for the language service host
+ * @param host Virtual FS host
+ * @param options Typescript compiler options for the service
+ */
+export function getLanguageService(filePaths: string[], host: Tree, options: ts.CompilerOptions = {}): ts.LanguageService {
+    // https://stackoverflow.com/a/14221483
+    const fileVersions = new Map<string, number>();
+    patchHostOverwrite(host, fileVersions);
+
+    const servicesHost: ts.LanguageServiceHost = {
+        getCompilationSettings: () => options,
+        getScriptFileNames: () => filePaths,
+        getScriptVersion: fileName => {
+            // return host.actions.filter(x => x.path === fileName && x.kind !== 'c').length.toString();
+            const version = fileVersions.get(fileName) || 0;
+            return version.toString();
+        },
+        getScriptSnapshot: fileName => {
+            if (!host.exists(fileName)) {
+                return undefined;
+            }
+            return ts.ScriptSnapshot.fromString(host.read(fileName).toString());
+        },
+        getCurrentDirectory: () => process.cwd(),
+        getDefaultLibFileName: opts => ts.getDefaultLibFilePath(opts),
+        fileExists: fileName => {
+            return filePaths.indexOf(fileName) !== -1;
+        }
+    };
+    return ts.createLanguageService(servicesHost, ts.createDocumentRegistry());
+}
+
+function patchHostOverwrite(host: Tree, fileVersions: Map<string, number>) {
+    const original = host.overwrite;
+    host.overwrite = (path: string, content: Buffer | string) => {
+        if (!fileVersions.has(path)) {
+            fileVersions.set(path, 1);
+        } else {
+            fileVersions.set(path, fileVersions.get(path) + 1);
+        }
+        original.call(host, path, content);
+    };
+}
+
+//#endregion
