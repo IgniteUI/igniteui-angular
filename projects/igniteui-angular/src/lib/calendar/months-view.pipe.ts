1db5ed70374464d24dcc4f7bc6bc6c3fcfdad4fe
Hristo Anastasov <HAnastasov@infragistics.com>
2020-02-19 15:42:42 +0200

    fix(calendar): keep months order #6375

---
+++ projects/igniteui-angular/src/lib/calendar/months-view.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Calendar } from './calendar';
+
+@Pipe({
+    name: 'IgxMonthViewSlots'
+})
+export class IgxMonthViewSlotsCalendar implements PipeTransform {
+    public transform(monthViews: number) {
+        return new Array(monthViews);
+    }
+}
+
+@Pipe({
+    name: 'IgxGetViewDate'
+})
+export class IgxGetViewDateCalendar implements PipeTransform {
+    private calendar: Calendar;
+    constructor() {
+        this.calendar = new Calendar();
+    }
+    public transform(index: number, viewDate: Date, wholeDate = true) {
+        const date = this.calendar.timedelta(viewDate, 'month', index);
+        return wholeDate ? date : date.getMonth();
+    }
+}
