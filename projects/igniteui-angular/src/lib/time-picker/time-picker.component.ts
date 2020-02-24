4ab63867bda153141cb31da46edf245863a54dda
wnvko <milko.venkov@gmail.com>
2020-02-24 14:46:47 +0200

    test(time-picker): fix failing tests, #6471
    # Conflicts:
    #	projects/igniteui-angular/src/lib/time-picker/time-picker.component.ts

--- projects/igniteui-angular/src/lib/time-picker/time-picker.component.ts
+++ projects/igniteui-angular/src/lib/time-picker/time-picker.component.ts
@@ -549,10 +549,14 @@ export class IgxTimePickerComponent implements
      * @hidden
      */
     @ViewChild('input', { read: ElementRef })
-    private input: ElementRef;
+    private inputElementRef: ElementRef;
 
     @ViewChild(IgxInputDirective, { read: IgxInputDirective})
     private _inputDirective: IgxInputDirective;
+
+    /** @hidden @internal */
+    @ContentChild(IgxInputDirective)
+    protected input: IgxInputDirective;
 
     /**
      * @hidden
@@ -894,8 +898,8 @@ export class IgxTimePickerComponent implements
      * @hidden
      */
     public ngAfterViewInit(): void {
-        if (this.mode === InteractionMode.DropDown && this.input) {
-            fromEvent(this.input.nativeElement, 'keydown').pipe(
+        if (this.mode === InteractionMode.DropDown && this.inputElementRef) {
+            fromEvent(this.inputElementRef.nativeElement, 'keydown').pipe(
                 throttle(() => interval(0, animationFrameScheduler)),
                 takeUntil(this._destroy$)
             ).subscribe((event: KeyboardEvent) => {
@@ -1383,11 +1387,11 @@ export class IgxTimePickerComponent implements
     }
 
     private _getCursorPosition(): number {
-        return this.input.nativeElement.selectionStart;
+        return this.inputElementRef.nativeElement.selectionStart;
     }
 
     private _setCursorPosition(start: number, end: number = start): void {
-        this.input.nativeElement.setSelectionRange(start, end);
+        this.inputElementRef.nativeElement.setSelectionRange(start, end);
     }
 
     private _updateEditableInput(): void {
@@ -2089,8 +2093,8 @@ export class IgxTimePickerComponent implements
         }
 
         // minor hack for preventing cursor jumping in IE
-        this.displayValue = this.inputFormat.transform(displayVal);
-        this.input.nativeElement.value = this.displayValue;
+        this._displayValue = this.inputFormat.transform(displayVal);
+        this.inputElementRef.nativeElement.value = this._displayValue;
         this._setCursorPosition(cursor);
 
         requestAnimationFrame(() => {
@@ -2125,10 +2129,11 @@ export class IgxTimePickerComponent implements
 
     private updateValidity() {
         this._onTouchedCallback();
+        const inputDirective = this._inputDirective || this.input;
         if (this._ngControl && !this._ngControl.valid) {
-            this._inputDirective.valid = IgxInputState.INVALID;
+            inputDirective.valid = IgxInputState.INVALID;
         } else {
-            this._inputDirective.valid = IgxInputState.INITIAL;
+            inputDirective.valid = IgxInputState.INITIAL;
         }
     }
 }
