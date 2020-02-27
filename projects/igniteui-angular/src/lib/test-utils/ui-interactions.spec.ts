9824e7a2a179dba0ae40fd341ed50559a877644b
plamenamiteva <plamenamiteva@abv.bg>
2020-02-27 12:17:47 +0200

    test(combo): change getKeyboardEvent method#6450

--- projects/igniteui-angular/src/lib/test-utils/ui-interactions.spec.ts
+++ projects/igniteui-angular/src/lib/test-utils/ui-interactions.spec.ts
@@ -9,19 +9,19 @@ declare var Touch: {
     new(prop): Touch;
 };
 export class UIInteractions {
-    public static enterEvent = { key: 'Enter', stopPropagation: () => { }, preventDefault: () => { } };
-    public static endEvent = { key: 'End', stopPropagation: () => { }, preventDefault: () => { } };
-    public static homeEvent = { key: 'Home', stopPropagation: () => { }, preventDefault: () => { } };
-    public static spaceEvent = { key: 'Space', stopPropagation: () => { }, preventDefault: () => { }, stopImmediatePropagation: () => { } };
-    public static tabEvent = { key: 'Tab', stopPropagation: () => { }, preventDefault: () => { } };
     public static escapeEvent = { key: 'Escape', stopPropagation: () => { }, preventDefault: () => { } };
-    public static arrowDownEvent = { key: 'ArrowDown', stopPropagation: () => { }, preventDefault: () => { } };
-    public static altAndArrowDownEvent = { key: 'ArrowDown', altKey: true, stopPropagation: () => { }, preventDefault: () => { } };
-    public static arrowUpEvent = { key: 'ArrowUp', stopPropagation: () => { }, preventDefault: () => { } };
-    public static altAndArrowUpEvent = { key: 'ArrowUp', altKey: true, stopPropagation: () => { }, preventDefault: () => { } };
     public static clickEvent = new MouseEvent('click');
 
-    public static getKeyboardEvent(eventType: string, keyboardEvent: KeyboardEventInit) {
+    public static getKeyboardEvent(eventType: string, keyPressed: string, altKey = false, shift = false, ctrl = false) {
+        const keyboardEvent = {
+            key: keyPressed,
+            altKey: altKey,
+            shiftKey: shift,
+            ctrlKey: ctrl,
+            stopPropagation: () => { },
+            stopImmediatePropagation: () => { },
+            preventDefault: () => { }
+        };
         return new KeyboardEvent(eventType, keyboardEvent);
     }
 
