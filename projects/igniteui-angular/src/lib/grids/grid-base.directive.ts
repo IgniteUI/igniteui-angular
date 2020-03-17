39b1a0230a5d4ec2cd320376dbbbea5a8c37866a
Deyan Kamburov <dkamburov@infragistics.com>
2020-03-17 10:42:49 +0200

    chore(hgrid): Apply review notes

--- projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
@@ -3007,16 +3007,11 @@ export class IgxGridBaseDirective extends DisplayDensityBase implements
     }
 
     public set expansionStates(value) {
-        value = this.filterExpansionStates(value);
         this._expansionStates = new Map<any, boolean>(value);
         this.expansionStatesChange.emit(this._expansionStates);
         if (this.gridAPI.grid) {
             this.cdr.detectChanges();
         }
-    }
-
-    protected filterExpansionStates(value) {
-        return value;
     }
 
    /**
