dd37b6d30980681c94dae780ed81fc3c0a29ed33
Deyan Kamburov <dkamburov@infragistics.com>
2020-03-23 17:59:50 +0200

    test(row-pinning): Add test for row pinning and hiding #6640

--- projects/igniteui-angular/src/lib/grids/grid/row-pinning.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/row-pinning.spec.ts
@@ -611,6 +611,50 @@ describe('Row Pinning #grid', () => {
             verifyDOMMatchesLayoutSettings(gridUnpinnedRow, fix.componentInstance.colGroups);
         });
     });
+    describe(' Hiding', () => {
+        beforeEach(fakeAsync(() => {
+            fix = TestBed.createComponent(GridRowPinningComponent);
+            fix.detectChanges();
+            grid = fix.componentInstance.instance;
+            tick();
+            fix.detectChanges();
+        }));
+
+        it('should hide columns in pinned and unpinned area', () => {
+            // pin 2nd data row
+            grid.pinRow(fix.componentInstance.data[1]);
+            fix.detectChanges();
+            const hiddenCol = grid.columns[1];
+            hiddenCol.hidden = true;
+            fix.detectChanges();
+
+            const pinnedCells = grid.pinnedRows[0].cells;
+            expect(pinnedCells.filter(cell => cell.column.field === hiddenCol.field).length).toBe(0);
+
+            const unpinnedCells = grid.rowList.first.cells;
+            expect(unpinnedCells.filter(cell => cell.column.field === hiddenCol.field).length).toBe(0);
+
+            expect(pinnedCells.length).toBe(unpinnedCells.length);
+
+            const headerCells = grid.headerCellList;
+            expect(headerCells.filter(cell => cell.column.field === hiddenCol.field).length).toBe(0);
+
+            expect(grid.pinnedRows.length).toBe(1);
+            const pinRowContainer = fix.debugElement.queryAll(By.css(FIXED_ROW_CONTAINER));
+            expect(pinRowContainer.length).toBe(1);
+            expect(pinRowContainer[0].children.length).toBe(1);
+            expect(pinRowContainer[0].children[0].context.rowID).toBe(fix.componentInstance.data[1]);
+            expect(pinRowContainer[0].children[0].nativeElement).toBe(grid.getRowByIndex(0).nativeElement);
+
+            expect(grid.getRowByIndex(1).rowID).toBe(fix.componentInstance.data[0]);
+            expect(grid.getRowByIndex(2).rowID).toBe(fix.componentInstance.data[2]);
+
+            // 1 records pinned + 2px border
+            expect(grid.pinnedRowHeight).toBe(grid.renderedRowHeight + 2);
+            const expectedHeight = parseInt(grid.height, 10) - grid.pinnedRowHeight - 18 - grid.theadRow.nativeElement.offsetHeight;
+            expect(grid.calcHeight - expectedHeight).toBeLessThanOrEqual(1);
+        });
+    });
 });
 
 @Component({
