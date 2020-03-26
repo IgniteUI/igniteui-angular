f824cf42f64701d5555ba630a1db1a5e9f8371bd
MKirova <MKirova@DEV-MKIROVA>
2020-03-26 14:39:05 +0200

    chore(*): Refactor styles to accomodate expanders and show a handle for resize in the middle.

--- projects/igniteui-angular/src/lib/splitter/splitter.module.ts
+++ projects/igniteui-angular/src/lib/splitter/splitter.module.ts
@@ -4,10 +4,11 @@ import { IgxSplitBarComponent } from './splitbar/split-bar.component';
 import { IgxSplitterPaneComponent } from './splitpane/split-pane.component';
 import { IgxSplitterComponent } from './splitter.component';
 import { SplitterDirective } from './splitter.directives';
+import { IgxIconModule } from '../icon';
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule, IgxIconModule
     ],
     declarations: [
         IgxSplitterComponent,
