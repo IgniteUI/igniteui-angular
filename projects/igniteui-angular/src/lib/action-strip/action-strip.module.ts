fe11a6e8bb47e9d59a921da48ca55816487815c3
Deyan Kamburov <dkamburov@infragistics.com>
2020-04-22 17:45:06 +0300

    chore(*): Fix build error

--- projects/igniteui-angular/src/lib/action-strip/action-strip.module.ts
+++ projects/igniteui-angular/src/lib/action-strip/action-strip.module.ts
@@ -4,10 +4,10 @@ import { IgxGridPinningActionsComponent } from './grid-actions/grid-pinning-acti
 import { IgxGridEditingActionsComponent } from './grid-actions/grid-editing-actions.component';
 import { IgxGridActionsBaseDirective } from './grid-actions/grid-actions-base.directive';
 import { CommonModule } from '@angular/common';
-import { IgxDropDownModule } from '../drop-down';
+import { IgxDropDownModule } from '../drop-down/index';
 import { IgxToggleModule } from '../directives/toggle/toggle.directive';
 import { IgxButtonModule } from '../directives/button/button.directive';
-import { IgxIconModule } from '../icon';
+import { IgxIconModule } from '../icon/index';
 import { IgxRippleModule } from '../directives/ripple/ripple.directive';
 
 /**
