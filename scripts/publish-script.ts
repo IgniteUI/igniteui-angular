040cea118daf0823a07d92924cfc871c8cbb391f
HristoP96 <HPopov@infragistics.com>
2020-04-23 13:45:44 +0300

    chore(*): update publish script

--- scripts/publish-script.ts
+++ scripts/publish-script.ts
@@ -1,16 +1,10 @@
 import * as child_process from 'child_process';
 import * as path from 'path';
-import * as util from 'util';
 const dist = '../dist/';
 
 const projectNames = ['igniteui-angular', 'igniteui-angular-extras'];
-const exec = util.promisify(child_process.exec);
 
-projectNames.forEach( async(project) => {
+projectNames.forEach( (project) => {
     const cwd = path.join(__dirname, `${dist + project}`);
-    const {stdout, stderr} = await exec(`npm publish --tag ${process.env.NPM_TAG}`, {cwd});
-    if (stderr) {
-        throw new Error(`error: ${stderr}`);
-      }
-      console.log(`${stdout}`);
+    child_process.execSync(`npm publish --tag ${process.env.NPM_TAG}`, {cwd});
 });
