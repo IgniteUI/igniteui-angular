5993ace7cc1752903af76a1484d0cc0e8168cec8
MKirova <MKirova@DEV-MKIROVA>
2020-05-05 16:21:09 +0300

    chore(*): Move child grid column initialization on grid ContentInit.

--- projects/igniteui-angular/src/lib/grids/hierarchical-grid/hierarchical-grid.component.ts
+++ projects/igniteui-angular/src/lib/grids/hierarchical-grid/hierarchical-grid.component.ts
@@ -377,6 +377,14 @@ export class IgxHierarchicalGridComponent extends IgxHierarchicalGridBaseDirecti
         }
     }
 
+
+    protected setupColumns() {
+        if (this.parentIsland && this.parentIsland.childColumns.length > 0 && !this.autoGenerate) {
+            this.createColumnsList(this.parentIsland.childColumns.toArray());
+        }
+        super.setupColumns();
+    }
+
     protected onColumnsChanged(change: QueryList<IgxColumnComponent>) {
         Promise.resolve().then(() => {
             this.updateColumnList();
