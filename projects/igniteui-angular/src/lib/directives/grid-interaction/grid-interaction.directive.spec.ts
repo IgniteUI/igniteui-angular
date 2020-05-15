b49d74f5d5b4c6cc496f0e83cf10ce1c48c30585
Deyan Kamburov <dkamburov@infragistics.com>
2020-05-15 18:11:52 +0300

    chore(grid-interaction): Add hierarchical grid test

--- projects/igniteui-angular/src/lib/directives/grid-interaction/grid-interaction.directive.spec.ts
+++ projects/igniteui-angular/src/lib/directives/grid-interaction/grid-interaction.directive.spec.ts
@@ -10,6 +10,7 @@ import { IgxActionStripComponent, IgxActionStripModule } from '../../action-stri
 import { IgxGridInteractionModule } from './grid-interaction.directive';
 import { IgxTreeGridComponent, IgxTreeGridModule } from '../../grids/tree-grid';
 import { SampleTestData } from '../../test-utils/sample-test-data.spec';
+import { IgxHierarchicalGridComponent, IgxRowIslandComponent, IgxHierarchicalGridModule, IgxHierarchicalRowComponent } from '../../grids/hierarchical-grid';
 
 
 describe('igxGridInteractionDirective #grid ', () => {
@@ -18,18 +19,21 @@ describe('igxGridInteractionDirective #grid ', () => {
     let actionStrip: IgxActionStripComponent;
     let grid: IgxGridComponent;
     let treegrid: IgxTreeGridComponent;
+    let hierarchicalGrid: IgxHierarchicalGridComponent;
     configureTestSuite();
     beforeAll(async(() => {
         TestBed.configureTestingModule({
             declarations: [
                 IgxGridInteractionTestComponent,
-                IgxTreeGridInteractionTestComponent
+                IgxTreeGridInteractionTestComponent,
+                IgxHierarchicalGridTestBaseComponent
             ],
             imports: [
                 NoopAnimationsModule,
                 IgxActionStripModule,
                 IgxGridModule,
                 IgxTreeGridModule,
+                IgxHierarchicalGridModule,
                 IgxIconModule,
                 IgxGridInteractionModule
             ]
@@ -67,6 +71,31 @@ describe('igxGridInteractionDirective #grid ', () => {
 
         it('should allow row interaction', () => {
             const firstCell = fixture.debugElement.query(By.css('igx-tree-grid-cell'));
+            UIInteractions.simulateMouseEvent('mouseover', firstCell.nativeElement, 0, 0);
+            fixture.detectChanges();
+            expect(fixture.componentInstance.interactionStart).toBe(true);
+            UIInteractions.simulateMouseEvent('mouseleave', firstCell.nativeElement, 0, 0);
+            fixture.detectChanges();
+            expect(fixture.componentInstance.interactionStart).toBe(false);
+        });
+    });
+
+    describe("in igxHierarchicalGrid", () => {
+        beforeEach(() => {
+            fixture = TestBed.createComponent(IgxHierarchicalGridTestBaseComponent);
+            fixture.detectChanges();
+            hierarchicalGrid = fixture.componentInstance.hgrid;
+        });
+
+        it('should allow row interaction', () => {
+            const row = hierarchicalGrid.getRowByIndex(0) as IgxHierarchicalRowComponent;
+            UIInteractions.simulateClickAndSelectEvent(row.expander);
+            fixture.detectChanges();
+
+            const child1Grids =  fixture.debugElement.queryAll(By.css('igx-child-grid-row'));
+            const child1Grid = child1Grids[0].query(By.css('igx-hierarchical-grid'));
+
+            const firstCell = child1Grid.query(By.css('igx-hierarchical-grid-cell'));
             UIInteractions.simulateMouseEvent('mouseover', firstCell.nativeElement, 0, 0);
             fixture.detectChanges();
             expect(fixture.componentInstance.interactionStart).toBe(true);
@@ -150,4 +179,55 @@ class IgxTreeGridInteractionTestComponent {
     @ViewChild(IgxTreeGridComponent, { static: true }) public treeGrid: IgxTreeGridComponent;
     public data = SampleTestData.employeeScrollingData();
     public interactionStart = false;
+}
+
+@Component({
+    template: `
+    <igx-hierarchical-grid #grid1 [data]="data"
+     [autoGenerate]="false" [height]="'400px'" [width]="width" #hierarchicalGrid>
+     <igx-column field="ID"></igx-column>
+     <igx-column field="ProductName"></igx-column>
+        <igx-row-island
+        [igxRowInteraction]="{start:['mouseover'], end: ['mouseleave']}"
+        (rowInteractionStart)="interactionStart = true"
+        (rowInteractionEnd)="interactionStart = false"
+        [key]="'childData'" [autoGenerate]="false" #rowIsland>
+            <igx-column field="ID"></igx-column>
+            <igx-column field="ProductName"></igx-column>
+            <igx-column field="Col1"></igx-column>
+            <igx-column field="Col2"></igx-column>
+            <igx-column field="Col3"></igx-column>
+            <igx-row-island [key]="'childData'" [autoGenerate]="true" #rowIsland2 >
+            </igx-row-island>
+        </igx-row-island>
+    </igx-hierarchical-grid>`
+})
+export class IgxHierarchicalGridTestBaseComponent {
+    public data;
+    public interactionStart = false;
+    public width = '500px';
+    @ViewChild('hierarchicalGrid', { read: IgxHierarchicalGridComponent, static: true }) public hgrid: IgxHierarchicalGridComponent;
+    @ViewChild('rowIsland', { read: IgxRowIslandComponent, static: true }) public rowIsland: IgxRowIslandComponent;
+    @ViewChild('rowIsland2', { read: IgxRowIslandComponent, static: true }) public rowIsland2: IgxRowIslandComponent;
+
+    constructor() {
+        // 3 level hierarchy
+        this.data = this.generateDataUneven(20, 3);
+    }
+    generateDataUneven(count: number, level: number, parendID: string = null) {
+        const prods = [];
+        const currLevel = level;
+        let children;
+        for (let i = 0; i < count; i++) {
+            const rowID = parendID ? parendID + i : i.toString();
+            if (level > 0 ) {
+               // Have child grids for row with even id less rows by not multiplying by 2
+               children = this.generateDataUneven((i % 2 + 1) * Math.round(count / 3) , currLevel - 1, rowID);
+            }
+            prods.push({
+                ID: rowID, ChildLevels: currLevel,  ProductName: 'Product: A' + i, 'Col1': i,
+                'Col2': i, 'Col3': i, childData: children, childData2: children });
+        }
+        return prods;
+    }
 }
