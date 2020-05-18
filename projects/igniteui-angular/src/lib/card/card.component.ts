6bba286ad1ef91c52df4dd5657e56af66b9c0506
Konstantin Dinev <kdinev@mail.bw.edu>
2020-05-18 11:25:07 +0300

    chore(*): updating some more depencencies

--- projects/igniteui-angular/src/lib/card/card.component.ts
+++ projects/igniteui-angular/src/lib/card/card.component.ts
@@ -346,7 +346,6 @@ export class IgxCardActionsComponent implements OnInit, OnChanges {
     public reverse = false;
 
     ngOnChanges(changes: SimpleChanges) {
-        change = changes;
         for (const prop in changes) {
             if (prop === 'vertical') {
                 this.isVerticalSet = true;
