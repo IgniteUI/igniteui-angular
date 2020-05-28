b80e470907cf3e0ebd101b1a6e5096d45674b535
Nadia Robakova <nrobakova@infragistics.com>
2020-05-28 20:34:41 +0300

    chore(*): reset auto watch ti true

--- projects/igniteui-angular/karma.azure.hierarchical-grid.conf.js
+++ projects/igniteui-angular/karma.azure.hierarchical-grid.conf.js
@@ -40,7 +40,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
+    autoWatch: true,
     browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
