a5688653c83790245a6fec016a9ff594b790c971
Nadia Robakova <nrobakova@infragistics.com>
2020-05-28 18:51:58 +0300

    chore(*):  set clearContext to true

--- projects/igniteui-angular/karma.azure.non-grid.conf.js
+++ projects/igniteui-angular/karma.azure.non-grid.conf.js
@@ -21,7 +21,7 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      clearContext: true, // leave Jasmine Spec Runner output visible in browser
       jasmine: {
         random: false
       },
