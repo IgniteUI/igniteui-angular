e1dfc5a420c553d83a20faddbfbdcf140b5705d8
Nadia Robakova <nrobakova@infragistics.com>
2020-05-29 10:16:12 +0300

    chore(*): extend browserNoActivityTimeout

--- projects/igniteui-angular/karma.azure.grid.conf.js
+++ projects/igniteui-angular/karma.azure.grid.conf.js
@@ -40,6 +40,7 @@ module.exports = function (config) {
     },
     port: 9876,
     colors: true,
+    browserNoActivityTimeout: 60000,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadlessNoSandbox'],
