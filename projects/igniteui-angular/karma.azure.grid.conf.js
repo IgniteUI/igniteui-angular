47d04ab70534b3c3e67e50376b388c0cb35728c4
Nadia Robakova <nrobakova@infragistics.com>
2020-06-01 21:45:50 +0300

    chore(*): small update to rerun the tests

--- projects/igniteui-angular/karma.azure.grid.conf.js
+++ projects/igniteui-angular/karma.azure.grid.conf.js
@@ -36,7 +36,7 @@ module.exports = function (config) {
     },
     reporters: ['junit'],
     junitReporter: {
-        outputDir: ''
+      outputDir: ''
     },
     port: 9876,
     colors: true,
