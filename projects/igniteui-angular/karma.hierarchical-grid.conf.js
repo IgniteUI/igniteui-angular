2761ccb2b555875febfb454671efd1495ac813d6
Nadia Robakova <nrobakova@infragistics.com>
2020-06-05 12:33:29 +0300

    chore(*): add no-sandbox flas to the karma configs

--- projects/igniteui-angular/karma.hierarchical-grid.conf.js
+++ projects/igniteui-angular/karma.hierarchical-grid.conf.js
@@ -41,7 +41,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu'],
+        debug: false
+      }
+    },
     singleRun: false
   });
 };
