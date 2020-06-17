9f3fc6e0910210977cfb254de56f9292fa8a5a5e
Vasil Mihalkov <VMihalkov@infragistics.com>
2020-06-17 15:05:50 +0300

    feat(grid): Add Pin/Unpin All functionality #6549

--- projects/igniteui-angular/src/lib/grids/pinning/column-pinning.component.ts
+++ projects/igniteui-angular/src/lib/grids/pinning/column-pinning.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, ChangeDetectorRef } from '@angular/core';
+import { Component, ChangeDetectorRef, Input } from '@angular/core';
 import { ColumnChooserBaseDirective } from '../column-chooser-base';
 import { IgxColumnPinningItemDirective } from './pinning.directive';
 
@@ -11,6 +11,79 @@ import { IgxColumnPinningItemDirective } from './pinning.directive';
     templateUrl: './column-pinning.component.html'
 })
 export class IgxColumnPinningComponent extends ColumnChooserBaseDirective {
+    /**
+     * Returns a boolean indicating whether the `PIN ALL` button is disabled.
+     * ```typescript
+     * let isPinAlldisabled =  this.columnPinning.disablePinAll;
+     * ```
+     * @memberof IgxColumnPinningComponent
+     */
+    @Input()
+    get disablePinAll(): boolean {
+        if (!this.columnItems || this.columnItems.length < 1 ||
+            this.pinnedColumnsCount === this.columns.length) {
+            return true;
+        } else if (this.pinnableColumns.length < 1 ||
+            this.pinnableColumns.length === this.pinnableColumns.filter((col) => col.value).length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /**
+     * Returns a boolean indicating whether the `UNPIN ALL` button is disabled.
+     * ```typescript
+     * let isUnpinAlldisabled =  this.columnPinning.disableUnpinAll;
+     * ```
+     * @memberof IgxColumnPinningComponent
+     */
+    @Input()
+    get disableUnpinAll(): boolean {
+        if (!this.columnItems || this.columnItems.length < 1 ||
+            this.pinnedColumnsCount < 1 || this.pinnableColumns.length < 1) {
+            return true;
+        } else if (this.pinnableColumns.length === this.pinnableColumns.filter((col) => !col.value).length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /**
+     * Sets/gets the text of the button that pins all columns if they are unpinned.
+     * ```typescript
+     * let pinAllButtonText =  this.columnPinning.pinAllText;
+     * ```
+     *
+     * ```html
+     * <igx-column-pinning [pinAllText] = "'Pin Columns'"></igx-column-pinning>
+     * ```
+     * @memberof IgxColumnPinningComponent
+     */
+    @Input()
+    public pinAllText = 'Pin All';
+    /**
+     * Sets/gets the text of the button that unpins all columns if they are pinned.
+     * ```typescript
+     * let unpinAllButtonText =  this.columnPinning.unpinAllText;
+     * ```
+     *
+     * ```html
+     * <igx-column-pinning [unpinAllText] = "'Unpin Columns'"></igx-column-pinning>
+     * ```
+     * @memberof IgxColumnPinningComponent
+     */
+    @Input()
+    public unpinAllText = 'Unpin All';
+    /**
+     * Gets the count of the pinned columns.
+     * ```typescript
+     * let pinnedColumnsCount =  this.columnPinning.pinnedColumnsCount;
+     * ```
+     * @memberof IgxColumnPinningComponent
+     */
+    public get pinnedColumnsCount() {
+        return (this.columns) ? this.columns.filter((col) => col.pinned).length : 0;
+    }
 
     constructor(public cdr: ChangeDetectorRef) {
         super(cdr);
@@ -45,6 +118,34 @@ export class IgxColumnPinningComponent extends ColumnChooserBaseDirective {
      */
     public get pinnableColumns() {
         return this.columnItems.filter((col) => !col.pinningDisabled);
+    }
+
+    /**
+     * Pins all columns in the grid.
+     * ```typescript
+     * this.columnPinning.pinAllColumns();
+     * ```
+     * @memberof IgxColumnPinningComponent
+     */
+    public pinAllColumns() {
+        const collection = this.pinnableColumns;
+        for (const col of collection) {
+            col.value = true;
+        }
+    }
+
+    /**
+     * Unpins all columns in the grid.
+     * ```typescript
+     * this.columnPinning.unpinAllColumns();
+     * ```
+     * @memberof IgxColumnPinningComponent
+     */
+    public unpinAllColumns() {
+        const collection = this.pinnableColumns;
+        for (const col of collection) {
+            col.value = false;
+        }
     }
 }
 
