13cc45f915a9df7fef1672f4c4d382576720b2f5
plamenamiteva <plamenamiteva@abv.bg>
2020-07-15 18:05:40 +0300

    chore(combo): change property name to searchTerm #7282

--- projects/igniteui-angular/src/lib/combo/combo.component.ts
+++ projects/igniteui-angular/src/lib/combo/combo.component.ts
@@ -100,7 +100,7 @@ export interface IComboSelectionChangeEventArgs extends CancelableEventArgs, IBa
 /** Event emitted when the igx-combo's search input changes */
 export interface IComboSearchInputEventArgs extends CancelableEventArgs {
     /** The change that has been made to the search input */
-    change: string;
+    searchTerm: string;
 }
 
 export interface IComboItemAdditionEvent extends IBaseEventArgs {
@@ -989,7 +989,7 @@ export class IgxComboComponent extends DisplayDensityBase implements IgxComboBas
     public handleInputChange(event?: string) {
         if (event !== undefined) {
             const args: IComboSearchInputEventArgs = {
-                change: event,
+                searchTerm: event,
                 cancel: false
             };
             this.onSearchInput.emit(args);
