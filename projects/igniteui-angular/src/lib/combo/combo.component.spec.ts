af45ba8bfcc7ea1daeadde93c2e4cfbb52f90ac2
plamenamiteva <plamenamiteva@abv.bg>
2020-07-16 13:59:18 +0300

    chore(combo): extend IBaseEventArgs & rename searchTerm #7282

--- projects/igniteui-angular/src/lib/combo/combo.component.spec.ts
+++ projects/igniteui-angular/src/lib/combo/combo.component.spec.ts
@@ -532,7 +532,7 @@ describe('igxCombo', () => {
             expect(combo.onSearchInput.emit).toHaveBeenCalledTimes(0);
 
             const args = {
-                searchTerm: 'Fake',
+                searchText: 'Fake',
                 cancel: false
             };
             combo.handleInputChange('Fake');
@@ -540,7 +540,7 @@ describe('igxCombo', () => {
             expect(combo.onSearchInput.emit).toHaveBeenCalledTimes(1);
             expect(combo.onSearchInput.emit).toHaveBeenCalledWith(args);
 
-            args.searchTerm = '';
+            args.searchText = '';
             combo.handleInputChange('');
             expect(matchSpy).toHaveBeenCalledTimes(3);
             expect(combo.onSearchInput.emit).toHaveBeenCalledTimes(2);
