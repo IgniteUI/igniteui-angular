3d141475c5314c68c91730bd955970139e5c0d0e
Viktor Aladzhov <valadzhov@infragistics.com>
2020-07-23 15:20:59 +0300

    feat(transactions): Refactor commit method, #5205 (#7838)

---
+++ projects/igniteui-angular/src/lib/services/transaction/hierarchical-transaction.ts
@@ -0,0 +1,20 @@
+import { TransactionService, HierarchicalState, HierarchicalTransaction } from './transaction';
+
+export interface HierarchicalTransactionService<T extends HierarchicalTransaction, S extends HierarchicalState>
+    extends TransactionService<T, S> {
+    /**
+     * Applies all transactions over the provided data
+     * @param data Data source to update
+     * @param id Optional record id to commit transactions for
+     */
+    commit(data: any[], id?: any): void;
+    /**
+     * Applies all transactions over the provided data
+     * @param data Data source to update
+     * @param primaryKey Primary key of the hierarchical data
+     * @param childDataKey Key of child data collection
+     * @param id Optional record id to commit transactions for
+     */
+    // tslint:disable-next-line: unified-signatures
+    commit(data: any[], primaryKey: any, childDataKey: any, id?: any): void;
+}
