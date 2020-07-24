9bebb0f0b05044e68576aed4159f11a355f47e15
Hristo Popov <HPopov@infragistics.com>
2020-07-24 13:36:31 +0300

    feat(grid): Add Column Sorting Indicators (#7782)
    feat(grid): Add Column Sorting Indicators
    
    Co-authored-by: Zdravko Kolev <zkolev@infragistics.com>
    Co-authored-by: Silvia Ivanova <SIvanova@infragistics.com>

---
+++ projects/igniteui-angular/src/lib/grids/headers/sorting-index.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { ISortingExpression } from '../../data-operations/sorting-expression.interface';
+
+@Pipe({
+  name: 'sortingIndex',
+  pure: true
+})
+export class SortingIndexPipe implements PipeTransform {
+
+  transform(columnField: string, sortingExpressions: ISortingExpression[]): number {
+    let sortIndex = sortingExpressions.findIndex(expression => expression.fieldName === columnField);
+    return sortIndex !== -1 ? ++sortIndex : null;
+  }
+
+}
+
