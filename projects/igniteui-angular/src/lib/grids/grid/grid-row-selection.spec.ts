09d53cc9ceecc35a688055fda437384b7352e58c
ddincheva <desislava9dincheva@gmail.com>
2020-07-30 17:03:18 +0300

    chore(*): remove accidentally added test

--- projects/igniteui-angular/src/lib/grids/grid/grid-row-selection.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid-row-selection.spec.ts
@@ -1704,20 +1704,6 @@ describe('IgxGrid - Row Selection #grid', () => {
             GridSelectionFunctions.verifyHeaderRowCheckboxState(grid, false, true);
         }));
 
-        it('Should bind selectedRows properly', () => {
-            fix.componentInstance.selectedRows = [1, 2, 3];
-            fix.detectChanges();
-
-            expect(grid.getRowByIndex(0).selected).toBeTrue();
-            expect(grid.getRowByIndex(4).selected).toBeFalse();
-
-            fix.componentInstance.selectedRows = [4, 5, 6];
-            fix.detectChanges();
-
-            expect(grid.getRowByIndex(3).selected).toBeTrue();
-            expect(grid.getRowByIndex(0).selected).toBeFalse();
-        });
-
         it('Row Pinning: should update checkbox status correctly when there is pinned row and groupBy', () => {
             grid.pinRow(2);
             fix.detectChanges();
