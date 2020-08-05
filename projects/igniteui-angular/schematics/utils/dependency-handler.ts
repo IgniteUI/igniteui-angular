1a9e25280eb420ca2db5be968cdc6bc3e4d4d678
IBarakov <ibarakov@icloud.com>
2020-08-05 16:21:32 +0300

    chore(*): minor license changes and code cleanup

--- projects/igniteui-angular/schematics/utils/dependency-handler.ts
+++ projects/igniteui-angular/schematics/utils/dependency-handler.ts
@@ -29,7 +29,6 @@ export const DEPENDENCIES_MAP: PackageEntry[] = [
         { name: 'resize-observer-polyfill', target: PackageTarget.REGULAR },
         { name: '@types/hammerjs', target: PackageTarget.DEV },
         { name: 'igniteui-trial-watermark', target: PackageTarget.NONE },
-        { name: 'core-js-pure', target: PackageTarget.NONE },
         // peerDependencies
         { name: '@angular/forms', target: PackageTarget.NONE },
         { name: '@angular/common', target: PackageTarget.NONE },
