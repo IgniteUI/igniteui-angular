8f7c0959cf5a948950f44da672aec66df3d099ec
Vasil Mihalkov <VMihalkov@infragistics.com>
2020-08-05 10:01:07 +0300

    fix(navDrawer): Set width on open change #6321

--- projects/igniteui-angular/src/lib/navigation-drawer/navigation-drawer.component.ts
+++ projects/igniteui-angular/src/lib/navigation-drawer/navigation-drawer.component.ts
@@ -495,6 +495,10 @@ export class IgxNavigationDrawerComponent implements
             this.setDrawerWidth(changes.width.currentValue);
         }
 
+        if (changes.isOpen) {
+            this.setDrawerWidth(this.isOpen ? this.width : '');
+        }
+
         if (changes.miniWidth) {
             if (!this.isOpen) {
                 this.setDrawerWidth(changes.miniWidth.currentValue);
