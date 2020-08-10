554ac708fe79d62718499ffea049a32587616c75
Anastas Staev <astaev@infragistics.com>
2020-08-10 11:18:07 +0300

    fix(grid): Fix onClosing event handler logic #7569

--- projects/igniteui-angular/src/lib/grids/toolbar/grid-toolbar.component.ts
+++ projects/igniteui-angular/src/lib/grids/toolbar/grid-toolbar.component.ts
@@ -416,7 +416,8 @@ export class IgxGridToolbarComponent extends DisplayDensityBase implements After
      */
     public onClosingDropDown(args) {
         const activeElem = document.activeElement;
-        if (!args.event && activeElem !== this.grid.nativeElement &&
+        if (!args.event && !this.columnHidingDropdown.element.contains(activeElem) &&
+            activeElem !== this.grid.nativeElement &&
             !this.columnHidingButton.nativeElement.contains(activeElem) &&
             !this.columnPinningButton.nativeElement.contains(activeElem)) {
             args.cancel = true;
