f278319b2af61931fe114ed2572697ce193452ab
Martin Evtimov <MEvtimov@infragistics.com>
2020-08-12 09:13:13 +0300

    fix(igxGrid): clear activeNode when paginating#7314

--- projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
@@ -468,6 +468,7 @@ export class IgxGridBaseDirective extends DisplayDensityBase implements
             return;
         }
         this.selectionService.clear(true);
+        this.navigation.activeNode = null;
         this.onPagingDone.emit({ previous: this._page, current: val });
         this._page = val;
         this.pageChange.emit(this._page);
@@ -509,6 +510,7 @@ export class IgxGridBaseDirective extends DisplayDensityBase implements
             return;
         }
         this.selectionService.clear(true);
+        this.navigation.activeNode = null;
         this._perPage = val;
         this.perPageChange.emit(this._perPage);
         this.page = 0;
