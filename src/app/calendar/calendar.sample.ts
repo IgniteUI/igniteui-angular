11904d846e47cdc1ad7e8a2b23eac2811a628e03
didimmova <d.dimova11@gmail.com>
2020-08-20 13:07:02 +0300

    refactor(devdemos): add more refactored demos and generate css-vars

--- src/app/calendar/calendar.sample.ts
+++ src/app/calendar/calendar.sample.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { IgxCalendarComponent, DateRangeType, CalendarView } from 'igniteui-angular';
+import { IgxCalendarComponent, IgxDialogComponent, DateRangeType, CalendarView } from 'igniteui-angular';
 import { IViewDateChangeEventArgs } from '../../../projects/igniteui-angular/src/lib/calendar/calendar-base';
 
 @Component({
@@ -9,28 +9,37 @@ import { IViewDateChangeEventArgs } from '../../../projects/igniteui-angular/src
 })
 export class CalendarSampleComponent implements OnInit {
     @ViewChild('calendar', { static: true }) calendar: IgxCalendarComponent;
-    @ViewChild('calendar1', { read: IgxCalendarComponent, static: true }) calendar1: IgxCalendarComponent;
+    @ViewChild('calendar1', { static: true }) public calendar1: IgxCalendarComponent;
+    @ViewChild('alert', { static: true }) public dialog: IgxDialogComponent;
+    public range = [];
+    public today = new Date();
+    public rangeDisabled = [
+        new Date(this.today.getFullYear(), this.today.getMonth(), 10),
+        new Date(this.today.getFullYear(), this.today.getMonth(), 13)
+    ];
 
-    ngOnInit() {
-        this.calendar.disabledDates = [{
-            type: DateRangeType.Specific, dateRange: [
-                new Date(2019, 5, 13),
-                new Date(2019, 5, 27),
-                new Date(2019, 5, 30),
-                new Date(2019, 6, 1),
-                new Date(2019, 5, 1),
-                new Date(2019, 4, 31)
-            ]
-        }];
+    public ngOnInit() {
+        this.calendar1.disabledDates = [{ type: DateRangeType.Between, dateRange: this.rangeDisabled }];
+    }
 
-        this.calendar.specialDates = [{
-            type: DateRangeType.Specific, dateRange: [
-                new Date(2019, 7, 1),
-                new Date(2019, 7, 4),
-                new Date(2019, 7, 15),
-                new Date(2019, 7, 14)
-            ]
-        }];
+    public selectPTOdays(dates: Date[]) {
+        this.range = dates;
+    }
+
+    public submitPTOdays(eventArgs) {
+        this.calendar1.specialDates =
+            [{ type: DateRangeType.Specific, dateRange: this.range }];
+
+        this.range.forEach((item) => {
+            this.calendar1.selectDate(item);
+        });
+
+        if (this.range.length === 0) {
+            this.dialog.message = 'Select dates from the Calendar first.';
+        } else {
+            this.dialog.message = 'PTO days submitted.';
+        }
+        this.dialog.open();
     }
 
     public showHide() {
@@ -56,54 +65,11 @@ export class CalendarSampleComponent implements OnInit {
         this.calendar.monthsViewNumber = parseInt(args.value, 10);
     }
 
-    public multiSelect() {
-        this.calendar.selectDate([new Date('2018-09-29'), new Date('2018-09-20'), new Date('2018-09-26'), new Date('2018-09-05')]);
-    }
-
-    public multiDeselect() {
-        this.calendar.deselectDate();
-    }
-
     public select() {
-        // Working with range selection type
-        this.calendar1.selectDate([new Date('2018-09-20'), new Date('2018-09-26'), new Date('2018-09-28')]);
-
-        // Working with range/multi selection type
-        // this.calendar1.selectDate([new Date('2018-09-29'), new Date('2018-09-20'), new Date('2018-09-26'), new Date('2018-09-05')]);
-
-        // Deselect on Range should deselect the range not only passed dates
-        // this.calendar1.selectDate([new Date('2018-09-26'), new Date('2018-09-28'), new Date('2018-09-22'), new Date('2018-09-10')]);
+        this.calendar.selectDate(new Date(this.today.getFullYear(), this.today.getMonth() + 1, 11));
     }
 
     public deselect() {
-        // Working with range selection type
-        // this.calendar1.deselectDate([new Date('2018-09-20'), new Date('2018-09-26'), new Date('2018-09-28')]);
-
-        // Working with range/multi selection type
-        // this.calendar1.deselectDate([new Date('2018-09-29'), new Date('2018-09-20'), new Date('2018-09-26'), new Date('2018-09-05')]);
-
-        // Working
-        // this.calendar1.deselectDate();
-
-        // Working - deselect only fraction of the range
-        // this.calendar1.deselectDate([new Date('2018-09-26'), new Date('2018-09-28')]);
-
-        // Deselect today
-        this.calendar1.deselectDate([new Date(), new Date('2018-09-26')]);
-
-        // Deselect today array
-        // this.calendar1.deselectDate([new Date()]);
-
-        // Invalid date
-        // this.calendar1.deselectDate(new Date('22/09/18'));
-
-        // Deselect single selection with array of date
-        // this.calendar1.deselectDate([new Date()]);
-
-        // Deselect multi selection with array of date
-        // this.calendar1.deselectDate([new Date()]);
-
-        // Deselect multi selection with array of date that is in the array and one that isn't
-        // this.calendar1.deselectDate(new Date('2018-09-20'));
+        this.calendar.deselectDate();
     }
 }
