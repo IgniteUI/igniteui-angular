9b2fbafa89aba848d6b4c4d2d0252b3bb853e5eb
Simeon Simeonoff <sim.simeonoff@gmail.com>
2020-09-25 15:56:35 +0300

    refactor(docs): log events

--- src/app/toast/toast.sample.ts
+++ src/app/toast/toast.sample.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IgxToastComponent, IgxToastPosition } from 'igniteui-angular';
 
 @Component({
@@ -7,8 +7,30 @@ import { IgxToastComponent, IgxToastPosition } from 'igniteui-angular';
     templateUrl: 'toast.sample.html',
 })
 export class ToastSampleComponent {
+    @ViewChild('toast')
+    public toast: IgxToastComponent;
+
+    public toastVisibility = false;
+
     showToast(toast: IgxToastComponent, pos: IgxToastPosition) {
         toast.position = pos;
         toast.show();
+    }
+
+    handleShowing(event) {
+        console.log('showing toast', event);
+    }
+
+    handleShown(event) {
+        console.log('toast shown', event);
+
+    }
+
+    handleHiding(event) {
+        console.log('toast hiding', event);
+    }
+
+    handleHidden(event) {
+        console.log('toast hidden', event);
     }
 }
