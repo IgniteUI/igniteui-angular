9291de6d55e7c6b0c29aed4b8b84171105a5d2b8
3phase <pbozhinow@gmail.com>
2020-09-28 16:40:56 +0300

    chore(mrl): Moves performVerticalScrollToCell to base if visibleColIndex < 0

--- projects/igniteui-angular/src/lib/grids/grid-mrl-navigation.service.ts
+++ projects/igniteui-angular/src/lib/grids/grid-mrl-navigation.service.ts
@@ -76,8 +76,8 @@ export class IgxGridMRLNavigationService extends IgxGridNavigationService {
 
     public shouldPerformVerticalScroll(targetRowIndex: number, visibleColIndex: number): boolean {
         if (!super.shouldPerformVerticalScroll(targetRowIndex, visibleColIndex)) { return false; }
-        if (!this.isDataRow(targetRowIndex) || visibleColIndex < 0) { 
-            return super.shouldPerformVerticalScroll(targetRowIndex, visibleColIndex); 
+        if (!this.isDataRow(targetRowIndex) || visibleColIndex < 0) {
+            return super.shouldPerformVerticalScroll(targetRowIndex, visibleColIndex);
         }
 
         const targetRow = super.getRowElementByIndex(targetRowIndex);
@@ -146,7 +146,7 @@ export class IgxGridMRLNavigationService extends IgxGridNavigationService {
 
     public performVerticalScrollToCell(rowIndex: number, visibleColIndex: number, cb?: () => void) {
         const children = this.parentByChildIndex(visibleColIndex || 0)?.children;
-        if (!super.isDataRow(rowIndex) || (children && children.length < 2)) {
+        if (!super.isDataRow(rowIndex) || (children && children.length < 2) || visibleColIndex < 0) {
             return super.performVerticalScrollToCell(rowIndex, visibleColIndex, cb);
         }
 
