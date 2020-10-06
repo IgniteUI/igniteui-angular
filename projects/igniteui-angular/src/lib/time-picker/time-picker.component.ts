18f7fe3a355881e4c002e8c63439de9e792c80a7
Boris <jackofdiamond596@gmail.com>
2020-10-06 13:18:35 +0300

    fix(IgxTimePicker): emit onValueChanged if 0:0 is cleared

--- projects/igniteui-angular/src/lib/time-picker/time-picker.component.ts
+++ projects/igniteui-angular/src/lib/time-picker/time-picker.component.ts
@@ -1955,7 +1955,7 @@ export class IgxTimePickerComponent implements
             this.displayValue = '';
             this.value.setHours(0, 0);
 
-            if (oldVal.getTime() !== this.value.getTime()) {
+            if (oldVal.getTime() !== this.value.getTime() || this.isReset()) {
                 const args: IgxTimePickerValueChangedEventArgs = {
                     oldValue: oldVal,
                     newValue: this.value
@@ -1999,7 +1999,7 @@ export class IgxTimePickerComponent implements
             this.value.setHours(0, 0);
             this.displayValue = val;
 
-            if (oldVal.getTime() !== this.value.getTime()) {
+            if (oldVal.getTime() !== this.value.getTime() || this.isReset()) {
                 const args: IgxTimePickerValueChangedEventArgs = {
                     oldValue: oldVal,
                     newValue: this.value
@@ -2150,6 +2150,14 @@ export class IgxTimePickerComponent implements
         } else {
             input.valid = IgxInputState.INITIAL;
         }
+    }
+
+    // Workaround method for #8135
+    // TODO: It must be removed in #6482
+    private isReset(): boolean {
+        return this.value.getHours() === 0
+            && this.value.getMinutes() === 0
+            && this.value.getSeconds() === 0;
     }
 }
 
