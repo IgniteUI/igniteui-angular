0d357f737cd6e22432e3368dccc1c5f1546ddaef
Hristo Anastasov <HAnastasov@infragistics.com>
2020-10-09 11:43:33 +0300

    fix(calendar): expose calendar base in api #8322

--- src/app/calendar/calendar.sample.ts
+++ src/app/calendar/calendar.sample.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { IgxCalendarComponent, IgxDialogComponent, DateRangeType, CalendarView } from 'igniteui-angular';
-import { IViewDateChangeEventArgs } from '../../../projects/igniteui-angular/src/lib/calendar/calendar-base';
+import { IgxCalendarComponent, IgxDialogComponent, DateRangeType, CalendarView, IViewDateChangeEventArgs } from 'igniteui-angular';
 
 @Component({
     selector: 'app-calendar-sample',
