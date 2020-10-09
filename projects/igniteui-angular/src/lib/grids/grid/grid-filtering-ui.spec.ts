eacd85f2ed0ee7838e70af67d794dacb78a68904
IBarakov <ibarakov@infragistics.com>
2020-10-09 11:02:05 +0300

    chore(*): add label check to tests

--- projects/igniteui-angular/src/lib/grids/grid/grid-filtering-ui.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid-filtering-ui.spec.ts
@@ -4395,6 +4395,7 @@ describe('IgxGrid - Filtering actions - Excel style filtering #grid', () => {
             const searchComponent = GridFunctions.getExcelStyleSearchComponent(fix);
             const listItems = GridFunctions.getExcelStyleSearchComponentListItems(fix, searchComponent);
             expect(listItems.length).toBe(3, 'incorrect rendered list items count');
+            expect(listItems[2].innerText).toBe('Custom', 'incorrect list item label');
         }));
 
         it('Should not ignore duplicate records when column\'\s filteringIgnoreCase is false', fakeAsync(() => {
@@ -4409,6 +4410,9 @@ describe('IgxGrid - Filtering actions - Excel style filtering #grid', () => {
             const searchComponent = GridFunctions.getExcelStyleSearchComponent(fix);
             const listItems = GridFunctions.getExcelStyleSearchComponentListItems(fix, searchComponent);
             expect(listItems.length).toBe(5, 'incorrect rendered list items count');
+            expect(listItems[2].innerText).toBe('Custom', 'incorrect list item label');
+            expect(listItems[3].innerText).toBe('custoM', 'incorrect list item label');
+            expect(listItems[4].innerText).toBe('custom', 'incorrect list item label');
         }));
 
         it('Should display "Add to current filter selection" button on typing in input', fakeAsync(() => {
