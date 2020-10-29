f105f2fdda000babc6b98f219b0b02948b89a559
didimmova <d.dimova11@gmail.com>
2020-10-29 09:45:37 +0200

    feat(input): add localisation of the input

---
+++ projects/igniteui-angular/src/lib/core/i18n/input-resources.ts
@@ -0,0 +1,9 @@
+export interface IInputResourceStrings {
+    igx_input_upload_button?: string;
+    igx_input_clear_button?: string;
+}
+
+export const InputResourceStringsEN: IInputResourceStrings = {
+    igx_input_upload_button: 'Upload File',
+    igx_input_clear_button: 'clear',
+};
