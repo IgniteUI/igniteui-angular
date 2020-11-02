4ab6ebb6e4d18fe3adba644267958767fc52db20
MKirova <MKirova@DEV-MKIROVA>
2020-11-02 12:59:25 +0200

    chore(*): Disconnect observer on destroy.

--- projects/igniteui-angular/src/lib/directives/for-of/base.helper.component.ts
+++ projects/igniteui-angular/src/lib/directives/for-of/base.helper.component.ts
@@ -40,6 +40,7 @@ export class VirtualHelperBaseDirective implements OnDestroy, AfterViewInit {
 
     public ngOnDestroy() {
         this.destroyed = true;
+        this._observer.disconnect();
     }
 
     public set size(value) {
