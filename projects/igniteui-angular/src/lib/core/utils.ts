2249e4e69438a9c6b8c159533fa528def3b52d80
Hristo Anastasov <HAnastasov@infragistics.com>
2020-11-03 10:57:44 +0200

    chore(*): use instanceOf Date for type check #8350

--- projects/igniteui-angular/src/lib/core/utils.ts
+++ projects/igniteui-angular/src/lib/core/utils.ts
@@ -148,7 +148,7 @@ export function isObject(value: any): boolean {
  * @hidden
  */
 export function isDate(value: any): boolean {
-    return Object.prototype.toString.call(value) === '[object Date]';
+    return value instanceof Date;
 }
 
 /**
