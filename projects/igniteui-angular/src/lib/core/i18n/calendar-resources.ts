d141b380b978614481e42fa12cde99a29d57b0bb
MPopov <desig9stein@gmail.com>
2020-11-10 10:55:58 +0200

    fix resource strings

--- projects/igniteui-angular/src/lib/core/i18n/calendar-resources.ts
+++ projects/igniteui-angular/src/lib/core/i18n/calendar-resources.ts
@@ -5,15 +5,13 @@ export interface ICalendarResourceStrings {
  igx_calendar_select_year: string;
  igx_calendar_range_start: string;
  igx_calendar_range_end: string;
- igx_selected_month_is: string;
- first_picker: string;
- date_pickers: string;
- of: string;
- starts_from: string;
- multi_selection: string;
- range_selection: string;
- calendar: string;
- with: string;
+ igx_calendar_selected_month_is: string;
+ igx_calendar_first_picker_of: string;
+ igx_calendar_date_pickers: string;
+ igx_calendar_starts_from: string;
+ igx_calendar_multi_selection: string;
+ igx_calendar_range_selection: string;
+ igx_calendar_calendar_with: string;
 }
 
 export const CalendarResourceStringsEN: ICalendarResourceStrings = {
@@ -23,13 +21,11 @@ export const CalendarResourceStringsEN: ICalendarResourceStrings = {
  igx_calendar_select_year: 'Select Year',
  igx_calendar_range_start: 'Range start',
  igx_calendar_range_end: 'Range end',
- igx_selected_month_is: 'Selected month is',
- first_picker: 'first picker',
- date_pickers: 'date pickers',
- of: 'of',
- starts_from: 'starts from',
- multi_selection: 'multi selection',
- range_selection: 'range selection',
- calendar: 'calendar',
- with: 'with',
+ igx_calendar_selected_month_is: 'Selected month is',
+ igx_calendar_first_picker_of: 'first picker of',
+ igx_calendar_date_pickers: 'date pickers',
+ igx_calendar_starts_from: 'starts from',
+ igx_calendar_multi_selection: 'multi selection',
+ igx_calendar_range_selection: 'range selection',
+ igx_calendar_calendar_with: 'calendar with',
 };
