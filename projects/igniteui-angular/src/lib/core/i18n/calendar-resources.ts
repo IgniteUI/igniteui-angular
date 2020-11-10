ad2cb11d15feba7409f76f3bc89006057d90c791
MPopov <desig9stein@gmail.com>
2020-11-10 14:15:12 +0200

    Fix Resource Strings

--- projects/igniteui-angular/src/lib/core/i18n/calendar-resources.ts
+++ projects/igniteui-angular/src/lib/core/i18n/calendar-resources.ts
@@ -9,7 +9,10 @@ export interface ICalendarResourceStrings {
  igx_calendar_first_picker_of: string;
  igx_calendar_multi_selection: string;
  igx_calendar_range_selection: string;
- igx_calendar_with_date_pickers: string;
+ igx_calendar_single_selection: string;
+ igx_calendar_singular_multi_selection: string;
+ igx_calendar_singular_range_selection: string;
+ igx_calendar_singular_single_selection: string;
 }
 
 export const CalendarResourceStringsEN: ICalendarResourceStrings = {
@@ -19,9 +22,12 @@ export const CalendarResourceStringsEN: ICalendarResourceStrings = {
  igx_calendar_select_year: 'Select Year',
  igx_calendar_range_start: 'Range start',
  igx_calendar_range_end: 'Range end',
- igx_calendar_selected_month_is: 'Selected month is',
- igx_calendar_first_picker_of: 'first picker of {0} starts from',
- igx_calendar_multi_selection: 'multi selection',
- igx_calendar_range_selection: 'range selection',
- igx_calendar_with_date_pickers: 'calendar with {0} date pickers',
+ igx_calendar_selected_month_is: 'Selected month is ',
+ igx_calendar_first_picker_of: 'First picker of {0} starts from',
+ igx_calendar_multi_selection: 'Multi selection calendar with {0} date pickers',
+ igx_calendar_range_selection: 'Range selection calendar with {0} date pickers',
+ igx_calendar_single_selection: 'Calendar with {0} date pickers',
+ igx_calendar_singular_multi_selection: 'Multi selection calendar',
+ igx_calendar_singular_range_selection: 'Range selection calendar',
+ igx_calendar_singular_single_selection: 'Calendar',
 };
