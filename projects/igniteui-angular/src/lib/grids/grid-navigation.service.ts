160c438bf940a486c5f03a97eac1c8719376d29e
ddincheva <desislava9dincheva@gmail.com>
2020-12-18 16:41:50 +0200

    chore(*): activate the cell if is in the view

--- projects/igniteui-angular/src/lib/grids/grid-navigation.service.ts
+++ projects/igniteui-angular/src/lib/grids/grid-navigation.service.ts
@@ -229,7 +229,7 @@ export class IgxGridNavigationService {
             if (shouldClearSelection) {
                 this.grid.clearCellSelection();
                 this.grid.navigateTo(this.activeNode.row, this.activeNode.column, (obj) => {
-                    obj.target.activate(event);
+                    obj.target?.activate(event);
                     this.grid.cdr.detectChanges();
                 } );
             } else {
