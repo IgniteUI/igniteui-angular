6e58a5c9e3e656331e67711bff10b14c85d70251
Simeon Simeonoff <sim.simeonoff@gmail.com>
2021-02-03 12:47:16 +0200

    lint(buttongroup): fix warnings and add migrations

--- projects/igniteui-angular/migrations/update-11_1_0/index.spec.ts
+++ projects/igniteui-angular/migrations/update-11_1_0/index.spec.ts
@@ -5,22 +5,19 @@ import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/te
 
 describe('Update to 11.1.0', () => {
     let appTree: UnitTestTree;
-    const runner = new SchematicTestRunner(
-        'ig-migrate',
-        path.join(__dirname, '../migration-collection.json')
-    );
+    const runner = new SchematicTestRunner('ig-migrate', path.join(__dirname, '../migration-collection.json'));
     const configJson = {
         defaultProject: 'testProj',
         projects: {
             testProj: {
-                sourceRoot: '/testSrc',
-            },
+                sourceRoot: '/testSrc'
+            }
         },
         schematics: {
             '@schematics/angular:component': {
-                prefix: 'appPrefix',
-            },
-        },
+                prefix: 'appPrefix'
+            }
+        }
     };
     const migrationName = 'migration-19';
 
@@ -38,13 +35,33 @@ describe('Update to 11.1.0', () => {
             </igx-splitter>`
         );
 
-        const tree = await runner.runSchematicAsync(migrationName, {}, appTree)
-            .toPromise();
+        const tree = await runner.runSchematicAsync(migrationName, {}, appTree).toPromise();
 
         expect(tree.readContent('/testSrc/appPrefix/component/splitter.component.html'))
             .toEqual(`<igx-splitter style='height: 30vh;' [type]='0'>
                 <igx-splitter-pane (collapsedChange)="toggled()">
                 </igx-splitter-pane>
             </igx-splitter>`);
+    });
+
+    it('should replace onSelect and onUnselect with selected and deselected in igx-buttongroup', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/buttongroup.component.html`,
+            `<igx-buttongroup
+                (onSelect)="someHandler($event)"
+                (onUnselect)="someHandler($event)"
+            >
+            </igx-buttongroup>`
+        );
+
+        const tree = await runner.runSchematicAsync(migrationName, {}, appTree).toPromise();
+
+        expect(tree.readContent('/testSrc/appPrefix/component/buttongroup.component.html')).toEqual(
+            `<igx-buttongroup
+                (selected)="someHandler($event)"
+                (deselected)="someHandler($event)"
+            >
+            </igx-buttongroup>`
+        );
     });
 });
