b2c2d5c8eed6a0ac34bd9ba24fc42a8790e854bd
gedinakova <gedinakova@infragistics.com>
2021-02-22 15:08:35 +0200

    fix(Navigation): Adjust activeNode fix.

--- projects/igniteui-angular/src/lib/grids/grid-navigation.service.ts
+++ projects/igniteui-angular/src/lib/grids/grid-navigation.service.ts
@@ -220,8 +220,8 @@ export class IgxGridNavigationService {
 
     focusTbody(event) {
         const gridRows = this.grid.verticalScrollContainer.totalItemCount ?? this.grid.dataView.length;
-        if (gridRows < 1) { this.activeNode = null; return; }
-        if (!this.activeNode || !Object.keys(this.activeNode).length || this.activeNode.row < 0 || this.activeNode.row > gridRows - 1) {
+        if (gridRows < 1) { this.activeNode = {} as IActiveNode; return; }
+        if (!Object.keys(this.activeNode).length || this.activeNode.row < 0 || this.activeNode.row > gridRows - 1) {
             const hasLastActiveNode = Object.keys(this.lastActiveNode).length;
             const shouldClearSelection = hasLastActiveNode && (this.lastActiveNode.row < 0 || this.lastActiveNode.row > gridRows - 1);
             this.setActiveNode(this.lastActiveNode.row >= 0 && this.lastActiveNode.row < gridRows ?
