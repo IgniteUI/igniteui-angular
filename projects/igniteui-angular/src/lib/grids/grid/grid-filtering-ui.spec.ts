70c09c5f5de4f131ce44b7ac685cc3c9c563586c
IBarakov <ibarakov@infragistics.com>
2021-02-22 11:58:39 +0200

    chore(*): edit test to cover the scenario

--- projects/igniteui-angular/src/lib/grids/grid/grid-filtering-ui.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid-filtering-ui.spec.ts
@@ -2893,7 +2893,7 @@ describe('IgxGrid - Filtering actions - Excel style filtering #grid', () => {
             fix.detectChanges();
 
             expect(grid.onColumnVisibilityChanged.emit).toHaveBeenCalledTimes(1);
-            expect(grid.columns[2].hidden).toBeTruthy();
+            GridFunctions.verifyColumnIsHidden(grid.columns[2], true, 5);
         }));
 
         it('Should not select values in list if two values with And operator are entered.', fakeAsync(() => {
