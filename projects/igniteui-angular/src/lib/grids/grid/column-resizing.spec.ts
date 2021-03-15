ef0512db478fde19ff5b25fa006dbc7375542e6c
INFRAGISTICS\nrobakova <nrobakova@infragistics.com>
2021-03-15 19:14:22 +0200

    chore(*): include more tests

--- projects/igniteui-angular/src/lib/grids/grid/column-resizing.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/column-resizing.spec.ts
@@ -13,7 +13,7 @@ import { MultiColumnHeadersComponent } from '../../test-utils/grid-samples.spec'
 import { configureTestSuite } from '../../test-utils/configure-suite';
 import { GridFunctions } from '../../test-utils/grid-functions.spec';
 
-xdescribe('IgxGrid - Deferred Column Resizing #grid', () => {
+describe('IgxGrid - Deferred Column Resizing #grid', () => {
     configureTestSuite();
     const COLUMN_HEADER_GROUP_CLASS = '.igx-grid__thead-item';
 
@@ -680,7 +680,7 @@ xdescribe('IgxGrid - Deferred Column Resizing #grid', () => {
         }));
     });
 
-    describe('Multi Column Headers tests: ', () => {
+    xdescribe('Multi Column Headers tests: ', () => {
         let fixture: ComponentFixture<any>;
         let grid: IgxGridComponent;
 
