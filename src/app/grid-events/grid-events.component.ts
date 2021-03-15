19f51dc8a6a213744d9eb6164c8bf99c97d7da35
dobromirts <dobromir.ts@gmail.com>
2021-03-15 17:30:45 +0200

    fix(lint): cell styling lint warnings

--- src/app/grid-events/grid-events.component.ts
+++ src/app/grid-events/grid-events.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { IgxGridComponent, FilteringExpressionsTree,
-    ISortingExpression, IPinColumnEventArgs, IColumnVisibilityChangedEventArgs,
+    ISortingExpression, IPinColumnEventArgs,
     IColumnResizeEventArgs, IColumnSelectionEventArgs, IPageEventArgs, ISortingEventArgs,
     IFilteringEventArgs, IgxStringFilteringOperand, IColumnMovingEndEventArgs,
     IColumnMovingEventArgs, IColumnMovingStartEventArgs, IPinColumnCancellableEventArgs,
@@ -87,7 +87,7 @@ export class GridEventsComponent {
         event.cancel = this.$hiding;
         this.logAnEvent('=> columnVisibilityChanging', event.cancel);
     }
-    public onColumnVisibilityChanged(event: IColumnVisibilityChangedEventArgs) {
+    public onColumnVisibilityChanged() {
         this.logAnEvent(`=> onColumnVisibilityChanged`);
     }
 
