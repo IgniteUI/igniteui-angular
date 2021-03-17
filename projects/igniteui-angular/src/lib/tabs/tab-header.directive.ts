03fab178e6d1a0ac25ab2e322a90af6fcfb85e12
gedinakova <gedinakova@infragistics.com>
2021-03-17 10:26:46 +0200

    feat(Tabs): Moved Keyboard support to header comp

--- projects/igniteui-angular/src/lib/tabs/tab-header.directive.ts
+++ projects/igniteui-angular/src/lib/tabs/tab-header.directive.ts
@@ -1,6 +1,5 @@
 
-import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
-import { KEYS } from '../core/utils';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 import { IgxTabItemDirective } from './tab-item.directive';
 import { IgxTabHeaderBase, IgxTabsBase } from './tabs.base';
 
@@ -15,71 +14,6 @@ export abstract class IgxTabHeaderDirective implements IgxTabHeaderBase {
     @HostListener('click')
     public onClick() {
         this.tabs.selectTab(this.tab, true);
-    }
-
-    @HostBinding('attr.tabindex')
-    public get tabIndex() {
-        return this.tab.selected ? 0 : -1;
-    }
-
-     /** @hidden */
-    @HostListener('keydown', ['$event'])
-    public keyDown(event: KeyboardEvent) {
-        let unsupportedKey = false;
-        const itemsArray = this.tabs.items.toArray();
-        const previousIndex = itemsArray.indexOf(this.tab);
-        let newIndex = previousIndex;
-        const hasDisabledItems = itemsArray.some((item) => item.disabled);
-        switch (event.key) {
-            case KEYS.RIGHT_ARROW:
-            case KEYS.RIGHT_ARROW_IE:
-            newIndex = newIndex === itemsArray.length - 1 ? 0 : newIndex + 1;
-                while (hasDisabledItems && itemsArray[newIndex].disabled && newIndex < itemsArray.length) {
-                    newIndex = newIndex === itemsArray.length - 1 ? 0 : newIndex + 1;
-                }
-                break;
-            case KEYS.LEFT_ARROW:
-            case KEYS.LEFT_ARROW_IE:
-                newIndex = newIndex === 0 ? itemsArray.length - 1 : newIndex - 1;
-                while (hasDisabledItems && itemsArray[newIndex].disabled && newIndex >= 0) {
-                    newIndex = newIndex === 0 ? itemsArray.length - 1 : newIndex - 1;
-                }
-                break;
-            case KEYS.HOME:
-                event.preventDefault();
-                newIndex = 0;
-                while (itemsArray[newIndex].disabled && newIndex < itemsArray.length) {
-                    newIndex = newIndex === itemsArray.length - 1 ? 0 : newIndex + 1;
-                }
-                break;
-            case KEYS.END:
-                event.preventDefault();
-                newIndex = itemsArray.length - 1;
-                while (hasDisabledItems && itemsArray[newIndex].disabled && newIndex > 0) {
-                    newIndex = newIndex === 0 ? itemsArray.length - 1 : newIndex - 1;
-                }
-                break;
-            case KEYS.TAB:
-                break;
-            case KEYS.ENTER:
-            case KEYS.SPACE:
-            case KEYS.SPACE_IE:
-                event.preventDefault();
-                this.nativeElement.click();
-                unsupportedKey = true;
-                break;
-            default:
-                event.preventDefault();
-                unsupportedKey = true;
-                break;
-        }
-
-        if (!unsupportedKey) {
-            (itemsArray[newIndex] as IgxTabItemDirective).headerComponent.nativeElement.focus();
-            if (this.tab.panelComponent) {
-                this.tabs.selectedIndex = newIndex;
-            }
-        }
     }
 
     /** @hidden */
--- projects/igniteui-angular/src/lib/tabs/tabs/tab-header.component.ts
+++ projects/igniteui-angular/src/lib/tabs/tabs/tab-header.component.ts
@@ -1,9 +1,10 @@
-import { AfterViewInit, Component, ElementRef, HostBinding, NgZone, OnDestroy } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostBinding, HostListener, NgZone, OnDestroy } from '@angular/core';
 import { IgxTabItemDirective } from '../tab-item.directive';
 import { IgxTabHeaderDirective } from '../tab-header.directive';
 import { IgxTabHeaderBase } from '../tabs.base';
 import { IgxTabsComponent } from './tabs.component';
 import ResizeObserver from 'resize-observer-polyfill';
+import { KEYS } from '../../core/utils';
 
 @Component({
     selector: 'igx-tab-header',
@@ -31,9 +32,74 @@ export class IgxTabHeaderComponent extends IgxTabHeaderDirective implements Afte
         return (!this.tab.disabled && !this.tab.selected);
     }
 
+    @HostBinding('attr.tabindex')
+    public get tabIndex() {
+        return this.tab.selected ? 0 : -1;
+    }
+
     /** @hidden */
     constructor(protected tabs: IgxTabsComponent, tab: IgxTabItemDirective, elementRef: ElementRef, private ngZone: NgZone) {
         super(tabs, tab, elementRef);
+    }
+
+     /** @hidden */
+    @HostListener('keydown', ['$event'])
+    public keyDown(event: KeyboardEvent) {
+        let unsupportedKey = false;
+        const itemsArray = this.tabs.items.toArray();
+        const previousIndex = itemsArray.indexOf(this.tab);
+        let newIndex = previousIndex;
+        const hasDisabledItems = itemsArray.some((item) => item.disabled);
+        switch (event.key) {
+            case KEYS.RIGHT_ARROW:
+            case KEYS.RIGHT_ARROW_IE:
+            newIndex = newIndex === itemsArray.length - 1 ? 0 : newIndex + 1;
+                while (hasDisabledItems && itemsArray[newIndex].disabled && newIndex < itemsArray.length) {
+                    newIndex = newIndex === itemsArray.length - 1 ? 0 : newIndex + 1;
+                }
+                break;
+            case KEYS.LEFT_ARROW:
+            case KEYS.LEFT_ARROW_IE:
+                newIndex = newIndex === 0 ? itemsArray.length - 1 : newIndex - 1;
+                while (hasDisabledItems && itemsArray[newIndex].disabled && newIndex >= 0) {
+                    newIndex = newIndex === 0 ? itemsArray.length - 1 : newIndex - 1;
+                }
+                break;
+            case KEYS.HOME:
+                event.preventDefault();
+                newIndex = 0;
+                while (itemsArray[newIndex].disabled && newIndex < itemsArray.length) {
+                    newIndex = newIndex === itemsArray.length - 1 ? 0 : newIndex + 1;
+                }
+                break;
+            case KEYS.END:
+                event.preventDefault();
+                newIndex = itemsArray.length - 1;
+                while (hasDisabledItems && itemsArray[newIndex].disabled && newIndex > 0) {
+                    newIndex = newIndex === 0 ? itemsArray.length - 1 : newIndex - 1;
+                }
+                break;
+            case KEYS.TAB:
+                break;
+            case KEYS.ENTER:
+            case KEYS.SPACE:
+            case KEYS.SPACE_IE:
+                event.preventDefault();
+                this.nativeElement.click();
+                unsupportedKey = true;
+                break;
+            default:
+                event.preventDefault();
+                unsupportedKey = true;
+                break;
+        }
+
+        if (!unsupportedKey) {
+            (itemsArray[newIndex] as IgxTabItemDirective).headerComponent.nativeElement.focus();
+            if (this.tab.panelComponent) {
+                this.tabs.selectedIndex = newIndex;
+            }
+        }
     }
 
     /** @hidden */
