88851d5595cb6e2f31e94f1111cdce1bd57ff43e
Aleksandyr <aleksandyr.kamenov@gmail.com>
2021-03-19 13:36:59 +0200

    fix(grid-accessability): expose column group id for norrator

--- projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
@@ -2256,8 +2256,7 @@ export abstract class IgxGridBaseDirective extends DisplayDensityBase implements
             return this.id;
         }
 
-        return !this.navigation.isDataRow(activeElem.row, true) ? this.id + '_' + activeElem.row :
-            this.id + '_' + activeElem.row + '_' + activeElem.column;
+        return this.id + '_' + activeElem.row + '_' + activeElem.column;
     }
 
     /**
