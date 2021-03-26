4caa377fc84706f79472e7aae3f28df1e7cd16ce
Hristo Anastasov <hanastasov@infragistics.com>
2021-03-26 11:40:54 +0200

    chore(*): fix lint error in ts file

--- src/app/grid-finjs/grid-finjs.component.ts
+++ src/app/grid-finjs/grid-finjs.component.ts
@@ -61,6 +61,7 @@ export class GridFinJSComponent {
     private strongPositive = (rowData: any): boolean => rowData['Change(%)'] >= 1;
     private strongNegative = (rowData: any): boolean => rowData['Change(%)'] <= -1;
 
+    // eslint-disable-next-line @typescript-eslint/member-ordering
     public trends = {
         changeNeg: this.changeNegative,
         changePos: this.changePositive,
@@ -69,7 +70,7 @@ export class GridFinJSComponent {
         strongNegative: this.strongNegative,
         strongPositive: this.strongPositive
     };
-
+    // eslint-disable-next-line @typescript-eslint/member-ordering
     public trendsChange = {
         changeNeg2: this.changeNegative,
         changePos2: this.changePositive,
