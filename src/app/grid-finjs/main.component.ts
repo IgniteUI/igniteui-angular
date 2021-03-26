6a4d682cf122e681f5c46d4f0f17941289d0b630
Hristo Anastasov <hanastasov@infragistics.com>
2021-03-26 11:29:32 +0200

    chore(*): update finjs sample as samples browser logic

--- src/app/grid-finjs/main.component.ts
+++ src/app/grid-finjs/main.component.ts
@@ -1,14 +1,17 @@
-import { AfterViewInit, Component, EventEmitter, OnDestroy, Output, ViewChild } from '@angular/core';
-import { FinancialData } from '../shared/financialData';
+import { Component, EventEmitter, OnDestroy, Output, ViewChild } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { LocalService } from '../shared/local.service';
 import { ControllerComponent } from './controllers.component';
 import { GridFinJSComponent } from './grid-finjs.component';
 
 @Component({
+    providers: [LocalService],
     selector: 'app-finjs-main',
     styleUrls: ['./main.component.scss'],
     templateUrl: './main.component.html'
 })
-export class MainComponent implements AfterViewInit, OnDestroy {
+export class MainComponent implements OnDestroy {
     @ViewChild('grid', { static: true }) public finGrid: GridFinJSComponent;
     @ViewChild('controllers', { static: true }) public controller: ControllerComponent;
 
@@ -16,22 +19,22 @@ export class MainComponent implements AfterViewInit, OnDestroy {
     @Output() public recordsVolume = new EventEmitter<any>();
     @Output() public frequencyTimer = new EventEmitter<any>();
     @Output() public player = new EventEmitter<any>();
-    public theme = false;
+
     public volume = 1000;
     public frequency = 500;
-
-    private subscription$;
+    public darkTheme = true;
+    public data$: any;
+    private destroy$ = new Subject<any>();
     private _timer;
 
-    constructor() { }
-
-    public ngAfterViewInit() {
-        // this.subscription$ = this.localService.records.subscribe(x => {
-        //     this.finGrid.data = x;
-        // });
+    constructor(public finService: LocalService) {
+        this.finService.getFinancialData(this.volume);
+        this.data$ = this.finService.records.pipe(takeUntil(this.destroy$));
     }
 
     public ngOnDestroy() {
+        this.destroy$.next(true);
+        this.destroy$.complete();
         this.stopFeed();
     }
 
@@ -42,11 +45,7 @@ export class MainComponent implements AfterViewInit, OnDestroy {
                 break;
             }
             case 'grouped': {
-                this.finGrid.toggleGrouping();
-                break;
-            }
-            case 'theme': {
-                this.changeTheme(event.value);
+                this.finGrid.toggleGrouping(event.value);
                 break;
             }
             default:
@@ -58,19 +57,19 @@ export class MainComponent implements AfterViewInit, OnDestroy {
 
     public onVolumeChanged(volume: any) {
         this.finGrid.grid.deselectAllRows();
-        this.finGrid.finService.getFinancialData(volume);
+        this.finService.getFinancialData(volume);
     }
 
     public onPlayAction(event: any) {
         switch (event.action) {
             case 'playAll': {
                 const currData = this.finGrid.grid.filteredSortedData ?? this.finGrid.data;
-                this._timer = setInterval(() => this.finGrid.finService.updateAllPriceValues(currData), this.controller.frequency);
+                this._timer = setInterval(() => this.finService.updateAllPriceValues(currData), this.controller.frequency);
                 break;
             }
             case 'playRandom': {
                 const currData = this.finGrid.data;
-                this._timer = setInterval(() => this.finGrid.finService.updateRandomPriceValues(currData), this.controller.frequency);
+                this._timer = setInterval(() => this.finService.updateRandomPriceValues(currData), this.controller.frequency);
                 break;
             }
             case 'stop': {
@@ -84,33 +83,9 @@ export class MainComponent implements AfterViewInit, OnDestroy {
         }
     }
 
-    /**
-     * the below code is needed when accessing the sample through the navigation
-     * it will style all the space below the sample component element, but not the navigation menu
-     */
-    public changeTheme(dark: true) {
-        const parentEl = this.parentComponentEl();
-        if (dark && parentEl.classList.contains('main')) {
-            parentEl.classList.add('fin-dark-theme');
-        } else {
-            parentEl.classList.remove('fin-dark-theme');
-        }
-    }
-
     public stopFeed() {
         if (this._timer) {
             clearInterval(this._timer);
         }
-        if (this.subscription$) {
-            this.subscription$.unsubscribe();
-        }
-    }
-
-    /**
-     * returns the main div container of the Index Component,
-     * if path is /samples/sample-url, or the appRoot, if path is /sample-url
-     */
-    private parentComponentEl() {
-        return this.elRef.nativeElement.parentElement.parentElement;
     }
 }
