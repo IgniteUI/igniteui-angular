6f13585febd3daf415a1486ffe33d6adbda6a2c1
Anastas Staev <astaev@infragistics.com>
2021-03-31 17:35:45 +0300

    fix(tabs): Update tabs migrations

--- projects/igniteui-angular/migrations/update-12_0_0/index.ts
+++ projects/igniteui-angular/migrations/update-12_0_0/index.ts
@@ -2,27 +2,8 @@ import { Element } from '@angular/compiler';
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { UpdateChanges } from '../common/UpdateChanges';
 import { FileChange, getAttribute, findElementNodes, getSourceOffset, hasAttribute, parseFile } from '../common/util';
-import { BindingChange, SelectorChange } from '../common/schema';
 
 const version = '12.0.0';
-
-class CustomUpdate extends UpdateChanges {
-    private removedSelectorChanges: SelectorChange[];
-    private removedInputChanges: BindingChange[];
-    constructor(rootPath: string, host: Tree, context?: SchematicContext) {
-        super(rootPath, host, context);
-    }
-
-    public applyOnlyBottomNavSelectorChange() {
-        this.removedSelectorChanges = this.selectorChanges.changes.splice(1, this.selectorChanges.changes.length - 2);
-        this.removedInputChanges = this.inputChanges.changes.splice(0);
-    }
-
-    public applyAllOtherChanges() {
-        this.selectorChanges.changes = this.removedSelectorChanges;
-        this.inputChanges.changes = this.removedInputChanges;
-    }
-}
 
 export default (): Rule => (host: Tree, context: SchematicContext) => {
     context.logger.info(
@@ -32,7 +13,7 @@ export default (): Rule => (host: Tree, context: SchematicContext) => {
     const COMPONENTS = [
         {
             component: 'igx-bottom-nav',
-            tags: ['igx-bottom-nav-item', 'igx-tab'],
+            tags: ['igx-bottom-nav-item', 'igx-tab-panel', 'igx-tab'],
             tabItem: 'igx-bottom-nav-item',
             headerItem: 'igx-bottom-nav-header',
             panelItem: 'igx-bottom-nav-content',
@@ -49,7 +30,7 @@ export default (): Rule => (host: Tree, context: SchematicContext) => {
             labelDirective: 'igxTabHeaderLabel'
         }
     ];
-    const update = new CustomUpdate(__dirname, host, context);
+    const update = new UpdateChanges(__dirname, host, context);
     const changes = new Map<string, FileChange[]>();
     const htmlFiles = update.templateFiles;
 
@@ -74,11 +55,6 @@ export default (): Rule => (host: Tree, context: SchematicContext) => {
     };
 
     const isEmptyOrSpaces = (str) => str === null || str === '' || str === '\n' || str.match(/^ *$/) !== null;
-
-
-    // Apply selector changes for igx-tab-panel(igx-bottom-nav-item) component
-    update.applyOnlyBottomNavSelectorChange();
-    update.applyChanges();
 
     // Replace the tabsType input with tabsAligment
     for (const path of htmlFiles) {
@@ -105,7 +81,7 @@ export default (): Rule => (host: Tree, context: SchematicContext) => {
         for (const path of htmlFiles) {
             // Replace the bottom-nav -> igx-tab with igx-bottom-nav-item
             if (comp.component === 'igx-bottom-nav') {
-                findElementNodes(parseFile(host, path), comp.tags[1])
+                findElementNodes(parseFile(host, path), comp.tags[2])
                     .map(node => getSourceOffset(node as Element))
                     .forEach(offset => {
                         const tab = offset.file.content.substring(offset.startTag.start, offset.endTag.end);
@@ -176,8 +152,6 @@ export default (): Rule => (host: Tree, context: SchematicContext) => {
                     const tabHeader = offset.node.children.find(c => (c as Element).name === comp.headerItem);
                     if (tabHeader) {
                         const content = offset.file.content.substring(tabHeader.sourceSpan.end.offset, offset.endTag.start);
-                        console.log('CONTENT', content);
-
                         if (!isEmptyOrSpaces(content)) {
                             const tabPanel = `\n<${comp.panelItem}>${content}</${comp.panelItem}>\n`;
                             addChange(offset.file.url, new FileChange(tabHeader.sourceSpan.end.offset, tabPanel, content, 'replace'));
@@ -190,7 +164,7 @@ export default (): Rule => (host: Tree, context: SchematicContext) => {
         }
     }
 
-    // Apply all input changes plus selector change for igx-tabs-group
-    update.applyAllOtherChanges();
+    // Apply all selector and input changes
+    // update.applyAllOtherChanges();
     update.applyChanges();
 };
