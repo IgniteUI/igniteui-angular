19098240c08007ae56f78513a6b9c6d268f00d2b
Aleksandyr <aleksandyr.kamenov@gmail.com>
2021-04-02 12:07:04 +0300

    fix(grid): remove crudService endEdit invocation

--- projects/igniteui-angular/src/lib/grids/grid-navigation.service.ts
+++ projects/igniteui-angular/src/lib/grids/grid-navigation.service.ts
@@ -527,8 +527,8 @@ export class IgxGridNavigationService {
     protected handleEditing(shift: boolean, event: KeyboardEvent) {
         const next = shift ? this.grid.getPreviousCell(this.activeNode.row, this.activeNode.column, col => col.editable) :
             this.grid.getNextCell(this.activeNode.row, this.activeNode.column, col => col.editable);
-        if (!this.grid.crudService.rowInEditMode && this.isActiveNode(next.rowIndex, next.visibleColumnIndex)) {
-            this.grid.crudService.endEdit(true, event);
+        if (!this.grid.rowInEditMode && this.isActiveNode(next.rowIndex, next.visibleColumnIndex)) {
+            this.grid.endEdit(true, event);
             this.grid.tbody.nativeElement.focus();
             return;
         }
