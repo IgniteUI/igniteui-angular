c4d528f333882e5ffdde18383ef1575687269812
plamenamiteva <plamenamiteva@abv.bg>
2021-04-02 11:54:09 +0300

    refactor(time-picker): add time pipe #6482

---
+++ projects/igniteui-angular/src/lib/time-picker/time-picker.pipes.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform, Inject } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { IGX_TIME_PICKER_COMPONENT } from './time-picker.common';
+import { IgxTimePickerComponent } from './time-picker.component';
+
+@Pipe({
+    name: 'timeFormatPipe'
+})
+export class TimeFormatPipe implements PipeTransform {
+    constructor(@Inject(IGX_TIME_PICKER_COMPONENT) private timePicker: IgxTimePickerComponent) { }
+
+    public transform(value: Date): string {
+        const format = this.timePicker.inputFormat.replace('tt', 'aa');
+        var datePipe = new DatePipe(this.timePicker.locale);
+        return datePipe.transform(value, format);
+    }
+}
