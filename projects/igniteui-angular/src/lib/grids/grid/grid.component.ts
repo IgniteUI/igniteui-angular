af54138aecff08cd430561b76904e4a83128c883
teodosia <teodosiah@gmail.com>
2021-04-05 14:51:42 +0300

    fix(grid): add square brackets around autoGenerate property in grid  API doc

--- projects/igniteui-angular/src/lib/grids/grid/grid.component.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid.component.ts
@@ -50,7 +50,7 @@ export interface IGroupingDoneEventArgs extends IBaseEventArgs {
  * has been bound, it can be manipulated through filtering, sorting & editing operations.
  * @example
  * ```html
- * <igx-grid [data]="employeeData" autoGenerate="false">
+ * <igx-grid [data]="employeeData" [autoGenerate]="false">
  *   <igx-column field="first" header="First Name"></igx-column>
  *   <igx-column field="last" header="Last Name"></igx-column>
  *   <igx-column field="role" header="Role"></igx-column>
