afedec95a2ea48aceeda0f95dc896f50257b8b07
Deyan Kamburov <dkamburov@infragistics.com>
2021-04-09 18:49:26 +0300

    fix(groupby): Dont set groupingExpressions if value is the same reference #9295

--- projects/igniteui-angular/src/lib/grids/grid/grid.groupby.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid.groupby.spec.ts
@@ -565,6 +565,25 @@ describe('IgxGrid - GroupBy #grid', () => {
         }
     }));
 
+    it('should not apply grouping if the grouping expressions value is the same reference', fakeAsync(() => {
+        const fix = TestBed.createComponent(DefaultGridComponent);
+        fix.detectChanges();
+
+        // group by string column
+        const grid = fix.componentInstance.instance;
+        fix.detectChanges();
+        grid.groupBy({
+            fieldName: 'ReleaseDate', dir: SortingDirection.Asc, ignoreCase: false
+        });
+        fix.detectChanges();
+        spyOn(grid.groupingExpressionsChange, 'emit');
+        fix.detectChanges();
+        const firstCell = grid.getCellByColumn(2, 'Downloads');
+        UIInteractions.simulateClickAndSelectEvent(firstCell);
+        fix.detectChanges();
+        expect(grid.groupingExpressionsChange.emit).toHaveBeenCalledTimes(0);
+    }));
+
     // GroupBy + Sorting integration
     it('should apply sorting on each group\'s records when non-grouped column is sorted.', fakeAsync(() => {
         const fix = TestBed.createComponent(DefaultGridComponent);
@@ -2620,6 +2639,7 @@ describe('IgxGrid - GroupBy #grid', () => {
 @Component({
     template: `
         <igx-grid
+            [(groupingExpressions)]='currentGroupingExpressions'
             [width]='width'
             [height]='height'
             [dropAreaTemplate]='currentDropArea'
@@ -2648,6 +2668,7 @@ export class DefaultGridComponent extends DataParent {
     public enableEditing = false;
     public enableGrouping = true;
     public currentSortExpressions;
+    public currentGroupingExpressions = [];
 
     public columnsCreated(column: IgxColumnComponent) {
         column.sortable = this.enableSorting;
