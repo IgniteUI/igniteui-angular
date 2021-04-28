60a41e7e0ea0bc43f85ad427fe94b70c050ed81f
ddincheva <desislava9dincheva@gmail.com>
2021-04-28 17:03:31 +0300

    chore(*): improve column types sample

--- src/app/grid-column-types/grid-column-types.sample.ts
+++ src/app/grid-column-types/grid-column-types.sample.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
-import { DisplayDensity, IgxGridComponent } from 'igniteui-angular';
+import { FilterMode, IgxGridComponent } from 'igniteui-angular';
 import { SAMPLE_DATA } from '../shared/sample-data';
 
 @Component({
@@ -15,27 +15,36 @@ export class GridColumnTypesSampleComponent implements OnInit {
     public columns: Array<any>;
     public newIndex = 0;
     public gridMCHNewIndex = 0;
-    public density: DisplayDensity = 'comfortable';
-    public displayDensities;
+    public filterMode: FilterMode = FilterMode.quickFilter;
+    public filterModes;
 
     public log(event) {
         console.log(event);
     }
 
     public ngOnInit(): void {
-        this.displayDensities = [
-            { label: 'comfortable', selected: this.density === 'comfortable', togglable: true },
-            { label: 'cosy', selected: this.density === 'cosy', togglable: true },
-            { label: 'compact', selected: this.density === 'compact', togglable: true }
+        this.filterModes = [
+            { label: 'quickFilter', selected: this.filterMode === FilterMode.quickFilter, togglable: true },
+            { label: 'excelStyleFilter', selected: this.filterMode === FilterMode.excelStyleFilter, togglable: true }
         ];
 
         this.data = SAMPLE_DATA;
 
         this.columns = [
             { field: 'ID', width: 150, resizable: true, movable: true, sortable: false, filterable: true, groupable: true,
-                summary: true, type: 'string', pinned: true },
+                summary: true, type: 'string', pinned: false },
             { field: 'CompanyName', width: 150, resizable: true, movable: true, sortable: true, filterable: true, groupable: true,
                 summary: true, type: 'string'},
+            { field: 'Employees', width: 150, resizable: true, movable: true, sortable: true, filterable: true, groupable: true,
+                summary: false, type: 'number' },
+            { field: 'DateCreated', width: 150, resizable: true, pinned: false, sortable: true, filterable: true, groupable: true,
+                summary: true, type: 'dateTime' },
+            { field: 'Time', width: 150, resizable: true, pinned: false, sortable: true, filterable: true, groupable: true,
+                summary: true, type: 'time' },
+            { field: 'Discount', width: 150, resizable: true, pinned: false, sortable: true, filterable: true, groupable: true,
+                summary: true, type: 'percent' },
+            { field: 'Contract', width: 150, resizable: true, movable: true, sortable: true, filterable: true, groupable: true,
+                summary: true, type: 'boolean' },
             { field: 'ContactName', width: 150, resizable: true, movable: true, sortable: true, filterable: true, groupable: true,
                 summary: true, type: 'string' },
             { field: 'ContactTitle', width: 150, resizable: true, movable: true, sortable: true, filterable: true, groupable: true,
@@ -44,27 +53,13 @@ export class GridColumnTypesSampleComponent implements OnInit {
                 summary: true, type: 'string' },
             { field: 'City', width: 150, resizable: true, movable: true, sortable: false, filterable: false, groupable: true,
                 summary: true, type: 'string' },
-            { field: 'Region', width: 150, resizable: true, movable: true, sortable: true, filterable: true, groupable: true,
-                summary: true, type: 'string' },
-            { field: 'PostalCode', width: 150, resizable: true, movable: true, sortable: true, filterable: true, groupable: true,
-                summary: true, type: 'string', pinned: false },
             { field: 'Phone', width: 150, resizable: true, movable: true, sortable: true, filterable: true, groupable: true,
-                summary: true, type: 'string', pinned: false },
-            { field: 'Fax', width: 150, resizable: true, movable: true, sortable: true, filterable: true, groupable: true,
-                summary: true, type: 'string' },
-            { field: 'Employees', width: 150, resizable: true, movable: true, sortable: true, filterable: true, groupable: true,
-                summary: false, type: 'number' },
-            { field: 'DateCreated', width: 150, resizable: true, pinned: true, sortable: true, filterable: true, groupable: true,
-                summary: true, type: 'dateTime' },
-            { field: 'Time', width: 150, resizable: true, pinned: true, sortable: true, filterable: true, groupable: true,
-                summary: true, type: 'time' },
-            { field: 'Contract', width: 150, resizable: true, movable: true, sortable: true, filterable: true, groupable: true,
-                summary: true, type: 'boolean' }
+                summary: true, type: 'string', pinned: false }
         ];
     }
 
     public selectDensity(event) {
-        this.density = this.displayDensities[event.index].label;
+        this.filterMode = this.filterModes[event.index].label;
     }
 
     public moveColumn() {
