4faae1723c5bf8ddaab8249774604d09de72b8e4
TTonev <phelbs@gmail.com>
2021-04-28 13:55:39 +0300

    Merge branch 'mkirkova/fix-8644-master' of https://github.com/IgniteUI/igniteui-angular into master

--- projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
@@ -3008,7 +3008,7 @@ export abstract class IgxGridBaseDirective extends DisplayDensityBase implements
     /**
      * @hidden @internal
      */
-    private get hasNoData(): boolean {
+    public get hasNoData(): boolean {
         return !this.data || this.dataLength === 0;
     }
 
