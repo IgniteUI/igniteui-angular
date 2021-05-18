9ace262ab5932a0bac4b9a10d8fe8d8d00c7da67
ddincheva <desislava9dincheva@gmail.com>
2021-05-18 09:13:08 +0300

    fix(Grid): do not reset colums colection when is initially empty #7681

--- projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
@@ -6638,11 +6638,9 @@ export abstract class IgxGridBaseDirective extends DisplayDensityBase implements
             this.columnList.reset(columnLayoutColumns);
         }
         this._maxLevelHeaderDepth = null;
-        if (this._columns.length) {
-            this._columns = this.columnList.toArray();
-        } else {
-            this._columns.push(...this.columnList.toArray());
-        }
+        this._columns.length = 0;
+        this._columns.push(...this.columnList.toArray());
+
         collection.forEach((column: IgxColumnComponent) => {
             column.defaultWidth = this.columnWidthSetByUser ? this._columnWidth : column.defaultWidth ? column.defaultWidth : '';
 
