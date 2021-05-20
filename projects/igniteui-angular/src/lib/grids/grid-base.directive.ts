4d8cf368fb417e2a9ca9d565b0a7465b6318c1ca
Martin Asenov <masenov@infragistics.com>
2021-05-20 11:07:20 +0300

    chore(grid): This reverts commit f408f53f9fdb5c86e628bb2cf2d090c5f7fae363

--- projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
@@ -4240,6 +4240,14 @@ export abstract class IgxGridBaseDirective extends DisplayDensityBase implements
      * ```
      */
     public moveColumn(column: IgxColumnComponent, target: IgxColumnComponent, pos: DropPosition = DropPosition.AfterDropTarget) {
+        // M.A. May 11th, 2021 #9508 Make the event cancelable
+        const eventArgs: IColumnMovingEndEventArgs = { source: column, target, cancel: false };
+
+        this.columnMovingEnd.emit(eventArgs);
+
+        if (eventArgs.cancel) {
+            return;
+        }
 
         if (column === target || (column.level !== target.level) ||
             (column.topLevelParent !== target.topLevelParent)) {
@@ -4273,18 +4281,8 @@ export abstract class IgxGridBaseDirective extends DisplayDensityBase implements
             this._reorderColumns(column, target, pos, this._unpinnedColumns);
         }
 
-        this._columnsReordered(column);
-
-        // M.A. May 11th, 2021 #9508 Make the event cancelable
-        const eventArgs: IColumnMovingEndEventArgs = { source: column, target, cancel: false };
-
-        this.columnMovingEnd.emit(eventArgs);
-
-        if (eventArgs.cancel) {
-            return;
-        }
-
         this._moveColumns(column, target, pos);
+        this._columnsReordered(column);
     }
 
     /**
