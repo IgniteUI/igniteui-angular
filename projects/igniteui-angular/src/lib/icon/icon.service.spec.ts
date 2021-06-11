768659a204e6a0de410e7c71305a64806e45bfd8
Silvia Ivanova <SIvanova@infragistics.com>
2021-06-11 11:11:02 +0300

    fix(icons): igx-svg-container should be inside the body

--- projects/igniteui-angular/src/lib/icon/icon.service.spec.ts
+++ projects/igniteui-angular/src/lib/icon/icon.service.spec.ts
@@ -113,4 +113,17 @@ describe('Icon Service', () => {
 
         iconService.addSvgIcon(name, 'test.svg', family);
     });
+
+    it('should create svg container inside the body', () => {
+        const iconService = TestBed.inject(IgxIconService) as IgxIconService;
+        const document = TestBed.inject(DOCUMENT);
+
+        const name = 'test';
+        const family = 'svg-icons';
+
+        iconService.addSvgIconFromText(name, svgText, family);
+
+        const svgContainer = document.body.querySelector('.igx-svg-container');
+        expect(svgContainer).toHaveClass('igx-svg-container');
+    });
 });
