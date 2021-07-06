d506bdeb7fc14c61557ddc6b6363fb122c99a68a
Maria Tsvyatkova <mtsvyatkova@infragistics.com>
2021-07-06 10:10:38 +0300

    chore(test): update test await #9496

--- projects/igniteui-angular/src/lib/tabs/tabs/tabs.component.spec.ts
+++ projects/igniteui-angular/src/lib/tabs/tabs/tabs.component.spec.ts
@@ -1244,6 +1244,7 @@ describe('IgxTabs', () => {
         it('should hide scroll buttons if visible when alignment is set to "justify".', async () => {
             fixture.componentInstance.wrapperDiv.nativeElement.style.width = '360px';
             fixture.detectChanges();
+            await wait(200);
 
             const rightScrollButton = tabs.headerContainer.nativeElement.children[2];
             expect(rightScrollButton.clientWidth).toBeTruthy();
