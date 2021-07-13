d1202c55ce3d5dc1edf8680a5c084eb2c56f23a0
iganchev <iganchev@infragistics.com>
2021-07-13 17:14:41 +0300

    refactor(i18n): Move i18n package under product repo,
    Plug i18n into CI and CD.
    Migrate from tslint to eslint.

---
+++ projects/igniteui-angular-i18n/src/i18n/FR/resources.ts
@@ -0,0 +1,24 @@
+import { GridResourceStringsFR } from './grid-resources';
+import { PaginatorResourceStringsFR } from './paginator-resources';
+import { TimePickerResourceStringsFR } from './time-picker-resources';
+import { DatePickerResourceStringsFR } from './date-picker-resources';
+import { DateRangePickerResourceStringsFR } from './date-range-picker-resources';
+import { CarouselResourceStringsFR } from './carousel-resources';
+import { ListResourceStringsFR } from './list-resources';
+import { CalendarResourceStringsFR } from './calendar-resources';
+import { TreeResourceStringsFR } from './tree-resources';
+
+/**
+ * French resource strings for all components
+ */
+export const IgxResourceStringsFR = {
+    ...GridResourceStringsFR,
+    ...PaginatorResourceStringsFR,
+    ...TimePickerResourceStringsFR,
+    ...DatePickerResourceStringsFR,
+    ...DateRangePickerResourceStringsFR,
+    ...CarouselResourceStringsFR,
+    ...ListResourceStringsFR,
+    ...CalendarResourceStringsFR,
+    ...TreeResourceStringsFR
+};
---
+++ projects/igniteui-angular-i18n/src/i18n/RO/list-resources.ts
@@ -0,0 +1,12 @@
+import { IListResourceStrings } from 'igniteui-angular';
+
+// exported below as re-cast to create declaration type with expanded properties
+const ListResourceStringsRO_: ExpandRequire<IListResourceStrings> = {
+    igx_list_no_items: 'Nu există articole în listă.',
+    igx_list_loading: 'Se încarcă datele de pe server...'
+};
+
+/**
+ * Romanian resource strings for IgxList
+ */
+export const ListResourceStringsRO = ListResourceStringsRO_ as ExpandRequire<IListResourceStrings>;
