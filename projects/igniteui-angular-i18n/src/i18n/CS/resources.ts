ab5332b61213ad510b3e65dd87a2e1e03f9bcdc6
IvayloG <inj.iganchev@gmail.com>
2021-07-16 14:44:17 +0300

    chore(*): move i18n package under the product repo #6602 (#9826)

---
+++ projects/igniteui-angular-i18n/src/i18n/CS/resources.ts
@@ -0,0 +1,24 @@
+import { GridResourceStringsCS } from './grid-resources';
+import { PaginatorResourceStringsCS } from './paginator-resources';
+import { TimePickerResourceStringsCS } from './time-picker-resources';
+import { DatePickerResourceStringsCS } from './date-picker-resources';
+import { DateRangePickerResourceStringsCS } from './date-range-picker-resources';
+import { CarouselResourceStringsCS } from './carousel-resources';
+import { ListResourceStringsCS } from './list-resources';
+import { CalendarResourceStringsCS } from './calendar-resources';
+import { TreeResourceStringsCS } from './tree-resources';
+
+/**
+ * Czech resource strings for all components
+ */
+export const IgxResourceStringsCS = {
+    ...GridResourceStringsCS,
+    ...PaginatorResourceStringsCS,
+    ...TimePickerResourceStringsCS,
+    ...DatePickerResourceStringsCS,
+    ...DateRangePickerResourceStringsCS,
+    ...CarouselResourceStringsCS,
+    ...ListResourceStringsCS,
+    ...CalendarResourceStringsCS,
+    ...TreeResourceStringsCS
+};
---
+++ projects/igniteui-angular-i18n/src/i18n/DA/date-range-picker-resources.ts
@@ -0,0 +1,12 @@
+import { IDateRangePickerResourceStrings } from 'igniteui-angular';
+
+// exported below as re-cast to create declaration type with expanded properties
+const DateRangePickerResourceStringsDA_: ExpandRequire<IDateRangePickerResourceStrings> = {
+    igx_date_range_picker_date_separator: 'till',
+    igx_date_range_picker_done_button: 'Færdigt'
+};
+
+/**
+ * Danish resource strings for IgxDateRangePicker
+ */
+export const DateRangePickerResourceStringsDA = DateRangePickerResourceStringsDA_ as ExpandRequire<IDateRangePickerResourceStrings>;
