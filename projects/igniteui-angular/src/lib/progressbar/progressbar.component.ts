3ecb43357244ee49119d52c5144d1aa4e86a6377
Katrin Dragieva <KDragieva@infragistics.local>
2021-09-14 10:05:38 +0300

    fix(BaseProgressDirective): fix tslint issues

--- projects/igniteui-angular/src/lib/progressbar/progressbar.component.ts
+++ projects/igniteui-angular/src/lib/progressbar/progressbar.component.ts
@@ -147,7 +147,6 @@ export abstract class BaseProgressDirective {
         if(maxNum < this._value) {
             return;
         }
-        
         this._max = maxNum;
         this._valueInPercent = toPercent(this._value, this._max);
     }
@@ -163,7 +162,7 @@ export abstract class BaseProgressDirective {
      * }
      * ```
      */
-    get max() {
+    public get max() {
         return this._max;
     }
 
@@ -181,9 +180,8 @@ export abstract class BaseProgressDirective {
         if (value < 0 || value > 100) {
             return;
         }
-        
         this._valueInPercent = value;
-        this._value = toValue(value, this._max)
+        this._value = toValue(value, this._max);
     }
 
     /**
