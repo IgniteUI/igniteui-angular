58f167940b5be4a269ab1d108370b1702ca1bb7a
Rumyana Andriova <54146583+randriova@users.noreply.github.com>
2021-09-15 15:08:40 +0300

    feat(i18n): Removing redundant comma.

--- projects/igniteui-angular-i18n/src/i18n/NB/paginator-resources.ts
+++ projects/igniteui-angular-i18n/src/i18n/NB/paginator-resources.ts
@@ -7,7 +7,7 @@ const PaginatorResourceStringsNB_: ExpandRequire<IPaginatorResourceStrings> = {
     igx_paginator_first_page_button_text: 'Gå til første side',
     igx_paginator_previous_page_button_text: 'Forrige side',
     igx_paginator_last_page_button_text: 'Gå til siste side',
-    igx_paginator_next_page_button_text: 'Neste side',
+    igx_paginator_next_page_button_text: 'Neste side'
 };
 
 /**
