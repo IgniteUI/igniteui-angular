107722c1416ec4befb103c51c6efbb48d0eeeac4
dobromirts <dobromir.ts@gmail.com>
2021-09-16 15:45:47 +0300

    chore(dev-demos): Add drag and drop sample overlapping shadow dom component

---
+++ src/app/drag-drop/shadow-dom-grid/shadow-grid-sample.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { IgxColumnComponent } from 'igniteui-angular';
+
+@Component({
+  selector: 'app-shadow-grid-sample',
+  templateUrl: './shadow-grid-sample.html',
+  styleUrls: ['./shadow-grid-sample.scss']
+})
+export class ShadowGridSampleComponent implements OnInit {
+  public localData: any[];
+  public selectedCell;
+
+  public ngOnInit(): void {
+    this.localData = [
+      { ID: 1, Name: 'A' },
+      { ID: 2, Name: 'B' },
+      { ID: 3, Name: 'C' },
+      { ID: 4, Name: 'D' },
+      { ID: 5, Name: 'E' }
+    ];
+  }
+
+  public initColumns(event: IgxColumnComponent) {
+    const column: IgxColumnComponent = event;
+    column.movable = true;
+    if (column.field === 'Name') {
+      column.filterable = true;
+      column.sortable = true;
+      column.editable = true;
+    }
+  }
+
+  public selectCell(event) {
+    this.selectedCell = event;
+  }
+}
