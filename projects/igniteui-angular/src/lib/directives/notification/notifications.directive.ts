12baba5531a3b9bf1a6625f55efc5ab01907da53
dobromirts <dobromir.ts@gmail.com>
2021-09-17 11:18:00 +0300

    feat(snackbar): Add input for custom position strategy#10126 - 12.1.x

--- projects/igniteui-angular/src/lib/directives/notification/notifications.directive.ts
+++ projects/igniteui-angular/src/lib/directives/notification/notifications.directive.ts
@@ -64,7 +64,7 @@ export abstract class IgxNotificationsDirective extends IgxToggleDirective
      * @hidden
      * @internal
      */
-    public textMessage: string | OverlaySettings = '';
+    public textMessage = '';
 
     /**
      * @hidden
