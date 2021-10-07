085d7631d01772860d2058eafd38e0efb80c1b74
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2021-10-07 09:06:55 +0300

    Migrate to beta 14

--- projects/igniteui-angular/src/test.ts
+++ projects/igniteui-angular/src/test.ts
@@ -22,10 +22,10 @@ registerLocaleData(localeDe);
 registerLocaleData(localeJa);
 
 // First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
-);
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
+    teardown: { destroyAfterEach: false }
+});
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
