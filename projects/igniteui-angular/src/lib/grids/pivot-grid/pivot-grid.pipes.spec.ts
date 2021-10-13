27c8e686043f60fc472cf59da9cbaad5a9e22d33
Deyan Kamburov <dkamburov@infragistics.com>
2021-10-13 14:37:38 +0300

    test(pivot): Fix test expectations

--- projects/igniteui-angular/src/lib/grids/pivot-grid/pivot-grid.pipes.spec.ts
+++ projects/igniteui-angular/src/lib/grids/pivot-grid/pivot-grid.pipes.spec.ts
@@ -53,17 +53,17 @@ describe('Pivot pipes', () => {
         const columnPipeResult = columnPipe.transform(rowPipeResult, pivotConfigHierarchy.columns, pivotConfigHierarchy.values);
         expect(columnPipeResult).toEqual([
             {
-                field1: 'All', All: 2127, Bulgaria: 774, USA: 829, Uruguay: 524, level: 0, records: [
-                    { field1: 'Clothing', All: 1526, Bulgaria: 774, USA: 296, Uruguay: 456, level: 1 },
-                    { field1: 'Bikes', All: 68, Uruguay: 68, level: 1 },
-                    { field1: 'Accessories', All: 293, USA: 293, level: 1 },
-                    { field1: 'Components', All: 240, USA: 240, level: 1 }
+                field1: 'All', All: 2127, 'All-Bulgaria': 774, 'All-USA': 829, 'All-Uruguay': 524, level: 0, records: [
+                    { field1: 'Clothing', All: 1526, 'All-Bulgaria': 774,'All-USA': 296,'All-Uruguay': 456, level: 1 },
+                    { field1: 'Bikes', All: 68, 'All-Uruguay': 68, level: 1 },
+                    { field1: 'Accessories', All: 293, 'All-USA': 293, level: 1 },
+                    { field1: 'Components', All: 240, 'All-USA': 240, level: 1 }
                 ]
             },
-            { field1: 'Clothing', All: 1526, Bulgaria: 774, USA: 296, Uruguay: 456, level: 1 },
-            { field1: 'Bikes', All: 68, Uruguay: 68, level: 1 },
-            { field1: 'Accessories', All: 293, USA: 293, level: 1 },
-            { field1: 'Components', All: 240, USA: 240, level: 1 }
+            { field1: 'Clothing', All: 1526, 'All-Bulgaria': 774,'All-USA': 296,'All-Uruguay': 456, level: 1 },
+            { field1: 'Bikes', All: 68, 'All-Uruguay': 68, level: 1 },
+            { field1: 'Accessories', All: 293, 'All-USA': 293, level: 1 },
+            { field1: 'Components', All: 240, 'All-USA': 240, level: 1 }
         ]);
     });
 
@@ -99,10 +99,10 @@ describe('Pivot pipes', () => {
         ]);
         const columnPipeResult = columnPipe.transform(rowPipeResult, pivotConfigHierarchy.columns, pivotConfigHierarchy.values);
         expect(columnPipeResult).toEqual([
-            { ProductCategory: 'Clothing', All: 1526, Bulgaria: 774, USA: 296, Uruguay: 456, level: 0 },
-            { ProductCategory: 'Bikes', All: 68, Uruguay: 68, level: 0 },
-            { ProductCategory: 'Accessories', All: 293, USA: 293, level: 0 },
-            { ProductCategory: 'Components', All: 240, USA: 240, level: 0 }
+            { ProductCategory: 'Clothing', All: 1526, 'All-Bulgaria': 774, 'All-USA': 296, 'All-Uruguay': 456, level: 0 },
+            { ProductCategory: 'Bikes', All: 68, 'All-Uruguay': 68, level: 0 },
+            { ProductCategory: 'Accessories', All: 293, 'All-USA': 293, level: 0 },
+            { ProductCategory: 'Components', All: 240, 'All-USA': 240, level: 0 }
         ]);
     });
 
@@ -212,14 +212,14 @@ describe('Pivot pipes', () => {
          ]);
     });
 
-    xit('transforms flat data to pivot data multiple row dimensions', () => {
+    it('transforms flat data to pivot data multiple row dimensions', () => {
         const rowPipeResult = rowPipe.transform(data, [{
             member: 'ProductCategory',
             enabled: true,
             childLevels: []
         },
         {
-            member: Date,
+            member: 'Date',
             enabled: true,
             childLevels: []
         }], expansionStates, pivotConfigHierarchy.values);
@@ -227,38 +227,67 @@ describe('Pivot pipes', () => {
         expect(rowPipeResult).toEqual([
             {
                 ProductCategory: 'Clothing',
+                Date: '',
                 records: [
                     {
-                        ProductCategory: 'Clothing',
-                        UnitPrice: 12.81,
-                        SellerName: 'Stanley',
-                        Country: 'Bulgaria',
                         Date: '01/01/2021',
-                        UnitsSold: 282
+                        records: [
+                            {
+                                ProductCategory: 'Clothing',
+                                UnitPrice: 12.81,
+                                SellerName: 'Stanley',
+                                Country: 'Bulgaria',
+                                Date: '01/01/2021',
+                                UnitsSold: 282
+                            }
+                        ],
+                        level: 1,
+                        ProductCategory: 'Clothing'
                     },
                     {
-                        ProductCategory: 'Clothing',
-                        UnitPrice: 49.57,
-                        SellerName: 'Elisa',
-                        Country: 'USA',
                         Date: '01/05/2019',
-                        UnitsSold: 296
+                        records: [
+                            {
+                                ProductCategory: 'Clothing',
+                                UnitPrice: 49.57,
+                                SellerName: 'Elisa',
+                                Country: 'USA',
+                                Date: '01/05/2019',
+                                UnitsSold: 296
+                            }
+                        ],
+                        level: 1,
+                        ProductCategory: 'Clothing'
                     },
                     {
-                        ProductCategory: 'Clothing',
-                        UnitPrice: 68.33,
-                        SellerName: 'Larry',
-                        Country: 'Uruguay',
                         Date: '05/12/2020',
-                        UnitsSold: 456
+                        records: [
+                            {
+                                ProductCategory: 'Clothing',
+                                UnitPrice: 68.33,
+                                SellerName: 'Larry',
+                                Country: 'Uruguay',
+                                Date: '05/12/2020',
+                                UnitsSold: 456
+                            }
+                        ],
+                        level: 1,
+                        ProductCategory: 'Clothing'
                     },
                     {
-                        ProductCategory: 'Clothing',
-                        UnitPrice: 16.05,
-                        SellerName: 'Walter',
-                        Country: 'Bulgaria',
                         Date: '02/19/2020',
-                        UnitsSold: 492
+                        records: [
+                            {
+                                ProductCategory: 'Clothing',
+                                UnitPrice: 16.05,
+                                SellerName: 'Walter',
+                                Country: 'Bulgaria',
+                                Date: '02/19/2020',
+                                UnitsSold: 492
+                            }
+                        ],
+                        level: 1,
+                        ProductCategory: 'Clothing'
                     }
                 ],
                 level: 0
@@ -325,14 +354,22 @@ describe('Pivot pipes', () => {
             },
             {
                 ProductCategory: 'Bikes',
+                Date: '',
                 records: [
                     {
-                        ProductCategory: 'Bikes',
-                        UnitPrice: 3.56,
-                        SellerName: 'Lydia',
-                        Country: 'Uruguay',
                         Date: '01/06/2020',
-                        UnitsSold: 68
+                        records: [
+                            {
+                                ProductCategory: 'Bikes',
+                                UnitPrice: 3.56,
+                                SellerName: 'Lydia',
+                                Country: 'Uruguay',
+                                Date: '01/06/2020',
+                                UnitsSold: 68
+                            }
+                        ],
+                        level: 1,
+                        ProductCategory: 'Bikes'
                     }
                 ],
                 level: 0
@@ -354,14 +391,22 @@ describe('Pivot pipes', () => {
             },
             {
                 ProductCategory: 'Accessories',
+                Date: '',
                 records: [
                     {
-                        ProductCategory: 'Accessories',
-                        UnitPrice: 85.58,
-                        SellerName: 'David',
-                        Country: 'USA',
                         Date: '04/07/2021',
-                        UnitsSold: 293
+                        records: [
+                            {
+                                ProductCategory: 'Accessories',
+                                UnitPrice: 85.58,
+                                SellerName: 'David',
+                                Country: 'USA',
+                                Date: '04/07/2021',
+                                UnitsSold: 293
+                            }
+                        ],
+                        level: 1,
+                        ProductCategory: 'Accessories'
                     }
                 ],
                 level: 0
@@ -383,14 +428,22 @@ describe('Pivot pipes', () => {
             },
             {
                 ProductCategory: 'Components',
+                Date: '',
                 records: [
                     {
-                        ProductCategory: 'Components',
-                        UnitPrice: 18.13,
-                        SellerName: 'John',
-                        Country: 'USA',
                         Date: '12/08/2021',
-                        UnitsSold: 240
+                        records: [
+                            {
+                                ProductCategory: 'Components',
+                                UnitPrice: 18.13,
+                                SellerName: 'John',
+                                Country: 'USA',
+                                Date: '12/08/2021',
+                                UnitsSold: 240
+                            }
+                        ],
+                        level: 1,
+                        ProductCategory: 'Components'
                     }
                 ],
                 level: 0
