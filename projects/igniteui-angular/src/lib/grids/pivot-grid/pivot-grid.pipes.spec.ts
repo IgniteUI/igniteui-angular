aef4b74993739759286ba172c00ec105b6c1f4bc
Deyan Kamburov <dkamburov@infragistics.com>
2021-10-15 16:27:42 +0300

    test(pivot): Merge test expecations with row dimension changes

--- projects/igniteui-angular/src/lib/grids/pivot-grid/pivot-grid.pipes.spec.ts
+++ projects/igniteui-angular/src/lib/grids/pivot-grid/pivot-grid.pipes.spec.ts
@@ -2,7 +2,7 @@ import { IgxNumberSummaryOperand } from '../summaries/grid-summary';
 import { IPivotConfiguration, IPivotValue } from './pivot-grid.interface';
 import { IgxPivotColumnPipe, IgxPivotRowPipe } from './pivot-grid.pipes';
 
-describe('Pivot pipes', () => {
+fdescribe('Pivot pipes', () => {
     // This pipe is a pure, stateless function so no need for BeforeEach
     const rowPipe = new IgxPivotRowPipe();
     const columnPipe = new IgxPivotColumnPipe();
@@ -113,7 +113,7 @@ describe('Pivot pipes', () => {
        expect(rowPipeCollapseResult).toEqual([
            { field1:'All',records: [
                {
-                    field1:'All-Clothing',
+                    field1:'Clothing',
                     records:[
                     {ProductCategory:'Clothing',UnitPrice:12.81,SellerName:'Stanley',Country:'Bulgaria',Date:'01/01/2021',UnitsSold:282},
                     {ProductCategory:'Clothing',UnitPrice:49.57,SellerName:'Elisa',Country:'USA',Date:'01/05/2019',UnitsSold:296},
@@ -123,20 +123,20 @@ describe('Pivot pipes', () => {
                     ,level:1
                 },
                 {
-                    field1:'All-Bikes',
+                    field1:'Bikes',
                     records:[
                         {ProductCategory:'Bikes',UnitPrice:3.56,SellerName:'Lydia',Country:'Uruguay',Date:'01/06/2020',UnitsSold:68}
                     ]
                     ,level:1
                 },
                 {
-                    field1:'All-Accessories',
+                    field1:'Accessories',
                     records:[
                         {ProductCategory:'Accessories',UnitPrice:85.58,SellerName:'David',Country:'USA',Date:'04/07/2021',UnitsSold:293}
                     ]
                     ,level:1},
                 {
-                field1:'All-Components',
+                field1:'Components',
                 records:[
                     {ProductCategory:'Components',UnitPrice:18.13,SellerName:'John',Country:'USA',Date:'12/08/2021',UnitsSold:240}
                 ]
@@ -150,7 +150,7 @@ describe('Pivot pipes', () => {
         expect(rowPipeExpandResult).toEqual([
             { field1:'All',records: [
                 {
-                     field1:'All-Clothing',
+                     field1:'Clothing',
                      records:[
                      {ProductCategory:'Clothing',UnitPrice:12.81,SellerName:'Stanley',Country:'Bulgaria',Date:'01/01/2021',UnitsSold:282},
                      {ProductCategory:'Clothing',UnitPrice:49.57,SellerName:'Elisa',Country:'USA',Date:'01/05/2019',UnitsSold:296},
@@ -160,20 +160,20 @@ describe('Pivot pipes', () => {
                      ,level:1
                  },
                  {
-                     field1:'All-Bikes',
+                     field1:'Bikes',
                      records:[
                          {ProductCategory:'Bikes',UnitPrice:3.56,SellerName:'Lydia',Country:'Uruguay',Date:'01/06/2020',UnitsSold:68}
                      ]
                      ,level:1
                  },
                  {
-                     field1:'All-Accessories',
+                     field1:'Accessories',
                      records:[
                          {ProductCategory:'Accessories',UnitPrice:85.58,SellerName:'David',Country:'USA',Date:'04/07/2021',UnitsSold:293}
                      ]
                      ,level:1},
                  {
-                 field1:'All-Components',
+                 field1:'Components',
                  records:[
                      {ProductCategory:'Components',UnitPrice:18.13,SellerName:'John',Country:'USA',Date:'12/08/2021',UnitsSold:240}
                  ]
@@ -181,7 +181,7 @@ describe('Pivot pipes', () => {
              ]
              ,level:0},
              {
-                field1:'All-Clothing',
+                field1:'Clothing',
                 records:[
                 {ProductCategory:'Clothing',UnitPrice:12.81,SellerName:'Stanley',Country:'Bulgaria',Date:'01/01/2021',UnitsSold:282},
                 {ProductCategory:'Clothing',UnitPrice:49.57,SellerName:'Elisa',Country:'USA',Date:'01/05/2019',UnitsSold:296},
@@ -191,20 +191,20 @@ describe('Pivot pipes', () => {
                 ,level:1
             },
             {
-                field1:'All-Bikes',
+                field1:'Bikes',
                 records:[
                     {ProductCategory:'Bikes',UnitPrice:3.56,SellerName:'Lydia',Country:'Uruguay',Date:'01/06/2020',UnitsSold:68}
                 ]
                 ,level:1
             },
             {
-                field1:'All-Accessories',
+                field1:'Accessories',
                 records:[
                     {ProductCategory:'Accessories',UnitPrice:85.58,SellerName:'David',Country:'USA',Date:'04/07/2021',UnitsSold:293}
                 ]
                 ,level:1},
             {
-            field1:'All-Components',
+            field1:'Components',
             records:[
                 {ProductCategory:'Components',UnitPrice:18.13,SellerName:'John',Country:'USA',Date:'12/08/2021',UnitsSold:240}
             ]
@@ -348,19 +348,19 @@ describe('Pivot pipes', () => {
         /* eslint-disable quote-props */
         expect(columnPipeResult).toEqual([
             {'field1':'All','records':[
-                {'field1':'All-Clothing','level':1,'Bulgaria':774,'Bulgaria-01/01/2021':282,
+                {'field1':'Clothing','level':1,'Bulgaria':774,'Bulgaria-01/01/2021':282,
                 'Bulgaria-02/19/2020':492,'USA':296,'USA-01/05/2019':296,'Uruguay':456,'Uruguay-05/12/2020':456},
-                {'field1':'All-Bikes','level':1,'Uruguay':68,'Uruguay-01/06/2020':68},
-                {'field1':'All-Accessories','level':1,'USA':293,'USA-04/07/2021':293},
-                {'field1':'All-Components','level':1,'USA':240,'USA-12/08/2021':240}]
+                {'field1':'Bikes','level':1,'Uruguay':68,'Uruguay-01/06/2020':68},
+                {'field1':'Accessories','level':1,'USA':293,'USA-04/07/2021':293},
+                {'field1':'Components','level':1,'USA':240,'USA-12/08/2021':240}]
                 ,'level':0,'Bulgaria':774,'Bulgaria-01/01/2021':282,'Bulgaria-02/19/2020':492,
                 'USA':829,'USA-01/05/2019':296,'USA-04/07/2021':293,'USA-12/08/2021':240,
                 'Uruguay':524,'Uruguay-05/12/2020':456,'Uruguay-01/06/2020':68},
-            {'field1':'All-Clothing','level':1,'Bulgaria':774,'Bulgaria-01/01/2021':282,
+            {'field1':'Clothing','level':1,'Bulgaria':774,'Bulgaria-01/01/2021':282,
             'Bulgaria-02/19/2020':492,'USA':296,'USA-01/05/2019':296,'Uruguay':456,'Uruguay-05/12/2020':456},
-            {'field1':'All-Bikes','level':1,'Uruguay':68,'Uruguay-01/06/2020':68},
-            {'field1':'All-Accessories','level':1,'USA':293,'USA-04/07/2021':293},
-            {'field1':'All-Components','level':1,'USA':240,'USA-12/08/2021':240}]);
+            {'field1':'Bikes','level':1,'Uruguay':68,'Uruguay-01/06/2020':68},
+            {'field1':'Accessories','level':1,'USA':293,'USA-04/07/2021':293},
+            {'field1':'Components','level':1,'USA':240,'USA-12/08/2021':240}]);
     });
 
     it('transforms flat data to pivot data 3 column dimensions', () => {
@@ -384,23 +384,23 @@ describe('Pivot pipes', () => {
         expect(columnPipeResult).toEqual([
             {'field1':'All','records':
             [
-                {'field1':'All-Clothing','level':1,'Bulgaria':774,'Bulgaria-Stanley':282,'Bulgaria-Stanley-01/01/2021':282,
+                {'field1':'Clothing','level':1,'Bulgaria':774,'Bulgaria-Stanley':282,'Bulgaria-Stanley-01/01/2021':282,
                 'Bulgaria-Walter':492,'Bulgaria-Walter-02/19/2020':492,'USA':296,'USA-Elisa':296,'USA-Elisa-01/05/2019':296,
                 'Uruguay':456,'Uruguay-Larry':456,'Uruguay-Larry-05/12/2020':456},
-                {'field1':'All-Bikes','level':1,'Uruguay':68,'Uruguay-Lydia':68,'Uruguay-Lydia-01/06/2020':68},
-                {'field1':'All-Accessories','level':1,'USA':293,'USA-David':293,'USA-David-04/07/2021':293},
-                {'field1':'All-Components','level':1,'USA':240,'USA-John':240,'USA-John-12/08/2021':240}
+                {'field1':'Bikes','level':1,'Uruguay':68,'Uruguay-Lydia':68,'Uruguay-Lydia-01/06/2020':68},
+                {'field1':'Accessories','level':1,'USA':293,'USA-David':293,'USA-David-04/07/2021':293},
+                {'field1':'Components','level':1,'USA':240,'USA-John':240,'USA-John-12/08/2021':240}
             ],
             'level':0,'Bulgaria':774,'Bulgaria-Stanley':282,'Bulgaria-Stanley-01/01/2021':282,'Bulgaria-Walter':492,
             'Bulgaria-Walter-02/19/2020':492,'USA':829,'USA-Elisa':296,'USA-Elisa-01/05/2019':296,'USA-David':293,
             'USA-David-04/07/2021':293,'USA-John':240,'USA-John-12/08/2021':240,'Uruguay':524,'Uruguay-Larry':456,
             'Uruguay-Larry-05/12/2020':456,'Uruguay-Lydia':68,'Uruguay-Lydia-01/06/2020':68},
-            {'field1':'All-Clothing','level':1,'Bulgaria':774,'Bulgaria-Stanley':282,'Bulgaria-Stanley-01/01/2021':282,
+            {'field1':'Clothing','level':1,'Bulgaria':774,'Bulgaria-Stanley':282,'Bulgaria-Stanley-01/01/2021':282,
             'Bulgaria-Walter':492,'Bulgaria-Walter-02/19/2020':492,'USA':296,'USA-Elisa':296,'USA-Elisa-01/05/2019':296,'Uruguay':456,
             'Uruguay-Larry':456,'Uruguay-Larry-05/12/2020':456},
-            {'field1':'All-Bikes','level':1,'Uruguay':68,'Uruguay-Lydia':68,'Uruguay-Lydia-01/06/2020':68},
-            {'field1':'All-Accessories','level':1,'USA':293,'USA-David':293,'USA-David-04/07/2021':293},
-            {'field1':'All-Components','level':1,'USA':240,'USA-John':240,'USA-John-12/08/2021':240}
+            {'field1':'Bikes','level':1,'Uruguay':68,'Uruguay-Lydia':68,'Uruguay-Lydia-01/06/2020':68},
+            {'field1':'Accessories','level':1,'USA':293,'USA-David':293,'USA-David-04/07/2021':293},
+            {'field1':'Components','level':1,'USA':240,'USA-John':240,'USA-John-12/08/2021':240}
         ]);
     });
 
@@ -421,25 +421,25 @@ describe('Pivot pipes', () => {
         const columnPipeResult = columnPipe.transform(rowPipeResult, pivotConfigHierarchy.columns, values);
         expect(columnPipeResult).toEqual([{
             'field1':'All','records':[
-            {'field1':'All-Clothing','level':1,'All-UnitsSold':1526,'All-Bulgaria-UnitsSold':774,
+            {'field1':'Clothing','level':1,'All-UnitsSold':1526,'All-Bulgaria-UnitsSold':774,
             'All-Bulgaria-UnitPrice':28.86,'All-USA-UnitsSold':296,'All-USA-UnitPrice':49.57,'All-Uruguay-UnitsSold':456,
             'All-Uruguay-UnitPrice':68.33,'All-UnitPrice':146.76},
-            {'field1':'All-Bikes','level':1,'All-UnitsSold':68,'All-Uruguay-UnitsSold':68,
+            {'field1':'Bikes','level':1,'All-UnitsSold':68,'All-Uruguay-UnitsSold':68,
             'All-Uruguay-UnitPrice':3.56,'All-UnitPrice':3.56},
-            {'field1':'All-Accessories','level':1,'All-UnitsSold':293,'All-USA-UnitsSold':293,
+            {'field1':'Accessories','level':1,'All-UnitsSold':293,'All-USA-UnitsSold':293,
             'All-USA-UnitPrice':85.58,'All-UnitPrice':85.58},
-            {'field1':'All-Components','level':1,'All-UnitsSold':240,'All-USA-UnitsSold':240,
+            {'field1':'Components','level':1,'All-UnitsSold':240,'All-USA-UnitsSold':240,
             'All-USA-UnitPrice':18.13,'All-UnitPrice':18.13}
             ],'level':0,'All-UnitsSold':2127,'All-Bulgaria-UnitsSold':774,'All-Bulgaria-UnitPrice':28.86,'All-USA-UnitsSold':829,
             'All-USA-UnitPrice':153.28,'All-Uruguay-UnitsSold':524,'All-Uruguay-UnitPrice':71.89,'All-UnitPrice':254.02999999999997},
-            {'field1':'All-Clothing','level':1,'All-UnitsSold':1526,'All-Bulgaria-UnitsSold':774,
+            {'field1':'Clothing','level':1,'All-UnitsSold':1526,'All-Bulgaria-UnitsSold':774,
             'All-Bulgaria-UnitPrice':28.86,'All-USA-UnitsSold':296,'All-USA-UnitPrice':49.57,'All-Uruguay-UnitsSold':456,
             'All-Uruguay-UnitPrice':68.33,'All-UnitPrice':146.76},
-            {'field1':'All-Bikes','level':1,'All-UnitsSold':68,'All-Uruguay-UnitsSold':68,
+            {'field1':'Bikes','level':1,'All-UnitsSold':68,'All-Uruguay-UnitsSold':68,
             'All-Uruguay-UnitPrice':3.56,'All-UnitPrice':3.56},
-            {'field1':'All-Accessories','level':1,'All-UnitsSold':293,'All-USA-UnitsSold':293,
+            {'field1':'Accessories','level':1,'All-UnitsSold':293,'All-USA-UnitsSold':293,
             'All-USA-UnitPrice':85.58,'All-UnitPrice':85.58},
-            {'field1':'All-Components','level':1,'All-UnitsSold':240,'All-USA-UnitsSold':240,
+            {'field1':'Components','level':1,'All-UnitsSold':240,'All-USA-UnitsSold':240,
             'All-USA-UnitPrice':18.13,'All-UnitPrice':18.13}
             ]);
     });
