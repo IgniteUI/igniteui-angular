1c3f5ccf2c30f03856f4a5d7a28a956a00e86413
katherinedragieva <katherinedragieva@gmail.com>
2021-10-27 16:18:18 +0300

    refactor(IgxGridBaseDirective): remove trailing space

--- projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
@@ -5698,16 +5698,13 @@ export abstract class IgxGridBaseDirective extends DisplayDensityBase implements
             } else {
                 data = selectedData;
                 result = this.prepareCopyData(event, data);
-               
                 if (Object.values(selectedData[0]).length <= 1) {
                     result = result.slice(0, -2);
-                    
                     if (!this.clipboardOptions.copyHeaders) {
                         result = result.substring(result[0], result.indexOf('\n'));
                     }
                 } 
             }
-
             if (this.platform.isIE) {
                 (window as any).clipboardData.setData('Text', result);
                 return;
