471f5ef660a0e44dc1719c0428ab24fd2c996204
Deyan Kamburov <dkamburov@infragistics.com>
2021-10-29 18:42:40 +0300

    chore(pivot): Pass proper parameters in flatten function

--- projects/igniteui-angular/src/lib/grids/pivot-grid/pivot-util.ts
+++ projects/igniteui-angular/src/lib/grids/pivot-grid/pivot-util.ts
@@ -173,7 +173,7 @@ export class PivotUtil {
             acc.push(item);
           if (item.childLevel) {
             item.expandable = true;
-            acc = acc.concat(this.flatten(item.childLevel, lvl + 1));
+            acc = acc.concat(this.flatten([item.childLevel], lvl + 1));
           }
           return acc;
         }, []);
