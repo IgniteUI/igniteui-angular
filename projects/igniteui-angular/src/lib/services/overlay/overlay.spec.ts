ce947a2cd0862d2fb38f241894efc48b8e070f25
Hristo Anastasov <HAnastasov@infragistics.com>
2021-11-11 16:48:12 +0200

    chore(*): revert xit tests

--- projects/igniteui-angular/src/lib/services/overlay/overlay.spec.ts
+++ projects/igniteui-angular/src/lib/services/overlay/overlay.spec.ts
@@ -516,7 +516,7 @@ describe('igxOverlay', () => {
             expect((overlayDiv.children[1] as HTMLElement).style.visibility).toEqual('hidden');
         }));
 
-        xit('Should show and hide component via directive.', fakeAsync(() => {
+        it('Should show and hide component via directive.', fakeAsync(() => {
             const fixture = TestBed.createComponent(SimpleDynamicWithDirectiveComponent);
             fixture.detectChanges();
             fixture.componentInstance.show();
