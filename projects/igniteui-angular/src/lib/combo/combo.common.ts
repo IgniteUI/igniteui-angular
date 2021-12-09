644614650267a1337f18527e5cf56aa38a27e3b9
VDyulgerov <volen1999@gmail.com>
2021-12-09 14:21:41 +0200

    fix(combo): fix properties of undefined error

--- projects/igniteui-angular/src/lib/combo/combo.common.ts
+++ projects/igniteui-angular/src/lib/combo/combo.common.ts
@@ -1154,6 +1154,7 @@ export abstract class IgxComboBaseDirective extends DisplayDensityBase implement
 
     protected findMatch = (element: any): boolean => {
         const value = this.displayKey ? element[this.displayKey] : element;
+        if (!value) return false;
         return value.toString().toLowerCase() === this.searchValue.trim().toLowerCase();
     };
 
