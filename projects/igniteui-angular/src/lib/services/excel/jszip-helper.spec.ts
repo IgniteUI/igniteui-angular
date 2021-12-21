8a637c6d26b1f35cfb1c667b93eb4205e5bc9442
Galina Edinakova <gedinakova@infragistics.com>
2021-12-21 10:33:09 +0200

    fix(excel-export): sucessfully export hgrid with skipped columns (#10750)
    * fix(excel-export): sucessfully export hgrid with skipped columns
    
    Co-authored-by: IBarakov <ibarakov@infragistics.com>

--- projects/igniteui-angular/src/lib/services/excel/jszip-helper.spec.ts
+++ projects/igniteui-angular/src/lib/services/excel/jszip-helper.spec.ts
@@ -166,11 +166,13 @@ export class JSZipFiles {
             `xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac">${ sheetData }` +
             `<pageMargins left="0.7" right="0.7" top="0.75" bottom="0.75" header="0.3" footer="0.3"/>${tablePart}</worksheet>`;
         } else {
+            const dimensionsPart = isHGrid ? '' : '<dimension ref="A1"/>';
+
             return `<?xml version="1.0" encoding="UTF-8"?>
             <worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" ` +
             `xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" ` +
             `xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" mc:Ignorable="x14ac" ` +
-            `xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac"><dimension ref="A1"/>` +
+            `xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac">${dimensionsPart}` +
             `<sheetViews><sheetView tabSelected="1" workbookViewId="0"></sheetView></sheetViews><sheetFormatPr defaultRowHeight="15" ` +
             `x14ac:dyDescent="0.25"/><sheetData/><pageMargins left="0.7" right="0.7" top="0.75" bottom="0.75" header="0.3" ` +
             `footer="0.3"/></worksheet>`;
