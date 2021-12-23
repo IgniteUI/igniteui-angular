db09bcd147fd3a507daa3904ccec24a794a7f106
teodosia <teodosiah@gmail.com>
2021-12-23 11:27:05 +0200

    chore(grid): add migration tests

--- projects/igniteui-angular/migrations/update-13_1_0/index.spec.ts
+++ projects/igniteui-angular/migrations/update-13_1_0/index.spec.ts
@@ -28,7 +28,60 @@ describe(`Update to ${version}`, () => {
     });
 
     const migrationName = 'migration-23';
-    const lineBreaksAndSpaceRegex = /\s/g;
 
-   
+    it('should remove columns` movable prop and enable grid`s moving prop if there is movable columns', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.html`,
+            `
+<igx-grid>
+  <igx-column [movable]="true"></igx-column>
+  <igx-column></igx-column>
+</igx-grid>
+`
+        );
+
+        const tree = await schematicRunner
+            .runSchematicAsync(migrationName, {}, appTree)
+            .toPromise();
+
+        expect(
+            tree.readContent('/testSrc/appPrefix/component/test.component.html')
+        ).toEqual(
+            `
+<igx-grid [moving]="true">
+  <igx-column ></igx-column>
+  <igx-column></igx-column>
+</igx-grid>
+`
+        );
+    });
+
+    it('should remove columns` movable prop and don`t set the grid`s moving prop if there isn`t movable columns', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.html`,
+            `
+<igx-grid>
+  <igx-column [movable]="false"></igx-column>
+  <igx-column></igx-column>
+</igx-grid>
+`
+        );
+
+        const tree = await schematicRunner
+            .runSchematicAsync(migrationName, {}, appTree)
+            .toPromise();
+
+        expect(
+            tree.readContent('/testSrc/appPrefix/component/test.component.html')
+        ).toEqual(
+            `
+<igx-grid>
+  <igx-column ></igx-column>
+  <igx-column></igx-column>
+</igx-grid>
+`
+        );
+    });
+
+
 });
