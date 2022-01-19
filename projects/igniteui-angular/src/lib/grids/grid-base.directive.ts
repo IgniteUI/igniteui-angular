74076c2079cbf85a181a547705c726a21146ae05
Stamen Stoychev <SStoychev@infragistics.com>
2022-01-19 13:53:58 +0200

    chore(*): fixing a lint issue

--- projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
@@ -3414,7 +3414,7 @@ export abstract class IgxGridBaseDirective extends DisplayDensityBase implements
      * @hidden @internal
      */
     public dataRebinding(event: IForOfDataChangingEventArgs) {
-        this.dataChanging.emit(event)
+        this.dataChanging.emit(event);
     }
 
     /**
