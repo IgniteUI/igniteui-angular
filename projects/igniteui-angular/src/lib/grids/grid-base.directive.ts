2a106990e983794b0a124cda5f8cec50630e83af
Stamen Stoychev <SStoychev@infragistics.com>
2022-01-19 15:38:35 +0200

    chore(*): fixing api docs for new events

--- projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
@@ -1058,7 +1058,7 @@ export abstract class IgxGridBaseDirective extends DisplayDensityBase implements
      * 
      * @example
      * ```typescript
-     *  <igx-grid #grid [data]="localData" [autoGenerate]="true" (dataPreLoad)='handleDataPreloadEvent()'></igx-grid>
+     *  <igx-grid #grid [data]="localData" [autoGenerate]="true" (dataChanging)='handleDataChangingEvent()'></igx-grid>
      * ```
      */
      @Output()
@@ -1069,7 +1069,7 @@ export abstract class IgxGridBaseDirective extends DisplayDensityBase implements
      * 
      * @example
      * ```typescript
-     *  <igx-grid #grid [data]="localData" [autoGenerate]="true" (dataPreLoad)='handleDataPreloadEvent()'></igx-grid>
+     *  <igx-grid #grid [data]="localData" [autoGenerate]="true" (dataChanged)='handleDataChangedEvent()'></igx-grid>
      * ```
      */
     @Output()
