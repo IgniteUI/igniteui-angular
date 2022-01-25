ac801f69f8d5ddfbff084686a507b409f9cc0708
MKirova <MKirova@DEV-MKIROVA>
2022-01-25 13:22:11 +0200

    chore(*): Adjust scroll position after sizes change.

--- projects/igniteui-angular/src/lib/directives/for-of/for_of.directive.ts
+++ projects/igniteui-angular/src/lib/directives/for-of/for_of.directive.ts
@@ -1413,14 +1413,17 @@ export class IgxForOfDirective<T> implements OnInit, OnChanges, DoCheck, OnDestr
     }
 
 
-    private _adjustScrollPositionAfterSizeChange(sizeDiff) {
+    protected _adjustScrollPositionAfterSizeChange(sizeDiff) {
         // if data has been changed while container is scrolled
         // should update scroll top/left according to change so that same startIndex is in view
         if (Math.abs(sizeDiff) > 0 && this.scrollPosition > 0) {
             this.recalcUpdateSizes();
             const offset = parseInt(this.dc.instance._viewContainer.element.nativeElement.style.top, 10);
             const newSize = this.sizesCache[this.state.startIndex] - offset;
-            this.scrollPosition = newSize === this.scrollPosition ? newSize + 1 : newSize;
+            this.scrollPosition = newSize;
+            if (this.scrollPosition !== newSize) {
+                this.scrollComponent.scrollAmount = newSize;
+            }
         }
     }
 
@@ -1575,8 +1578,11 @@ export class IgxGridForOfDirective<T> extends IgxForOfDirective<T> implements On
                 }
                 this.syncService.setMaster(this);
                 this.igxForContainerSize = args.containerSize;
-                this._updateSizeCache(changes);
+                const sizeDiff = this._updateSizeCache(changes);
                 this._applyChanges();
+                if (sizeDiff) {
+                    this._adjustScrollPositionAfterSizeChange(sizeDiff);
+                }
                 this._updateScrollOffset();
                 this.dataChanged.emit();
             }
@@ -1681,6 +1687,7 @@ export class IgxGridForOfDirective<T> extends IgxForOfDirective<T> implements On
                 }
             });
         }
+        return diff;
     }
 
     protected handleCacheChanges(changes: IterableChanges<T>) {
