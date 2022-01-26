d33f1a98be98fd40b244289c06dd321d9748cee3
Simeon Simeonoff <sim.simeonoff@gmail.com>
2022-01-26 18:53:18 +0200

    refactor(data-selector): add moving and visual improvements

--- projects/igniteui-angular/src/lib/grids/pivot-grid/pivot-data-selector.component.ts
+++ projects/igniteui-angular/src/lib/grids/pivot-grid/pivot-data-selector.component.ts
@@ -1,9 +1,32 @@
 import { Component, HostBinding, Input } from "@angular/core";
-import { IDragBaseEventArgs, IDropDroppedEventArgs, PivotDimensionType } from "igniteui-angular";
+import {
+    AbsoluteScrollStrategy,
+    AutoPositionStrategy,
+    fadeIn,
+    fadeOut,
+    GridColumnDataType,
+    IDragBaseEventArgs,
+    IDragGhostBaseEventArgs,
+    IDropBaseEventArgs,
+    IDropDroppedEventArgs,
+    IgxDropDownComponent,
+    IgxPivotAggregate,
+    IgxPivotDateAggregate,
+    IgxPivotTimeAggregate,
+    IPivotAggregator,
+    IPivotValue,
+    ISelectionEventArgs,
+    OverlaySettings,
+    PivotDimensionType,
+    PositionSettings,
+    VerticalAlignment,
+} from "igniteui-angular";
 import { DisplayDensity } from "../../core/displayDensity";
 import { PivotGridType } from "../common/grid.interface";
 import { IPivotDimension } from "./pivot-grid.interface";
-import { SortingDirection } from '../../data-operations/sorting-strategy';
+import { SortingDirection } from "../../data-operations/sorting-strategy";
+import { useAnimation } from "@angular/animations";
+import { first } from "rxjs/operators";
 
 interface IDataSelectorPanel {
     name: string;
@@ -27,6 +50,38 @@ export class IgxPivotDataSelectorComponent {
     public cssClass = "igx-pivot-data-selector";
     public dimensions: IPivotDimension[];
 
+    private _subMenuPositionSettings: PositionSettings = {
+        verticalStartPoint: VerticalAlignment.Bottom,
+        closeAnimation: undefined,
+    };
+
+    private _subMenuOverlaySettings: OverlaySettings = {
+        closeOnOutsideClick: true,
+        modal: false,
+        positionStrategy: new AutoPositionStrategy(
+            this._subMenuPositionSettings
+        ),
+        scrollStrategy: new AbsoluteScrollStrategy(),
+    };
+    public animationSettings = {
+        closeAnimation: useAnimation(fadeOut, {
+            params: {
+                duration: "0ms",
+            },
+        }),
+        openAnimation: useAnimation(fadeIn, {
+            params: {
+                duration: "0ms",
+            },
+        }),
+    };
+
+    public aggregateList: IPivotAggregator[] = [];
+    public value: IPivotValue;
+    public ghostText: string;
+    public ghostWidth: number;
+    public droppableChannels: string[];
+
     /**
      * @hidden @internal
      */
@@ -38,7 +93,7 @@ export class IgxPivotDataSelectorComponent {
             icon: "filter_list",
             itemKey: "memberName",
             sortable: false,
-            dragChannels: ["Filters", "Columns", "Rows"]
+            dragChannels: ["Filters", "Columns", "Rows"],
         },
         {
             name: "Columns",
@@ -47,7 +102,7 @@ export class IgxPivotDataSelectorComponent {
             icon: "view_column",
             itemKey: "memberName",
             sortable: true,
-            dragChannels: ["Filters", "Columns", "Rows"]
+            dragChannels: ["Filters", "Columns", "Rows"],
         },
         {
             name: "Rows",
@@ -56,15 +111,16 @@ export class IgxPivotDataSelectorComponent {
             icon: "table_rows",
             itemKey: "memberName",
             sortable: true,
-            dragChannels: ["Filters", "Columns", "Rows"]
+            dragChannels: ["Filters", "Columns", "Rows"],
         },
         {
             name: "Values",
+            type: null,
             dataKey: "values",
             icon: "functions",
             itemKey: "member",
             sortable: false,
-            dragChannels: ["Values"]
+            dragChannels: ["Values"],
         },
     ];
 
@@ -90,15 +146,30 @@ export class IgxPivotDataSelectorComponent {
         return this._grid;
     }
 
-    public onItemSort(_: Event, dimension: IPivotDimension, dimensionType: PivotDimensionType) {
-        if(!this._panels.find((panel: IDataSelectorPanel) => panel.type === dimensionType).sortable) return;
+    /**
+     * @hidden
+     * @internal
+     */
+    public onItemSort(
+        _: Event,
+        dimension: IPivotDimension,
+        dimensionType: PivotDimensionType
+    ) {
+        if (
+            !this._panels.find(
+                (panel: IDataSelectorPanel) => panel.type === dimensionType
+            ).sortable
+        )
+            return;
 
         if (!dimension.sortDirection) {
             dimension.sortDirection = SortingDirection.None;
         }
 
-        dimension.sortDirection = dimension.sortDirection + 1 > SortingDirection.Desc ?
-            SortingDirection.None : dimension.sortDirection + 1;
+        dimension.sortDirection =
+            dimension.sortDirection + 1 > SortingDirection.Desc
+                ? SortingDirection.None
+                : dimension.sortDirection + 1;
         // apply same sort direction to children.
         let dim = dimension;
 
@@ -108,12 +179,17 @@ export class IgxPivotDataSelectorComponent {
         }
 
         this.grid.pipeTrigger++;
+        this.grid.cdr.markForCheck();
 
         if (dimensionType === PivotDimensionType.Column) {
             this.grid.setupColumns();
         }
     }
 
+    /**
+     * @hidden
+     * @internal
+     */
     protected getDimensionsByType(dimensionType: PivotDimensionType) {
         switch (dimensionType) {
             case PivotDimensionType.Row:
@@ -136,6 +212,10 @@ export class IgxPivotDataSelectorComponent {
         }
     }
 
+    /**
+     * @hidden
+     * @internal
+     */
     protected getDimensionState(dimensionType: PivotDimensionType) {
         switch (dimensionType) {
             case PivotDimensionType.Row:
@@ -149,45 +229,221 @@ export class IgxPivotDataSelectorComponent {
         }
     }
 
+    /**
+     * @hidden
+     * @internal
+     */
     public onItemDragMove(event: IDragBaseEventArgs) {
-        const clientRect = event.owner.element.nativeElement.getBoundingClientRect();
-        this._dropDelta =  Math.round((event.pageY - event.startY) / clientRect.height);
+        const clientRect =
+            event.owner.element.nativeElement.getBoundingClientRect();
+        this._dropDelta = Math.round(
+            (event.pageY - event.startY) / clientRect.height
+        );
+
+        this.droppableChannels = event.owner.dragChannel as any;
     }
 
-    public onItemDropped(event: IDropDroppedEventArgs, dimensionType: PivotDimensionType) {
+    public onItemDragEnd() {
+       this.droppableChannels = null;
+    }
+
+    /**
+     * @hidden
+     * @internal
+     */
+    public onItemDropped(
+        event: IDropDroppedEventArgs,
+        dimensionType: PivotDimensionType
+    ) {
         const dimension = this.getDimensionsByType(dimensionType);
         const dimensionState = this.getDimensionState(dimensionType);
         const itemId = event.drag.element.nativeElement.id;
-        const dimensionItem = dimension.find(x => x.memberName === itemId);
-        const itemIndex = dimension?.findIndex(x => x?.memberName === itemId) !== -1 ?
-            dimension?.findIndex(x => x.memberName === itemId) : dimension?.length;
+        const dimensionItem = dimension?.find((x) => x.memberName === itemId);
+        const itemIndex =
+            dimension?.findIndex((x) => x?.memberName === itemId) !== -1
+                ? dimension?.findIndex((x) => x.memberName === itemId)
+                : dimension?.length;
         const dimensions = this.grid.pivotConfiguration.rows
             .concat(this.grid.pivotConfiguration.columns)
             .concat(this.grid.pivotConfiguration.filters)
-            .filter(x => x && x.memberName === itemId);
+            .filter((x) => x && x.memberName === itemId);
 
-        const reorder = dimensionState.findIndex(item => item.memberName === itemId) !== -1;;
-        let targetIndex = dimension.length;
+        const reorder =
+            dimensionState?.findIndex((item) => item.memberName === itemId) !==
+            -1;
+        let targetIndex = dimension?.length;
 
-        if(reorder) {
-            targetIndex = itemIndex + this._dropDelta < 0 ? 0 : itemIndex + this._dropDelta;
+        if (!dimension) {
+            const aggregation = this.grid.pivotConfiguration.values;
+            const valueIndex =
+                aggregation.findIndex((x) => x.member === itemId) !== -1
+                    ? aggregation?.findIndex((x) => x.member === itemId)
+                    : aggregation.length;
+            const newValueIndex =
+                valueIndex + this._dropDelta < 0
+                    ? 0
+                    : valueIndex + this._dropDelta;
+
+            const aggregationItem = aggregation.find(
+                (x) => x.member === itemId || x.displayName === itemId
+            );
+
+            if (aggregationItem) {
+                aggregation.splice(valueIndex, 1);
+                aggregation.splice(newValueIndex, 0, aggregationItem);
+                this.grid.setupColumns();
+                this.grid.valuesChange.emit({
+                    values: this.grid.pivotConfiguration.values,
+                });
+            }
         }
 
-        dimensions.forEach(item => {
+        if (reorder) {
+            targetIndex =
+                itemIndex + this._dropDelta < 0
+                    ? 0
+                    : itemIndex + this._dropDelta;
+        }
+
+        dimensions?.forEach((item) => {
             item.enabled = false;
         });
 
         if (dimensionItem) {
             dimensionItem.enabled = true;
             dimension.splice(itemIndex, 1);
-            dimension.splice(targetIndex , 0, dimensionItem);
+            dimension.splice(targetIndex, 0, dimensionItem);
         } else {
             const newDim = Object.assign({}, dimensions[0]);
             newDim.enabled = true;
-            dimension.splice(targetIndex, 0, newDim);
+            dimension?.splice(targetIndex, 0, newDim);
         }
 
         this.grid.pipeTrigger++;
-        this.grid.dimensionsChange.emit({ dimensions: dimension, dimensionCollectionType: dimensionType });
+        this.grid.dimensionsChange.emit({
+            dimensions: dimension,
+            dimensionCollectionType: dimensionType,
+        });
+    }
+
+    protected updateDropDown(
+        value: IPivotValue,
+        dropdown: IgxDropDownComponent
+    ) {
+        this.value = value;
+        dropdown.width = "200px";
+        this.aggregateList = this.getAggregateList(value);
+        dropdown.open(this._subMenuOverlaySettings);
+    }
+
+    /**
+     * @hidden
+     * @internal
+     */
+    public onSummaryClick(
+        event: MouseEvent,
+        value: IPivotValue,
+        dropdown: IgxDropDownComponent
+    ) {
+        this._subMenuOverlaySettings.target =
+            event.currentTarget as HTMLElement;
+
+        if (dropdown.collapsed) {
+            this.updateDropDown(value, dropdown);
+        } else {
+            // close for previous chip
+            dropdown.close();
+            dropdown.closed.pipe(first()).subscribe(() => {
+                this.updateDropDown(value, dropdown);
+            });
+        }
+    }
+
+    /**
+     * @hidden
+     * @internal
+     */
+    protected getAggregatorsForValue(value: IPivotValue): IPivotAggregator[] {
+        const dataType =
+            value.dataType ||
+            this.grid.resolveDataTypes(this.grid.data[0][value.member]);
+        switch (dataType) {
+            case GridColumnDataType.Number:
+            case GridColumnDataType.Currency:
+                return IgxPivotDateAggregate.aggregators();
+            case GridColumnDataType.Date:
+            case GridColumnDataType.DateTime:
+                return IgxPivotDateAggregate.aggregators();
+            case GridColumnDataType.Time:
+                return IgxPivotTimeAggregate.aggregators();
+            default:
+                return IgxPivotAggregate.aggregators();
+        }
+    }
+
+    /**
+     * @hidden
+     * @internal
+     */
+    public getAggregateList(val: IPivotValue): IPivotAggregator[] {
+        if (!val.aggregateList) {
+            let defaultAggr = this.getAggregatorsForValue(val);
+            const isDefault = defaultAggr.find(
+                (x) => x.key === val.aggregate.key
+            );
+            // resolve custom aggregations
+            if (!isDefault && this.grid.data[0][val.member] !== undefined) {
+                // if field exists, then we can apply default aggregations and add the custom one.
+                defaultAggr.unshift(val.aggregate);
+            } else if (!isDefault) {
+                // otherwise this is a custom aggregation that is not compatible
+                // with the defaults, since it operates on field that is not in the data
+                // leave only the custom one.
+                defaultAggr = [val.aggregate];
+            }
+            val.aggregateList = defaultAggr;
+        }
+        return val.aggregateList;
+    }
+
+    /**
+     * @hidden
+     * @internal
+     */
+    public onAggregationChange(event: ISelectionEventArgs) {
+        if (!this.isSelected(event.newSelection.value)) {
+            this.value.aggregate = event.newSelection.value;
+            this.grid.pipeTrigger++;
+            this.grid.cdr.markForCheck();
+        }
+    }
+
+    /**
+     * @hidden
+     * @internal
+     */
+    public isSelected(val: IPivotAggregator) {
+        return this.value.aggregate.key === val.key;
+    }
+
+    /**
+     * @hidden
+     * @internal
+     */
+    public ghostCreated(event: IDragGhostBaseEventArgs, value: string) {
+        const { width: itemWidth } =
+            event.owner.element.nativeElement.getBoundingClientRect();
+        this.ghostWidth = itemWidth;
+        this.ghostText = value;
+    }
+
+    public onEnterPanel(event: IDropBaseEventArgs) {
+        console.log(event.drag.dragChannel, event.owner.dropChannel);
+    }
+
+    public toggleDimension(dimension: IPivotDimension | IPivotValue) {
+        dimension.enabled = !dimension.enabled;
+        this.grid.pipeTrigger++;
+        this.grid.cdr.markForCheck();
     }
 }
