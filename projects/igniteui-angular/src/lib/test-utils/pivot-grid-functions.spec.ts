8dd99c7ad950782bfb17477cf8982bd3267ebf16
MKirova <MKirova@DEV-MKIROVA>
2022-02-10 12:05:38 +0200

    chore(*): Start tests update. Add some common functions for data extraction from the new PivotGridRecord obj.

--- projects/igniteui-angular/src/lib/test-utils/pivot-grid-functions.spec.ts
+++ projects/igniteui-angular/src/lib/test-utils/pivot-grid-functions.spec.ts
@@ -1,4 +1,4 @@
-import { DEFAULT_PIVOT_KEYS, IPivotDimension } from '../grids/pivot-grid/pivot-grid.interface';
+import { DEFAULT_PIVOT_KEYS, IPivotDimension, IPivotGridRecord } from '../grids/pivot-grid/pivot-grid.interface';
 import { PivotUtil } from '../grids/pivot-grid/pivot-util';
 
 export class PivotGridFunctions {
@@ -18,5 +18,20 @@ export class PivotGridFunctions {
 
     public static checkUniqueValuesCount(data: any[], value: string, count:number) {
         expect(data.filter(x => x === data).length).toBe(count);
+    }
+
+    public static getDimensionValues(records: IPivotGridRecord[]) {
+        return records.map(x => this.transformMapToObject(x.dimensionValues));
+    }
+
+    public static getAggregationValues(records: IPivotGridRecord[]) {
+        return records.map(x => this.transformMapToObject(x.aggregationValues));
+    }
+    public static transformMapToObject(map: Map<string, any>) {
+        const obj = {};
+        map.forEach((value, key) => {
+            obj[key] = value;
+        });
+        return obj;
     }
 }
