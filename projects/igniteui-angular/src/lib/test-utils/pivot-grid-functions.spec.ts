2503f9cfefa3b996f39171e30964f0ea56facc11
MKirova <MKirova@DEV-MKIROVA>
2022-02-10 17:26:47 +0200

    chore(*): Update tests.

--- projects/igniteui-angular/src/lib/test-utils/pivot-grid-functions.spec.ts
+++ projects/igniteui-angular/src/lib/test-utils/pivot-grid-functions.spec.ts
@@ -2,19 +2,6 @@ import { DEFAULT_PIVOT_KEYS, IPivotDimension, IPivotGridRecord } from '../grids/
 import { PivotUtil } from '../grids/pivot-grid/pivot-util';
 
 export class PivotGridFunctions {
-    /* Extract the value populated for each record for the given dimension and its children.*/
-    public static getDimensionData(data: any[], dimensions: IPivotDimension[]) {
-       return data.map(x => {
-           const obj = {};
-            for (let dim of dimensions) {
-                const dimData = PivotUtil.getDimensionLevel(dim, x, DEFAULT_PIVOT_KEYS);
-                const field = dimData.dimension.memberName;
-                const value = x[field];
-                obj[field] = value;
-            }
-            return obj;
-       });
-    }
 
     public static checkUniqueValuesCount(data: any[], value: string, count:number) {
         expect(data.filter(x => x === data).length).toBe(count);
