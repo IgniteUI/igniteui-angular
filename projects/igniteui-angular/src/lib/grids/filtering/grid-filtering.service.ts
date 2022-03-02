c6c8ad644cb1cefa75caffdd4fc67a45ab8acc7a
Diyan Dimitrov <ddimitrov@infragistics.com>
2022-03-02 18:18:15 +0200

    fix(filtering): clone filter operands

--- projects/igniteui-angular/src/lib/grids/filtering/grid-filtering.service.ts
+++ projects/igniteui-angular/src/lib/grids/filtering/grid-filtering.service.ts
@@ -558,9 +558,6 @@ export class IgxFilteringService implements OnDestroy {
         }
 
         const fieldFilterIndex = filteringTree.findIndex(fieldName);
-        // if (fieldFilterIndex > -1) {
-        //     filteringTree.filteringOperands.splice(fieldFilterIndex, 1);
-        // }
         this.prepare_filtering_expression(filteringTree, fieldName, term, conditionOrExpressionsTree, ignoreCase, fieldFilterIndex);
         grid.filteringExpressionsTree = filteringTree;
     }
@@ -583,8 +580,12 @@ export class IgxFilteringService implements OnDestroy {
         const condition = conditionOrExpressionsTree instanceof FilteringExpressionsTree ?
             null : conditionOrExpressionsTree as IFilteringOperation;
 
-        const newExpressionsTree: FilteringExpressionsTree = createNewTree ?
-            new FilteringExpressionsTree(filteringState.operator, filteringState.fieldName) : filteringState as FilteringExpressionsTree;
+        let newExpressionsTree = filteringState as FilteringExpressionsTree;
+
+        if (createNewTree) {
+            newExpressionsTree = new FilteringExpressionsTree(filteringState.operator, filteringState.fieldName)
+            newExpressionsTree.filteringOperands = [...filteringState.filteringOperands];
+        }
 
         if (condition) {
             const newExpression: IFilteringExpression = { fieldName, searchVal, condition, ignoreCase };
@@ -592,7 +593,6 @@ export class IgxFilteringService implements OnDestroy {
             expressionsTree.filteringOperands.push(newExpression);
         }
 
-        // no expressions tree found for this field
         if (expressionsTree) {
             if (insertAtIndex > -1) {
                 newExpressionsTree.filteringOperands[insertAtIndex] = expressionsTree;
