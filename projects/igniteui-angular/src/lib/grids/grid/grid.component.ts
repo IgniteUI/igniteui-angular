f394917dd9a4ee2223e63d6b7d2f4444c6f18245
MKirova <MKirova@DEV-MKIROVA>
2022-03-31 18:58:29 +0300

    fix(igxGrid): Fix virtualization index for remote virtualization scenario.

--- projects/igniteui-angular/src/lib/grids/grid/grid.component.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid.component.ts
@@ -1162,13 +1162,11 @@ export class IgxGridComponent extends IgxGridBaseDirective implements GridType, 
         let row: RowType;
         let rec: any;
 
-        if (index < 0 || index >= this.dataView.length) {
-            if (this.pagingMode === 1 && this.paginator.page !== 0) {
-                rec = data ?? this.dataView[index - this.paginator.perPage * this.paginator.page];
-            } else if (index >= this.dataView.length) {
-                const virtIndex = index - this.gridAPI.grid.virtualizationState.startIndex;
-                rec = data ?? this.dataView[virtIndex];
-            }
+        if (this.pagingMode === 1 && this.paginator.page !== 0) {
+            rec = data ?? this.dataView[index - this.paginator.perPage * this.paginator.page];
+        } else if (this.gridAPI.grid.verticalScrollContainer.isRemote) {
+            const virtIndex = index - this.gridAPI.grid.virtualizationState.startIndex;
+            rec = data ?? this.dataView[virtIndex];
         } else {
             rec = data ?? this.dataView[index];
         }
