ec239b45f97ed705d908df0f5e02cc9fe3af0553
MKirova <MKirova@DEV-MKIROVA>
2022-04-04 11:55:13 +0300

    chore(*): Move common code to base grid and re-use across all grid components.

--- projects/igniteui-angular/src/lib/grids/grid/grid.component.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid.component.ts
@@ -1162,14 +1162,8 @@ export class IgxGridComponent extends IgxGridBaseDirective implements GridType, 
         let row: RowType;
         let rec: any;
 
-        if ((index < 0 || index >= this.dataView.length) && this.pagingMode === 1 && this.paginator.page !== 0) {
-            rec = data ?? this.dataView[index - this.paginator.perPage * this.paginator.page];
-        } else if (this.gridAPI.grid.verticalScrollContainer.isRemote) {
-            const virtIndex = index - this.gridAPI.grid.virtualizationState.startIndex;
-            rec = data ?? this.dataView[virtIndex];
-        } else {
-            rec = data ?? this.dataView[index];
-        }
+        const dataIndex = this._getResolvedDataIndex(index);
+        rec = data ?? this.dataView[dataIndex];
 
         if (rec && this.isGroupByRecord(rec)) {
             row = new IgxGroupByRow(this as any, index, rec);
