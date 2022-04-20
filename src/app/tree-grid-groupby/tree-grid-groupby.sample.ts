ce179ce13b4f6493074577621aaaa8ea697c4c67
Hristo <hanastasov@infragistics.com>
2022-04-20 10:02:20 +0300

    Update src/app/tree-grid-groupby/tree-grid-groupby.sample.ts

--- src/app/tree-grid-groupby/tree-grid-groupby.sample.ts
+++ src/app/tree-grid-groupby/tree-grid-groupby.sample.ts
@@ -57,7 +57,6 @@ export class TreeGridGroupBySampleComponent implements OnInit {
     }
 
     public print() {
-        debugger;
         console.log('Sorting: ', this.grid1.sortingExpressions);
         console.log('Grouping: ', this.grid1.treeGroupArea, this.groupingExpressions);
     }
