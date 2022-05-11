b255872f67a222683c032387681d58e7b0b9fb29
VDyulgerov <volen1999@gmail.com>
2022-05-11 16:40:50 +0300

    fix(grid): check fix(grid): check if advancedFilteringExpressionsTree is empty

--- projects/igniteui-angular/src/lib/grids/grid/grid-filtering-advanced.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid-filtering-advanced.spec.ts
@@ -2821,6 +2821,24 @@ describe('IgxGrid - Advanced Filtering #grid - ', () => {
             expect(grid.filteredData).toBe(null);
             expect(grid.rowList.length).toBe(8);
         }));
+
+        it('should correctly set filteredData if advancedFilteringExpressionsTree is empty', fakeAsync(() => {
+            // Verify filtering expressions tree binding state
+            expect(grid.advancedFilteringExpressionsTree).toBe(fix.componentInstance.filterTree);
+
+            const tree = new FilteringExpressionsTree(FilteringLogic.And);
+            tree.filteringOperands = [];
+
+            // Clear filter
+            grid.advancedFilteringExpressionsTree = tree;
+            fix.detectChanges();
+
+            // Verify filtering expressions tree binding state
+            expect(grid.advancedFilteringExpressionsTree).toBe(fix.componentInstance.filterTree);
+
+            // Verify no filtered data
+            expect(grid.filteredData).toBe(null);
+        }));
     });
 });
 
