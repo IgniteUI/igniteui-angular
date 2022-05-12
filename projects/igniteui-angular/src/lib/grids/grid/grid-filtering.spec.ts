4c7b6601ea73cc62eda39fa6a9250eefadce36fe
VDyulgerov <volen1999@gmail.com>
2022-05-12 10:08:50 +0300

    fix(grid): add missing semicolon

--- projects/igniteui-angular/src/lib/grids/grid/grid-filtering.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid-filtering.spec.ts
@@ -1208,7 +1208,7 @@ describe('IgxGrid - Filtering expression tree bindings #grid', () => {
 
         // Verify no filtered data
         expect(grid.filteredData).toBe(null);
-    })
+    });
 });
 
 const verifyExpressionUI = (expressionUI: ExpressionUI, expression: IFilteringExpression,
