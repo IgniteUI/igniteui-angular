da9071700f8bca97f7c73029d1607a5d8ca08b11
Martin Dragnev <mddragnev@gmail.com>
2022-05-13 17:32:22 +0300

    chore(*): reflow when scrollbar has changed visibility due to new size

--- projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
@@ -3547,7 +3547,7 @@ export abstract class IgxGridBaseDirective extends DisplayDensityBase implements
         this.verticalScrollContainer.scrollbarVisibilityChanged.pipe(destructor, filter(() => !this._init)).subscribe(() => {
             // called to recalc all widths that may have changes as a result of
             // the vert. scrollbar showing/hiding
-            this.notifyChanges(true);
+            this.reflow();
         });
 
         this.verticalScrollContainer.contentSizeChange.pipe(destructor, filter(() => !this._init)).subscribe(() => {
