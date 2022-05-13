935f39ce5540bc378cba8acdc4b8caffeec490b8
MKirova <MKirova@DEV-MKIROVA>
2022-05-13 15:45:01 +0300

    chore(*): Update another test timing.

--- projects/igniteui-angular/src/lib/grids/hierarchical-grid/hierarchical-grid.virtualization.spec.ts
+++ projects/igniteui-angular/src/lib/grids/hierarchical-grid/hierarchical-grid.virtualization.spec.ts
@@ -74,15 +74,18 @@ describe('IgxHierarchicalGrid Virtualization #hGrid', () => {
         elem.scrollTop = 400;
         fixture.detectChanges();
         fixture.componentRef.hostView.detectChanges();
-        await wait();
+        await wait(30);
+        fixture.detectChanges();
 
         // collapse and expand the row
         (firstRow.nativeElement.children[0] as HTMLElement).click();
         fixture.detectChanges();
-        await wait();
+        await wait(30);
+        fixture.detectChanges();
         (firstRow.nativeElement.children[0] as HTMLElement).click();
         fixture.detectChanges();
-        await wait();
+        await wait(30);
+        fixture.detectChanges();
 
         expect(elem.scrollTop).toBe(400);
         /** row toggle rAF */
