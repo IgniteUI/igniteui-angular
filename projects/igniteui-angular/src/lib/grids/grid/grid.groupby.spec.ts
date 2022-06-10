6dc223f7d3e9e9d0a840935b02bd5e5e9184a772
MKirova <MKirova@DEV-MKIROVA>
2022-06-10 18:59:05 +0300

    chore(*): Fix a few more tests.

--- projects/igniteui-angular/src/lib/grids/grid/grid.groupby.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid.groupby.spec.ts
@@ -1014,7 +1014,7 @@ describe('IgxGrid - GroupBy #grid', () => {
         grid.groupBy({ fieldName: 'Released', dir: SortingDirection.Desc, ignoreCase: false });
         fix.detectChanges();
 
-        grid.columnList.get(0).width = '500px';
+        grid.columns[0].width = '500px';
         fix.detectChanges();
         const groupRows = grid.groupsRowList.toArray();
         groupRows[0].toggle();
@@ -2905,7 +2905,7 @@ describe('IgxGrid - GroupBy #grid', () => {
         const fix = TestBed.createComponent(DefaultGridComponent);
         const grid = fix.componentInstance.instance;
         fix.detectChanges();
-        grid.columnList.get(0).header = 'Custom Header Text';
+        grid.columns[0].header = 'Custom Header Text';
         tick();
         fix.detectChanges();
 
