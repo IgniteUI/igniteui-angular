0a38ac8e7fafebbcbb7c33548540b4398ddd58b7
RivaIvanova <ivanova.riva@gmail.com>
2022-06-20 09:39:49 +0300

    test(combos): refactor valueKey as display text test

--- projects/igniteui-angular/src/lib/simple-combo/simple-combo.component.spec.ts
+++ projects/igniteui-angular/src/lib/simple-combo/simple-combo.component.spec.ts
@@ -1136,22 +1136,52 @@ describe('IgxSimpleCombo', () => {
             combo.valueKey = 'key';
             combo.displayKey = 'value';
             combo.data = [
-                { key: 0, value: 0 },
-                { key: 'truthy value', value: false },
-                { key: null, value: null },
-                { key: undefined, value: undefined },
+                { key: 'item1', value: false },
+                { key: 'item2', value: null },
+                { key: 'item3', value: '' },
+                { key: 'item4', value: NaN },
+                { key: 'item5', value: undefined },
             ];
+
             combo.open();
             fixture.detectChanges();
-
-            const item = fixture.debugElement.queryAll(By.css(`.${CSS_CLASS_DROPDOWNLISTITEM}`))[1];
-            const displayText = item.nativeElement.textContent.trim();
-            expect(displayText).toEqual('truthy value');
-
-            item.triggerEventHandler('click', UIInteractions.getMouseEvent('click'));
+            const item1 = fixture.debugElement.queryAll(By.css(`.${CSS_CLASS_DROPDOWNLISTITEM}`))[0];
+            expect(item1.nativeElement.textContent.trim()).toEqual('item1');
+            item1.triggerEventHandler('click', UIInteractions.getMouseEvent('click'));
             fixture.detectChanges();
+            expect(combo.value).toBe('item1');
 
-            expect(combo.value).toBe('truthy value');
+            combo.open();
+            fixture.detectChanges();
+            const item2 = fixture.debugElement.queryAll(By.css(`.${CSS_CLASS_DROPDOWNLISTITEM}`))[1];
+            expect(item2.nativeElement.textContent.trim()).toEqual('item2');
+            item2.triggerEventHandler('click', UIInteractions.getMouseEvent('click'));
+            fixture.detectChanges();
+            expect(combo.value).toBe('item2');
+
+            combo.open();
+            fixture.detectChanges();
+            const item3 = fixture.debugElement.queryAll(By.css(`.${CSS_CLASS_DROPDOWNLISTITEM}`))[2];
+            expect(item3.nativeElement.textContent.trim()).toEqual('item3');
+            item3.triggerEventHandler('click', UIInteractions.getMouseEvent('click'));
+            fixture.detectChanges();
+            expect(combo.value).toBe('item3');
+
+            combo.open();
+            fixture.detectChanges();
+            const item4 = fixture.debugElement.queryAll(By.css(`.${CSS_CLASS_DROPDOWNLISTITEM}`))[3];
+            expect(item4.nativeElement.textContent.trim()).toEqual('item4');
+            item4.triggerEventHandler('click', UIInteractions.getMouseEvent('click'));
+            fixture.detectChanges();
+            expect(combo.value).toBe('item4');
+
+            combo.open();
+            fixture.detectChanges();
+            const item5 = fixture.debugElement.queryAll(By.css(`.${CSS_CLASS_DROPDOWNLISTITEM}`))[4];
+            expect(item5.nativeElement.textContent.trim()).toEqual('item5');
+            item5.triggerEventHandler('click', UIInteractions.getMouseEvent('click'));
+            fixture.detectChanges();
+            expect(combo.value).toBe('item5');
         });
     });
 
