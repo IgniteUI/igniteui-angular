cfcd9baeb6b638327831bf1b234733875552023d
MKirova <MKirova@DEV-MKIROVA>
2022-06-27 17:47:55 +0300

    fix(igxPivot): Fix issue with dimensionsSortingExpressionsChange not emitting column expressions. Expose a dimensionsSortingExpressions getter.

--- projects/igniteui-angular/src/lib/grids/pivot-grid/pivot-grid.spec.ts
+++ projects/igniteui-angular/src/lib/grids/pivot-grid/pivot-grid.spec.ts
@@ -1093,7 +1093,7 @@ describe('IgxPivotGrid #pivotGrid', () => {
                 const pivotGrid = fixture.componentInstance.pivotGrid;
                 const headerRow = fixture.nativeElement.querySelector('igx-pivot-header-row');
                 const colChip = headerRow.querySelector('igx-chip[id="Country"]');
-
+                spyOn(pivotGrid.dimensionsSortingExpressionsChange, 'emit');
                 // sort
                 colChip.click();
                 fixture.detectChanges();
@@ -1109,6 +1109,13 @@ describe('IgxPivotGrid #pivotGrid', () => {
                 colHeaders = pivotGrid.columns.filter(x => x.level === 0).map(x => x.header);
                 expected = ['Uruguay', 'USA', 'Bulgaria'];
                 expect(colHeaders).toEqual(expected);
+                const expectedExpressions: ISortingExpression[] = [
+                    { dir: SortingDirection.None, fieldName: 'All', strategy: DefaultPivotSortingStrategy.instance()}, 
+                    { dir: SortingDirection.None, fieldName: 'ProductCategory', strategy: DefaultPivotSortingStrategy.instance()},
+                    { dir: SortingDirection.Desc, fieldName: 'Country', strategy: DefaultPivotSortingStrategy.instance() }
+                ];
+                expect(pivotGrid.dimensionsSortingExpressionsChange.emit).toHaveBeenCalledWith(expectedExpressions);
+                expect(pivotGrid.dimensionsSortingExpressions).toEqual(expectedExpressions);
             });
 
             it('should sort on column for single row dimension.', () => {
