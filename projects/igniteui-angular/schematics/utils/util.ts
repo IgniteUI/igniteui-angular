5049a6b712c0dcbe7879d796ececc49205e7ee32
lipata <nikolay.alipiev@gmail.com>
2022-06-27 16:31:58 +0300

    chore(schematics): add one more default project check

--- projects/igniteui-angular/schematics/utils/util.ts
+++ projects/igniteui-angular/schematics/utils/util.ts
@@ -25,6 +25,6 @@ export const createHost = (tree: Tree): workspaces.WorkspaceHost => ({
 
 export const getDefaultProject = async (tree: Tree) => {
     const { workspace } = await workspaces.readWorkspace(tree.root.path, createHost(tree));
-    const defaultProject = workspace.projects.get(workspace.extensions['defaultProject'] as string);
+    const defaultProject = workspace.projects.get(workspace.extensions['defaultProject'] as string) || workspace.projects.values().next().value as workspaces.ProjectDefinition;
     return defaultProject;
 };
