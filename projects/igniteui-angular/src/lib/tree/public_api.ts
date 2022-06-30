054fe507c07c28ba5ab466db5e54c37114572692
Dobromir Tsvetkov <dobromir.ts@gmail.com>
2022-06-30 13:28:17 +0300

    feat(tree): Add tree compatability for appbuilder -13.2.x

--- projects/igniteui-angular/src/lib/tree/public_api.ts
+++ projects/igniteui-angular/src/lib/tree/public_api.ts
@@ -1,5 +1,3 @@
 export * from './tree.component';
 export * from './tree-node/tree-node.component';
-export { IgxTreeSearchResolver, ITreeNodeSelectionEvent, ITreeNodeEditingEvent,
-    ITreeNodeEditedEvent, ITreeNodeTogglingEventArgs, ITreeNodeToggledEventArgs,
-    IgxTreeSelectionType, IgxTree, IgxTreeNode } from './common';
+export * from './common';
