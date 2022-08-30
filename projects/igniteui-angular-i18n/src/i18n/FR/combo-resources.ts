afe1bc678a1070eb8ff5953359a439e29c710b1d
Stamen Stoychev <SStoychev@infragistics.com>
2022-08-30 14:27:04 +0300

    fix(combo): adding localization for igxcombo

---
+++ projects/igniteui-angular-i18n/src/i18n/FR/combo-resources.ts
@@ -0,0 +1,11 @@
+import { IComboResourceStrings } from 'igniteui-angular';
+
+// exported below as re-cast to create declaration type with expanded properties
+const ComboResourceStringsFR_: ExpandRequire<IComboResourceStrings> = {
+    igx_combo_empty_message: 'The list is empty'
+};
+
+/**
+ * French resource strings for IgxCombo
+ */
+export const ComboResourceStringsFR = ComboResourceStringsFR_ as ExpandRequire<IComboResourceStrings>;
