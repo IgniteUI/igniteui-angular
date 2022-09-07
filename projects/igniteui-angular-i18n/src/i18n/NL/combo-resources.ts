f5f3bd494d85a1e3486b6dfbd64e76de23623aa2
Rumyana Andriova <54146583+randriova@users.noreply.github.com>
2022-09-07 14:09:35 +0300

    fix(i18n): Updating Combo localization - 13.2.x

--- projects/igniteui-angular-i18n/src/i18n/NL/combo-resources.ts
+++ projects/igniteui-angular-i18n/src/i18n/NL/combo-resources.ts
@@ -2,7 +2,7 @@ import { IComboResourceStrings } from 'igniteui-angular';
 
 // exported below as re-cast to create declaration type with expanded properties
 const ComboResourceStringsNL_: ExpandRequire<IComboResourceStrings> = {
-    igx_combo_empty_message: 'The list is empty'
+    igx_combo_empty_message: 'De lijst is leeg'
 };
 
 /**
--- projects/igniteui-angular-i18n/src/i18n/ZH-HANT/combo-resources.ts
+++ projects/igniteui-angular-i18n/src/i18n/ZH-HANT/combo-resources.ts
@@ -2,7 +2,7 @@ import { IComboResourceStrings } from 'igniteui-angular';
 
 // exported below as re-cast to create declaration type with expanded properties
 const ComboResourceStringsZHHANT_: ExpandRequire<IComboResourceStrings> = {
-    igx_combo_empty_message: 'The list is empty'
+    igx_combo_empty_message: '清單是空的'
 };
 
 /**
