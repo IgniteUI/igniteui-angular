046620e7ba334d3fac9c0a47b34889445a0d9e96
Martin Dragnev <mddragnev@gmail.com>
2022-09-16 17:15:42 +0300

    Add template context guards for ES Loading, MDV and GroupBy row

--- projects/igniteui-angular/src/lib/grids/filtering/excel-style/excel-style-search.component.ts
+++ projects/igniteui-angular/src/lib/grids/filtering/excel-style/excel-style-search.component.ts
@@ -31,7 +31,11 @@ import { IgxTreeComponent, ITreeNodeSelectionEvent } from '../../../tree/public_
     selector: '[igxExcelStyleLoading]'
 })
 export class IgxExcelStyleLoadingValuesTemplateDirective {
-    constructor(public template: TemplateRef<any>) { }
+    public static ngTemplateContextGuard(dir: IgxExcelStyleLoadingValuesTemplateDirective,
+        ctx: unknown): ctx is undefined {
+        return true
+    };
+    constructor(public template: TemplateRef<undefined>) { }
 }
 
 /**
@@ -71,14 +75,14 @@ export class IgxExcelStyleSearchComponent implements AfterViewInit, OnDestroy {
     /**
      * @hidden @internal
      */
-     @ViewChild('addToCurrentFilterCheckbox', { read: IgxCheckboxComponent, static: false })
-     public addToCurrentFilterCheckbox: IgxCheckboxComponent;
+    @ViewChild('addToCurrentFilterCheckbox', { read: IgxCheckboxComponent, static: false })
+    public addToCurrentFilterCheckbox: IgxCheckboxComponent;
 
     /**
      * @hidden @internal
      */
-     @ViewChild('tree', { read: IgxTreeComponent, static: false })
-     public tree: IgxTreeComponent;
+    @ViewChild('tree', { read: IgxTreeComponent, static: false })
+    public tree: IgxTreeComponent;
 
     /**
      * @hidden @internal
@@ -295,7 +299,7 @@ export class IgxExcelStyleSearchComponent implements AfterViewInit, OnDestroy {
     /**
      * @hidden @internal
      */
-     public onNodeSelectionChange(eventArgs: ITreeNodeSelectionEvent) {
+    public onNodeSelectionChange(eventArgs: ITreeNodeSelectionEvent) {
         eventArgs.added.forEach(node => {
             (node.data as FilterListItem).isSelected = true;
         });
@@ -349,7 +353,7 @@ export class IgxExcelStyleSearchComponent implements AfterViewInit, OnDestroy {
      */
     public get applyButtonDisabled(): boolean {
         return (this._selectAllItem && !this._selectAllItem.isSelected && !this._selectAllItem.indeterminate) ||
-                (this.displayedListData && this.displayedListData.length === 0);
+            (this.displayedListData && this.displayedListData.length === 0);
     }
 
     /**
@@ -555,7 +559,7 @@ export class IgxExcelStyleSearchComponent implements AfterViewInit, OnDestroy {
     /**
      * @hidden @internal
      */
-     public isTreeEmpty() {
+    public isTreeEmpty() {
         return this.esf.isHierarchical && this.displayedListData.length === 0;
     }
 
