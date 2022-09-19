994e69b884d5ffe1dbaf8068e6591bce61b8c4fb
RivaIvanova <ivanova.riva@gmail.com>
2022-09-19 10:39:14 +0300

    fix(simple-combo): select falsy values with "writeValue" method

--- projects/igniteui-angular/src/lib/simple-combo/simple-combo.component.ts
+++ projects/igniteui-angular/src/lib/simple-combo/simple-combo.component.ts
@@ -176,7 +176,9 @@ export class IgxSimpleComboComponent extends IgxComboBaseDirective implements Co
     /** @hidden @internal */
     public writeValue(value: any): void {
         const oldSelection = this.selection;
-        this.selectionService.select_items(this.id, value ? [value] : [], true);
+        // the second check is used when value is undefined, i.e., item with valueKey=undefined is selected
+        let hasSelection = value !== undefined || this.valueKey && this.data.find(x => x[this.valueKey] === undefined);
+        this.selectionService.select_items(this.id, hasSelection ? [value] : [], true);
         this.cdr.markForCheck();
         this._value = this.createDisplayText(this.selection, oldSelection);
     }
