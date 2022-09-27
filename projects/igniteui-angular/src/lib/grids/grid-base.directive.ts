0c09e9f6258c5bd4427d95edf2ce58633fd1a10e
MKirova <MKirova@DEV-MKIROVA>
2022-09-27 17:22:55 +0300

    chore(*): Fix issue with forof re-evaling sizes before widths are calculated on columns.

--- projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
@@ -3507,10 +3507,10 @@ export abstract class IgxGridBaseDirective extends DisplayDensityBase implements
         if (recalcFeatureWidth) {
             this._headerFeaturesWidth = NaN;
         }
-        this.resetForOfCache();
         this.resetColumnsCaches();
         this.resetColumnCollections();
         this.resetCachedWidths();
+        this.resetForOfCache();
         this.hasVisibleColumns = undefined;
         this._columnGroups = this._columns.some(col => col.columnGroup);
     }
