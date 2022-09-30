4c0d80e806ef095b41b616cd67e69ae1de977727
Simeon Simeonoff <sim.simeonoff@gmail.com>
2022-09-30 10:07:10 +0300

    chore(migrations): change target version

--- projects/igniteui-angular/migrations/update-15_0_0/index.spec.ts
+++ projects/igniteui-angular/migrations/update-15_0_0/index.spec.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import { EmptyTree } from '@angular-devkit/schematics';
 import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
 
-const version = '14.2.0';
+const version = '15.0.0';
 
 describe(`Update to ${version}`, () => {
     let appTree: UnitTestTree;
--- projects/igniteui-angular/migrations/update-15_0_0/index.ts
+++ projects/igniteui-angular/migrations/update-15_0_0/index.ts
@@ -5,7 +5,7 @@ import {
 } from '@angular-devkit/schematics';
 import { UpdateChanges } from '../common/UpdateChanges';
 
-const version = '14.2.0';
+const version = '15.0.0';
 
 export default (): Rule => async (host: Tree, context: SchematicContext) => {
     context.logger.info(`Applying migration for Ignite UI for Angular to version ${version}`);
