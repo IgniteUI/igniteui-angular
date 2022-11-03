22502186f75e483cd67d3acaa8d8f46c7e0c84ac
sivanova <sivanova@infragistics.com>
2022-11-03 14:53:29 +0200

    fix(radio-group): failing migrations

--- projects/igniteui-angular/migrations/update-15_0_0/index.spec.ts
+++ projects/igniteui-angular/migrations/update-15_0_0/index.spec.ts
@@ -150,35 +150,25 @@ describe(`Update to ${version}`, () => {
 
     it('should remove the disabled property from the igx-radio-group', async () => {
         appTree.create(
-            `/testSrc/appPrefix/component/test.component.scss`,
-`<igx-radio-group disabled></igx-radio-group>`
-        );
-
-        const tree = await schematicRunner
-            .runSchematicAsync(migrationName, {}, appTree)
+            '/testSrc/appPrefix/component/test.component.html', `
+            <igx-radio-group disabled="true"></igx-radio-group>`);
+        const tree = await schematicRunner.runSchematicAsync(migrationName, {}, appTree)
             .toPromise();
 
-        expect(
-            tree.readContent('/testSrc/appPrefix/component/test.component.scss')
-        ).toEqual(
-`<igx-radio-group></igx-radio-group>`
-        );
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.html'))
+            .toEqual(`
+            <igx-radio-group></igx-radio-group>`);
     });
 
     it('should remove the labelPosition property from the igx-radio-group', async () => {
         appTree.create(
-            `/testSrc/appPrefix/component/test.component.scss`,
-`<igx-radio-group labelPosition="before"></igx-radio-group>`
-        );
-
-        const tree = await schematicRunner
-            .runSchematicAsync(migrationName, {}, appTree)
+            '/testSrc/appPrefix/component/test.component.html', `
+            <igx-radio-group labelPosition="before"></igx-radio-group>`);
+        const tree = await schematicRunner.runSchematicAsync(migrationName, {}, appTree)
             .toPromise();
 
-        expect(
-            tree.readContent('/testSrc/appPrefix/component/test.component.scss')
-        ).toEqual(
-`<igx-radio-group></igx-radio-group>`
-        );
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.html'))
+            .toEqual(`
+            <igx-radio-group></igx-radio-group>`);
     });
 });
