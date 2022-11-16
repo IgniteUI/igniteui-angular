06bef52482481263d8d7348f6dc51dcab8d0ef24
lipata <nikolay.alipiev@gmail.com>
2022-11-16 15:20:40 +0200

    chore(migrations): add root which is required in the new angular.json schema

--- projects/igniteui-angular/migrations/update-6_01/index.spec.ts
+++ projects/igniteui-angular/migrations/update-6_01/index.spec.ts
@@ -10,6 +10,7 @@ describe('Update 6.0.1', () => {
         defaultProject: 'testProj',
         projects: {
             testProj: {
+                root: '/',
                 sourceRoot: '/testSrc'
             }
         },
