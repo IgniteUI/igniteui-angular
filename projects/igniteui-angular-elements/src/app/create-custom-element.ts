df5aac29ee9eb29fee9eddc251ce80e7a7015c26
Damyan Petev <damyanpetev@users.noreply.github.com>
2022-12-07 19:56:17 +0200

    fix(elements): adjust attribute(template) names of props #12437

--- projects/igniteui-angular-elements/src/app/create-custom-element.ts
+++ projects/igniteui-angular-elements/src/app/create-custom-element.ts
@@ -7,6 +7,9 @@ export type IgxNgElementConfig = Omit<NgElementConfig, 'strategyFactory'> & { re
 
 export function createIgxCustomElement<T>(component: Type<any>, config: IgxNgElementConfig): NgElementConstructor<T> {
     const strategyFactory = new IgxCustomNgElementStrategyFactory(component, config.injector, config.registerConfig);
+
+    guardAttributeNames<T>(strategyFactory);
+
     const elementCtor = createCustomElement<T>(component, {
         ...config,
         strategyFactory
@@ -37,4 +40,36 @@ export function createIgxCustomElement<T>(component: Type<any>, config: IgxNgEle
     // });
 
     return elementCtor;
+}
+
+/**
+ * Prevent direct dash case conversion from mishandling some property name cases
+ * (e.g. `showPivotConfigurationUI`-> `show-pivot-configuration-u-i`)
+ *
+ * @remarks
+ * TODO(D.P.): ATM `createCustomElement` maps attribute to props with simple dasherize
+ * and both the util and produced map are all context-locked and can't be extended. Also
+ * `componentFactory.inputs` is a logical getter and thus can't be modified directly.
+ *
+ * @param strategyFactory IgxCustomNgElementStrategyFactory with resolved componentFactory
+ */
+function guardAttributeNames<T>(strategyFactory: IgxCustomNgElementStrategyFactory) {
+    // const inputs = strategyFactory.componentFactory.inputs; // technically readonly
+
+    // getComponentDef not public, also technically readonly map
+    // the key is the non-minified (template) name
+    const inputs: {[P in keyof T]: string} = (strategyFactory.componentFactory as any).componentDef.inputs;
+
+    for (const key in inputs) {
+        const input = inputs[key];
+        // detect consecutive capital letters in `templateName` and lower a portion of them
+        if (/[A-Z]{2,}/.test(key)) {
+            // showPivotConfigurationUI -> showPivotConfigurationUi -> show-pivot-configuration-ui
+            // evenRowCSS -> evenRowCss -> even-row-css
+            // DOMRect -> DomRect -> dom-rect
+            const newKey = key.replace(/(?<=[A-Z])[A-Z]+(?![a-z])/g, char => char.toLowerCase());
+            inputs[newKey] = input;
+            // TODO: consider deleting the original key
+        }
+    }
 }
