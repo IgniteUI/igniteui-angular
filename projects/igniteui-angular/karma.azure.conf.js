691b40d94d5b76fb5f457ccbdef46512365fc29e
Konstantin Dinev <kdinev@mail.bw.edu>
2022-12-14 18:33:54 +0200

    chore(*): attempting non-parallel run

--- projects/igniteui-angular/karma.azure.conf.js
+++ projects/igniteui-angular/karma.azure.conf.js
@@ -12,13 +12,13 @@ module.exports = function (config) {
       { pattern: '../../dist/igniteui-angular/styles/igniteui-angular.css', watched: false }
     ],
     plugins: [
-        'karma-parallel',
-        'karma-jasmine',
-        'karma-coverage',
-        'karma-chrome-launcher',
-        'karma-spec-reporter',
-        'karma-junit-reporter',
-        '@angular-devkit/build-angular/plugins/karma'
+      'karma-parallel',
+      'karma-jasmine',
+      'karma-coverage',
+      'karma-chrome-launcher',
+      'karma-spec-reporter',
+      'karma-junit-reporter',
+      '@angular-devkit/build-angular/plugins/karma'
     ],
     parallelOptions: {
       executors: 2,
