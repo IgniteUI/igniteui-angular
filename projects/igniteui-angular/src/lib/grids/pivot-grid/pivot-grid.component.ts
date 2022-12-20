2978616ddc2e5cc3ea9c99b9f1a6c2a3a66efadb
Svetoslav Krastev <SKrastev@infragistics.com>
2022-12-20 19:00:46 +0200

    fix(pivot-grid): Defer assignng width to pivot elements after columns have been generated.

--- projects/igniteui-angular/src/lib/grids/pivot-grid/pivot-grid.component.ts
+++ projects/igniteui-angular/src/lib/grids/pivot-grid/pivot-grid.component.ts
@@ -1136,6 +1136,10 @@ export class IgxPivotGridComponent extends IgxGridBaseDirective implements OnIni
      * @hidden @internal
      */
     public rowDimensionWidthToPixels(dim: IPivotDimension): number {
+        if (this.shouldGenerate) {
+            return 0;
+        }
+
         if (!dim.width) {
             return MINIMUM_COLUMN_WIDTH;
         }
@@ -1159,6 +1163,14 @@ export class IgxPivotGridComponent extends IgxGridBaseDirective implements OnIni
             this.rowDimensions.map((dim) => this.rowDimensionWidthToPixels(dim)).reduce((prev, cur) => prev + cur) :
             0;
         return this.calcWidth - totalDimWidth;
+    }
+
+    public get pivotPinnedWidth() {
+        return !this.shouldGenerate ? (this.isPinningToStart ? this.pinnedWidth : this.headerFeaturesWidth) : 0;
+    }
+
+    public get pivotUnpinnedWidth() {
+        return !this.shouldGenerate ? this.unpinnedWidth : 0;
     }
 
     public get rowDimensions() {
