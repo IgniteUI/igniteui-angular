d272cd0f237d764772423bd8992e065c3591af81
lipata <nikolay.alipiev@gmail.com>
2023-01-12 16:07:28 +0200

    fix(migrations): do not migrate for libs & migrate for server #12508

--- projects/igniteui-angular/migrations/update-15_0_4/index.spec.ts
+++ projects/igniteui-angular/migrations/update-15_0_4/index.spec.ts
@@ -24,18 +24,43 @@ describe(`Update to ${version}`, () => {
         },
         version: 1
     };
+    const configJsonLib = {
+        defaultProject: 'testProj',
+        projects: {
+            testProj: {
+                root: '/',
+                sourceRoot: '/testSrc',
+                prefix: 'lib',
+                architect: {
+                    build: {
+                      options: {
+                      }
+                    }
+                }
+            }
+        },
+        version: 1
+    };
 
     beforeEach(() => {
         appTree = new UnitTestTree(new EmptyTree());
-        appTree.create('/angular.json', JSON.stringify(configJson));
     });
 
     const migrationName = 'migration-27';
 
     it(`should add igniteui-theming to pacakage json and configure it`, async () => {
+        appTree.create('/angular.json', JSON.stringify(configJson));
         const tree = await schematicRunner.runSchematicAsync(migrationName, {}, appTree)
             .toPromise();
 
         expect(JSON.parse(JSON.stringify(tree.readContent('angular.json')))).toContain("stylePreprocessorOptions");
+    });
+
+    it(`should not add igniteui-theming to library pacakage json and configure it`, async () => {
+        appTree.create('/angular.json', JSON.stringify(configJsonLib));
+        const tree = await schematicRunner.runSchematicAsync(migrationName, {}, appTree)
+            .toPromise();
+
+        expect(JSON.parse(JSON.stringify(tree.readContent('angular.json')))).not.toContain("stylePreprocessorOptions");
     });
 });
