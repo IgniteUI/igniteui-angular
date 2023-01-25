943227805a68054453adaa94d622e9694c755673
desig9stein <mspopovv@gmail.com>
2023-01-25 10:24:05 +0200

    fix(scrollbar): Update migration test

--- projects/igniteui-angular/migrations/update-15_0_8/index.spec.ts
+++ projects/igniteui-angular/migrations/update-15_0_8/index.spec.ts
@@ -2,47 +2,12 @@ import * as path from 'path';
 
 import { EmptyTree } from '@angular-devkit/schematics';
 import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
-import { ProjectType } from '../../schematics/utils/util';
 
 const version = '15.0.8';
 
 describe(`Update to ${version}`, () => {
     let appTree: UnitTestTree;
     const schematicRunner = new SchematicTestRunner('ig-migrate', path.join(__dirname, '../migration-collection.json'));
-    const configJson = {
-        defaultProject: 'testProj',
-        projects: {
-            testProj: {
-                root: '/',
-                sourceRoot: '/testSrc',
-                architect: {
-                    build: {
-                      options: {
-                      }
-                    }
-                }
-            }
-        },
-        version: 1
-    };
-    const configJsonLib = {
-        defaultProject: 'testProj',
-        projects: {
-            testProj: {
-                root: '/',
-                sourceRoot: '/testSrc',
-                prefix: 'lib',
-                projectType: ProjectType.Library,
-                architect: {
-                    build: {
-                      options: {
-                      }
-                    }
-                }
-            }
-        },
-        version: 1
-    };
 
     beforeEach(() => {
         appTree = new UnitTestTree(new EmptyTree());
@@ -50,19 +15,22 @@ describe(`Update to ${version}`, () => {
 
     const migrationName = 'migration-28';
 
-    it(`should add igniteui-theming to pacakage json and configure it`, async () => {
-        appTree.create('/angular.json', JSON.stringify(configJson));
-        const tree = await schematicRunner.runSchematicAsync(migrationName, {}, appTree)
+    it('should rename the $size property to the $scrollbar-size', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.scss`,
+            `$custom-scrollbar: scrollbar-theme(
+                $size: 10px,
+            );`
+        );
+
+        const tree = await schematicRunner
+            .runSchematicAsync(migrationName, {}, appTree)
             .toPromise();
 
-        expect(JSON.parse(JSON.stringify(tree.readContent('angular.json')))).toContain("stylePreprocessorOptions");
-    });
-
-    it(`should not add igniteui-theming to library pacakage json and configure it`, async () => {
-        appTree.create('/angular.json', JSON.stringify(configJsonLib));
-        const tree = await schematicRunner.runSchematicAsync(migrationName, {}, appTree)
-            .toPromise();
-
-        expect(JSON.parse(JSON.stringify(tree.readContent('angular.json')))).not.toContain("stylePreprocessorOptions");
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.scss')).toEqual(
+            `$custom-scrollbar: scrollbar-theme(
+                $scrollbar-size: 10px
+            );`
+        );
     });
 });
