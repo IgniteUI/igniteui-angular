e030aabb1e961844964d787dde2d888e42858163
jackofdiamond5 <jackofdiamond596@gmail.com>
2023-02-09 16:11:40 +0200

    test(update-15.0): should update typography, scrollbar, grays

--- projects/igniteui-angular/migrations/update-15_0_0/index.spec.ts
+++ projects/igniteui-angular/migrations/update-15_0_0/index.spec.ts
@@ -134,6 +134,126 @@ describe(`Update to ${version}`, () => {
         );
     });
 
+    it('should replace CSS $grays parameters', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.scss`,
+`$my-palette: palette(
+    $primary: #09f,
+    $secondary: #e41c77,
+    $grays: #000
+);
+
+$my-palette: palette(
+    $primary: #09f,
+    $secondary: #e41c77,
+    $grays: #000123
+);`
+        );
+
+        const tree = await schematicRunner
+            .runSchematicAsync(migrationName, {}, appTree)
+            .toPromise();
+
+        expect(
+            tree.readContent('/testSrc/appPrefix/component/test.component.scss')
+        ).toEqual(
+`$my-palette: palette(
+    $primary: #09f,
+    $secondary: #e41c77,
+    $gray: #000
+);
+
+$my-palette: palette(
+    $primary: #09f,
+    $secondary: #e41c77,
+    $gray: #000123
+);`
+        );
+    });
+
+    it('should NOT replace $grays as custom var', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.scss`,
+`$grays: #FFFFFF;`
+        );
+
+        const tree = await schematicRunner
+            .runSchematicAsync(migrationName, {}, appTree)
+            .toPromise();
+
+        expect(
+            tree.readContent('/testSrc/appPrefix/component/test.component.scss')
+        ).toEqual(
+`$grays: #FFFFFF;`
+        );
+    });
+
+    it('should replace grays as value', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.scss`,
+`.my-class {
+    color: contrast-color($color: 'grays', $variant: 300);
+}`
+        );
+
+        const tree = await schematicRunner
+            .runSchematicAsync(migrationName, {}, appTree)
+            .toPromise();
+
+        expect(
+            tree.readContent('/testSrc/appPrefix/component/test.component.scss')
+        ).toEqual(
+`.my-class {
+    color: contrast-color($color: 'gray', $variant: 300);
+}`
+        );
+    });
+
+    it('should replace .igx-typography with .ig-typography as style', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.scss`,
+`.igx-typography {
+    h1, h2, h3, h4, h5, h6, p, .igx-typography__body-1 {
+      margin: 0;
+    }
+}`);
+
+        const tree = await schematicRunner
+            .runSchematicAsync(migrationName, {}, appTree)
+            .toPromise();
+
+        expect(
+            tree.readContent('/testSrc/appPrefix/component/test.component.scss')
+        ).toEqual(
+`.ig-typography {
+    h1, h2, h3, h4, h5, h6, p, .ig-typography__body-1 {
+      margin: 0;
+    }
+}`
+        );
+    });
+
+    it('should replace igx-typography & igx-scrollbar from template', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.html`,
+`<body class="igx-typography igx-scrollbar">
+    <app-root></app-root>
+</body>`
+        );
+
+        const tree = await schematicRunner
+            .runSchematicAsync(migrationName, {}, appTree)
+            .toPromise();
+
+        expect(
+            tree.readContent('/testSrc/appPrefix/component/test.component.html')
+        ).toEqual(
+`<body class="ig-typography ig-scrollbar">
+    <app-root></app-root>
+</body>`
+        );
+    });
+    
     it('should remove the $palette prop from component themes', async () => {
         appTree.create(
             `/testSrc/appPrefix/component/test.component.scss`,
