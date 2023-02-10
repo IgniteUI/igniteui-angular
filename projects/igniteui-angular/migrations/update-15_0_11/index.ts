0820971b86ad3e08698ba41dbed7b6279fbf2d25
Boris Penkov <jackofdiamond596@gmail.com>
2023-02-10 14:41:58 +0200

    Update projects/igniteui-angular/migrations/update-15_0_11/index.ts

--- projects/igniteui-angular/migrations/update-15_0_11/index.ts
+++ projects/igniteui-angular/migrations/update-15_0_11/index.ts
@@ -5,7 +5,7 @@ import {
 } from '@angular-devkit/schematics';
 import { UpdateChanges } from '../common/UpdateChanges';
 
-const version = '15.0.4';
+const version = '15.0.11';
 
 export default (): Rule => async (host: Tree, context: SchematicContext) => {
     context.logger.info(`Applying migration for Ignite UI for Angular to version ${version}`);
