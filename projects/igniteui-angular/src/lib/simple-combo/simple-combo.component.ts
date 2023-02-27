332c12a6998dab0f4c11fa182e0f43e68bf4ba23
Georgi Anastasov <ggeorgianastasov@gmail.com>
2023-02-27 12:14:52 +0200

    fix(IgxComboBase,IgxSimpleCombo): simple combo in correct vertical direction

--- projects/igniteui-angular/src/lib/simple-combo/simple-combo.component.ts
+++ projects/igniteui-angular/src/lib/simple-combo/simple-combo.component.ts
@@ -357,6 +357,8 @@ export class IgxSimpleComboComponent extends IgxComboBaseDirective implements Co
         }
         this.clearSelection(true);
         if (this.collapsed) {
+            this.filterValue = '';
+            this.cdr.detectChanges();
             this.open();
             this.dropdown.navigateFirst();
         } else {
