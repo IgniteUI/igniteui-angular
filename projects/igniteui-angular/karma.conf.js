445ef594c296cf26a9b02e0d0527fbd1786d054c
Konstantin Dinev <kdinev@mail.bw.edu>
2023-03-07 10:10:42 +0200

    chore(*): more executors don't make run faster

--- projects/igniteui-angular/karma.conf.js
+++ projects/igniteui-angular/karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function (config) {
       '@angular-devkit/build-angular/plugins/karma'
     ],
     parallelOptions: {
-      executors: 3,
+      executors: 2,
       shardStrategy: 'round-robin'
     },
     client: {
