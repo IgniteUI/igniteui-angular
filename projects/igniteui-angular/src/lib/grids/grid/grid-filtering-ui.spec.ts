f650723bb934bfa2ec95a645657ab7e1e86f8228
Svetoslav Krastev <SKrastev@infragistics.com>
2023-04-04 10:30:35 +0300

    test(filtering): Fix filter date test flickering due to relying on todays date.

--- projects/igniteui-angular/src/lib/grids/grid/grid-filtering-ui.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid-filtering-ui.spec.ts
@@ -2101,6 +2101,7 @@ describe('IgxGrid - Filtering Row UI actions #grid', () => {
 
             const filterUIRow = fix.debugElement.query(By.directive(IgxGridFilteringRowComponent));
             const datePicker = filterUIRow.query(By.directive(IgxDatePickerComponent));
+            const expectedResults = GridFunctions.createDateFilterConditions(grid, today);
 
             // Equals condition
             datePicker.triggerEventHandler('click', null);
@@ -2122,7 +2123,7 @@ describe('IgxGrid - Filtering Row UI actions #grid', () => {
             fix.detectChanges();
 
             expect(grid.filteringRow.expression.condition.name).toEqual('thisMonth');
-            expect(grid.rowList.length).toEqual(3);
+            expect(grid.rowList.length).toEqual(expectedResults[5]);
 
             const conditionChips = filterUIRow.queryAll(By.directive(IgxChipComponent));
             expect(conditionChips.length).toBe(1);
