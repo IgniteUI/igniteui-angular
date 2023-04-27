615936c3b62afb21db5f7ffe1f25497e0cc7b38c
RivaIvanova <ivanova.riva@gmail.com>
2023-04-27 16:07:11 +0300

    refactor(expansion-panel): add HeaderContentBaseDirective

--- projects/igniteui-angular/src/lib/expansion-panel/expansion-panel.directives.ts
+++ projects/igniteui-angular/src/lib/expansion-panel/expansion-panel.directives.ts
@@ -1,78 +1,37 @@
-import { Directive, ElementRef, HostBinding } from '@angular/core';
+import { Directive, HostBinding } from '@angular/core';
+import { HeaderContentBaseDirective } from './expansion-panel.common';
 
-/**
- * Returns the `textContent` of an element
- *
- * ```html
- * <igx-expansion-panel-title>
- *  Tooltip content
- * </igx-expansion-panel-title>
- * ```
- *
- *  or the `title` content
- *
- * ```html
- * <igx-expansion-panel-title [title]="'Tooltip content'">
- * </igx-expansion-panel-title>
- * ```
- *
- * If both are provided, returns the `title` content.
- *
- * @param element
- * @returns tooltip content for an element
- */
-const getTooltipContent = (element: ElementRef): string => {
-    if (element.nativeElement.title) {
-        return element.nativeElement.title;
-    }
-    if (element.nativeElement.textContent) {
-        return element.nativeElement.textContent.trim();
-    }
-
-    return null;
-};
-
-/**
- * @hidden @internal
- */
+/** @hidden @internal */
 @Directive({
     // eslint-disable-next-line @angular-eslint/directive-selector
     selector: 'igx-expansion-panel-title'
 })
-export class IgxExpansionPanelTitleDirective {
+export class IgxExpansionPanelTitleDirective extends HeaderContentBaseDirective {
     @HostBinding('class.igx-expansion-panel__header-title')
     public cssClass = `igx-expansion-panel__header-title`;
 
     @HostBinding('attr.title')
     private get title(): string {
-        return getTooltipContent(this.element);
+        return this.getTooltipContent(this.element);
     }
-
-    constructor(private element: ElementRef) { }
 }
 
-/**
- * @hidden @internal
- */
+/** @hidden @internal */
 @Directive({
     // eslint-disable-next-line @angular-eslint/directive-selector
     selector: 'igx-expansion-panel-description'
 })
-export class IgxExpansionPanelDescriptionDirective {
+export class IgxExpansionPanelDescriptionDirective extends HeaderContentBaseDirective {
     @HostBinding('class.igx-expansion-panel__header-description')
     public cssClass = `igx-expansion-panel__header-description`;
 
     @HostBinding('attr.title')
     private get title(): string {
-        return getTooltipContent(this.element);
+        return this.getTooltipContent(this.element);
     }
-
-    constructor(private element: ElementRef) { }
 }
 
-/**
- * @hidden @internal
- */
+/** @hidden @internal */
 @Directive({
     // eslint-disable-next-line @angular-eslint/directive-selector
     selector: 'igx-expansion-panel-icon'
