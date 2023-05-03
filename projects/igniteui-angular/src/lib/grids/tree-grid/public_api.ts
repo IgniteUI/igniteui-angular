ef7ad4392234e462d4b19b1c41b8c0849d458ff7
Konstantin Dinev <kdinev@mail.bw.edu>
2023-05-03 18:24:50 +0300

    feat(standalone): adding IgxTreeGridGroupByArea

--- projects/igniteui-angular/src/lib/grids/tree-grid/public_api.ts
+++ projects/igniteui-angular/src/lib/grids/tree-grid/public_api.ts
@@ -1,13 +1,16 @@
 import { IGX_GRID_COMMON_DIRECTIVES } from '../public_api';
 import { IgxTreeGridComponent } from './tree-grid.component';
+import { IgxTreeGridGroupByAreaComponent } from '../grouping/tree-grid-group-by-area.component';
 
 export * from './tree-grid.component';
 export * from './tree-grid.interfaces';
 export * from './tree-grid.filtering.strategy';
 export * from './tree-grid.grouping.pipe';
+export * from '../grouping/tree-grid-group-by-area.component';
 
 /* NOTE: Tree grid directives collection for ease-of-use import in standalone components scenario */
 export const IGX_TREE_GRID_DIRECTIVES = [
     IgxTreeGridComponent,
+    IgxTreeGridGroupByAreaComponent,
     ...IGX_GRID_COMMON_DIRECTIVES
 ] as const;
