f7d47ef3150465bb28cfcd0db702e2d6f84885cf
MKirova <MKirova@DEV-MKIROVA>
2023-05-03 12:57:13 +0300

    chore(*): Add check in case setter is called before init.

--- projects/igniteui-angular/src/lib/grids/grid/grid.component.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid.component.ts
@@ -285,7 +285,10 @@ export class IgxGridComponent extends IgxGridBaseDirective implements GridType, 
     public set data(value: any[] | null) {
         this._data = value || [];
         this.summaryService.clearSummaryCache();
-        this.validation.updateAll(this._data);
+        if (!this._init) {
+            this.validation.updateAll(this._data);
+        }
+
         if (this.shouldGenerate) {
             this.setupColumns();
         }
