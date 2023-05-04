d5fbd6e859cec78be70f5beb7dfcfee0280eb2d1
Konstantin Dinev <kdinev@mail.bw.edu>
2023-05-04 15:39:18 +0300

    fix(tree-grid): exporting  IgxTreeGridGroupingPipe

--- projects/igniteui-angular/src/lib/grids/tree-grid/public_api.ts
+++ projects/igniteui-angular/src/lib/grids/tree-grid/public_api.ts
@@ -1,16 +1,19 @@
 import { IGX_GRID_COMMON_DIRECTIVES } from '../public_api';
 import { IgxTreeGridComponent } from './tree-grid.component';
 import { IgxTreeGridGroupByAreaComponent } from '../grouping/tree-grid-group-by-area.component';
+import { IgxTreeGridGroupingPipe } from './tree-grid.grouping.pipe';
 
 export * from './tree-grid.component';
 export * from './tree-grid.interfaces';
 export * from './tree-grid.filtering.strategy';
 export * from './tree-grid.grouping.pipe';
 export * from '../grouping/tree-grid-group-by-area.component';
+export * from './tree-grid.grouping.pipe';
 
 /* NOTE: Tree grid directives collection for ease-of-use import in standalone components scenario */
 export const IGX_TREE_GRID_DIRECTIVES = [
     IgxTreeGridComponent,
     IgxTreeGridGroupByAreaComponent,
+    IgxTreeGridGroupingPipe,
     ...IGX_GRID_COMMON_DIRECTIVES
 ] as const;
