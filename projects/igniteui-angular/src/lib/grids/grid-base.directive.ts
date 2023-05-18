8ec051769df2eb3436defbfebea02319ea0bb620
Hristo Anastasov <HAnastasov@infragistics.com>
2023-05-18 15:13:20 +0300

    chore(*): document clearCellSelection

--- projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
@@ -5438,7 +5438,11 @@ export abstract class IgxGridBaseDirective extends DisplayDensityBase implements
     }
 
     /**
-     * @hidden @internal
+     * Deselected selected cells.
+     * @example
+     * ```typescript
+     * this.grid.clearCellSelection();
+     * ```
      */
     public clearCellSelection(): void {
         this.selectionService.clear(true);
