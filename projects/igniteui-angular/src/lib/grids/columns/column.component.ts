420c6da6898f01f2210511b19e562929f48e394c
MKirova <MKirova@DEV-MKIROVA>
2023-06-19 14:55:31 +0300

    chore(*): Fix lint.

--- projects/igniteui-angular/src/lib/grids/columns/column.component.ts
+++ projects/igniteui-angular/src/lib/grids/columns/column.component.ts
@@ -1737,7 +1737,7 @@ export class IgxColumnComponent implements AfterContentInit, OnDestroy, ColumnTy
     /**
      * @hidden
      */
-    protected _groupable: boolean = false;
+    protected _groupable = false;
     /**
      *  @hidden
      */
