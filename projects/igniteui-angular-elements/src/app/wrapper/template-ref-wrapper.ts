cefe68cd7eb84fac6124b45c21bef96c248692b5
Damyan Petev <damyanpetev@users.noreply.github.com>
2023-06-20 16:00:44 +0300

    fix(elements): add implicit to template context to match typings #13067

--- projects/igniteui-angular-elements/src/app/wrapper/template-ref-wrapper.ts
+++ projects/igniteui-angular-elements/src/app/wrapper/template-ref-wrapper.ts
@@ -2,6 +2,8 @@ import { ElementRef, EmbeddedViewRef, Injector, TemplateRef } from '@angular/cor
 
 
 const CONTEXT_PROP = 'context';
+const IMPLICIT_PROP = 'implicit';
+const PREFIX_IMPLICIT_PROP = '$implicit';
 
 /**
  * Wraps a template ref and exposes the entire context to the template as additional prop
@@ -24,11 +26,13 @@ export class TemplateRefWrapper<C> extends TemplateRef<C> {
     /** @internal Angular 16 impl gets called directly... */
     createEmbeddedViewImpl(context: C, injector?: Injector, _hydrationInfo: any = null): EmbeddedViewRef<C> {
         context[CONTEXT_PROP] = context;
+        context[IMPLICIT_PROP] = context[PREFIX_IMPLICIT_PROP];
         const viewRef = (this.innerTemplateRef as any).createEmbeddedViewImpl(context, injector);
         var original = Object.getOwnPropertyDescriptor(Object.getPrototypeOf(viewRef), 'context');
         Object.defineProperty(viewRef, "context", {
             set: function(val) {
                 val[CONTEXT_PROP] = val;
+                val[IMPLICIT_PROP] = val[PREFIX_IMPLICIT_PROP];
                 original.set.call(this, val);
             },
             get: function() {
