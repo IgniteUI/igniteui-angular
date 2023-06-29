3b8f8f9cf0b9ff176fd2960cd62da748186d2de5
Maya <MKirova@infragistics.com>
2023-06-29 15:18:30 +0300

    chore(*): Apply review suggestion.
    Co-authored-by: Damyan Petev <damyanpetev@users.noreply.github.com>

--- projects/igniteui-angular/src/lib/grids/common/grid.interface.ts
+++ projects/igniteui-angular/src/lib/grids/common/grid.interface.ts
@@ -311,6 +311,7 @@ export interface GridType extends IGridDataBindable {
     renderedRowHeight: number;
     pipeTrigger: number;
     summaryPipeTrigger: number;
+    /** @hidden @internal */
     groupablePipeTrigger: number;
     filteringPipeTrigger: number;
     hasColumnLayouts: boolean;
