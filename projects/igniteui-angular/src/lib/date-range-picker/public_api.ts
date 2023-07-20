a692d90aae4b507508f13f0fd9a510dfa8a9d127
Boris Penkov <jackofdiamond596@gmail.com>
2023-07-20 13:54:00 +0300

    fix(range-picker): export picker toggle component (#13293)
    (cherry picked from commit cb642c7801b4952cb4db1de472b7c66ca17c9902)

--- projects/igniteui-angular/src/lib/date-range-picker/public_api.ts
+++ projects/igniteui-angular/src/lib/date-range-picker/public_api.ts
@@ -1,3 +1,4 @@
+import { IgxPickerToggleComponent } from '../date-common/picker-icons.common';
 import { IgxHintDirective } from '../directives/hint/hint.directive';
 import { IgxLabelDirective } from '../directives/label/label.directive';
 import { IgxPrefixDirective } from '../directives/prefix/prefix.directive';
@@ -11,6 +12,7 @@ export * from './date-range-picker.component';
 /* NOTE: Date-range picker directives collection for ease-of-use import in standalone components scenario */
 export const IGX_DATE_RANGE_PICKER_DIRECTIVES = [
     IgxDateRangePickerComponent,
+    IgxPickerToggleComponent,
     IgxDateRangeStartComponent,
     IgxDateRangeEndComponent,
     IgxDateRangeSeparatorDirective,
