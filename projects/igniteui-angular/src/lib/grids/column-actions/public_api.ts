eacb2c74aaccc60577b168122a310407bd4ff01e
Konstantin Dinev <kdinev@mail.bw.edu>
2023-08-08 12:01:39 +0300

    fix(grid): adding back columnactionsbase export

--- projects/igniteui-angular/src/lib/grids/column-actions/public_api.ts
+++ projects/igniteui-angular/src/lib/grids/column-actions/public_api.ts
@@ -5,6 +5,7 @@ import { IgxColumnPinningDirective } from './column-pinning.directive';
 export { IgxColumnActionsComponent } from './column-actions.component';
 export { IgxColumnHidingDirective } from './column-hiding.directive';
 export { IgxColumnPinningDirective } from './column-pinning.directive';
+export { IgxColumnActionsBaseDirective } from './column-actions-base.directive';
 
 /* NOTE: Grid column actions directives collection for ease-of-use import in standalone components scenario */
 export const IGX_GRID_COLUMN_ACTIONS_DIRECTIVES = [
