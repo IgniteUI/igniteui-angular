f228d76b44171238afeecb6436048fc2c87422aa
Hristiana Hristova <HHristova@infragistics.com>
2023-08-25 10:38:49 +0300

    fix(cellEditing): removed extra space #13201

--- projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
@@ -4318,7 +4318,7 @@ export abstract class IgxGridBaseDirective extends DisplayDensityBase implements
     public getColumnByName(name: string): IgxColumnComponent {
         return this._columns.find((col) => col.field === name);
     }
-    
+
     public getColumnByVisibleIndex(index: number): IgxColumnComponent {
         return this.visibleColumns.find((col) =>
             !col.columnGroup && !col.columnLayout &&
