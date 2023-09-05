6fe7156d96a471a677ddca98a74bce984be59fde
teodosia <teodosiah@gmail.com>
2023-09-05 16:23:04 +0300

    fix(grid): add return type and return value to endEdit method

--- projects/igniteui-angular/src/lib/grids/pivot-grid/pivot-grid.component.ts
+++ projects/igniteui-angular/src/lib/grids/pivot-grid/pivot-grid.component.ts
@@ -1407,7 +1407,7 @@ export class IgxPivotGridComponent extends IgxGridBaseDirective implements OnIni
     /**
      * @hidden @internal
      */
-    public override endEdit(_commit = true, _event?: Event) {
+    public override endEdit(_commit = true, _event?: Event): any {
     }
 
     /**
