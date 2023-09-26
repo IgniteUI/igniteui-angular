2049de1b423f45e1f01f1131a32bbfa7754737fa
desig9stein <mspopovv@gmail.com>
2023-09-26 11:16:20 +0300

    refactor(divider): refactor divider component and create a sample

--- src/app/divider/divider.component.ts
+++ src/app/divider/divider.component.ts
@@ -1,39 +1,95 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, QueryList, ViewChildren } from '@angular/core';
+import {
+	IgxButtonDirective,
+	IgxDividerDirective,
+	IgxIconComponent,
+	IgxInputDirective,
+	IgxInputGroupComponent, IgxLabelDirective, IgxSwitchComponent,
+} from 'igniteui-angular';
 import { CommonModule } from '@angular/common';
-import { IgxButtonDirective, IgxDividerDirective } from 'igniteui-angular';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-divider',
 	standalone: true,
-	imports: [CommonModule, IgxDividerDirective, IgxButtonDirective],
+	imports: [CommonModule, FormsModule, IgxSwitchComponent, IgxButtonDirective, IgxDividerDirective, IgxInputGroupComponent, IgxInputDirective, IgxIconComponent, IgxLabelDirective, ReactiveFormsModule, IgxSwitchComponent],
 	templateUrl: './divider.component.html',
 	styleUrls: ['./divider.component.scss'],
 })
 export class DividerComponent {
-	@ViewChild(IgxDividerDirective) igxDivider: IgxDividerDirective;
+	@ViewChildren(IgxDividerDirective) private dividers: QueryList<IgxDividerDirective>;
 
 	public direction = false;
 	public dashed = false;
+	public middle = false;
+	public inset = '0px';
 
-	private toggleDividerDirection() {
-		this.direction = !this.direction;
+	private insetControl = new FormControl('', [this.validateCSSUnit()]);
+
+	// Define a custom validator function
+	private validateCSSUnit() {
+		return (control: FormControl) => {
+			const value = control.value;
+
+			if (value === '0') {
+				return null; // Validation passed
+			} else {
+				// Check if the value contains only text or starts with text
+				if (!/^[a-zA-Z]+$/.test(value) && !/^[a-zA-Z].*$/.test(value)) {
+					// Define a regular expression for valid CSS units (px, em, %)
+					const validCSSUnits = /(px|em|%)$/;
+
+					if (!validCSSUnits.test(value)) {
+						return { invalidCSSUnit: true }; // Validation failed
+					}
+				} else {
+					return { invalidCSSUnit: true }; // Validation failed
+				}
+			}
+
+			return null; // Validation passed
+		};
+	}
+
+	public onBlur() {
+		const value = this.insetControl.value;
+
+		// Check if the input is a valid number
+		const parsedValue = parseFloat(value);
+		if (!isNaN(parsedValue)) {
+			// Append "px" to the numeric value and update the form control
+			this.insetControl.setValue(parsedValue + 'px');
+		}
+	}
+
+	public onInsetInputChange(event: any) {
+		const value = event.target.value;
+		// Update the inset value whenever the user types
+		this.inset = value;
+	}
+
+	public toggleDirection(event: any) {
+		this.direction = event.checked;
 		this.updateDivider();
 	}
 
-	private toggleDividerType() {
-		this.dashed = !this.dashed;
+	public toggleType(event: any) {
+		this.dashed = event.checked;
 		this.updateDivider();
 	}
 
-	get verticalClass() {
-		return this.direction ? 'vertical' : '';
+	public toggleMiddle(event: any) {
+		this.middle = event.checked;
+		this.updateDivider();
 	}
 
 	private updateDivider() {
-		// Update the igx-divider's 'vertical' property
-		this.igxDivider.vertical = this.direction;
-
-		// Update the igx-divider's 'type' property
-		this.igxDivider.type = this.dashed ? 'dashed' : 'solid';
+		// Update the igx-divider properties
+		this.dividers.forEach((divider) => {
+			divider.vertical = this.direction;
+			divider.type = this.dashed ? 'dashed' : 'solid';
+			divider.middle = this.middle;
+			divider.inset = this.inset;
+		});
 	}
 }
