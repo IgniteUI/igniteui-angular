e06907589dcfde8e00bf16eb83bfcb482dc92ab3
Bozhidara Pachilova <35433937+ddaribo@users.noreply.github.com>
2023-10-09 17:26:57 +0300

    fix(grids): refactor cell width - 16.0.x (#13501)

--- projects/igniteui-angular/src/lib/grids/common/grid.interface.ts
+++ projects/igniteui-angular/src/lib/grids/common/grid.interface.ts
@@ -153,6 +153,10 @@ export interface ColumnType extends FieldType {
     headerGroupClasses: any;
     headerGroupStyles: any;
 
+    /** 
+     * Custom CSS styling, appplied to every column 
+     * calcWidth, minWidthPx, maxWidthPx, minWidth, maxWidth, minWidthPercent, maxWidthPercent, resolvedWidth
+     */
     calcWidth: any;
     minWidthPx: number;
     maxWidthPx: number;
@@ -160,6 +164,7 @@ export interface ColumnType extends FieldType {
     maxWidth: string;
     minWidthPercent: number;
     maxWidthPercent: number;
+    resolvedWidth: string;
 
     header?: string;
     index: number;
