6182422d0cdbf6943954069361eef360e919f34d
Maya <MKirova@infragistics.com>
2023-10-18 00:40:34 +0300

    Unsuppress col group collapsible prop. (#13550)

--- projects/igniteui-angular/src/lib/grids/columns/column-group.component.ts
+++ projects/igniteui-angular/src/lib/grids/columns/column-group.component.ts
@@ -44,7 +44,6 @@ export class IgxColumnGroupComponent extends IgxColumnComponent implements After
     @ContentChildren(IgxColumnComponent, { read: IgxColumnComponent })
     public override children = new QueryList<IgxColumnComponent>();
 
-    /* blazorSuppress */
     /**
      * Set if the column group is collapsible.
      * Default value is `false`
