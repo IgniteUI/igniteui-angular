331a84b9d3f2380731bc6b7c78e1b06864636cf7
ddaribo <bozhidara.pp@gmail.com>
2023-11-29 17:36:59 +0200

    fix(igxForOf): not reset startIndex w/ scroll pos

--- projects/igniteui-angular/src/lib/directives/for-of/for_of.directive.ts
+++ projects/igniteui-angular/src/lib/directives/for-of/for_of.directive.ts
@@ -863,7 +863,6 @@ export class IgxForOfDirective<T, U extends T[] = T[]> implements OnInit, OnChan
     public resetScrollPosition() {
         this.scrollPosition = 0;
         this.scrollComponent.scrollAmount = 0;
-        this.state.startIndex = 0;
     }
 
     /**
