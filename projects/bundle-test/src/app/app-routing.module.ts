25cb1542906882e26e14f1ed39f02b6f9c801123
Konstantin Dinev <kdinev@mail.bw.edu>
2024-02-13 11:26:54 +0200

    chore(tree-shaking): adding a grid component for test

--- projects/bundle-test/src/app/app-routing.module.ts
+++ projects/bundle-test/src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ const routes: Routes = [
     { path: '', redirectTo: '/chip', pathMatch: 'full' },
     { path: 'chip', component: ChipComponent },
     // { path: 'form', loadComponent: () => import('./form/form.component').then(m => m.FormComponent) },
-    // { path: 'stepper', loadComponent: () => import('./stepper/stepper.component').then(m => m.StepperComponent) }
+    // { path: 'stepper', loadComponent: () => import('./stepper/stepper.component').then(m => m.StepperComponent) },
+    // { path: 'grid', loadComponent: () => import('./grid/grid.component').then(m => m.GridComponent) }
 ];
 
 @NgModule({
