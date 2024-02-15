7ace3a69756e2f613a8c17bcd32a98bc64b11660
Konstantin Dinev <kdinev@mail.bw.edu>
2024-02-15 13:39:46 +0200

    chore(bundle-test): updating style extension

--- projects/bundle-test/src/app/button-group/button-group.component.ts
+++ projects/bundle-test/src/app/button-group/button-group.component.ts
@@ -8,7 +8,7 @@ import { IGX_BUTTON_GROUP_DIRECTIVES } from 'igniteui-angular';
         IGX_BUTTON_GROUP_DIRECTIVES
     ],
     templateUrl: './button-group.component.html',
-    styleUrl: './button-group.component.css',
+    styleUrl: './button-group.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ButtonGroupComponent { }
