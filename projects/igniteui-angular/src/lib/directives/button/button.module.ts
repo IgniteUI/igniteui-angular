2d6b376d23dfc88532a77d1a3bcaba32d970b51e
Silvia Ivanova <59446295+SisIvanova@users.noreply.github.com>
2024-02-29 14:08:14 +0200

    fix(icon-button): add directive into button module (#13965)

--- projects/igniteui-angular/src/lib/directives/button/button.module.ts
+++ projects/igniteui-angular/src/lib/directives/button/button.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { IgxButtonDirective } from './button.directive';
+import { IgxIconButtonDirective } from './icon-button.directive';
 
 /**
  * @hidden
  * IMPORTANT: The following is NgModule exported for backwards-compatibility before standalone components
  */
 @NgModule({
-    imports: [IgxButtonDirective],
-    exports: [IgxButtonDirective]
+    imports: [IgxButtonDirective, IgxIconButtonDirective],
+    exports: [IgxButtonDirective, IgxIconButtonDirective]
 })
 export class IgxButtonModule {}
