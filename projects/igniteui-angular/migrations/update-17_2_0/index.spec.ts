b2cda5cba007de68fc653b32bbff027257b3d913
desig9stein <mspopovv@gmail.com>
2024-03-05 15:02:19 +0200

    refactor(calendar): fix migrations

---
+++ projects/igniteui-angular/migrations/update-17_2_0/index.spec.ts
@@ -0,0 +1,254 @@
+import * as path from 'path';
+
+import { EmptyTree } from '@angular-devkit/schematics';
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
+
+const version = '17.2.0';
+
+describe(`Update to ${version}`, () => {
+    let appTree: UnitTestTree;
+    const schematicRunner = new SchematicTestRunner('ig-migrate', path.join(__dirname, '../migration-collection.json'));
+
+    const configJson = {
+        projects: {
+            testProj: {
+                root: '/',
+                sourceRoot: '/testSrc'
+            }
+        },
+        schematics: {
+            '@schematics/angular:component': {
+                prefix: 'appPrefix'
+            }
+        }
+    };
+
+    beforeEach(() => {
+        appTree = new UnitTestTree(new EmptyTree());
+        appTree.create('/angular.json', JSON.stringify(configJson));
+    });
+
+    const migrationName = 'migration-34';
+
+    it('should rename the $header-text-color property to the $header-foreground', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.scss`,
+            `$custom-calendar: calendar-theme($header-text-color: red);`
+        );
+
+        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.scss')).toEqual(
+            `$custom-calendar: calendar-theme($header-foreground: red);`
+        );
+    });
+
+    it('should rename the $picker-text-color property to the $picker-foreground', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.scss`,
+            `$custom-calendar: calendar-theme($picker-text-color: red);`
+        );
+
+        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.scss')).toEqual(
+            `$custom-calendar: calendar-theme($picker-foreground: red);`
+        );
+    });
+
+    it('should rename the $picker-text-hover-color property to the $picker-hover-foreground', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.scss`,
+            `$custom-calendar: calendar-theme($picker-text-hover-color: red);`
+        );
+
+        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.scss')).toEqual(
+            `$custom-calendar: calendar-theme($picker-hover-foreground: red);`
+        );
+    });
+
+    it('should rename the $picker-arrow-color property to the $month-nav-arrow-color', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.scss`,
+            `$custom-calendar: calendar-theme($picker-arrow-color: red);`
+        );
+
+        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.scss')).toEqual(
+            `$custom-calendar: calendar-theme($month-nav-arrow-color: red);`
+        );
+    });
+
+    it('should rename the $picker-arrow-hover-color property to the $month-nav-arrow-hover-color', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.scss`,
+            `$custom-calendar: calendar-theme($picker-arrow-hover-color: red);`
+        );
+
+        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.scss')).toEqual(
+            `$custom-calendar: calendar-theme($month-nav-arrow-hover-color: red);`
+        );
+    });
+
+    it('should rename the $inactive-text-color property to the $inactive-color', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.scss`,
+            `$custom-calendar: calendar-theme($inactive-text-color: red);`
+        );
+
+        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.scss')).toEqual(
+            `$custom-calendar: calendar-theme($inactive-color: red);`
+        );
+    });
+
+    it('should rename the $weekend-text-color property to the $weekend-color', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.scss`,
+            `$custom-calendar: calendar-theme($weekend-text-color: red);`
+        );
+
+        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.scss')).toEqual(
+            `$custom-calendar: calendar-theme($weekend-color: red);`
+        );
+    });
+
+    it('should rename the $year-current-text-color property to the $year-current-foreground', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.scss`,
+            `$custom-calendar: calendar-theme($year-current-text-color: red);`
+        );
+
+        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.scss')).toEqual(
+            `$custom-calendar: calendar-theme($year-current-foreground: red);`
+        );
+    });
+
+    it('should rename the $year-hover-text-color property to the $year-hover-foreground', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.scss`,
+            `$custom-calendar: calendar-theme($year-hover-text-color: red);`
+        );
+
+        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.scss')).toEqual(
+            `$custom-calendar: calendar-theme($year-hover-foreground: red);`
+        );
+    });
+
+    it('should rename the $month-hover-text-color property to the $month-hover-foreground', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.scss`,
+            `$custom-calendar: calendar-theme($month-hover-text-color: red);`
+        );
+
+        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.scss')).toEqual(
+            `$custom-calendar: calendar-theme($month-hover-foreground: red);`
+        );
+    });
+
+    it('should rename the $date-selected-text-color property to the $date-selected-foreground', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.scss`,
+            `$custom-calendar: calendar-theme($date-selected-text-color: red);`
+        );
+
+        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.scss')).toEqual(
+            `$custom-calendar: calendar-theme($date-selected-foreground: red);`
+        );
+    });
+
+    it('should rename the $date-current-text-color property to the $date-current-foreground', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.scss`,
+            `$custom-calendar: calendar-theme($date-current-text-color: red);`
+        );
+
+        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.scss')).toEqual(
+            `$custom-calendar: calendar-theme($date-current-foreground: red);`
+        );
+    });
+
+    it('should rename the $date-special-text-color property to the $date-special-foreground', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.scss`,
+            `$custom-calendar: calendar-theme($date-special-text-color: red);`
+        );
+
+        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.scss')).toEqual(
+            `$custom-calendar: calendar-theme($date-special-foreground: red);`
+        );
+    });
+
+    it('should rename the $date-disabled-text-color property to the $date-disabled-foreground', async () => {
+        appTree.create(
+            `/testSrc/appPrefix/component/test.component.scss`,
+            `$custom-calendar: calendar-theme($date-disabled-text-color: red);`
+        );
+
+        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.scss')).toEqual(
+            `$custom-calendar: calendar-theme($date-disabled-foreground: red);`
+        );
+
+    it('should remove toolbar grid property', async () => {
+        appTree.create(`/testSrc/appPrefix/component/test.component.html`,
+        `
+        <igx-hierarchical-grid #grid1>
+            <igx-grid-toolbar>
+                <app-grid-search-box [grid]="grid1"></app-grid-search-box>
+            </igx-grid-toolbar>
+            <igx-row-island>
+                <igx-grid-toolbar [grid]="childGrid" *igxGridToolbar="let childGrid">
+                    <igx-grid-toolbar-title>Child toolbar {{ gridRef.parentIsland.level }}</igx-grid-toolbar-title>
+                    <igx-grid-toolbar-actions>
+                        <igx-grid-toolbar-exporter></igx-grid-toolbar-exporter>
+                    </igx-grid-toolbar-actions>
+                </igx-grid-toolbar>
+            </igx-row-island>
+        </igx-hierarchical-grid>
+        `
+        );
+
+        const tree = await schematicRunner.runSchematic(migrationName, {}, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.html')).toEqual(
+        `
+        <igx-hierarchical-grid #grid1>
+            <igx-grid-toolbar>
+                <app-grid-search-box [grid]="grid1"></app-grid-search-box>
+            </igx-grid-toolbar>
+            <igx-row-island>
+                <igx-grid-toolbar *igxGridToolbar="let childGrid">
+                    <igx-grid-toolbar-title>Child toolbar {{ gridRef.parentIsland.level }}</igx-grid-toolbar-title>
+                    <igx-grid-toolbar-actions>
+                        <igx-grid-toolbar-exporter></igx-grid-toolbar-exporter>
+                    </igx-grid-toolbar-actions>
+                </igx-grid-toolbar>
+            </igx-row-island>
+        </igx-hierarchical-grid>
+        `
+        );
+    });
+});
+});
