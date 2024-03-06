899742b33c7cf10d86c2d63929fe70c4f996f15c
Simeon Simeonoff <sim.simeonoff@gmail.com>
2024-03-06 18:11:49 +0200

    chore(migrations): add migration for vertical property

--- projects/igniteui-angular/migrations/update-17_2_0/index.ts
+++ projects/igniteui-angular/migrations/update-17_2_0/index.ts
@@ -1,5 +1,5 @@
 import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
-import { UpdateChanges } from "../common/UpdateChanges";
+import { BoundPropertyObject, InputPropertyType, UpdateChanges } from "../common/UpdateChanges";
 
 const version = "17.2.0";
 
@@ -8,5 +8,21 @@ export default (): Rule => async (host: Tree, context: SchematicContext) => {
         `Applying migration for Ignite UI for Angular to version ${version}`,
     );
     const update = new UpdateChanges(__dirname, host, context);
+
+    update.addValueTransform('vertical_to_orientation', (args: BoundPropertyObject): void => {
+        args.bindingType = InputPropertyType.STRING;
+
+        switch (args.value) {
+            case 'true':
+                args.value = 'vertical';
+                break;
+            case 'false':
+                args.value = 'horizontal';
+                break;
+            default:
+                args.value += ` ? 'vertical' : 'horizontal' `;
+        }
+    });
+
     update.applyChanges();
 };
