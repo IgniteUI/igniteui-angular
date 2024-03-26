2c08bad050260852b9db0e9364c228f98d64fc14
didimmova <d.dimova11@gmail.com>
2024-03-26 09:00:44 +0200

    feat(splitter): update pointer events and toggle button in sample

--- src/app/splitter/splitter.sample.ts
+++ src/app/splitter/splitter.sample.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
-import { IgxSplitterModule, SplitterType } from 'igniteui-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IgxSplitterComponent, IgxSplitterModule, SplitterType } from 'igniteui-angular';
 
 @Component({
     selector: 'app-splitter-sample',
     styleUrls: ['splitter.sample.scss'],
     templateUrl: 'splitter.sample.html',
     standalone: true,
-    imports: [IgxSplitterModule]
+    imports: [IgxSplitterModule, IgxSplitterComponent]
 })
 
 export class SplitterSampleComponent {
+    @ViewChild('splitter') public splitterComponent: IgxSplitterComponent;
+    
     public typeHorizontal = SplitterType.Horizontal;
     public typeVertical = SplitterType.Vertical;
+
+    public toggleCollapsible() {
+        this.splitterComponent.collapsible = !this.splitterComponent.collapsible;
+    }
 }
