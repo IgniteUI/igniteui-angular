db9949795ffa91efa121fa75d5fbb84b86c0e674
Martin Dragnev <mddragnev@gmail.com>
2024-03-27 17:21:15 +0200

    chore(*): update how we get the document node

--- projects/igniteui-angular/src/lib/grids/common/crud.service.ts
+++ projects/igniteui-angular/src/lib/grids/common/crud.service.ts
@@ -237,6 +237,8 @@ export class IgxCellCrudState {
         // this is needed when we are not using ngModel to update the editValue
         // so that the change event of the inlineEditorTemplate is hit before
         // trying to update any cell
+        const cellNode = this.grid.gridAPI.get_cell_by_index(this.cell.id.rowIndex, this.cell.column.field).nativeElement;
+        const document = cellNode.getRootNode() as Document | ShadowRoot;
         const activeElement = document.activeElement as HTMLElement;
         activeElement.blur();
 
