f783768fe2270972fd0b063dffbdbd67862265ac
Simeon Simeonoff <sim.simeonoff@gmail.com>
2024-04-01 11:05:37 +0300

    chore(splitter): update sample

--- src/app/splitter/splitter.sample.ts
+++ src/app/splitter/splitter.sample.ts
@@ -1,21 +1,24 @@
-import { Component, ViewChild } from '@angular/core';
-import { IgxSplitterComponent, IgxSplitterModule, SplitterType } from 'igniteui-angular';
+import { ChangeDetectionStrategy, Component, ViewChild } from "@angular/core";
+import {
+    IgxButtonModule,
+    IgxDividerModule,
+    IgxSplitterComponent,
+    IgxSplitterModule,
+} from "igniteui-angular";
 
 @Component({
-    selector: 'app-splitter-sample',
-    styleUrls: ['splitter.sample.scss'],
-    templateUrl: 'splitter.sample.html',
+    selector: "app-splitter-sample",
+    styleUrls: ["splitter.sample.scss"],
+    templateUrl: "splitter.sample.html",
     standalone: true,
-    imports: [IgxSplitterModule, IgxSplitterComponent]
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [IgxButtonModule, IgxSplitterModule, IgxSplitterComponent, IgxDividerModule],
 })
-
 export class SplitterSampleComponent {
-    @ViewChild('splitter') public splitterComponent: IgxSplitterComponent;
-    
-    public typeHorizontal = SplitterType.Horizontal;
-    public typeVertical = SplitterType.Vertical;
+    @ViewChild("splitter") public splitterComponent: IgxSplitterComponent;
 
     public toggleCollapsible() {
-        this.splitterComponent.nonCollapsible = !this.splitterComponent.nonCollapsible;
+        this.splitterComponent.nonCollapsible =
+            !this.splitterComponent.nonCollapsible;
     }
 }
