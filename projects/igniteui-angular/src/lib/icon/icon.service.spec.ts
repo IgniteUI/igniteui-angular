8446e2f4ecf7e8cb2d84cd54c6325daf18c9be53
Simeon Simeonoff <sim.simeonoff@gmail.com>
2024-04-09 18:05:57 +0300

    spec(icon-service): add more tests

--- projects/igniteui-angular/src/lib/icon/icon.service.spec.ts
+++ projects/igniteui-angular/src/lib/icon/icon.service.spec.ts
@@ -1,108 +1,175 @@
-import { TestBed, fakeAsync } from '@angular/core/testing';
-import { IgxIconService } from './icon.service';
+import { TestBed, fakeAsync } from "@angular/core/testing";
+import { IconFamily, IconMeta, IgxIconService } from "./icon.service";
 
-import { configureTestSuite } from '../test-utils/configure-suite';
-import { first } from 'rxjs/operators';
-import { HttpClientModule } from '@angular/common/http';
+import { configureTestSuite } from "../test-utils/configure-suite";
+import { first } from "rxjs/operators";
+import { HttpClientModule } from "@angular/common/http";
 
-describe('Icon Service', () => {
+describe("Icon Service", () => {
     configureTestSuite();
-    const MY_FONT = 'my-awesome-icons';
-    const ALIAS = 'awesome';
+    const FAMILY: IconFamily = {
+        name: "awesome",
+        meta: { className: "my-awesome-icons", type: "font" },
+    };
 
     const svgText = `<svg id="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
     <path d="M74 74h54v54H74" />
     <path d="M10 10h181v181H10V10zm38.2 38.2v104.6h104.6V48.2H48.2z"/>
 </svg>`;
 
+    let iconRef: { name: string; family: string; icon: IconMeta };
+    let iconService: IgxIconService;
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [HttpClientModule],
-            providers: [IgxIconService]
+            providers: [IgxIconService],
         }).compileComponents();
+
+        iconService = TestBed.inject(IgxIconService);
+
+        iconService.setFamily("fa-solid", {
+            className: "fa",
+            prefix: "fa-",
+            type: "font",
+        });
+
+        iconRef = {
+            name: "car",
+            family: "default",
+            icon: {
+                name: "car",
+                family: "fa-solid",
+            },
+        };
+
+        iconService.addIconRef(iconRef.name, iconRef.family, iconRef.icon);
     });
 
-    it('should set the default icon set', () => {
-        const iconService = TestBed.inject(IgxIconService);
+    it("should have material as the default icon set", () => {
+        expect(iconService.defaultFamily.name).toBe("material");
+        expect(iconService.defaultFamily.meta.className).toBe("material-icons");
+        expect(iconService.defaultFamily.meta.type).toBe("liga");
+    });
 
+    it("should set the default icon set", () => {
         expect(() => {
-            iconService.defaultFamily = MY_FONT;
+            iconService.defaultFamily = FAMILY;
         }).not.toThrow();
     });
 
-    it('should get the default icon set', () => {
-        const iconService = TestBed.inject(IgxIconService);
-        iconService.defaultFamily = MY_FONT;
+    it("should get the default icon set", () => {
+        iconService.defaultFamily = FAMILY;
 
-        expect(iconService.defaultFamily).toBe(MY_FONT);
+        expect(iconService.defaultFamily).toBe(FAMILY);
     });
 
-    it('should associate alias name with icon set name', () => {
-        const iconService = TestBed.inject(IgxIconService);
-
+    it("should associate a name with family metadata", () => {
         expect(() => {
-            iconService.registerFamilyAlias(ALIAS, MY_FONT);
+            iconService.setFamily(FAMILY.name, FAMILY.meta);
         }).not.toThrow();
     });
 
-    it('should get icon set name from alias name', () => {
-        const iconService = TestBed.inject(IgxIconService);
-        iconService.registerFamilyAlias(ALIAS, MY_FONT);
+    it("should create meta icon and family reference to an icon in another family", () => {
+        const icon = iconService.getIconRef(iconRef.name, iconRef.family);
 
-        expect(iconService.familyClassName(ALIAS)).toBe(MY_FONT);
+        expect(icon).toEqual({
+            className: "fa",
+            type: "font",
+            name: "fa-car",
+            family: "fa-solid",
+        });
     });
 
-    it('should add custom svg icon from url', fakeAsync((done) => {
-        const iconService = TestBed.inject(IgxIconService) as IgxIconService;
+    it("should not be able to overwrite an icon reference by calling addIconRef", () => {
+        iconService.addIconRef(iconRef.name, iconRef.family, {
+            name: "android",
+            family: "material",
+        });
 
-        const name = 'test';
-        const family = 'svg-icons';
+        const icon = iconService.getIconRef(iconRef.name, iconRef.family);
 
-        spyOn(XMLHttpRequest.prototype, 'open').and.callThrough();
-        spyOn(XMLHttpRequest.prototype, 'send');
+        expect(icon).toEqual({
+            className: "fa",
+            type: "font",
+            name: "fa-car",
+            family: "fa-solid",
+        });
+    });
 
-        iconService.addSvgIcon(name, 'test.svg', family);
+    it("should overwrite an icon reference via setIconRef", () => {
+        iconService.setIconRef(iconRef.name, iconRef.family, {
+            name: "android",
+            family: "material",
+        });
+
+        const icon = iconService.getIconRef(iconRef.name, iconRef.family);
+
+        expect(icon).toEqual({
+            className: "material-icons",
+            type: "liga",
+            name: "android",
+            family: "material",
+        });
+    });
+
+    it("should get the className by family name", () => {
+        iconService.setFamily(FAMILY.name, FAMILY.meta);
+
+        expect(iconService.familyClassName(FAMILY.name)).toBe(
+            FAMILY.meta.className,
+        );
+    });
+
+    it("should add custom svg icon from url", fakeAsync((
+        done: () => object,
+    ) => {
+        const iconName = "test";
+        const familyName = "svg-icons";
+
+        spyOn(XMLHttpRequest.prototype, "open").and.callThrough();
+        spyOn(XMLHttpRequest.prototype, "send");
+
+        iconService.addSvgIcon(iconName, "test.svg", familyName);
 
         expect(XMLHttpRequest.prototype.open).toHaveBeenCalledTimes(1);
         expect(XMLHttpRequest.prototype.send).toHaveBeenCalledTimes(1);
 
         iconService.iconLoaded.pipe().subscribe(() => {
-            expect(iconService.isSvgIconCached(name, family)).toBeTruthy();
+            expect(
+                iconService.isSvgIconCached(iconName, familyName),
+            ).toBeTruthy();
             done();
         });
     }));
 
-    it('should add custom svg icon from text', () => {
-        const iconService = TestBed.inject(IgxIconService) as IgxIconService;
+    it("should add custom svg icon from text", () => {
+        const iconName = "test";
+        const familyName = "svg-icons";
 
-        const name = 'test';
-        const family = 'svg-icons';
-
-        iconService.addSvgIconFromText(name, svgText, family);
-        expect(iconService.isSvgIconCached(name, family)).toBeTruthy();
+        iconService.addSvgIconFromText(iconName, svgText, familyName);
+        expect(iconService.isSvgIconCached(iconName, familyName)).toBeTruthy();
     });
 
-    it('should emit loading event for a custom svg icon from url', done => {
-        const iconService = TestBed.inject(IgxIconService) as IgxIconService;
-
-        iconService.iconLoaded.pipe(first()).subscribe(event => {
-            expect(event.name).toMatch('test');
-            expect(event.family).toMatch('svg-icons');
+    it("should emit loading event for a custom svg icon from url", (done) => {
+        iconService.iconLoaded.pipe(first()).subscribe((event) => {
+            expect(event.name).toMatch("test");
+            expect(event.family).toMatch("svg-icons");
             done();
         });
 
-        const name = 'test';
-        const family = 'svg-icons';
+        const iconName = "test";
+        const familyName = "svg-icons";
 
-        spyOn(XMLHttpRequest.prototype, 'open').and.callThrough();
-        spyOn(XMLHttpRequest.prototype, 'send').and.callFake(() => {
+        spyOn(XMLHttpRequest.prototype, "open").and.callThrough();
+        spyOn(XMLHttpRequest.prototype, "send").and.callFake(() => {
             (iconService as any)._iconLoaded.next({
-                name,
+                name: iconName,
                 value: svgText,
-                family
+                family: familyName,
             });
         });
 
-        iconService.addSvgIcon(name, 'test.svg', family);
+        iconService.addSvgIcon(iconName, "test.svg", familyName);
     });
 });
