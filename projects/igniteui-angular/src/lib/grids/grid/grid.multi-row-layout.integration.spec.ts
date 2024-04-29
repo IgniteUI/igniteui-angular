b0565c4d506c177ed313b47087b449dcb2aab402
Martin Dragnev <mddragnev@gmail.com>
2024-04-29 15:54:09 +0300

    chore(*): fix lint

--- projects/igniteui-angular/src/lib/grids/grid/grid.multi-row-layout.integration.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid.multi-row-layout.integration.spec.ts
@@ -1,5 +1,5 @@
 ﻿import { configureTestSuite } from '../../test-utils/configure-suite';
-import { TestBed, fakeAsync, tick, waitForAsync, ComponentFixture } from '@angular/core/testing';
+import { TestBed, waitForAsync, ComponentFixture } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { IgxGridComponent } from './grid.component';
