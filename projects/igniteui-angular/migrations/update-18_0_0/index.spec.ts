e6e765a38b8de05f0ad93c31cba6512dbd0e901e
Martin Dragnev <mddragnev@gmail.com>
2024-05-10 17:03:02 +0300

    chore(*): Add migrations from 17.2.x to 18.0.0

---
+++ projects/igniteui-angular/migrations/update-18_0_0/index.spec.ts
@@ -0,0 +1,196 @@
+import * as path from 'path';
+
+import { EmptyTree } from '@angular-devkit/schematics';
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
+
+const version = '18.0.0';
+
+describe(`Update to ${version}`, () => {
+    let appTree: UnitTestTree;
+    const schematicRunner = new SchematicTestRunner('ig-migrate', path.join(__dirname, '../migration-collection.json'));
+
+    const configJson = {
+        projects: {
+            testProj: {
+                root: '/',
+                sourceRoot: '/testSrc'
+            }
+        },
+        schematics: {
+            '@schematics/angular:component': {
+                prefix: 'appPrefix'
+            }
+        }
+    };
+
+    beforeEach(() => {
+        appTree = new UnitTestTree(new EmptyTree());
+        appTree.create('/angular.json', JSON.stringify(configJson));
+    });
+
+    const migrationName = 'migration-36';
+
+    it('should remove displayDensity property from igx-grid and replace it with inline style if its value is not set to a component member', async () => {
+        appTree.create(
+            '/testSrc/appPrefix/component/test.component.html', `
+        <igx-grid [data]="data" [displayDensity]="'cosy'" height="300px" width="300px">
+            <igx-column field="Name" header="Athlete"></igx-column>
+            <igx-column field="TrackProgress" header="Track Progress"></igx-column>
+            <igx-column field="CountryFlag" header="Country"></igx-column>
+        </igx-grid>
+        <igx-grid [data]="data" displayDensity="cosy" height="300px" width="300px">
+            <igx-column field="Name" header="Athlete"></igx-column>
+            <igx-column field="TrackProgress" header="Track Progress"></igx-column>
+            <igx-column field="CountryFlag" header="Country"></igx-column>
+        </igx-grid>`);
+
+        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.html'))
+        .toEqual(`
+        <igx-grid [data]="data" height="300px" width="300px" [style.--ig-size]="2">
+            <igx-column field="Name" header="Athlete"></igx-column>
+            <igx-column field="TrackProgress" header="Track Progress"></igx-column>
+            <igx-column field="CountryFlag" header="Country"></igx-column>
+        </igx-grid>
+        <igx-grid [data]="data" height="300px" width="300px" [style.--ig-size]="2">
+            <igx-column field="Name" header="Athlete"></igx-column>
+            <igx-column field="TrackProgress" header="Track Progress"></igx-column>
+            <igx-column field="CountryFlag" header="Country"></igx-column>
+        </igx-grid>`);
+    });
+
+    it('should only remove and not replace displayDensity property from igx-grid if it is bound to something different than a value', async () => {
+        appTree.create(
+            '/testSrc/appPrefix/component/test.component.html', `
+        <igx-grid [data]="data" [displayDensity]="density" height="300px" width="300px">
+            <igx-column field="Name" header="Athlete"></igx-column>
+            <igx-column field="TrackProgress" header="Track Progress"></igx-column>
+            <igx-column field="CountryFlag" header="Country"></igx-column>
+        </igx-grid>`);
+
+        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.html'))
+        .toEqual(`
+        <igx-grid [data]="data" height="300px" width="300px">
+            <igx-column field="Name" header="Athlete"></igx-column>
+            <igx-column field="TrackProgress" header="Track Progress"></igx-column>
+            <igx-column field="CountryFlag" header="Country"></igx-column>
+        </igx-grid>`);
+    });
+
+    it('should remove displayDensity property from igx-tree-grid and replace it with inline style if its value is not set to a component member', async () => {
+        appTree.create(
+            '/testSrc/appPrefix/component/test.component.html', `
+        <igx-tree-grid [data]="data" [displayDensity]="'comfortable'" height="300px" width="300px">
+            <igx-column field="Name" header="Athlete"></igx-column>
+            <igx-column field="TrackProgress" header="Track Progress"></igx-column>
+            <igx-column field="CountryFlag" header="Country"></igx-column>
+        </igx-tree-grid>
+        <igx-tree-grid [data]="data" displayDensity="compact" height="300px" width="300px">
+            <igx-column field="Name" header="Athlete"></igx-column>
+            <igx-column field="TrackProgress" header="Track Progress"></igx-column>
+            <igx-column field="CountryFlag" header="Country"></igx-column>
+        </igx-tree-grid>`);
+
+        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.html'))
+        .toEqual(`
+        <igx-tree-grid [data]="data" height="300px" width="300px" [style.--ig-size]="3">
+            <igx-column field="Name" header="Athlete"></igx-column>
+            <igx-column field="TrackProgress" header="Track Progress"></igx-column>
+            <igx-column field="CountryFlag" header="Country"></igx-column>
+        </igx-tree-grid>
+        <igx-tree-grid [data]="data" height="300px" width="300px" [style.--ig-size]="1">
+            <igx-column field="Name" header="Athlete"></igx-column>
+            <igx-column field="TrackProgress" header="Track Progress"></igx-column>
+            <igx-column field="CountryFlag" header="Country"></igx-column>
+        </igx-tree-grid>`);
+    });
+
+    it('should remove displayDensity property from igx-pivot-grid and replace it with inline style if its value is not set to a component member', async () => {
+        appTree.create(
+            '/testSrc/appPrefix/component/test.component.html', `
+        <igx-pivot-grid [data]="data" [displayDensity]="'comfortable'" height="300px" width="300px">
+            <igx-column field="Name" header="Athlete"></igx-column>
+            <igx-column field="TrackProgress" header="Track Progress"></igx-column>
+            <igx-column field="CountryFlag" header="Country"></igx-column>
+        </igx-pivot-grid>
+        <igx-pivot-grid [data]="data" displayDensity="compact" height="300px" width="300px">
+            <igx-column field="Name" header="Athlete"></igx-column>
+            <igx-column field="TrackProgress" header="Track Progress"></igx-column>
+            <igx-column field="CountryFlag" header="Country"></igx-column>
+        </igx-pivot-grid>`);
+
+        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.html'))
+        .toEqual(`
+        <igx-pivot-grid [data]="data" height="300px" width="300px" [style.--ig-size]="3">
+            <igx-column field="Name" header="Athlete"></igx-column>
+            <igx-column field="TrackProgress" header="Track Progress"></igx-column>
+            <igx-column field="CountryFlag" header="Country"></igx-column>
+        </igx-pivot-grid>
+        <igx-pivot-grid [data]="data" height="300px" width="300px" [style.--ig-size]="1">
+            <igx-column field="Name" header="Athlete"></igx-column>
+            <igx-column field="TrackProgress" header="Track Progress"></igx-column>
+            <igx-column field="CountryFlag" header="Country"></igx-column>
+        </igx-pivot-grid>`);
+    });
+
+    it('should remove displayDensity property from igx-hierarchical-grid and igx-row-island and replace it with inline style if its value is not set to a component member', async () => {
+        appTree.create(
+            '/testSrc/appPrefix/component/test.component.html', `
+        <igx-hierarchical-grid [data]="data" [displayDensity]="'comfortable'" height="300px" width="300px">
+            <igx-column field="Name" header="Athlete"></igx-column>
+            <igx-column field="TrackProgress" header="Track Progress"></igx-column>
+            <igx-column field="CountryFlag" header="Country"></igx-column>
+            <igx-row-island [displayDensity]="'comfortable'">
+                <igx-column field="Name" header="Athlete"></igx-column>
+                <igx-row-island [displayDensity]="'compact'">
+                    <igx-column field="Name" header="Athlete"></igx-column>
+                <igx-row-island/>
+            <igx-row-island/>
+        </igx-hierarchical-grid>
+        <igx-hierarchical-grid [data]="data" displayDensity="compact" height="300px" width="300px">
+            <igx-column field="Name" header="Athlete"></igx-column>
+            <igx-column field="TrackProgress" header="Track Progress"></igx-column>
+            <igx-column field="CountryFlag" header="Country"></igx-column>
+            <igx-row-island displayDensity="comfortable">
+                <igx-column field="Name" header="Athlete"></igx-column>
+                <igx-row-island displayDensity="cosy">
+                    <igx-column field="Name" header="Athlete"></igx-column>
+                <igx-row-island/>
+            <igx-row-island/>
+        </igx-hierarchical-grid>`);
+
+        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
+
+        expect(tree.readContent('/testSrc/appPrefix/component/test.component.html'))
+        .toEqual(`
+        <igx-hierarchical-grid [data]="data" height="300px" width="300px" [style.--ig-size]="3">
+            <igx-column field="Name" header="Athlete"></igx-column>
+            <igx-column field="TrackProgress" header="Track Progress"></igx-column>
+            <igx-column field="CountryFlag" header="Country"></igx-column>
+            <igx-row-island [style.--ig-size]="3">
+                <igx-column field="Name" header="Athlete"></igx-column>
+                <igx-row-island [style.--ig-size]="1">
+                    <igx-column field="Name" header="Athlete"></igx-column>
+                <igx-row-island/>
+            <igx-row-island/>
+        </igx-hierarchical-grid>
+        <igx-hierarchical-grid [data]="data" height="300px" width="300px" [style.--ig-size]="1">
+            <igx-column field="Name" header="Athlete"></igx-column>
+            <igx-column field="TrackProgress" header="Track Progress"></igx-column>
+            <igx-column field="CountryFlag" header="Country"></igx-column>
+            <igx-row-island [style.--ig-size]="3">
+                <igx-column field="Name" header="Athlete"></igx-column>
+                <igx-row-island [style.--ig-size]="2">
+                    <igx-column field="Name" header="Athlete"></igx-column>
+                <igx-row-island/>
+            <igx-row-island/>
+        </igx-hierarchical-grid>`);
+    });
+});
---
+++ projects/igniteui-angular/migrations/update-18_0_0/index.ts
@@ -0,0 +1,65 @@
+import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
+import { UpdateChanges } from '../common/UpdateChanges';
+import { FileChange, findElementNodes, getAttribute, getSourceOffset, hasAttribute, parseFile } from '../common/util';
+import { nativeImport } from 'igniteui-angular/migrations/common/import-helper.js';
+import type { Element } from '@angular/compiler';
+
+const version = "18.0.0";
+
+export default (): Rule => async (host: Tree, context: SchematicContext) => {
+    context.logger.info(
+        `Applying migration for Ignite UI for Angular to version ${version}`,
+    );
+    const { HtmlParser } = await nativeImport('@angular/compiler') as typeof import('@angular/compiler');
+    const update = new UpdateChanges(__dirname, host, context);
+    const changes = new Map<string, FileChange[]>();
+    const prop = ["displayDensity", "[displayDensity]"];
+    const tags = ["igx-grid", "igx-hierarchical-grid", "igx-row-island", "igx-tree-grid", "igx-pivot-grid"]
+
+    const applyChanges = () => {
+        for (const [path, change] of changes.entries()) {
+            let buffer = host.read(path).toString();
+            change.sort((c, c1) => c.position - c1.position)
+                .reverse()
+                .forEach(c => buffer = c.apply(buffer));
+
+            host.overwrite(path, buffer);
+        }
+    };
+
+    const addChange = (path: string, change: FileChange) => {
+        if (changes.has(path)) {
+          changes.get(path).push(change);
+        } else {
+          changes.set(path, [change]);
+        }
+    };
+
+    for (const path of update.templateFiles) {
+        const grid = findElementNodes(parseFile(new HtmlParser(), host, path), tags);
+        grid
+            .filter(node => hasAttribute(node as Element, prop))
+            .map(node => getSourceOffset(node as Element))
+            .forEach(offset => {
+                const { startTag, file, node } = offset;
+                const { value } = getAttribute(node, prop)[0];
+                // using includes and not the value itself because the value might be either [displayDensity]='comfortable' or displayDensity="'comfortable'"
+                if (value.includes('comfortable')) {
+                    const newProp = ' [style.--ig-size]="3"';
+                    addChange(file.url, new FileChange(startTag.end - 1, newProp, '', "insert"));
+                }
+                else if (value.includes('cosy')) {
+                    const newProp = ' [style.--ig-size]="2"';
+                    addChange(file.url, new FileChange(startTag.end - 1, newProp, '', "insert"));
+                } else if (value.includes('compact')) {
+                    const newProp = ' [style.--ig-size]="1"';
+                    addChange(file.url, new FileChange(startTag.end - 1, newProp, '', "insert"));
+                }
+                // We don`t have else because if density it set like this: [displayDensity]="customDensity"
+                // then we can`t do anything and we just remove the property.
+        });
+    }
+    applyChanges();
+
+    update.applyChanges();
+};
