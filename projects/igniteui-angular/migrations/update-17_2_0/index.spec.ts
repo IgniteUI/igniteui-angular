fbec64b74b0290b6b13d40a25ef6e7e8081876c0
Simeon Simeonoff <sim.simeonoff@gmail.com>
2024-05-16 11:22:03 +0300

    migrations(bottom-nav-theme): retarget migration version

--- projects/igniteui-angular/migrations/update-17_2_0/index.spec.ts
+++ projects/igniteui-angular/migrations/update-17_2_0/index.spec.ts
@@ -30,32 +30,6 @@ describe(`Update to ${version}`, () => {
 
     const migrationName = 'migration-35';
 
-    it('should rename the $idle-item-color property to the $label-color', async () => {
-        appTree.create(
-            `/testSrc/appPrefix/component/test.component.scss`,
-            `$custom-bottom-nav: bottom-nav-theme($idle-item-color: red);`
-        );
-
-        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
-
-        expect(tree.readContent('/testSrc/appPrefix/component/test.component.scss')).toEqual(
-            `$custom-bottom-nav: bottom-nav-theme($label-color: red);`
-        );
-    });
-
-    it('should rename the $selected-item-color property to the $label-selected-color', async () => {
-        appTree.create(
-            `/testSrc/appPrefix/component/test.component.scss`,
-            `$custom-bottom-nav: bottom-nav-theme($selected-item-color: red);`
-        );
-
-        const tree = await schematicRunner.runSchematic(migrationName, { shouldInvokeLS: false }, appTree);
-
-        expect(tree.readContent('/testSrc/appPrefix/component/test.component.scss')).toEqual(
-            `$custom-bottom-nav: bottom-nav-theme($label-selected-color: red);`
-        );
-    });
-
     it('should rename the $content-text-color property to the $content-text-color', async () => {
         appTree.create(
             `/testSrc/appPrefix/component/test.component.scss`,
