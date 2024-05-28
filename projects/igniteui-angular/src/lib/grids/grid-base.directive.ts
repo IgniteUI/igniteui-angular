1acd5a7a89d53d552616e6018b095fef3e869c2c
MKirova <MKirova@DEV-MKIROVA>
2024-05-28 12:01:13 +0300

    chore(*): Attempt a more targeted fix for misalignment when data is changed and scrollbar hides.

--- projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
@@ -3603,6 +3603,7 @@ export abstract class IgxGridBaseDirective extends DisplayDensityBase implements
             // the vert. scrollbar showing/hiding
             this.notifyChanges(true);
             this.cdr.detectChanges();
+            Promise.resolve().then(() => this.headerContainer.updateScroll());
         });
 
         this.verticalScrollContainer.contentSizeChange.pipe(filter(() => !this._init), throttleTime(30), destructor).subscribe(() => {
