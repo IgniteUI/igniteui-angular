d1eedd0b33e58bb4aa27aee0788d31847ac8a0cf
Georgi Anastasov <ggeorgianastasov@gmail.com>
2024-06-03 11:20:40 +0300

    fix(simple-combo): trigger selectionChanging on input clear

--- projects/igniteui-angular/src/lib/simple-combo/simple-combo.component.ts
+++ projects/igniteui-angular/src/lib/simple-combo/simple-combo.component.ts
@@ -116,6 +116,8 @@ export class IgxSimpleComboComponent extends IgxComboBaseDirective implements Co
 
     private _collapsing = false;
 
+    private _previousSelection = { selectedItem: '', selection: [] };
+
     /** @hidden @internal */
     public get filteredData(): any[] | null {
         return this._filteredData;
@@ -281,6 +283,9 @@ export class IgxSimpleComboComponent extends IgxComboBaseDirective implements Co
 
     /** @hidden @internal */
     public override handleInputChange(event?: any): void {
+        if (this.hasSelectedItem) {
+            this._previousSelection = { selectedItem: this.selectedItem, selection: this.selection };
+        }
         if (event !== undefined) {
             this.filterValue = this.searchValue = typeof event === 'string' ? event : event.target.value;
         }
@@ -469,6 +474,10 @@ export class IgxSimpleComboComponent extends IgxComboBaseDirective implements Co
         };
         if (args.newSelection !== args.oldSelection) {
             this.selectionChanging.emit(args);
+        } else if (this._updateInput && newSelection.size === 0 && this._previousSelection.selectedItem !== '') {
+            args.oldValue = this._previousSelection.selectedItem;
+            args.oldSelection = this._previousSelection.selection;
+            this.selectionChanging.emit(args);
         }
         // TODO: refactor below code as it sets the selection and the display text
         if (!args.cancel) {
@@ -487,7 +496,13 @@ export class IgxSimpleComboComponent extends IgxComboBaseDirective implements Co
             this._updateInput = true;
         } else if (this.isRemote) {
             this.registerRemoteEntries(newValueAsArray, false);
+        } else if (args.cancel) {
+            if (this._updateInput && this._previousSelection.selectedItem !== '') {
+                this.selectionService.select_items(this.id, [this._previousSelection.selectedItem], true);
+                this._value = [this._previousSelection.selectedItem];
+            }
         }
+        this._previousSelection = { selectedItem: '', selection: [] };
     }
 
     protected createDisplayText(newSelection: any[], oldSelection: any[]): string {
