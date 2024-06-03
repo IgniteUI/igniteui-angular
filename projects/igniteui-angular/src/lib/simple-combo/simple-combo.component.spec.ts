918263d10dc501702a4589713d60e9d7c57a9699
RivaIvanova <ivanova.riva@gmail.com>
2024-06-03 17:00:55 +0300

    test(simple-combo): use KeyboardEvent object

--- projects/igniteui-angular/src/lib/simple-combo/simple-combo.component.spec.ts
+++ projects/igniteui-angular/src/lib/simple-combo/simple-combo.component.spec.ts
@@ -1530,8 +1530,8 @@ describe('IgxSimpleCombo', () => {
         });
 
         it('should prevent Enter key default behavior when filtering data', fakeAsync(() => {
-            const component = fixture.componentInstance;
-            spyOn(component, 'onKeyDownEvent').and.callThrough();
+            const keyEvent = new KeyboardEvent('keydown', { key: 'Enter' });
+            const spy = spyOn(keyEvent, 'preventDefault');
 
             expect(combo.collapsed).toBe(true);
             expect(combo.selection).toBeUndefined();
@@ -1542,19 +1542,19 @@ describe('IgxSimpleCombo', () => {
 
             expect(combo.collapsed).toBe(false);
 
-            UIInteractions.triggerKeyDownEvtUponElem('Enter', input.nativeElement);
+            combo.handleKeyDown(keyEvent);
             tick();
             fixture.detectChanges();
 
             expect(combo.selection).toBeDefined();
             expect(combo.collapsed).toBe(true);
-            expect(component.onKeyDownEvent).not.toHaveBeenCalled();
+            expect(spy).toHaveBeenCalled();
 
-            UIInteractions.triggerKeyDownEvtUponElem('Enter', input.nativeElement);
+            combo.handleKeyDown(keyEvent);
             tick();
             fixture.detectChanges();
 
-            expect(component.onKeyDownEvent).toHaveBeenCalled();
+            expect(spy).toHaveBeenCalledTimes(1);
         }));
     });
 
@@ -2204,7 +2204,7 @@ describe('IgxSimpleCombo', () => {
     template: `
     <igx-simple-combo #combo [placeholder]="'Location'" [data]='items' [displayDensity]="density"
     [valueKey]="'field'" [groupKey]="'region'" [width]="'400px'" (selectionChanging)="selectionChanging($event)"
-    [allowCustomValues]="allowCustomValues" (keydown)="onKeyDownEvent()">
+    [allowCustomValues]="allowCustomValues">
         <ng-template igxComboItem let-display let-key="valueKey">
         <div class="state-card--simple">
         <span class="small-red-circle"></span>
@@ -2267,8 +2267,6 @@ class IgxSimpleComboSampleComponent {
 
     public selectionChanging() {
     }
-
-    public onKeyDownEvent() { }
 }
 
 @Component({
