c4176c7912e7d01cca6b314909651b2057cf8905
ddaribo <bozhidara.pp@gmail.com>
2024-06-27 10:47:04 +0300

    fix(combo): handle input focus when id is changed

--- projects/igniteui-angular/src/lib/combo/combo.common.ts
+++ projects/igniteui-angular/src/lib/combo/combo.common.ts
@@ -183,6 +183,10 @@ export abstract class IgxComboBaseDirective extends DisplayDensityBase implement
         if (this.dropdown.open) {
             this.dropdown.close();
         }
+        if (this.inputGroup?.isFocused) {
+            this.inputGroup.element.nativeElement.blur();
+            this.inputGroup.isFocused = false;
+        }
     }
 
     /**
