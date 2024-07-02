a2c706e44998c47fef7c0ffa52efaffbdd33cda9
Desislava Dincheva <34240583+ddincheva@users.noreply.github.com>
2024-07-02 17:05:28 +0300

    ESF on igx-grid does not reflect current filters in esf dialog --18.0.x (#14436)
    * feat(ESF): show the current filtering conditions in esf dialog
    
    * fix(ESF): return correctly if filtering is applied on a column #14188
    
    * feat(esf): selected style on filter condition & number of filter operands #14188
    
    * feat(*): show already applied filter operands in filter dialog #14188
    
    * feat(*): no emitted event on dd when change selected through API #14188
    
    * chore(*): fix linting error
    
    * chore(*): select drop down item without throwing selectionChanging event
    
    * feat(*): set bttn label from unique columns inst of num of filtering operands
    
    * chore(*): recalc the number of filtered columns only when filtering is changed
    
    * chore(*): address the review comments and remove the redundant code
    
    * chore(*): fix tests due to method signature change
    
    * chore(dropDown): return back the order of params for selectItem method
    
    * chore(*): remove unnecessary param from the tests
    
    * chore(*): address all the review comments
    
    * chore(*): add some ui test that verify number of filtered columns
    
    * chore(*): add test for selected state of filtered operand applied
    
    * chore(*): add more test to verify previous value is shown in esf dialog
    
    * chore(*): remove unnecessary test
    
    * chore(*): fix failing test
    
    * chore(*): remove leftover debugger
    
    * chore(*): set the name of the test properly
    
    * chore(*): address the styling changes regarding avd filter button border
    
    * chore(*): fix test expect
    
    * fix(*): optimize the logic around esf filtering show previous expr #14188
    
    * test(*): add more test when esf dialog is open from other filterOperand #14188
    
    ---------
    
    Co-authored-by: Deyvid Nenchev <DNenchev@infragistics.com>
    Co-authored-by: Hristo <hanastasov@infragistics.com>

--- projects/igniteui-angular/src/lib/drop-down/drop-down.component.ts
+++ projects/igniteui-angular/src/lib/drop-down/drop-down.component.ts
@@ -499,15 +499,16 @@ export class IgxDropDownComponent extends IgxDropDownBaseDirective implements ID
         }
     }
 
-    /**
+        /**
      * Handles the `selectionChanging` emit and the drop down toggle when selection changes
      *
      * @hidden
      * @internal
      * @param newSelection
      * @param event
+     * @param emit
      */
-    public override selectItem(newSelection?: IgxDropDownItemBaseDirective, event?: Event) {
+    public override selectItem(newSelection?: IgxDropDownItemBaseDirective, event?: Event, emit = true) {
         const oldSelection = this.selectedItem;
         if (!newSelection) {
             newSelection = this.focusedItem;
@@ -525,7 +526,10 @@ export class IgxDropDownComponent extends IgxDropDownBaseDirective implements ID
             } as IgxDropDownItemBaseDirective;
         }
         const args: ISelectionEventArgs = { oldSelection, newSelection, cancel: false, owner: this };
-        this.selectionChanging.emit(args);
+
+        if (emit) {
+            this.selectionChanging.emit(args);
+        }
 
         if (!args.cancel) {
             if (this.isSelectionValid(args.newSelection)) {
