f76085174fbba59914b83dd4a216e34cd3c644c8
Marina Stoyanova <m_stoianova_9@abv.bg>
2024-07-25 11:51:24 +0300

    test(*): fixing test in grid advanced filtering

--- projects/igniteui-angular/src/lib/grids/grid/grid-filtering-advanced.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid-filtering-advanced.spec.ts
@@ -708,12 +708,13 @@ describe('IgxGrid - Advanced Filtering #grid - ', () => {
             tick(100);
             fix.detectChanges();
 
-            // Ensure that filtering event was emitted with expected arguments
-            expect(grid.filtering.emit).toHaveBeenCalledWith(jasmine.objectContaining({
+            var expectedArgs: IFilteringEventArgs = {
                 owner: grid,
                 filteringExpressions: grid.advancedFilteringExpressionsTree,
                 cancel: false
-            }));
+            };
+            // Ensure that filtering event was emitted with expected arguments
+            expect(grid.filtering.emit).toHaveBeenCalledWith(jasmine.objectContaining(expectedArgs));
         }));
 
         it('Should cancel filtering if cancel is set to true.', fakeAsync(() => {
