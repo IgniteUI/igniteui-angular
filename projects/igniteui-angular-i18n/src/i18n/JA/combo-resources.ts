b61b6da00901c1619baa931c367a1d774c1c3eb9
MonikaKirkova <monika_kirkova@abv.bg>
2024-08-14 12:20:47 +0300

    fix(i18n): update Japanese resource strings

--- projects/igniteui-angular-i18n/src/i18n/JA/combo-resources.ts
+++ projects/igniteui-angular-i18n/src/i18n/JA/combo-resources.ts
@@ -3,8 +3,8 @@ import { IComboResourceStrings } from 'igniteui-angular';
 // exported below as re-cast to create declaration type with expanded properties
 const ComboResourceStringsJA_: ExpandRequire<IComboResourceStrings> = {
     igx_combo_empty_message: 'リストが空です',
-    igx_combo_enabledFilter_placeholder: '検索条件を入力',
-    igx_combo_addCustomValues_placeholder: 'アイテムを追加'
+    igx_combo_enabledFilter_placeholder: '検索条件の入力',
+    igx_combo_addCustomValues_placeholder: '項目の追加'
 };
 
 /**
