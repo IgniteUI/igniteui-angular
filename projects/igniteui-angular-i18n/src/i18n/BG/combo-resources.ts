2360ee33cf8ec418681390670b34bf175fb11ab5
Arkan Ahmedov <arkanahmedov02@gmail.com>
2024-08-15 17:06:54 +0300

    fix(i18n): updated resource strings

--- projects/igniteui-angular-i18n/src/i18n/BG/combo-resources.ts
+++ projects/igniteui-angular-i18n/src/i18n/BG/combo-resources.ts
@@ -2,7 +2,9 @@ import { IComboResourceStrings } from 'igniteui-angular';
 
 // exported below as re-cast to create declaration type with expanded properties
 const ComboResourceStringsBG_: ExpandRequire<IComboResourceStrings> = {
-    igx_combo_empty_message: 'Списъкът е празен'
+    igx_combo_empty_message: 'Списъкът е празен',
+    igx_combo_addCustomValues_placeholder: 'Добавете елемент',
+    igx_combo_clearItems_placeholder: 'Изчисти избора'
 };
 
 /**
--- projects/igniteui-angular-i18n/src/i18n/NB/combo-resources.ts
+++ projects/igniteui-angular-i18n/src/i18n/NB/combo-resources.ts
@@ -2,7 +2,9 @@ import { IComboResourceStrings } from 'igniteui-angular';
 
 // exported below as re-cast to create declaration type with expanded properties
 const ComboResourceStringsNB_: ExpandRequire<IComboResourceStrings> = {
-    igx_combo_empty_message: 'Listen er tom'
+    igx_combo_empty_message: 'Listen er tom',
+    igx_combo_addCustomValues_placeholder: 'Legg til element',
+    igx_combo_clearItems_placeholder: 'Tøm valg'
 };
 
 /**
