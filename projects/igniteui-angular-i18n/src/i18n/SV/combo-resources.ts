8f184fa875369dd5c21df0082d050a339c4c1e1a
Arkan Ahmedov <arkanahmedov02@gmail.com>
2024-08-15 17:06:54 +0300

    fix(i18n): updated resource strings

--- projects/igniteui-angular-i18n/src/i18n/SV/combo-resources.ts
+++ projects/igniteui-angular-i18n/src/i18n/SV/combo-resources.ts
@@ -2,7 +2,9 @@ import { IComboResourceStrings } from 'igniteui-angular';
 
 // exported below as re-cast to create declaration type with expanded properties
 const ComboResourceStringsSV_: ExpandRequire<IComboResourceStrings> = {
-    igx_combo_empty_message: 'Listan är tom'
+    igx_combo_empty_message: 'Listan är tom',
+    igx_combo_addCustomValues_placeholder: 'Lägg till objekt',
+    igx_combo_clearItems_placeholder: 'Rensa urval'
 };
 
 /**
