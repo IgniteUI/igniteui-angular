1a1df9aa9b81a85f92681cbf549ca568005cbb1a
igdmdimitrov <dmdimitrov@infragistics.com>
2024-08-23 15:05:40 +0300

    feat(query-builder): move icon service back to query builder

--- projects/igniteui-angular/src/lib/query-builder/query-builder.component.ts
+++ projects/igniteui-angular/src/lib/query-builder/query-builder.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ContentChild, EventEmitter, Output } from '@angular/core';
+import { ContentChild, EventEmitter, Output } from '@angular/core';
 import { NgIf} from '@angular/common';
 import {
     Component, Input, ViewChild, ElementRef, OnDestroy, HostBinding
@@ -11,6 +11,8 @@ import { EntityType } from '../grids/common/grid.interface';
 import { IgxQueryBuilderHeaderComponent } from './query-builder-header.component';
 import { getCurrentResourceStrings } from '../core/i18n/resources';
 import { IgxQueryBuilderTreeComponent } from './query-builder-tree.component';
+import { IgxIconService } from '../icon/icon.service';
+import { editor } from '@igniteui/material-icons-extended';
 
 /**
  * A component used for operating with complex filters by creating or editing conditions
@@ -29,7 +31,7 @@ import { IgxQueryBuilderTreeComponent } from './query-builder-tree.component';
     standalone: true,
     imports: [NgIf, IgxQueryBuilderHeaderComponent, IgxQueryBuilderTreeComponent]
 })
-export class IgxQueryBuilderComponent implements AfterViewInit, OnDestroy {
+export class IgxQueryBuilderComponent implements OnDestroy {
     /**
      * @hidden @internal
      */
@@ -121,11 +123,8 @@ export class IgxQueryBuilderComponent implements AfterViewInit, OnDestroy {
     private destroy$ = new Subject<any>();
     private _resourceStrings = getCurrentResourceStrings(QueryBuilderResourceStringsEN);
 
-    /**
-     * @hidden @internal
-     */
-    public ngAfterViewInit(): void {
-        // this._overlaySettings.outlet = this.overlayOutlet;
+    constructor(protected iconService: IgxIconService) {
+        this.registerSVGIcons();
     }
 
     /**
@@ -182,6 +181,67 @@ export class IgxQueryBuilderComponent implements AfterViewInit, OnDestroy {
     public onExpressionTreeChange(tree: IExpressionTree) {
         this.expressionTree = tree;
         this.expressionTreeChange.emit();
+    }
+
+    private registerSVGIcons(): void {
+        const editorIcons = editor as any[];
+
+        editorIcons.forEach((icon) => {
+            this.iconService.addSvgIconFromText(icon.name, icon.value, 'imx-icons');
+            this.iconService.addIconRef(icon.name, 'default', {
+                name: icon.name,
+                family: 'imx-icons'
+            });
+        });
+
+        const inIcon = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M560-280H120v-400h720v120h-80v-40H200v240h360v80Zm-360-80v-240 240Zm560 200v-120H640v-80h120v-120h80v120h120v80H840v120h-80Z"/></svg>';
+        this.iconService.addSvgIconFromText('in', inIcon, 'imx-icons');
+        this.iconService.addIconRef('in', 'default', {
+            name: 'in',
+            family: 'imx-icons'
+        });
+
+        const notInIcon = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M560-280H120v-400h720v120h-80v-40H200v240h360v80Zm-360-80v-240 240Zm440 104 84-84-84-84 56-56 84 84 84-84 56 56-83 84 83 84-56 56-84-83-84 83-56-56Z"/></svg>';
+        this.iconService.addSvgIconFromText('not-in', notInIcon, 'imx-icons');
+        this.iconService.addIconRef('not-in', 'default', {
+            name: 'not-in',
+            family: 'imx-icons'
+        });
+
+        this.iconService.addIconRef('add', 'default', {
+            name: 'add',
+            family: 'material',
+        });
+
+        this.iconService.addIconRef('close', 'default', {
+            name: 'close',
+            family: 'material',
+        });
+
+        this.iconService.addIconRef('check', 'default', {
+            name: 'check',
+            family: 'material',
+        });
+
+        this.iconService.addIconRef('delete', 'default', {
+            name: 'delete',
+            family: 'material',
+        });
+
+        this.iconService.addIconRef('edit', 'default', {
+            name: 'edit',
+            family: 'material',
+        });
+
+        this.iconService.addIconRef('unfold_less', 'default', {
+            name: 'unfold_less',
+            family: 'material',
+        });
+
+        this.iconService.addIconRef('unfold_more', 'default', {
+            name: 'unfold_more',
+            family: 'material',
+        });
     }
 }
 
