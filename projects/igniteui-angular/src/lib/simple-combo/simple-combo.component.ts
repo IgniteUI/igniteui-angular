ecb4fdd99137d8949cd33f15da5aca2d54777606
Georgi Anastasov <ggeorgianastasov@gmail.com>
2024-08-27 15:28:06 +0300

    fix(simple-combo): refine isValid method for better validation handling

--- projects/igniteui-angular/src/lib/simple-combo/simple-combo.component.ts
+++ projects/igniteui-angular/src/lib/simple-combo/simple-combo.component.ts
@@ -3,7 +3,7 @@ import {
     AfterViewInit, ChangeDetectorRef, Component, DoCheck, ElementRef, EventEmitter, HostListener, Inject, Injector,
     Optional, Output, ViewChild
 } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ControlValueAccessor, FormGroupDirective, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { takeUntil } from 'rxjs/operators';
 
 import { IgxComboAddItemComponent } from '../combo/combo-add-item.component';
@@ -150,7 +150,8 @@ export class IgxSimpleComboComponent extends IgxComboBaseDirective implements Co
         private platformUtil: PlatformUtil,
         @Optional() @Inject(DisplayDensityToken) _displayDensityOptions: IDisplayDensityOptions,
         @Optional() @Inject(IGX_INPUT_GROUP_TYPE) _inputGroupType: IgxInputGroupType,
-        @Optional() _injector: Injector) {
+        @Optional() _injector: Injector,
+        @Optional() private formGroupDirective?: FormGroupDirective) {
         super(elementRef, cdr, selectionService, comboAPI,
             _iconService, _displayDensityOptions, _inputGroupType, _injector);
         this.comboAPI.register(this);
@@ -600,10 +601,14 @@ export class IgxSimpleComboComponent extends IgxComboBaseDirective implements Co
     }
 
     private isValid(value: any): boolean {
-        if (this.ngControl || this.required) {
-            return value !== null && value !== '' && value !== undefined;
-        } else {
-            return value !== undefined;
+        if (this.formGroupDirective && value === null) {
+            return false;
         }
+
+        if (this.required) {
+            return value !== null && value !== '' && value !== undefined
+        }
+
+        return value !== undefined;
     }
 }
