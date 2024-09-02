215e4ec6da5af8959e1a6a4c5bfd5691dd5b00fb
Rumyana Andriova <54146583+randriova@users.noreply.github.com>
2024-09-02 14:12:06 +0300

    fix(i18n): localization updates

--- projects/igniteui-angular-i18n/src/i18n/BG/combo-resources.ts
+++ projects/igniteui-angular-i18n/src/i18n/BG/combo-resources.ts
@@ -3,8 +3,8 @@ import { IComboResourceStrings } from 'igniteui-angular';
 // exported below as re-cast to create declaration type with expanded properties
 const ComboResourceStringsBG_: ExpandRequire<IComboResourceStrings> = {
     igx_combo_empty_message: 'Списъкът е празен',
-    igx_combo_addCustomValues_placeholder: 'Добавете елемент',
-    igx_combo_clearItems_placeholder: 'Изчисти избора'
+    igx_combo_addCustomValues_placeholder: 'Добавяне на елемент',
+    igx_combo_clearItems_placeholder: 'Изчистване на избора'
 };
 
 /**
--- projects/igniteui-angular-i18n/src/i18n/DA/combo-resources.ts
+++ projects/igniteui-angular-i18n/src/i18n/DA/combo-resources.ts
@@ -3,7 +3,7 @@ import { IComboResourceStrings } from 'igniteui-angular';
 // exported below as re-cast to create declaration type with expanded properties
 const ComboResourceStringsDA_: ExpandRequire<IComboResourceStrings> = {
     igx_combo_empty_message: 'Listen er tom',
-    igx_combo_addCustomValues_placeholder: 'Tilføj emne',
+    igx_combo_addCustomValues_placeholder: 'Tilføj element',
     igx_combo_clearItems_placeholder: 'Ryd markering'
 };
 
