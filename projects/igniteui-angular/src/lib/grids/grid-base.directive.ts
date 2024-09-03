f242843882a2ccb0fa258b95777ab7c336324006
Robert Stoffers <rstoffers@infragistics.com>
2024-09-03 16:25:08 -0400

    fix(grid): add missing attribute for selectedrows prop

--- projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
@@ -2455,6 +2455,7 @@ export abstract class IgxGridBaseDirective implements GridType,
     /* blazorByValueArray */
     /* blazorAlwaysWriteback */
     /* @tsTwoWayProperty (true, "RowSelectionChanging", "Detail.NewSelection", false) */
+    /* blazorPrimitiveValue */
     /**
      * Gets/Sets the current selection state.
      *
