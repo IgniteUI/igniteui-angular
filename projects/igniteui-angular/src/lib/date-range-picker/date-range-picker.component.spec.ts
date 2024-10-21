d4c1c27948ff721b16de3da3eea077a324823df0
Damyan Petev <damyanpetev@users.noreply.github.com>
2024-10-21 19:11:39 +0300

    fix(date-range-picker): move focus to calendar wrapper on open

--- projects/igniteui-angular/src/lib/date-range-picker/date-range-picker.component.spec.ts
+++ projects/igniteui-angular/src/lib/date-range-picker/date-range-picker.component.spec.ts
@@ -644,13 +644,20 @@ describe('IgxDateRangePicker', () => {
                     expect(dateRange.opening.emit).toHaveBeenCalledTimes(1);
                     expect(dateRange.opened.emit).toHaveBeenCalledTimes(1);
 
-                    calendar = document.getElementsByClassName(CSS_CLASS_CALENDAR)[0];
-                    UIInteractions.triggerKeyDownEvtUponElem('ArrowUp', calendar, true, true);
+                    const calendarWrapper = fixture.debugElement.query(By.css('.igx-calendar__wrapper')).nativeElement;
+                    expect(calendarWrapper.contains(document.activeElement))
+                        .withContext('focus should move to calendar for KB nav')
+                        .toBeTrue();
+
+                    UIInteractions.triggerKeyDownEvtUponElem('ArrowUp', calendarWrapper, true, true);
                     tick();
                     fixture.detectChanges();
                     expect(dateRange.collapsed).toBeTruthy();
                     expect(dateRange.closing.emit).toHaveBeenCalledTimes(1);
                     expect(dateRange.closed.emit).toHaveBeenCalledTimes(1);
+                    expect(dateRange.inputDirective.nativeElement.contains(document.activeElement))
+                        .withContext('focus should return to the picker input')
+                        .toBeTrue();
                 }));
 
                 it('should close the calendar with ESC', fakeAsync(() => {
@@ -1048,6 +1055,8 @@ describe('IgxDateRangePicker', () => {
                     spyOn(dateRange.closed, 'emit').and.callThrough();
 
                     expect(dateRange.collapsed).toBeTruthy();
+                    startInput.nativeElement.focus();
+                    tick();
                     const range = fixture.debugElement.query(By.css(CSS_CLASS_DATE_RANGE));
                     UIInteractions.triggerEventHandlerKeyDown('ArrowDown', range, true);
                     tick(DEBOUNCE_TIME * 2);
@@ -1056,13 +1065,20 @@ describe('IgxDateRangePicker', () => {
                     expect(dateRange.opening.emit).toHaveBeenCalledTimes(1);
                     expect(dateRange.opened.emit).toHaveBeenCalledTimes(1);
 
-                    calendar = document.getElementsByClassName(CSS_CLASS_CALENDAR)[0];
-                    UIInteractions.triggerKeyDownEvtUponElem('ArrowUp', calendar, true, true);
+                    const calendarWrapper = document.getElementsByClassName('igx-calendar__wrapper')[0];
+                    expect(calendarWrapper.contains(document.activeElement))
+                        .withContext('focus should move to calendar for KB nav')
+                        .toBeTrue();
+
+                    UIInteractions.triggerKeyDownEvtUponElem('ArrowUp', calendarWrapper, true, true);
                     tick();
                     fixture.detectChanges();
                     expect(dateRange.collapsed).toBeTruthy();
                     expect(dateRange.closing.emit).toHaveBeenCalledTimes(1);
                     expect(dateRange.closed.emit).toHaveBeenCalledTimes(1);
+                    expect(startInput.nativeElement.contains(document.activeElement))
+                        .withContext('focus should return to the picker input')
+                        .toBeTrue();
                 }));
 
                 it('should toggle the calendar with ALT + DOWN/UP ARROW key - dialog mode', fakeAsync(() => {
