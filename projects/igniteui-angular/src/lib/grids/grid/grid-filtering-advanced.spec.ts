3436fb1d266db1b03ef2d0dec5ce7b26ed2146f4
Georgi Anastasov <ggeorgianastasov@gmail.com>
2024-10-25 17:03:30 +0300

    chore(query-builder): remove empty lines

--- projects/igniteui-angular/src/lib/grids/grid/grid-filtering-advanced.spec.ts
+++ projects/igniteui-angular/src/lib/grids/grid/grid-filtering-advanced.spec.ts
@@ -3058,7 +3058,6 @@ describe('IgxGrid - Advanced Filtering #grid - ', () => {
                 });
                 grid.advancedFilteringExpressionsTree = tree;
                 fix.detectChanges();
-
                 // Open Advanced Filtering dialog.
                 grid.openAdvancedFilteringDialog();
                 fix.detectChanges();
@@ -3239,7 +3238,6 @@ describe('IgxGrid - Advanced Filtering #grid - ', () => {
             tree.filteringOperands.push({
                 fieldName: 'ProductName', searchVal: 'a', condition: IgxStringFilteringOperand.instance().condition('contains')
             });
-
             grid.advancedFilteringExpressionsTree = tree;
             fix.detectChanges();
 
