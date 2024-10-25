915f0eb48ba61e26d5d90ff50c9ed034cb13bae8
Georgi Anastasov <ggeorgianastasov@gmail.com>
2024-10-25 16:39:23 +0300

    fix(query-builder): clear input when selecting not support values condition

--- projects/igniteui-angular/src/lib/query-builder/query-builder.component.ts
+++ projects/igniteui-angular/src/lib/query-builder/query-builder.component.ts
@@ -900,6 +900,18 @@ export class IgxQueryBuilderComponent implements AfterViewInit, OnDestroy {
         }
     }
 
+    /**
+     * @hidden @internal
+     */
+    public onConditionChange() {
+        if (this.selectedCondition && this.selectedField) {
+            const condition = this.selectedField.filters.condition(this.selectedCondition);
+            if (condition.isUnary) {
+                this.searchValue = null;
+            }
+        }
+    }
+
     private setFormat(field: FieldType) {
         if (!field.pipeArgs) {
             field.pipeArgs = { digitsInfo: DEFAULT_PIPE_DIGITS_INFO };
