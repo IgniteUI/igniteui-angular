81916da7b938ce70fa3c99bf889d1cf235ab1387
igdmdimitrov <49060557+igdmdimitrov@users.noreply.github.com>
2024-10-29 12:13:15 +0200

    feat(query-builder): add public api for commit and discard current state (#14910)
    * feat(query-builder): add public api for commit and discard current state
    * chore(*): update changelog
    ---------
    
    Co-authored-by: teodosia <teodosiah@gmail.com>

--- projects/igniteui-angular/src/lib/query-builder/query-builder-tree.component.ts
+++ projects/igniteui-angular/src/lib/query-builder/query-builder-tree.component.ts
@@ -747,20 +747,22 @@ export class IgxQueryBuilderTreeComponent implements AfterViewInit, OnDestroy {
 
             const innerQuery = this.innerQueries.filter(q => q.isInEditMode())[0]
             if (innerQuery && this.selectedField?.filters?.condition(this.selectedCondition)?.isNestedQuery) {
-                if (!this._editedExpression.expression.searchTree) {
+                if (!this._editedExpression.expression.searchTree && innerQuery.expressionTree) {
                     this._editedExpression.expression.searchTree = new FilteringExpressionsTree(innerQuery.expressionTree.operator);
                 }
 
-                this._editedExpression.expression.searchTree.entity = innerQuery.selectedEntity.name;
-                this._editedExpression.expression.searchTree.returnFields = innerQuery.selectedReturnFields;
-                this._editedExpression.expression.searchTree.filteringOperands = innerQuery.expressionTree.filteringOperands;
-                this._editedExpression.expression.searchTree.operator = innerQuery.expressionTree.operator;
-                this._editedExpression.expression.searchTree.fieldName = innerQuery.expressionTree.fieldName;
+                if (this._editedExpression.expression.searchTree) {
+                    this._editedExpression.expression.searchTree.entity = innerQuery.selectedEntity.name;
+                    this._editedExpression.expression.searchTree.returnFields = innerQuery.selectedReturnFields;
+                    this._editedExpression.expression.searchTree.filteringOperands = innerQuery.expressionTree?.filteringOperands;
+                    this._editedExpression.expression.searchTree.operator = innerQuery.expressionTree?.operator;
+                    this._editedExpression.expression.searchTree.fieldName = innerQuery.expressionTree?.fieldName;
+                }
             } else {
                 this._editedExpression.expression.searchTree = null;
             }
 
-            if (this.selectedField.filters.condition(this.selectedCondition).isUnary) {
+            if (this.selectedField.filters.condition(this.selectedCondition)?.isUnary) {
                 this._editedExpression.expression.searchVal = null;
             }
 
@@ -816,14 +818,50 @@ export class IgxQueryBuilderTreeComponent implements AfterViewInit, OnDestroy {
         return this.selectedField && this.selectedCondition &&
             (
                 (
-                    (!!this.searchValue.value || (!!this.searchValueTemplate && !!this._editedExpression.expression.searchVal)) &&
+                    ((!Array.isArray(this.searchValue.value) && !!this.searchValue.value) || (Array.isArray(this.searchValue.value) && this.searchValue.value.length !== 0)) &&
                     !(this.selectedField?.filters?.condition(this.selectedCondition)?.isNestedQuery)
                 ) ||
                 (
-                    innerQuery && !!innerQuery.expressionTree && innerQuery._editedExpression == undefined && innerQuery.selectedReturnFields.length > 0
+                    this.selectedField?.filters?.condition(this.selectedCondition)?.isNestedQuery && innerQuery && !!innerQuery.expressionTree && innerQuery._editedExpression == undefined && innerQuery.selectedReturnFields.length > 0
                 ) ||
-                this.selectedField.filters.condition(this.selectedCondition).isUnary
+                this.selectedField.filters.condition(this.selectedCondition)?.isUnary
             );
+    }
+
+    /**
+     * @hidden @internal
+     */
+    public canCommitCurrentState(): boolean {
+        const innerQuery = this.innerQueries.filter(q => q.isInEditMode())[0];
+        if (innerQuery) {
+            return this.selectedReturnFields?.length > 0 && innerQuery.canCommitCurrentState();
+        } else {
+            return this.selectedReturnFields?.length > 0 &&
+                (
+                    (!this._editedExpression && !!this.rootGroup) || // no edited expr, root group
+                    (this._editedExpression && !this.selectedField && (this.expressionTree && this.expressionTree.filteringOperands[0] !== this._editedExpression.expression)) || // empty edited expr with at least one other expr
+                    (this._editedExpression && this.operandCanBeCommitted() === true) // valid edited expr
+                );
+        }
+    }
+
+    /**
+     * @hidden @internal
+     */
+    public commitCurrentState(): void {
+        const innerQuery = this.innerQueries.filter(q => q.isInEditMode())[0];
+        if (innerQuery) {
+            innerQuery.commitCurrentState();
+        }
+
+        if (this._editedExpression) {
+            if (this.selectedField) {
+                this.commitOperandEdit();
+            } else {
+                this.deleteItem(this._editedExpression);
+                this._editedExpression = null;
+            }
+        }
     }
 
     /**
@@ -931,7 +969,7 @@ export class IgxQueryBuilderTreeComponent implements AfterViewInit, OnDestroy {
 
         if (!this.selectedField) {
             this.fieldSelect.input.nativeElement.focus();
-        } else if (this.selectedField.filters.condition(this.selectedCondition).isUnary) {
+        } else if (this.selectedField.filters.condition(this.selectedCondition)?.isUnary) {
             this.conditionSelect.input.nativeElement.focus();
         } else {
             const input = this.searchValueInput?.nativeElement || this.picker?.getEditElement();
@@ -1335,6 +1373,9 @@ export class IgxQueryBuilderTreeComponent implements AfterViewInit, OnDestroy {
         let groupItem: ExpressionGroupItem;
         if (expressionTree) {
             groupItem = new ExpressionGroupItem(expressionTree.operator, parent);
+            if (!expressionTree.filteringOperands) {
+                return groupItem;
+            }
 
             for (const expr of expressionTree.filteringOperands) {
                 if (expr instanceof FilteringExpressionsTree) {
