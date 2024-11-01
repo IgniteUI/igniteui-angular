897110f3e072bd84f689125f4e1cf55592b6d699
Martin Dragnev <mddragnev@gmail.com>
2024-11-01 16:31:01 +0200

    chore(*): fix tests

--- projects/igniteui-angular/src/lib/test-utils/grid-functions.spec.ts
+++ projects/igniteui-angular/src/lib/test-utils/grid-functions.spec.ts
@@ -2055,7 +2055,7 @@ export class GridFunctions {
                     }
                 }
                 const expectedWidth = Math.max(parseFloat(cell.column.calcWidth) * cell.column.gridColumnSpan, sum);
-                expect(cellElem.clientWidth - expectedWidth).toBeLessThan(1);
+                expect(cellElem.getBoundingClientRect().width - expectedWidth).toBeLessThan(1);
                 // check height
                 const expectedHeight = cell.grid.rowHeight * cell.gridRowSpan;
                 expect(cellElem.offsetHeight).toBe(expectedHeight);
