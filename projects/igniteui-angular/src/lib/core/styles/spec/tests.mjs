c68971b732a6903ebb420bd5d6b6689ff16e4d8b
jackofdiamond5 <jackofdiamond596@gmail.com>
2024-11-13 16:49:33 +0200

    refactor(styles): rename core/styles/tests.ts to core/styles/tests.mjs
    - without additional setup loader.js fails to load the file if it is test.mts

--- projects/igniteui-angular/src/lib/core/styles/spec/tests.mjs
+++ projects/igniteui-angular/src/lib/core/styles/spec/tests.mjs
@@ -1,6 +1,8 @@
 import * as path from 'path';
 import * as sassTrue from 'sass-true';
+import { fileURLToPath } from "url";
 import {} from 'jasmine';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const file = path.join(__dirname, '_index.scss');
 sassTrue.runSass({ file, includePaths: ['node_modules'] }, { describe, it });
