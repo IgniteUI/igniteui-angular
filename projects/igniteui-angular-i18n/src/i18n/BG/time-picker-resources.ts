954f8a1ba6c49f70a36f69611d5500827656b96e
Damyan Petev <damyanpetev@users.noreply.github.com>
2024-11-14 16:45:02 +0200

    refactor(i18n): satisfies operator to validate resource string objects (#14781)
    * refactor(i18n): use satisfies operator to validate resource string objects
    
    * refactor(i18n): simplify required util type w/o obsolete expansion
    
    * test(i18n): fix eslint i18n validation command
    
    ---------
    
    Co-authored-by: Nikolay Alipiev <nikolay.alipiev@gmail.com>
    Co-authored-by: Konstantin Dinev <kdinev@mail.bw.edu>
    Co-authored-by: Boris Penkov <jackofdiamond596@gmail.com>

--- projects/igniteui-angular-i18n/src/i18n/BG/time-picker-resources.ts
+++ projects/igniteui-angular-i18n/src/i18n/BG/time-picker-resources.ts
@@ -1,15 +1,12 @@
 import { ITimePickerResourceStrings } from 'igniteui-angular';
 
-// exported below as re-cast to create declaration type with expanded properties
-const TimePickerResourceStringsBG_: ExpandRequire<ITimePickerResourceStrings> = {
+// only use `satisfies` operator so export is typed by its schema
+/**
+ * Bulgarian resource strings for IgxTimePicker
+ */
+export const TimePickerResourceStringsBG = {
     igx_time_picker_ok: 'ОК',
     igx_time_picker_cancel: 'Отмяна',
     igx_time_picker_change_time: 'Промяна на времето',
     igx_time_picker_choose_time: 'Избор на време'
-};
-
-
-/**
- * Bulgarian resource strings for IgxTimePicker
- */
-export const TimePickerResourceStringsBG = TimePickerResourceStringsBG_ as ExpandRequire<ITimePickerResourceStrings>;
+} satisfies MakeRequired<ITimePickerResourceStrings>;
