3d7b276273a12ee8669b5d5a86ee9d57ece688ca
RivaIvanova <ivanova.riva@gmail.com>
2024-11-27 08:58:30 +0200

    chore(carousel): change deprecated version

--- projects/igniteui-angular/src/lib/carousel/enums.ts
+++ projects/igniteui-angular/src/lib/carousel/enums.ts
@@ -9,11 +9,11 @@ export type CarouselAnimationType = (typeof CarouselAnimationType)[keyof typeof 
 
 export const CarouselIndicatorsOrientation = /*@__PURE__*/mkenum({
     /**
-     * @deprecated in version 19.0.0. Use `end` instead.
+     * @deprecated in version 19.1.0. Use `end` instead.
      */
     bottom: 'bottom',
     /**
-     * @deprecated in version 19.0.0. Use `start` instead.
+     * @deprecated in version 19.1.0. Use `start` instead.
      */
     top: 'top',
     start: 'start',
