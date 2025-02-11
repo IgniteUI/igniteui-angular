d58883fc51550af20f349af483f06d3521274508
Dilyana Yarabanova <45598235+didimmova@users.noreply.github.com>
2025-02-11 14:43:38 +0200

    fix(combo): fix combo search input to always be line and fix componen… (#15323)

--- projects/igniteui-angular/src/lib/input-group/input-group.component.ts
+++ projects/igniteui-angular/src/lib/input-group/input-group.component.ts
@@ -1,6 +1,5 @@
 import { DOCUMENT, NgIf, NgTemplateOutlet, NgClass, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import {
-    AfterViewInit,
     ChangeDetectorRef,
     Component,
     ContentChild,
@@ -35,7 +34,7 @@ import { IgxTheme, THEME_TOKEN, ThemeToken } from '../services/theme/theme.token
     providers: [{ provide: IgxInputGroupBase, useExisting: IgxInputGroupComponent }],
     imports: [NgIf, NgTemplateOutlet, IgxPrefixDirective, IgxButtonDirective, NgClass, IgxSuffixDirective, IgxIconComponent, NgSwitch, NgSwitchCase, NgSwitchDefault]
 })
-export class IgxInputGroupComponent implements IgxInputGroupBase, AfterViewInit {
+export class IgxInputGroupComponent implements IgxInputGroupBase {
     /**
      * Sets the resource strings.
      * By default it uses EN resources.
@@ -455,7 +454,7 @@ export class IgxInputGroupComponent implements IgxInputGroupBase, AfterViewInit 
     }
 
     /** @hidden @internal */
-    public ngAfterViewInit() {
+    public ngAfterContentChecked() {
         this.setComponentTheme();
     }
 }
