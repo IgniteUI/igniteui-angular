875f6b9a0c89a005342b6f3da7cfd7e525bc63cb
Pablo <pmoleri@infragistics.com>
2025-02-25 10:36:31 -0300

    fix(leak): unsubscribe was not unsubscribing

--- src/app/card/card.sample.ts
+++ src/app/card/card.sample.ts
@@ -162,14 +162,14 @@ export class CardSampleComponent implements OnInit {
     ) {
         this.propertyChangeService.setPanelConfig(this.panelConfig);
 
-        const { unsubscribe } =
+        const propertyChange =
             this.propertyChangeService.propertyChanges.subscribe(
                 (properties) => {
                     this.properties = properties;
                 }
             );
 
-        this.destroyRef.onDestroy(() => unsubscribe);
+        this.destroyRef.onDestroy(() => propertyChange.unsubscribe());
     }
 
     public ngOnInit() {
--- src/app/input-controls/input-controls.sample.ts
+++ src/app/input-controls/input-controls.sample.ts
@@ -107,14 +107,14 @@ export class InputControlsSampleComponent {
     ) {
         this.propertyChangeService.setPanelConfig(this.panelConfig);
 
-        const { unsubscribe } =
+        const propertyChange =
             this.propertyChangeService.propertyChanges.subscribe(
                 (properties) => {
                     this.properties = properties;
                 }
             );
 
-        this.destroyRef.onDestroy(() => unsubscribe);
+        this.destroyRef.onDestroy(() => propertyChange.unsubscribe());
     }
 
     private alignmentMap = new Map<string, RadioGroupAlignment>([
