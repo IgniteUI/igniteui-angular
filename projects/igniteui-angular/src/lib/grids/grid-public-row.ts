175e9d0f8e20570b7f92a2dd83f59508d5b52af8
ttonev <ttonev@infragistics.com>
2025-02-26 18:37:54 +0200

    fix(grids): Updated IgxPivotGridRow

--- projects/igniteui-angular/src/lib/grids/grid-public-row.ts
+++ projects/igniteui-angular/src/lib/grids/grid-public-row.ts
@@ -792,27 +792,70 @@ export class IgxSummaryRow implements RowType {
     }
 }
 
-export class IgxPivotGridRow extends BaseRow {
-   /**
-     * @hidden
+export class IgxPivotGridRow implements RowType {
+
+    /** The index of the row within the grid */
+    public index: number;
+
+    /**
+     * The grid that contains the row.
      */
-    constructor(
-        public override grid: GridType,
-        public override index: number, data?: any
-    ) {
-        super();
+    public grid: GridType;
+    private _data?: any;
+
+    constructor(grid: GridType, index: number, data?: any) {
+        this.grid = grid;
+        this.index = index;
         this._data = data && data.addRow && data.recordRef ? data.recordRef : data;
     }
 
     /**
-     * Gets the rendered cells in the row component.
+     *  The data passed to the row component.
      */
-    public override get cells(): CellType[] {
-        const res: CellType[] = [];
-        this.grid.columns.forEach(col => {
-            const cell: CellType = new IgxGridCell(this.grid, this.index, col);
-            res.push(cell);
-        });
-        return res;
+    public get data(): any {
+        return this._data ?? this.grid.dataView[this.index];
+    }
+
+    /**
+     * Returns the view index calculated per the grid page.
+     */
+    public get viewIndex(): number {
+        return this.index + this.grid.page * this.grid.perPage;
+    }
+
+    /**
+     * Gets the row key.
+     * A row in the grid is identified either by:
+     * - primaryKey data value,
+     * - the whole rowData, if the primaryKey is omitted.
+     *
+     * ```typescript
+     * let rowKey = row.key;
+     * ```
+     */
+    public get key(): any {
+        const data = this._data ?? this.grid.dataView[this.index];
+        const primaryKey = this.grid.primaryKey;
+        return primaryKey ? data[primaryKey] : data;
+    }
+
+    /**
+     * Gets whether the row is selected.
+     * Default value is `false`.
+     * ```typescript
+     * row.selected = true;
+     * ```
+     */
+    public get selected(): boolean {
+        return this.grid.selectionService.isRowSelected(this.key);
+    }
+
+    public set selected(val: boolean) {
+        if (val) {
+            this.grid.selectionService.selectRowsWithNoEvent([this.key]);
+        } else {
+            this.grid.selectionService.deselectRowsWithNoEvent([this.key]);
+        }
+        this.grid.cdr.markForCheck();
     }
 }
