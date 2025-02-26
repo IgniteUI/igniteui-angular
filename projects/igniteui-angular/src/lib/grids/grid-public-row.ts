27b3da691a03a51d8254ee00934b22536179375a
ttonev <ttonev@infragistics.com>
2025-02-26 16:05:07 +0200

    fix(grid): added comments to public properties

--- projects/igniteui-angular/src/lib/grids/grid-public-row.ts
+++ projects/igniteui-angular/src/lib/grids/grid-public-row.ts
@@ -793,7 +793,13 @@ export class IgxSummaryRow implements RowType {
 }
 
 export class IgxPivotGridRow implements RowType {
+
+    /** The index of the row within the grid */
     public index: number;
+
+    /**
+     * The grid that contains the row.
+     */
     public grid: GridType;
     private _data?: any;
 
@@ -803,20 +809,43 @@ export class IgxPivotGridRow implements RowType {
         this._data = data && data.addRow && data.recordRef ? data.recordRef : data;
     }
 
+    /**
+     *  The data passed to the row component.
+     */
     public get data(): any {
         return this._data ?? this.grid.dataView[this.index];
     }
 
+    /**
+     * Returns the view index calculated per the grid page.
+     */
     public get viewIndex(): number {
         return this.index + this.grid.page * this.grid.perPage;
     }
 
+    /**
+     * Gets the row key.
+     * A row in the grid is identified either by:
+     * - primaryKey data value,
+     * - the whole rowData, if the primaryKey is omitted.
+     *
+     * ```typescript
+     * let rowKey = row.key;
+     * ```
+     */
     public get key(): any {
         const data = this._data ?? this.grid.dataView[this.index];
         const primaryKey = this.grid.primaryKey;
         return primaryKey ? data[primaryKey] : data;
     }
 
+    /**
+     * Gets whether the row is selected.
+     * Default value is `false`.
+     * ```typescript
+     * row.selected = true;
+     * ```
+     */
     public get selected(): boolean {
         return this.grid.selectionService.isRowSelected(this.key);
     }
