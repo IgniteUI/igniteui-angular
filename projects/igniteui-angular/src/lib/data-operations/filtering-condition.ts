d621b6945e05383e0316b5e0f6c3930cb46f3ebd
Damyan Petev <damyanpetev@users.noreply.github.com>
2025-03-04 19:14:27 +0200

    chore(filter-operands): restore findValueInSet as protected

--- projects/igniteui-angular/src/lib/data-operations/filtering-condition.ts
+++ projects/igniteui-angular/src/lib/data-operations/filtering-condition.ts
@@ -48,7 +48,7 @@ export class IgxFilteringOperand {
      * Returns an array of names of the conditions which are visible in the filtering UI
      */
     public conditionList(): string[] {
-        return this.operations.filter(f => !f.hidden && !f.isNestedQuery).map((element) => element.name);    
+        return this.operations.filter(f => !f.hidden && !f.isNestedQuery).map((element) => element.name);
     }
 
     /**
@@ -76,10 +76,7 @@ export class IgxFilteringOperand {
         this.operations.push(operation);
     }
 
-    /**
-     * @hidden
-     */
-    public findValueInSet(target: any, searchVal: Set<any>) {
+    protected findValueInSet(target: any, searchVal: Set<any>) {
         return searchVal.has(target);
     }
 }
@@ -119,7 +116,7 @@ export class IgxBooleanFilteringOperand extends IgxFilteringOperand {
             iconName: 'filter_not_empty',
             logic: (target: boolean) => target !== null && target !== undefined
         }];
-        
+
         this.operations = newOperations.concat(this.operations);
     }
 }
@@ -143,7 +140,7 @@ class IgxBaseDateTimeFilteringOperand extends IgxFilteringOperand {
             iconName: 'filter_not_empty',
             logic: (target: Date) => target !== null && target !== undefined
         }];
-        
+
         this.operations = newOperations.concat(this.operations);
     }
 
@@ -189,10 +186,7 @@ class IgxBaseDateTimeFilteringOperand extends IgxFilteringOperand {
         return res;
     }
 
-    /**
-     * @hidden
-     */
-    public override findValueInSet(target: any, searchVal: Set<any>) {
+    protected override findValueInSet(target: any, searchVal: Set<any>) {
         if (!target) {
             return false;
         }
@@ -416,11 +410,11 @@ export class IgxDateFilteringOperand extends IgxBaseDateTimeFilteringOperand {
                 return d.year === now.year + 1;
             }
         }];
-        
+
         this.operations = newOperations.concat(this.operations);
     }
 
-    public override findValueInSet(target: any, searchVal: Set<any>) {
+    protected override findValueInSet(target: any, searchVal: Set<any>) {
         if (!target) {
             return false;
         }
@@ -637,7 +631,7 @@ export class IgxDateTimeFilteringOperand extends IgxBaseDateTimeFilteringOperand
                 return d.year === now.year + 1;
             }
         }];
-        
+
         this.operations = newOperations.concat(this.operations);
     }
 }
@@ -741,14 +735,11 @@ export class IgxTimeFilteringOperand extends IgxBaseDateTimeFilteringOperand {
                     true : targetn.hours === search.hours && targetn.minutes === search.minutes && targetn.seconds > search.seconds;
             }
         }];
-        
+
         this.operations = newOperations.concat(this.operations);
     }
 
-    /**
-     * @hidden
-     */
-    public override findValueInSet(target: any, searchVal: Set<any>) {
+    protected override findValueInSet(target: any, searchVal: Set<any>) {
         if (!target) {
             return false;
         }
@@ -806,7 +797,7 @@ export class IgxNumberFilteringOperand extends IgxFilteringOperand {
             iconName: 'filter_not_empty',
             logic: (target: number) => target !== null && target !== undefined && !isNaN(target)
         }];
-        
+
         this.operations = newOperations.concat(this.operations);
     }
 }
@@ -885,7 +876,7 @@ export class IgxStringFilteringOperand extends IgxFilteringOperand {
             iconName: 'filter_not_empty',
             logic: (target: string) => target !== null && target !== undefined && target.length > 0
         }];
-        
+
         this.operations = newOperations.concat(this.operations);
     }
 
