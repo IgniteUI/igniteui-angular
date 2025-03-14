5fd1c75dc5e6c5ece059e6c67bbff4086552a9ec
skrustev <striker18@mail.bg>
2025-03-14 20:26:33 +0200

    fix(filtering): Enable fully use of only `conditionName` when defining a filtering expression.

--- projects/igniteui-angular/src/lib/data-operations/expressions-tree-util.ts
+++ projects/igniteui-angular/src/lib/data-operations/expressions-tree-util.ts
@@ -149,8 +149,8 @@ function recreateExpression(expression: IFilteringExpression, fields: FieldType[
         }
     }
 
-    if (!expression.condition && expression.conditionName) {
-        throw Error('Wrong `conditionName`, `condition` or `field` provided! It is possible that there is a type mismatch between the condition type and field type.');
+    if (!expression.condition && !expression.conditionName) {
+        throw Error('Wrong `conditionName`, `condition` or `field` provided!');
     }
 
     if (!expression.conditionName) {
