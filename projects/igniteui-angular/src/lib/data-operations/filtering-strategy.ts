cc520c1f369ab5e871f73f94b3a64005c228b95c
Hristo Anastasov <HAnastasov@infragistics.com>
2025-03-24 15:24:10 +0200

    fix(grid): apply conditon on condition.logic, not on condition

--- projects/igniteui-angular/src/lib/data-operations/filtering-strategy.ts
+++ projects/igniteui-angular/src/lib/data-operations/filtering-strategy.ts
@@ -40,7 +40,9 @@ export abstract class BaseFilteringStrategy implements IFilteringStrategy  {
     // protected
     public findMatchByExpression(rec: any, expr: IFilteringExpression, isDate?: boolean, isTime?: boolean, grid?: GridType): boolean {
         const val = this.getFieldValue(rec, expr.fieldName, isDate, isTime, grid);
-        return expr.condition?.logic(val, expr.searchVal, expr.ignoreCase);
+        if (expr.condition.logic) {
+            return expr.condition.logic(val, expr.searchVal, expr.ignoreCase);
+        }
     }
 
     // protected
