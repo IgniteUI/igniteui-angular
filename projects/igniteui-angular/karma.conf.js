8170752e1302df805990bdf108ef82927e91191b
Konstantin Dinev <kdinev@mail.bw.edu>
2025-03-25 18:08:57 +0200

    test(*): reducing shards to cpu count - 1

--- projects/igniteui-angular/karma.conf.js
+++ projects/igniteui-angular/karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function (config) {
       '@angular-devkit/build-angular/plugins/karma'
     ],
     parallelOptions: {
-      executors: 4,
+      executors: require('os').cpus().length - 1,
       shardStrategy: 'round-robin'
     },
     client: {
