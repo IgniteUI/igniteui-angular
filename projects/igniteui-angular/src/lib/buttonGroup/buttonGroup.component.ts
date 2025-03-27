fef5385c892ca6b8e743daa2af0f56a3b48a76a4
Simeon Simeonoff <sim.simeonoff@gmail.com>
2025-03-27 11:43:29 +0200

    refactor(button-group): remove wrapping div

--- projects/igniteui-angular/src/lib/buttonGroup/buttonGroup.component.ts
+++ projects/igniteui-angular/src/lib/buttonGroup/buttonGroup.component.ts
@@ -80,11 +80,31 @@ export class IgxButtonGroupComponent implements AfterViewInit, OnDestroy {
     @Input()
     public id = `igx-buttongroup-${NEXT_ID++}`;
 
+    /** @hidden @internal */
+    @HostBinding('class.igx-button-group')
+    public cssClass = 'igx-button-group';
+
+    /** @hidden @internal */
+
     /**
      * @hidden
      */
     @HostBinding('style.zIndex')
     public zIndex = 0;
+
+    /**
+     * Sets/gets the role attribute value.
+     *
+     * @example
+     * ```typescript
+     * @ViewChild("MyButtonGroup", { read: IgxButtonGroupComponent })
+     * public badge: IgxButtonGroupComponent;
+     *
+     * buttonGroup.role = 'group';
+     * ```
+     */
+    @HostBinding('attr.role')
+    public role = 'group';
 
     /**
      * Allows you to set a style using the `itemContentCssClass` input.
@@ -289,6 +309,7 @@ export class IgxButtonGroupComponent implements AfterViewInit, OnDestroy {
      * }
      * ```
      */
+    @HostBinding('class.igx-button-group--vertical')
     public get isVertical(): boolean {
         return this._isVertical;
     }
