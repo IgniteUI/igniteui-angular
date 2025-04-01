80c5b6c6aed95a67a4b90c868598411d95b11e0b
Stamen Stoychev <SStoychev@infragistics.com>
2025-04-01 14:59:26 +0300

    chore(*): applying changes from review

--- projects/igniteui-angular/src/lib/grids/common/random.ts
+++ projects/igniteui-angular/src/lib/grids/common/random.ts
@@ -2,8 +2,8 @@
  * Use the function to get a random UUID string when secure context is not guaranteed making crypto.randomUUID unavailable.
  * @returns A random UUID string.
  */
-export function getUUID() {
-    if (typeof crypto.randomUUID === "function") {
+export function getUUID(): `${string}-${string}-${string}-${string}-${string}` {
+    if (typeof crypto.randomUUID === 'function') {
         return crypto.randomUUID();
     }
     // Secure fallback using crypto.getRandomValues()
@@ -14,9 +14,6 @@ export function getUUID() {
     bytes[6] = (bytes[6] & 0x0f) | 0x40; // Version 4
     bytes[8] = (bytes[8] & 0x3f) | 0x80; // Variant 1
 
-    return [...bytes]
-        .map((b, i) =>
-            [4, 6, 8, 10].includes(i) ? `-${b.toString(16).padStart(2, "0")}` : b.toString(16).padStart(2, "0")
-        )
-        .join("");
+    const a = [...bytes].map((b) => b.toString(16).padStart(2, '0')).join('');
+    return `${a.slice(0, 8)}-${a.slice(8, 12)}-${a.slice(12, 16)}-${a.slice(16, 20)}-${a.slice(20)}`;
 }
