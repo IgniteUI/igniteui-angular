4188db2419e52ba7a2ba2225635d6a07d771d928
Simeon Simeonoff <sim.simeonoff@gmail.com>
2025-05-13 11:12:20 +0300

    deps(webcomponents): bump to latest version

--- src/app/tabs-showcase/tabs-showcase.sample.ts
+++ src/app/tabs-showcase/tabs-showcase.sample.ts
@@ -20,14 +20,25 @@ import {
     defineComponents,
     IgcTabsComponent,
     IgcTabComponent,
-    IgcTabPanelComponent,
+    registerIconFromText
 } from 'igniteui-webcomponents';
 import {
     PropertyChangeService,
     Properties,
 } from '../properties-panel/property-change.service';
 
-defineComponents(IgcTabsComponent, IgcTabComponent, IgcTabPanelComponent);
+defineComponents(IgcTabsComponent, IgcTabComponent);
+
+const icons = [
+    {
+        name: 'folder',
+        url: '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h240l80 80h320q33 0 56.5 23.5T880-640v400q0 33-23.5 56.5T800-160H160Z"/></svg>'
+    }
+];
+
+icons.forEach((icon) => {
+    registerIconFromText(icon.name, icon.url);
+});
 
 @Component({
     selector: 'app-tabs-showcase-sample',
