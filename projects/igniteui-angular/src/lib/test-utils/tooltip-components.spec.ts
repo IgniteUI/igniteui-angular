bb9ca698c5c99d0b40d6aa975ad213e6be2c19a9
RivaIvanova <ivanova.riva@gmail.com>
2025-05-15 15:33:33 +0300

    test(tooltip): should not call hideTooltip multiple times on document:touchstart

--- projects/igniteui-angular/src/lib/test-utils/tooltip-components.spec.ts
+++ projects/igniteui-angular/src/lib/test-utils/tooltip-components.spec.ts
@@ -6,7 +6,7 @@ import { IgxToggleActionDirective, IgxToggleDirective } from '../directives/togg
 @Component({
     template: `
     <div class="dummyDiv">dummy div for touch tests</div>
-    
+
     @if (showButton) {
         <button [igxTooltipTarget]="tooltipRef" [tooltip]="'Infragistics Inc. HQ'"
                 (tooltipShow)="showing($event)" (tooltipHide)="hiding($event)"
@@ -42,6 +42,8 @@ export class IgxTooltipSingleTargetComponent {
 
 @Component({
     template: `
+    <div class="dummyDiv">dummy div for touch tests</div>
+
     <button class="buttonOne" #targetOne="tooltipTarget" [igxTooltipTarget]="tooltipRef" style="margin: 100px">
         Target One
     </button>
