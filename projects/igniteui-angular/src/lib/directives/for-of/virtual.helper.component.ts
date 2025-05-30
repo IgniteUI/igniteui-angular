c13c40adbdbc25a0b3044ad0a2c03ace541badc6
didimmova <d.dimova11@gmail.com>
2025-05-30 11:21:08 +0300

    feat(igx-vhelper): create and use css var for scrollbar size

--- projects/igniteui-angular/src/lib/directives/for-of/virtual.helper.component.ts
+++ projects/igniteui-angular/src/lib/directives/for-of/virtual.helper.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, HostBinding, Input, ViewChild, ViewContainerRef,
-     ChangeDetectorRef, OnDestroy, OnInit, Inject, NgZone } from '@angular/core';
+     ChangeDetectorRef, OnDestroy, Inject, NgZone } from '@angular/core';
 import { VirtualHelperBaseDirective } from './base.helper.component';
 import { DOCUMENT } from '@angular/common';
 import { PlatformUtil } from '../../core/utils';
@@ -9,26 +9,25 @@ import { PlatformUtil } from '../../core/utils';
     template: '<div #container class="igx-vhelper__placeholder-content" [style.height.px]="size"></div>',
     standalone: true
 })
-export class VirtualHelperComponent extends VirtualHelperBaseDirective implements OnInit, OnDestroy  {
+export class VirtualHelperComponent extends VirtualHelperBaseDirective implements OnDestroy  {
     @HostBinding('scrollTop')
     public scrollTop;
 
-    @HostBinding('style.width.px')
-    public scrollWidth;
+    @HostBinding('style.--igx-vhelper--scrollbar-size')
+    public get scrollbarSize(): string {
+        const value = `${this.scrollNativeSize}px`;
+        this.document.documentElement.style.setProperty('--igx-vhelper--scrollbar-size', value);
+        return value;
+    }
 
     @ViewChild('container', { read: ViewContainerRef, static: true }) public _vcr;
     @Input() public itemsLength: number;
-
 
     @HostBinding('class')
     public cssClasses = 'igx-vhelper--vertical';
 
     constructor(elementRef: ElementRef, cdr: ChangeDetectorRef, zone: NgZone, @Inject(DOCUMENT) document, platformUtil: PlatformUtil) {
         super(elementRef, cdr, zone, document, platformUtil);
-    }
-
-    public ngOnInit() {
-        this.scrollWidth = this.scrollNativeSize;
     }
 
     protected override restoreScroll() {
