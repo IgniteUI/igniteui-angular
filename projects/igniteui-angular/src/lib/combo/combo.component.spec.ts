aa36463f717c3f33b447af995e55c08ee0ac3770
Ivan Kitanov <IKitanov@infragistics.com>
2025-06-26 10:28:52 +0300

    fix(combo): Sync aria required with required state

--- projects/igniteui-angular/src/lib/combo/combo.component.spec.ts
+++ projects/igniteui-angular/src/lib/combo/combo.component.spec.ts
@@ -58,7 +58,7 @@ const CSS_CLASS_ITME_CHECKBOX_CHECKED = 'igx-checkbox--checked';
 const defaultDropdownItemHeight = 40;
 const defaultDropdownItemMaxHeight = 400;
 
-describe('igxCombo', () => {
+fdescribe('igxCombo', () => {
     let fixture: ComponentFixture<any>;
     let combo: IgxComboComponent;
     let input: DebugElement;
@@ -3390,8 +3390,10 @@ describe('igxCombo', () => {
                 it('should add/remove asterisk when setting validators dynamically', () => {
                     let inputGroupIsRequiredClass = fixture.debugElement.query(By.css('.' + CSS_CLASS_INPUTGROUP_REQUIRED));
                     let asterisk = window.getComputedStyle(fixture.debugElement.query(By.css('.' + CSS_CLASS_INPUTGROUP_LABEL)).nativeElement, ':after').content;
+                    input = fixture.debugElement.query(By.css(`.${CSS_CLASS_COMBO_INPUTGROUP}`));
                     expect(asterisk).toBe('"*"');
                     expect(inputGroupIsRequiredClass).toBeDefined();
+                    expect(input.nativeElement.getAttribute('aria-required')).toMatch('true');
 
                     fixture.componentInstance.reactiveForm.controls.townCombo.clearValidators();
                     fixture.componentInstance.reactiveForm.controls.townCombo.updateValueAndValidity();
@@ -3400,6 +3402,7 @@ describe('igxCombo', () => {
                     asterisk = window.getComputedStyle(fixture.debugElement.query(By.css('.' + CSS_CLASS_INPUTGROUP_LABEL)).nativeElement, ':after').content;
                     expect(asterisk).toBe('none');
                     expect(inputGroupIsRequiredClass).toBeNull();
+                    expect(input.nativeElement.getAttribute('aria-required')).toMatch('false');
 
                     fixture.componentInstance.reactiveForm.controls.townCombo.setValidators(Validators.required);
                     fixture.componentInstance.reactiveForm.controls.townCombo.updateValueAndValidity();
@@ -3408,6 +3411,7 @@ describe('igxCombo', () => {
                     asterisk = window.getComputedStyle(fixture.debugElement.query(By.css('.' + CSS_CLASS_INPUTGROUP_LABEL)).nativeElement, ':after').content;
                     expect(asterisk).toBe('"*"');
                     expect(inputGroupIsRequiredClass).toBeDefined();
+                    expect(input.nativeElement.getAttribute('aria-required')).toMatch('true');
                 });
 
                 it('Should update validity state when programmatically setting errors on reactive form controls', fakeAsync(() => {
