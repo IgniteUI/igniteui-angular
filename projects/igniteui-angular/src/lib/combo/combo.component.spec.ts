926d79a3d625f689cee00940aa32296036109a0b
Ivan Kitanov <IKitanov@infragistics.com>
2025-06-27 09:56:33 +0300

    chore(combo): Remove fdescribe

--- projects/igniteui-angular/src/lib/combo/combo.component.spec.ts
+++ projects/igniteui-angular/src/lib/combo/combo.component.spec.ts
@@ -58,7 +58,7 @@ const CSS_CLASS_ITME_CHECKBOX_CHECKED = 'igx-checkbox--checked';
 const defaultDropdownItemHeight = 40;
 const defaultDropdownItemMaxHeight = 400;
 
-fdescribe('igxCombo', () => {
+describe('igxCombo', () => {
     let fixture: ComponentFixture<any>;
     let combo: IgxComboComponent;
     let input: DebugElement;
