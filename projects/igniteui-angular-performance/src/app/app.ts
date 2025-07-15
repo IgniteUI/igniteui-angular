1862839650b920b6b9e4636ebb7a8d41ce9fd921
Martin Dragnev <mddragnev@gmail.com>
2025-07-15 16:07:18 +0300

    feat(*): performance app initial commit

---
+++ projects/igniteui-angular-performance/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+})
+export class App {
+  protected title = 'igniteui-angular-performance';
+}
