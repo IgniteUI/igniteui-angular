ef71a877c56de3dbc2f3f23485e23680ae2ee2d2
Stamen Stoychev <SStoychev@infragistics.com>
2025-07-25 18:42:08 +0300

    chore(combo): modifying existing commit

--- projects/igniteui-angular/src/lib/combo/combo.common.ts
+++ projects/igniteui-angular/src/lib/combo/combo.common.ts
@@ -1348,7 +1348,6 @@ export abstract class IgxComboBaseDirective implements IgxComboBase, AfterViewCh
     protected manageRequiredAsterisk(): void {
         if (this.ngControl) {
             this.inputGroup.isRequired = this.required;
-            this.comboInput.nativeElement.setAttribute('aria-required', this.required.toString());
         }
     }
 
