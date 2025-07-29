34c6ea3acde7db8a98a7d0f4c39d0fc41e5b8388
Martin Dragnev <mddragnev@gmail.com>
2025-07-29 16:45:28 +0300

    chore(sample): Add grouping to the sample

--- projects/igniteui-angular-performance/src/app/grid/grid.component.ts
+++ projects/igniteui-angular-performance/src/app/grid/grid.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { GridColumnDataType, IGX_GRID_DIRECTIVES } from "igniteui-angular"
+import { Component, inject, ViewChild } from '@angular/core';
+import { GridColumnDataType, IGX_GRID_DIRECTIVES, IgxGridComponent } from "igniteui-angular"
 import { DataService } from '../services/data.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -16,22 +16,24 @@ export class GridComponent {
     private dataService = inject(DataService);
     private activatedRoute = inject(ActivatedRoute);
 
+    @ViewChild(IgxGridComponent, { static: true })
+    public grid: IgxGridComponent;
 
     constructor() {
         this.data = this.dataService.generateData(this.activatedRoute.snapshot.data.rows)
         this.columns = [
-            { field: "Id", dataType: GridColumnDataType.Number, sortable: true, width: 'auto' },
-            { field: "Name", dataType: GridColumnDataType.String, sortable: true, width: 'auto' },
-            { field: "AthleteNumber", dataType: GridColumnDataType.Number, sortable: true, width: 'auto' },
-            { field: "Registered", dataType: GridColumnDataType.DateTime, sortable: true, width: 'auto' },
-            { field: "CountryName", dataType: GridColumnDataType.String, sortable: true, width: 'auto' },
-            { field: "FirstAppearance", dataType: GridColumnDataType.Time, sortable: true, width: 'auto' },
-            { field: "CareerStart", dataType: GridColumnDataType.Date, sortable: true, width: 'auto' },
-            { field: "Active", dataType: GridColumnDataType.Boolean, sortable: true, width: 'auto' },
-            { field: "NetWorth", dataType: GridColumnDataType.Currency, sortable: true, width: 'auto' },
-            { field: "CountryFlag", dataType: GridColumnDataType.Image, sortable: true, width: 'auto' },
-            { field: "SuccessRate", dataType: GridColumnDataType.Percent, sortable: true, width: 'auto' },
-            { field: "Position", dataType: GridColumnDataType.String, sortable: true, width: 'auto' },
+            { field: "Id", dataType: GridColumnDataType.Number, sortable: true, width: 'auto', groupable: true },
+            { field: "Name", dataType: GridColumnDataType.String, sortable: true, width: 'auto', groupable: true },
+            { field: "AthleteNumber", dataType: GridColumnDataType.Number, sortable: true, width: 'auto', groupable: true },
+            { field: "Registered", dataType: GridColumnDataType.DateTime, sortable: true, width: 'auto', groupable: true },
+            { field: "CountryName", dataType: GridColumnDataType.String, sortable: true, width: 'auto', groupable: true },
+            { field: "FirstAppearance", dataType: GridColumnDataType.Time, sortable: true, width: 'auto', groupable: true },
+            { field: "CareerStart", dataType: GridColumnDataType.Date, sortable: true, width: 'auto', groupable: true },
+            { field: "Active", dataType: GridColumnDataType.Boolean, sortable: true, width: 'auto', groupable: true },
+            { field: "NetWorth", dataType: GridColumnDataType.Currency, sortable: true, width: 'auto', groupable: true },
+            { field: "CountryFlag", dataType: GridColumnDataType.Image, sortable: true, width: 'auto', groupable: true },
+            { field: "SuccessRate", dataType: GridColumnDataType.Percent, sortable: true, width: 'auto', groupable: true },
+            { field: "Position", dataType: GridColumnDataType.String, sortable: true, width: 'auto', groupable: true },
         ];
     }
 }
