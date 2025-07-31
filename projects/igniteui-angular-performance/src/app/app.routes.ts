2290abed3c163f451e722a4ed4d9175856a1ec05
Martin Dragnev <37667452+mddragnev@users.noreply.github.com>
2025-07-31 12:23:09 +0300

    chore(treegrid): Add performance sample for tree grid (#16101)

--- projects/igniteui-angular-performance/src/app/app.routes.ts
+++ projects/igniteui-angular-performance/src/app/app.routes.ts
@@ -1,21 +1,44 @@
 import { Routes } from '@angular/router';
 import { GridComponent } from './grid/grid.component';
+import { TreeGridComponent } from './tree-grid/tree-grid.component';
 
 export const routes: Routes = [
     {
-        path: "grid-100k",
-        component: GridComponent,
+        path: "tree-grid-1m",
+        title: "Tree Grid 1M records",
+        component: TreeGridComponent,
+        data: { rows: 1_000_000 }
+    },
+    {
+        path: "tree-grid-100k",
+        title: "Tree Grid 100k records",
+        component: TreeGridComponent,
         data: { rows: 100_000 }
     },
     {
+        path: "tree-grid",
+        title: "Tree Grid 1k records",
+        component: TreeGridComponent,
+        data: { rows: 1000 }
+    },
+    {
         path: "grid-1m",
+        title: "Grid 1M records",
         component: GridComponent,
         data: { rows: 1_000_000 }
     },
     {
+        path: "grid-100k",
+        title: "Grid 100k records",
+        component: GridComponent,
+        data: { rows: 100_000 }
+    },
+    {
         path: "",
+        title: "Grid 1k records",
         pathMatch: 'full',
         component: GridComponent,
         data: { rows: 1000 }
     }
+
 ];
---
+++ projects/igniteui-angular-performance/src/app/data/employeesData.ts
@@ -0,0 +1,164 @@
+export const EMPLOYEES_DATA = [
+    {
+        Age: 55,
+        Avatar: 'assets/images/men/5.jpg',
+        HireDate: new Date(2008, 3, 20),
+        ID: 1,
+        Name: 'Johnathan Winchester',
+        ParentID: -1,
+        Title: 'Development Manager'
+    },
+    {
+        Age: 42,
+        Avatar: 'assets/images/women/4.jpg',
+        HireDate: new Date(2014, 1, 22),
+        ID: 4,
+        Name: 'Ana Sanders',
+        ParentID: -1,
+        Title: 'CEO'
+    },
+    {
+        Age: 49,
+        Avatar: 'assets/images/women/2.jpg',
+        HireDate: new Date(2014, 1, 22),
+        ID: 18,
+        Name: 'Victoria Lincoln',
+        ParentID: -1,
+        Title: 'Accounting Manager'
+    },
+    {
+        Age: 61,
+        Avatar: 'assets/images/men/4.jpg',
+        HireDate: new Date(2010, 1, 1),
+        ID: 10,
+        Name: 'Yang Wang',
+        ParentID: -1,
+        Title: 'Localization Manager'
+    },
+    {
+        Age: 43,
+        Avatar: 'assets/images/men/1.jpg',
+        HireDate: new Date(2011, 6, 3),
+        ID: 3,
+        Name: 'Michael Burke',
+        ParentID: 1,
+        Title: 'Senior Software Developer'
+    },
+    {
+        Age: 29,
+        Avatar: 'assets/images/men/2.jpg',
+        HireDate: new Date(2009, 6, 19),
+        ID: 2,
+        Name: 'Thomas Anderson',
+        ParentID: 1,
+        Title: 'Senior Software Developer'
+    },
+    {
+        Age: 31,
+        Avatar: 'assets/images/women/3.jpg',
+        HireDate: new Date(2014, 8, 18),
+        ID: 11,
+        Name: 'Monica Reyes',
+        ParentID: 1,
+        Title: 'Software Development Team Lead'
+    },
+    {
+        Age: 35,
+        Avatar: 'assets/images/men/3.jpg',
+        HireDate: new Date(2015, 9, 17),
+        ID: 6,
+        Name: 'Roland Mendel',
+        ParentID: 11,
+        Title: 'Senior Software Developer'
+    },
+    {
+        Age: 44,
+        Avatar: 'assets/images/men/6.jpg',
+        HireDate: new Date(2009, 10, 11),
+        ID: 12,
+        Name: 'Sven Cooper',
+        ParentID: 11,
+        Title: 'Senior Software Developer'
+    },
+    {
+        Age: 44,
+        Avatar: 'assets/images/men/7.jpg',
+        HireDate: new Date(2014, 4, 4),
+        ID: 14,
+        Name: 'Laurence Johnson',
+        ParentID: 4,
+        Title: 'Director'
+    },
+    {
+        Age: 25,
+        Avatar: 'assets/images/women/1.jpg',
+        HireDate: new Date(2017, 11, 9),
+        ID: 5,
+        Name: 'Elizabeth Richards',
+        ParentID: 4,
+        Title: 'Vice President'
+    },
+    {
+        Age: 39,
+        Avatar: 'assets/images/men/8.jpg',
+        HireDate: new Date(2010, 3, 22),
+        ID: 13,
+        Name: 'Trevor Ashworth',
+        ParentID: 5,
+        Title: 'Director'
+    },
+    {
+        Age: 44,
+        Avatar: 'assets/images/men/9.jpg',
+        HireDate: new Date(2014, 4, 4),
+        ID: 17,
+        Name: 'Antonio Moreno',
+        ParentID: 18,
+        Title: 'Senior Accountant'
+    },
+    {
+        Age: 50,
+        Avatar: 'assets/images/men/10.jpg',
+        HireDate: new Date(2007, 11, 18),
+        ID: 7,
+        Name: 'Pedro Rodriguez',
+        ParentID: 10,
+        Title: 'Senior Localization Developer'
+    },
+    {
+        Age: 27,
+        Avatar: 'assets/images/women/5.jpg',
+        HireDate: new Date(2016, 2, 19),
+        ID: 8,
+        Name: 'Casey Harper',
+        ParentID: 10,
+        Title: 'Senior Localization Developer'
+    },
+    {
+        Age: 25,
+        Avatar: 'assets/images/women/6.jpg',
+        HireDate: new Date(2017, 11, 9),
+        ID: 15,
+        Name: 'Patricia Simpson',
+        ParentID: 7,
+        Title: 'Localization Intern'
+    },
+    {
+        Age: 39,
+        Avatar: 'assets/images/men/26.jpg',
+        HireDate: new Date(2010, 3, 22),
+        ID: 9,
+        Name: 'Francisco Chang',
+        ParentID: 7,
+        Title: 'Localization Intern'
+    },
+    {
+        Age: 25,
+        Avatar: 'assets/images/men/27.jpg',
+        HireDate: new Date(2018, 3, 18),
+        ID: 16,
+        Name: 'Peter Lewis',
+        ParentID: 7,
+        Title: 'Localization Intern'
+    }
+];
