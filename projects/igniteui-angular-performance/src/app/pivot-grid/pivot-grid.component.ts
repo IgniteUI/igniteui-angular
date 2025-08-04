e25b82fdaa9d55e08ff6b9d10f41645a19dac845
Martin Dragnev <mddragnev@gmail.com>
2025-08-04 17:28:22 +0300

    chore(pivot): Add pivot grid performance sample

---
+++ projects/igniteui-angular-performance/src/app/pivot-grid/pivot-grid.component.ts
@@ -0,0 +1,185 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { FilteringExpressionsTree, FilteringLogic, IgxPivotDateDimension, IgxPivotGridComponent, IPivotConfiguration, IPivotValue } from 'igniteui-angular';
+import DATA from "../data/pivot-data.json";
+
+// Custom aggregator to calculate profit value
+export class IgxSaleProfitAggregate {
+    public static totalProfit = (_, data: any[] | undefined) =>
+        data?.reduce((accumulator, value) => accumulator + (value.Sale - value.Cost), 0) || 0;
+
+    public static averageProfit = (_, data: any[] | undefined) => {
+        let average = 0;
+        if (data?.length === 1) {
+            average = data[0].Sale - data[0].Cost;
+        } else if (data && data.length > 1) {
+            const mappedData = data.map(x => x.Sale - x.Cost);
+            average = mappedData.reduce((a, b) => a + b) / mappedData.length;
+        }
+        return average;
+    }
+
+    public static minProfit = (_, data: any[] | undefined) => {
+        let min = 0;
+        if (data?.length === 1) {
+            min = data[0].Sale - data[0].Cost;
+        } else if (data && data.length > 1) {
+            const mappedData = data.map(x => x.Sale - x.Cost);
+            min = mappedData.reduce((a, b) => Math.min(a, b));
+        }
+        return min;
+    };
+
+    public static maxProfit = (_, data: any[] | undefined) => {
+        let max = 0;
+        if (data?.length === 1) {
+            max = data[0].Sale - data[0].Cost;
+        } else if (data && data.length > 1) {
+            const mappedData = data.map(x => x.Sale - x.Cost);
+            max = mappedData.reduce((a, b) => Math.max(a, b));
+        }
+        return max;
+    };
+}
+
+@Component({
+    selector: 'app-pivot-grid',
+    imports: [IgxPivotGridComponent],
+    templateUrl: './pivot-grid.component.html',
+    styleUrl: './pivot-grid.component.scss'
+})
+export class PivotGridComponent {
+    @ViewChild(IgxPivotGridComponent, { static: true })
+    public pivotGrid!: IgxPivotGridComponent;
+
+    private currencyPipe = new CurrencyPipe('en-US');
+    private brandFilter = new FilteringExpressionsTree(FilteringLogic.Or, 'Brand');
+
+    protected data: any = DATA;
+    protected saleValue: IPivotValue = {
+        enabled: true,
+        member: 'Sale',
+        displayName: 'Sales',
+        aggregate: {
+            key: 'SUM',
+            aggregatorName: 'SUM',
+            label: 'Sum'
+        },
+        aggregateList: [
+            {
+                key: 'AVG',
+                aggregatorName: 'AVG',
+                label: 'Average'
+            },
+            {
+                key: 'COUNT',
+                aggregatorName: 'COUNT',
+                label: 'Count'
+            },
+            {
+                key: 'MAX',
+                aggregatorName: 'MAX',
+                label: 'Maximum'
+            },
+            {
+                key: 'MIN',
+                aggregatorName: 'MIN',
+                label: 'Minimum'
+            },
+            {
+                key: 'SUM',
+                aggregatorName: 'SUM',
+                label: 'Sum'
+            },
+        ],
+        formatter: (value, _, __) => {
+            return this.currencyFormatter(value, 'Sale');
+        }
+    };
+    protected profitValue: IPivotValue = {
+        enabled: true,
+        member: 'Cost',
+        displayName: 'Profit',
+        aggregate: {
+            key: 'SUM',
+            aggregator: IgxSaleProfitAggregate.totalProfit,
+            label: 'Sum'
+        },
+        aggregateList: [
+            {
+                key: 'AVG',
+                aggregator: IgxSaleProfitAggregate.averageProfit,
+                label: 'Average'
+            },
+            {
+                key: 'COUNT',
+                aggregatorName: 'COUNT',
+                label: 'Count'
+            },
+            {
+                key: 'MAX',
+                aggregator: IgxSaleProfitAggregate.maxProfit,
+                label: 'Maximum'
+            },
+            {
+                key: 'MIN',
+                aggregator: IgxSaleProfitAggregate.minProfit,
+                label: 'Minimum'
+            },
+            {
+                key: 'SUM',
+                aggregator: IgxSaleProfitAggregate.totalProfit,
+                label: 'Sum'
+            },
+        ],
+        formatter: (value, _, __) => {
+            return this.currencyFormatter(value, 'Cost');
+        }
+    };
+
+    protected pivotConfigBrands: IPivotConfiguration = {
+        columns: [
+            {
+                enabled: true,
+                memberName: 'Country',
+                displayName: 'Country'
+            },
+            {
+                enabled: true,
+                memberName: 'Brand',
+                displayName: 'Brand',
+                filter: this.brandFilter
+            },
+            {
+                enabled: false,
+                memberName: 'Store',
+                displayName: 'Store'
+            },
+        ],
+        rows: [
+
+            new IgxPivotDateDimension({
+                memberName: 'Date',
+                displayName: 'All Periods',
+                enabled: true
+            },
+                {
+                    fullDate: true,
+                    quarters: true,
+                    months: true,
+                }),
+        ],
+        values: [
+            this.saleValue,
+            this.profitValue
+        ]
+    };
+
+    private currencyFormatter(value: any, field: string) {
+        var valueConfig = this.pivotGrid.values.find(value => value.member === field);
+        if (!valueConfig || valueConfig.aggregate.key === "COUNT") {
+            return value;
+        }
+        return this.currencyPipe.transform(value, 'USD', 'symbol', '1.0-2');
+    }
+}
