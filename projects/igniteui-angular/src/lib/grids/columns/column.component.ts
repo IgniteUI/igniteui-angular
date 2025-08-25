d73e3b7caa0d6159d60faabc4659afb190ddc683
Martin Dragnev <mddragnev@gmail.com>
2025-08-25 17:21:06 +0300

    chore(*): Remove calculation of offset when pinning to the right side

--- projects/igniteui-angular/src/lib/grids/columns/column.component.ts
+++ projects/igniteui-angular/src/lib/grids/columns/column.component.ts
@@ -1597,13 +1597,6 @@ export class IgxColumnComponent implements AfterContentInit, OnDestroy, ColumnTy
     }
 
     /** @hidden @internal **/
-    public get rightPinnedOffset(): string {
-        return this.pinned && !this.grid.isPinningToStart ?
-            - this.grid.pinnedWidth - this.grid.headerFeaturesWidth + 'px' :
-            null;
-    }
-
-    /** @hidden @internal **/
     public get gridRowSpan(): number {
         return this.rowEnd && this.rowStart ? this.rowEnd - this.rowStart : 1;
     }
