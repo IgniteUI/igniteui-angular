03f661d2199f9eb24be3c8a43b8fa0c5bc1f033b
Bozhidara Pachilova <35433937+ddaribo@users.noreply.github.com>
2025-08-27 14:38:13 +0300

    Align Date & Date Range Picker props related to calendar with WC (#16141)

--- projects/igniteui-angular/src/lib/date-common/types.ts
+++ projects/igniteui-angular/src/lib/date-common/types.ts
@@ -5,6 +5,13 @@ export const PickerHeaderOrientation = {
 } as const;
 export type PickerHeaderOrientation = (typeof PickerHeaderOrientation)[keyof typeof PickerHeaderOrientation];
 
+/** Calendar orientation. */
+export const PickerCalendarOrientation = {
+    Horizontal: 'horizontal',
+    Vertical: 'vertical'
+} as const;
+export type PickerCalendarOrientation = (typeof PickerCalendarOrientation)[keyof typeof PickerCalendarOrientation];
+
 /**
  * This enumeration is used to configure whether the date/time picker has an editable input with drop down
  * or is readonly - the date/time is selected only through a dialog.
