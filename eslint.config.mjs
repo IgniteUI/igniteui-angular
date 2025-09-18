706f78d02a83a60f59dd582259d80b83a926334c
damyanpetev <damyanpetev@users.noreply.github.com>
2025-09-18 15:30:28 +0300

    chore(*): cleanup and formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -40,7 +40,6 @@ export default [{
         parserOptions: {
             project: ["tsconfig.json"],
             createDefaultProgram: true,
-            suppressParseErrors: true
         },
     },
 
--- src/app/shared/invoiceData.ts
+++ src/app/shared/invoiceData.ts
@@ -1,5 +1,3 @@
- 
-
 export interface Invoice {
     ID: number;
     ShipAddress: string;
