9deae36a72d7191372c3d5a58b9bbbec2c5b2619
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2025-11-11 16:48:06 +0200

    fix(elements): createEnvironmentInjector build error

--- projects/igniteui-angular-elements/src/utils/injector-ref.ts
+++ projects/igniteui-angular-elements/src/utils/injector-ref.ts
@@ -1,4 +1,4 @@
-import { createEnvironmentInjector, EnvironmentInjector, getPlatform, importProvidersFrom, provideZoneChangeDetection, ɵChangeDetectionScheduler, ɵChangeDetectionSchedulerImpl } from '@angular/core';
+import { ClassProvider, createEnvironmentInjector, EnvironmentInjector, getPlatform, importProvidersFrom, provideZoneChangeDetection, ɵChangeDetectionScheduler, ɵprovideZonelessChangeDetectionInternal, } from '@angular/core';
 import { BrowserModule, platformBrowser } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { ELEMENTS_TOKEN } from 'igniteui-angular/src/lib/core/utils';
@@ -24,11 +24,14 @@ const platformInjector = getPlatform()?.injector ?? platformBrowser().injector;
 // createEnvironmentInjector is a public wrapper around EnvironmentNgModuleRefAdapter
 // https://github.com/angular/angular/blob/969dadc8e2fad8ca9d892858bdadbe3abb13de95/packages/core/src/application/create_application.ts#L56C25-L56C54
 const injector = createEnvironmentInjector([
-     // TODO: -> provideExperimentalZonelessChangeDetection
+    // TODO: -> provideExperimentalZonelessChangeDetection
     provideZoneChangeDetection({ eventCoalescing: true }),
     // Required, but only provided internally by `provideExperimentalZonelessChangeDetection`, `bootstrapModuleFactory`
     // & `internalCreateApplication` (`bootstrapApplication`/`createApplication`), so no public API alternative:
-    { provide: ɵChangeDetectionScheduler, useExisting: ɵChangeDetectionSchedulerImpl },
+
+    // Still no "direct" public API but at least `ɵprovideZonelessChangeDetectionInternal` exports it somewhat:
+    // https://github.com/angular/angular/commit/45fed3d2011bf6feffa8ee1365a5c88d603f826c#diff-10544e5a7c018dbc5dc5a1d4192919bb839c5d1b7cbcc1b20f57aa74c2ae7febR391-R397
+    ɵprovideZonelessChangeDetectionInternal().find((entity) => (entity as ClassProvider).provide === ɵChangeDetectionScheduler),
     importProvidersFrom(BrowserModule),
     // Elements specific:
     provideAnimations(),
