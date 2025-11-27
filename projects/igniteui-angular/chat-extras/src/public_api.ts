09ec5aabd3a30b6f72601506e9f7960e2da7bc21
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2025-11-27 17:17:05 +0200

    feat: add chat component Angular wrapper (#16255)
    Co-authored-by: Galina Edinakova <gedinakova@infragistics.com>

---
+++ projects/igniteui-angular/chat-extras/src/public_api.ts
@@ -0,0 +1,1 @@
+export { MarkdownPipe } from './markdown-pipe';
--- projects/igniteui-angular/schematics/utils/dependency-handler.ts
+++ projects/igniteui-angular/schematics/utils/dependency-handler.ts
@@ -27,6 +27,11 @@ export const DEPENDENCIES_MAP: PackageEntry[] = [
     { name: 'lodash-es', target: PackageTarget.NONE },
     { name: '@igniteui/material-icons-extended', target: PackageTarget.REGULAR },
     { name: 'igniteui-theming', target: PackageTarget.NONE },
+    { name: 'igniteui-webcomponents', target: PackageTarget.NONE },
+    { name: 'dompurify', target: PackageTarget.NONE },
+    { name: 'marked', target: PackageTarget.NONE },
+    { name: 'marked-shiki', target: PackageTarget.NONE },
+    { name: 'shiki', target: PackageTarget.NONE },
     // peerDependencies
     { name: '@angular/forms', target: PackageTarget.NONE },
     { name: '@angular/common', target: PackageTarget.NONE },
