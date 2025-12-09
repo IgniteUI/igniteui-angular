314b588ac1ce5a7c30b0780820b7d3d7440cee76
Ivan Minchev <IMinchev@infragistics.com>
2025-12-09 17:49:48 +0200

    fix(*): add mustCoerceToInt to relevant props

--- projects/igniteui-angular/grids/core/src/common/grid.interface.ts
+++ projects/igniteui-angular/grids/core/src/common/grid.interface.ts
@@ -1215,7 +1215,9 @@ export interface IgxGridPaginatorTemplateContext {
  * An interface describing settings for row/column pinning position.
  */
 export interface IPinningConfig {
+    /* mustCoerceToInt */
     columns?: ColumnPinningPosition;
+    /* mustCoerceToInt */
     rows?: RowPinningPosition;
 }
 
