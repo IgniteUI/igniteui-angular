c9e8e574f2ad52ca9ec74fc785dadddecf85d233
MKirova <MKirova@DEV-MKIROVA>
2025-12-09 17:16:11 +0200

    chore(*): Fix hiding integration.

--- projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
+++ projects/igniteui-angular/src/lib/grids/grid-base.directive.ts
@@ -5711,8 +5711,12 @@ export abstract class IgxGridBaseDirective implements GridType,
             }, 0);
 
         // When all columns are hidden, return 0px width
-        if (!sumExistingWidths || !columnsToSize) {
+        if (!sumExistingWidths && !columnsToSize) {
             return '0px';
+        }
+
+        if (!columnsToSize) {
+            return '';
         }
         computedWidth -= this.featureColumnsWidth();
 
@@ -6829,7 +6833,7 @@ export abstract class IgxGridBaseDirective implements GridType,
     protected _derivePossibleWidth() {
         if (!this.columnWidthSetByUser) {
             const possibleWidth = this.getPossibleColumnWidth();
-            this._columnWidth = this.width !== null && possibleWidth !== "0px" ? possibleWidth : this.minColumnWidth + 'px';
+            this._columnWidth = this.width !== null && possibleWidth !== "" ? possibleWidth : this.minColumnWidth + 'px';
         }
         this._columns.forEach((column: IgxColumnComponent) => {
             if (this.hasColumnLayouts && parseFloat(this._columnWidth)) {
