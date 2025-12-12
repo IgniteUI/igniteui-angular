2ee7903288128441cee0d7f7e89953b733eb2997
Simeon Simeonoff <sim.simeonoff@gmail.com>
2025-12-12 09:47:22 +0200

    refactor(button): unsubscribe from animationFrameScheduler on destroy

--- projects/igniteui-angular/src/lib/directives/button/button-base.ts
+++ projects/igniteui-angular/src/lib/directives/button/button-base.ts
@@ -9,9 +9,10 @@ import {
     booleanAttribute,
     inject,
     AfterViewInit,
+    OnDestroy
 } from '@angular/core';
 import { PlatformUtil } from '../../core/utils';
-import { animationFrameScheduler } from 'rxjs';
+import { animationFrameScheduler, Subscription } from 'rxjs';
 
 export const IgxBaseButtonType = {
     Flat: 'flat',
@@ -21,9 +22,10 @@ export const IgxBaseButtonType = {
 
 
 @Directive()
-export abstract class IgxButtonBaseDirective implements AfterViewInit{
+export abstract class IgxButtonBaseDirective implements AfterViewInit, OnDestroy {
     private _platformUtil = inject(PlatformUtil);
     private _viewInit = false;
+    private _animationScheduler: Subscription;
 
     /**
      * Emitted when the button is clicked.
@@ -111,10 +113,14 @@ export abstract class IgxButtonBaseDirective implements AfterViewInit{
         if (this._platformUtil.isBrowser && !this._viewInit) {
             this._viewInit = true;
 
-            animationFrameScheduler.schedule(() => {
+            this._animationScheduler = animationFrameScheduler.schedule(() => {
                 this.element.nativeElement.style.removeProperty('--_init-transition');
             });
         }
+    }
+
+    public ngOnDestroy(): void {
+        this._animationScheduler.unsubscribe();
     }
 
     /**
