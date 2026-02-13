b8e4573986a14c8d97165402a3f4ff4b7f7ac2d9
Martin Dragnev <37667452+mddragnev@users.noreply.github.com>
2026-02-13 18:32:23 +0200

    feat: Add Grid Lite Angular wrapper (#16777)
    Co-authored-by: damyanpetev <damyanpetev@users.noreply.github.com>

---
+++ src/app/grid-lite/grid-lite.sample.ts
@@ -0,0 +1,42 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, viewChild } from '@angular/core';
+import { IgxGridLiteCellTemplateDirective, IgxGridLiteColumnComponent, IgxGridLiteComponent, IgxGridLiteFilteringExpression, IgxGridLiteHeaderTemplateDirective, IgxGridLiteSortingExpression, IgxGridLiteSortingOptions } from "igniteui-angular/grids/lite";
+import { GridLiteDataService } from './data.service';
+@Component({
+    selector: 'app-grid-lite-sample',
+    templateUrl: 'grid-lite.sample.html',
+    styleUrls: ['grid-lite.sample.scss'],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    imports: [IgxGridLiteComponent, IgxGridLiteColumnComponent, IgxGridLiteHeaderTemplateDirective, IgxGridLiteCellTemplateDirective]
+})
+export class GridLiteSampleComponent {
+    protected grid = viewChild<IgxGridLiteComponent<any>>('grid');
+    protected data = [];
+    private dataService = inject(GridLiteDataService);
+
+    protected sortingExpressions: IgxGridLiteSortingExpression[] = [
+        {
+            key: 'firstName',
+            direction: "ascending"
+        }
+    ]
+
+    protected filteringExpressions: IgxGridLiteFilteringExpression[] = [
+        {
+            key: 'age',
+            condition: 'greaterThan',
+            searchTerm: 50
+        }
+    ]
+
+    protected sortingOptions: IgxGridLiteSortingOptions = {
+        mode: 'multiple'
+    }
+
+    constructor() {
+        this.data = this.dataService.generateUsers(10);
+    }
+
+    protected logEvent(name: string, event: any) {
+        console.log(name, event);
+    }
+}
