1871e491cbf1f34575dab4ecacebc3cd652e9d72
MKirova <MKirova@infragistics.com>
2026-03-11 13:05:18 +0200

    fix(grid): Exclude border from rowHeight calc.

--- projects/igniteui-angular/grids/core/src/row.directive.ts
+++ projects/igniteui-angular/grids/core/src/row.directive.ts
@@ -696,12 +696,8 @@ export class IgxRowDirective implements DoCheck, AfterViewInit, OnDestroy {
         }
         const isPinned = this.pinned && !this.disabled;
         const indexInData = this.grid.isRowPinningToTop && !isPinned ? this.index - this.grid.pinnedRecordsCount : this.index;
-        if ((this.grid as any)._cdrRequests) {
-            // recalc size if repaint is requested.
-            this.grid.verticalScrollContainer.recalcUpdateSizes();
-        }
         const size = this.grid.verticalScrollContainer.getSizeAt(indexInData);
-        return size || this.grid.rowHeight;
+        return (size - this.grid._borderSize) || this.grid.rowHeight;
     }
 
     /**
