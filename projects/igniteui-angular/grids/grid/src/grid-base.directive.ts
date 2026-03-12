b98f2089794df091e3295f88a938b85e880b8a70
Ivan Minchev <IMinchev@infragistics.com>
2026-03-12 16:05:29 +0200

    fix(grid): update action strip logic to check for menu items presence

--- projects/igniteui-angular/grids/grid/src/grid-base.directive.ts
+++ projects/igniteui-angular/grids/grid/src/grid-base.directive.ts
@@ -7798,7 +7798,7 @@ export abstract class IgxGridBaseDirective implements GridType,
         const keepActionStrip =
             !!context?.pinned &&
             !!contextEl?.isConnected &&
-            !context?.asMenuItems;
+            !(this.actionStrip?.menuItems?.length > 0);
 
         if (!keepActionStrip) {
             if (this.actionStrip) {
