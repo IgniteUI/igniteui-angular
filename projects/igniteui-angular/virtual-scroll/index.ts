e7ce7a18e03cbe6a7112cd5d2e7df1fa2998019f
Radoslav Karaivanov <rkaraivanov@infragistics.com>
2026-05-18 11:57:07 +0300

    fix: ng-packagr build errors for virtual scroll component

--- projects/igniteui-angular/virtual-scroll/index.ts
+++ projects/igniteui-angular/virtual-scroll/index.ts
@@ -1,3 +1,1 @@
-export { IgxVirtualScrollComponent } from './src/virtual-scroll.component';
-export { IgxVirtualItemDirective } from './src/virtual-scroll-item.directive';
-export { IgxVsItemContext, VirtualScrollDataRequest, VirtualScrollState } from './src/types';
+export * from './src/public_api';
