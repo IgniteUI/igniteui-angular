7fb23ce47d1060892e22bbb3f69dd7ccf4714f77
Viktor Kombov <vkombov@infragistics.com>
2026-07-31 13:42:34 +0300

    chore(*): add a missing semicolon

--- projects/igniteui-angular/core/src/core/touch.ts
+++ projects/igniteui-angular/core/src/core/touch.ts
@@ -354,7 +354,7 @@ export class IgxTouchManager {
         if (this._tracking && this._panStarted && event.cancelable) {
             event.preventDefault();
         }
-    }
+    };
 
     private _canStartPan(event: IgxGestureEvent): boolean {
         if (event.distance < this._panThreshold) {
