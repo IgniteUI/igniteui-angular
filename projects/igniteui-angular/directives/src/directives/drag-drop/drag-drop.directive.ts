af01155bcab9de733fe5407f72738fcfd3aab5fa
igdmdimitrov <dmdimitrov@infragistics.com>
2026-08-27 14:14:33 +0300

    chore(*): modified button check

--- projects/igniteui-angular/directives/src/directives/drag-drop/drag-drop.directive.ts
+++ projects/igniteui-angular/directives/src/directives/drag-drop/drag-drop.directive.ts
@@ -928,7 +928,7 @@ export class IgxDragDirective implements AfterContentInit, OnDestroy {
      */
     public onPointerDown(event) {
         // Start drag only with the primary pointer button.
-        if ((this.pointerEventsEnabled || !this.touchEventsEnabled) && event.button !== undefined && event.button !== 0) {
+        if ((this.pointerEventsEnabled || !this.touchEventsEnabled) && 'button' in event && event.button !== 0) {
             return;
         }
 
